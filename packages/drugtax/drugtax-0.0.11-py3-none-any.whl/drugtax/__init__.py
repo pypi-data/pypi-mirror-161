c70507c375bb93e03f227214a08db110c934ded8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:08 +0000

    Add package drugtax/0.0.11/drugtax-0.0.11-py3-none-any.whl

---
+++ packages/drugtax/drugtax-0.0.11-py3-none-any.whl/drugtax/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Initiate DrugTax
+"""
+
+__author__ = "A.J. Preto"
+__email__ = "martinsgomes.jose@gmail.com"
+__group__ = "Data-Driven Molecular Design"
+__group_leader__ = "Irina S. Moreira"
+__project__ = "DrugTax"
+
+from .variables import *
+from .drugtax import *
+from .drugtax import DrugTax
