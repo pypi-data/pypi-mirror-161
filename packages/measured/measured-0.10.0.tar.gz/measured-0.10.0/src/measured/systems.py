a8e9db784594c2f027de22a54ff9a116c4dea536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:29 +0000

    Add package measured/0.10.0/measured-0.10.0.tar.gz

---
+++ packages/measured/measured-0.10.0.tar.gz/measured-0.10.0/src/measured/systems.py
@@ -0,0 +1,16 @@
+from measured import (  # noqa: F401
+    astronomical,
+    avoirdupois,
+    computing,
+    energy,
+    eu,
+    fff,
+    iec,
+    iso,
+    metric,
+    music,
+    natural,
+    si,
+    troy,
+    us,
+)
