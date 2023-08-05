5f412ec17645ec5d03ba43e7a3cf10df446bc107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:25 +0000

    Add package odd-ml/0.6.0/odd_ml-0.6.0-py3-none-any.whl

---
+++ packages/odd-ml/odd_ml-0.6.0-py3-none-any.whl/odd_ml/domain/aws_config.py
@@ -0,0 +1,42 @@
+from typing import Optional, Tuple
+
+from pydantic import BaseSettings, SecretStr
+from pydantic.env_settings import SettingsSourceCallable
+
+
+class AwsConfig(BaseSettings):
+    """AWS config
+
+    Note:
+        Secrets can be passed either from arguments or .env file.
+
+    Attributes:
+        aws_secret_access_key (str, optional): aws secret access key.
+        aws_access_key_id (str, optional): aws access key id.
+        aws_region (str, optional): aws region
+        _env_file (str, optional): relative path to local .env file
+
+    Examples:
+        AwsConfig(_env_file='.env')
+
+        AwsConfig(
+            aws_secret_access_key='####',
+            aws_access_key_id='####',
+            aws_region='####'
+        )
+
+    """
+
+    aws_secret_access_key: Optional[SecretStr]
+    aws_access_key_id: Optional[SecretStr]
+    aws_region: Optional[str]
+
+    class Config:
+        @classmethod
+        def customize_sources(
+            cls,
+            init_settings: SettingsSourceCallable,
+            env_settings: SettingsSourceCallable,
+            file_secret_settings: SettingsSourceCallable,
+        ) -> Tuple[SettingsSourceCallable, ...]:
+            return file_secret_settings, init_settings, env_settings
