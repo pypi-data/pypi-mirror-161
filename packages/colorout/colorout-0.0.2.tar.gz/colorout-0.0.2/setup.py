137271c2baa66bba7227bca3f77bd1d950bd8f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:40 +0000

    Add package colorout/0.0.2/colorout-0.0.2.tar.gz

---
+++ packages/colorout/colorout-0.0.2.tar.gz/colorout-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='colorout',
+      version='0.0.2',
+      description='Printing with random colors',
+      url='https://github.com/gpchn/colorout',
+      author='gpchn',
+      author_email='gprogrammer@163.com',
+      license='Apache2.0',
+      packages=find_packages()
+)
