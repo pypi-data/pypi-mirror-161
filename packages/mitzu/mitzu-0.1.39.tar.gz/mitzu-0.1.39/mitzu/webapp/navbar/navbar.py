46ec32074d6800c5b0c35ffbabfaec2bd0651304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:09 +0000

    Add package mitzu/0.1.39/mitzu-0.1.39.tar.gz

---
+++ packages/mitzu/mitzu-0.1.39.tar.gz/mitzu-0.1.39/mitzu/webapp/navbar/navbar.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import dash_bootstrap_components as dbc
+import mitzu.webapp.navbar.metric_type_handler as MNB
+import mitzu.webapp.navbar.project_dropdown as PD
+import mitzu.webapp.webapp as WA
+from dash import html
+
+LOGO = "/assets/logo.png"
+
+
+def create_mitzu_navbar(webapp: WA.MitzuWebApp) -> dbc.Navbar:
+    res = dbc.Navbar(
+        children=dbc.Container(
+            children=[
+                dbc.NavbarBrand(
+                    dbc.Row(
+                        children=[
+                            dbc.Col(
+                                html.A(
+                                    # Use row and col to control vertical alignment of logo / brand
+                                    children=[html.Img(src=LOGO, height="32px")],
+                                    href="/",
+                                    style={"textDecoration": "none"},
+                                )
+                            ),
+                            dbc.Col(PD.create_project_dropdown(webapp)),
+                            dbc.Col(
+                                MNB.MetricTypeHandler.from_metric_type(
+                                    MNB.MetricType.SEGMENTATION
+                                ).component
+                            ),
+                        ]
+                    ),
+                ),
+                dbc.Row(
+                    children=[
+                        dbc.Col(
+                            dbc.DropdownMenu(
+                                children=[
+                                    dbc.DropdownMenuItem("Link"),
+                                    dbc.DropdownMenuItem("CSV"),
+                                    dbc.DropdownMenuItem("SQL Query"),
+                                    dbc.DropdownMenuItem("PNG"),
+                                ],
+                                label="Share",
+                                size="sm",
+                                color="primary",
+                                in_navbar=True,
+                                align_end=True,
+                            ),
+                        ),
+                        dbc.Col(
+                            dbc.Button(
+                                children=html.I(className="bi bi-gear"),
+                                size="sm",
+                                color="dark",
+                            )
+                        ),
+                    ],
+                    align="center",
+                    justify="end",
+                ),
+            ],
+            fluid=True,
+        ),
+        sticky="top",
+    )
+    return res
