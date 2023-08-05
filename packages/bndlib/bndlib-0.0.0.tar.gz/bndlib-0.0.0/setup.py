fd616ef7c2f3b7268879f78a5cf63794830edec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:52 +0000

    Add package bndlib/0.0.0/bndlib-0.0.0.tar.gz

---
+++ packages/bndlib/bndlib-0.0.0.tar.gz/bndlib-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bndlib',
+    version='',
+    description='',
+    long_description='',
+    url='',
+    author='bnd',
+    author_email='',
+    packages=find_packages()
+)
