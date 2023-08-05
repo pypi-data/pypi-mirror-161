8f45234bd07d1b25660d35939fa1c5dc2fe1a649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:01 +0000

    Add package foursight-cgap/2.0.0.1b1/foursight_cgap-2.0.0.1b1-py3-none-any.whl

---
+++ packages/foursight-cgap/foursight_cgap-2.0.0.1b1-py3-none-any.whl/chalicelib/app_utils.py
@@ -0,0 +1,14 @@
+from os.path import dirname
+from foursight_core.app_utils import AppUtils as AppUtils_from_core
+from .vars import FOURSIGHT_PREFIX, HOST
+
+
+class AppUtils(AppUtils_from_core):
+
+    # overwriting parent class
+    prefix = FOURSIGHT_PREFIX
+    FAVICON = 'https://cgap-dbmi.hms.harvard.edu/favicon.ico'
+    host = HOST
+    package_name = 'chalicelib'
+    check_setup_dir = dirname(__file__)
+    html_main_title = 'Foursight-CGAP'
