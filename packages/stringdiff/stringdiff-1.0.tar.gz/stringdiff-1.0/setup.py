a4f01cace02d730b653674e6067737957a583e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:14 +0000

    Add package stringdiff/1.0/stringdiff-1.0.tar.gz

---
+++ packages/stringdiff/stringdiff-1.0.tar.gz/stringdiff-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='stringdiff',
+    version='1.0',
+    description='Use edit distance to compare strings',
+    license='MIT',
+    author='Lillian Zhu',
+    author_email='zhuy0713@gmail.com',
+    url='https://github.com/yining0713/edit-distance'
+    )
