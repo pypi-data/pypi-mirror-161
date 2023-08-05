9e2b081edf9e2a1d8bc0c4e5dc69b66a3ce29e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:35 +0000

    Add package dots-python/1.4.3/dots-python-1.4.3.tar.gz

---
+++ packages/dots-python/dots-python-1.4.3.tar.gz/dots-python-1.4.3/src/dots/payout_link.py
@@ -0,0 +1,24 @@
+import dots
+
+class PayoutLink():
+
+    @classmethod
+    def create(cls, amount, delivery, payee=None):
+
+        json = {
+            'amount': amount,
+            'delivery': delivery
+        }
+
+        if payee is not None:
+            json['payee'] = payee
+
+        response = dots._session.post(dots.api_base + '/payouts/create_payout_link', json=json, auth=(dots.client_id, dots.api_key))
+        response.raise_for_status()
+
+        data = response.json()
+
+        if data['success']:
+            return data['payout_link']
+        else:
+            response.raise_for_status()
