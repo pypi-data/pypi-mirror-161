f41dbb9a710825a599a45dd9b724dfd7509def6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:32 +0000

    Add package snake-gym-grid/0.0.5/snake_gym_grid-0.0.5.tar.gz

---
+++ packages/snake-gym-grid/snake_gym_grid-0.0.5.tar.gz/snake_gym_grid-0.0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='snake_gym_grid',
+    version='0.0.5',
+    description="An implementation on Snake game with grid display",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=['gym==0.25.0', 'pygame==2.1.2', 'numpy==1.22.3']
+)
