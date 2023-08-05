9600dae216669a0da52b84ab6f9d516d6995d507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:31 +0000

    Add package je-editor/0.0.44/je_editor-0.0.44-py3-none-any.whl

---
+++ packages/je-editor/je_editor-0.0.44-py3-none-any.whl/je_editor/ui/ui_event/encoding/set_encoding.py
@@ -0,0 +1,10 @@
+from je_editor.utils.editor_content.editor_data import editor_data_dict
+
+
+def set_encoding(exec_manager, encoding):
+    """
+    :param exec_manager: which program exec manage change encoding
+    :param encoding: which encoding choose
+    """
+    exec_manager.program_encoding = encoding
+    editor_data_dict["encoding"] = encoding
