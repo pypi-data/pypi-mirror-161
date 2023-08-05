9460ee5ad373bb404ba8f505a60409dbec1bf087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:14 +0000

    Add package vuln-repo/0.0.4/vuln_repo-0.0.4.tar.gz

---
+++ packages/vuln-repo/vuln_repo-0.0.4.tar.gz/vuln_repo-0.0.4/app/greet.py
@@ -0,0 +1,2 @@
+def function(name):
+    print("HELLO", name)
---
+++ packages/vuln-repo/vuln_repo-0.0.4.tar.gz/vuln_repo-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+VERSION = '0.0.4'
+DESCRIPTION = 'vulnerable repo'
+
+# Setting up
+setup(
+    name="vuln_repo",
+    version=VERSION,
+    author="v1per",
+    author_email="test@test.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['insecure-package==0.1.0','flask==0.6.1','django<=4.0.1'],
+    keywords=['vulnerable', 'test'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
