d9c1e531d141e49189b031567afd78536dfec403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:17 +0000

    Add package HABApp/1.0.2/HABApp-1.0.2-py3-none-any.whl

---
+++ packages/HABApp/HABApp-1.0.2-py3-none-any.whl/HABApp/core/items/base_item_times.py
@@ -0,0 +1,67 @@
+import logging
+import typing
+from typing import Generic, TypeVar, List
+
+from pendulum import DateTime
+
+from HABApp.core.asyncio import create_task
+from HABApp.core.wrapper import log_exception
+from .base_item_watch import BaseWatch, ItemNoChangeWatch, ItemNoUpdateWatch
+
+log = logging.getLogger('HABApp')
+
+WATCH_TYPE = TypeVar("WATCH_TYPE", bound=BaseWatch)
+
+
+class ItemTimes(Generic[WATCH_TYPE]):
+    WATCH: typing.Union[typing.Type[ItemNoUpdateWatch], typing.Type[ItemNoChangeWatch]]
+
+    def __init__(self, name: str, dt: DateTime):
+        self.name: str = name
+        self.dt: DateTime = dt
+        self.tasks: List[WATCH_TYPE] = []
+
+    def set(self, dt: DateTime, events=True):
+        self.dt = dt
+        if not self.tasks:
+            return
+
+        if events:
+            create_task(self.schedule_events())
+        return None
+
+    def add_watch(self, secs: typing.Union[int, float]) -> WATCH_TYPE:
+        # don't add the watch two times
+        for t in self.tasks:
+            if not t.fut.is_canceled and t.fut.secs == secs:
+                log.warning(f'Watcher {self.WATCH.__name__} ({t.fut.secs}s) for {self.name} has already been created')
+                return t
+
+        w = self.WATCH(self.name, secs)
+        self.tasks.append(w)
+        log.debug(f'Added {self.WATCH.__name__} ({w.fut.secs}s) for {self.name}')
+        return w
+
+    @log_exception
+    async def schedule_events(self):
+        canceled = []
+        for t in self.tasks:
+            if t.fut.is_canceled:
+                canceled.append(t)
+            else:
+                t.fut.reset()
+
+        # remove canceled tasks
+        if canceled:
+            for c in canceled:
+                self.tasks.remove(c)
+                log.debug(f'Removed {self.WATCH.__name__} ({c.fut.secs}s) for {self.name}')
+        return None
+
+
+class UpdatedTime(ItemTimes[ItemNoUpdateWatch]):
+    WATCH = ItemNoUpdateWatch
+
+
+class ChangedTime(ItemTimes[ItemNoChangeWatch]):
+    WATCH = ItemNoChangeWatch
---
+++ packages/HABApp/HABApp-1.0.2-py3-none-any.whl/HABApp/util/multimode/mode_base.py
@@ -0,0 +1,36 @@
+import typing
+
+from HABApp.core.internals import AutoContextBoundObj
+
+
+class BaseMode(AutoContextBoundObj):
+
+    def __init__(self, name: str):
+        super(BaseMode, self).__init__()
+        self.name: str = name
+
+        self.__mode_lower_prio: typing.Optional[BaseMode] = None
+
+        self.parent: MultiModeItem
+
+        # Otherwise the missing assignment shows an error
+        if typing.TYPE_CHECKING:
+            self.parent = MultiModeItem('TYPE_CHECKING')
+        return
+
+    def _set_mode_lower_prio(self, mode_lower_prio):
+        assert isinstance(mode_lower_prio, BaseMode) or mode_lower_prio is None, type(mode_lower_prio)
+        self.__lower_priority_mode = mode_lower_prio
+
+    def calculate_value(self, lower_prio_value: typing.Any) -> typing.Any:
+        raise NotImplementedError()
+
+    def cancel(self):
+        """Remove the mode from the parent ``MultiModeItem`` and stop processing it
+        """
+        self._ctx_unlink()
+        self.parent.remove_mode(self.name)
+        self.parent = None
+
+
+from .item import MultiModeItem  # noqa: E402
