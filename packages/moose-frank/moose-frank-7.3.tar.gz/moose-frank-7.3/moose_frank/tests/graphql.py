6817269983702192195627e43624babe400dc387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:58 +0000

    Add package moose-frank/7.3/moose-frank-7.3.tar.gz

---
+++ packages/moose-frank/moose-frank-7.3.tar.gz/moose-frank-7.3/moose_frank/tests/graphql.py
@@ -0,0 +1,35 @@
+import json
+from typing import Optional
+
+from graphql_jwt.shortcuts import get_token
+
+
+class GraphQLMixin:
+    def execute(
+        self,
+        query: str,
+        variables: Optional[dict] = None,
+        as_user: Optional = None,
+        files: Optional[dict] = None,
+        extra_token_data: Optional[dict] = None,
+    ):
+        headers = {}
+        if as_user:
+            if extra_token_data is None:
+                extra_token_data = {}
+            headers[
+                "HTTP_AUTHORIZATION"
+            ] = f"JWT {get_token(as_user, **extra_token_data)}"
+
+        data = {
+            "operations": json.dumps({"query": query, "variables": variables or {}})
+        }
+        if files:
+            mapping = {i: [k] for i, k in enumerate(files.keys())}
+            data["map"] = json.dumps(mapping)
+            for i, keys in mapping.items():
+                data[i] = files[keys[0]]
+                if hasattr(data[i], "seek"):
+                    data[i].seek(0)
+
+        return self.client.post("/graphql", data=data, **headers)
