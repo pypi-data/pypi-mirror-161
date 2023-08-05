0bde185f94e2dd13760ea247b71db2f47dfcc231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:40 +0000

    Add package pydeflate/1.1.10/pydeflate-1.1.10-py3-none-any.whl

---
+++ packages/pydeflate/pydeflate-1.1.10-py3-none-any.whl/pydeflate/__init__.py
@@ -0,0 +1,14 @@
+__author__ = """Jorge Rivera"""
+__email__ = "jorge.rivera@one.org"
+__version__ = '1.1.10'
+
+
+from pydeflate.deflate.deflate import deflate
+from pydeflate.tools.exchange import exchange
+from pydeflate.tools.update_data import update_all_data
+from pydeflate.utils import warn_updates
+
+# check that data is fresh enough
+warn_updates()
+
+
