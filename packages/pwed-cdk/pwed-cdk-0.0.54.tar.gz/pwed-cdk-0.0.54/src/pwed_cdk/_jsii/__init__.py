d311cd25180d41fcd99ab829901fefad8fad25ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:54 +0000

    Add package pwed-cdk/0.0.54/pwed-cdk-0.0.54.tar.gz

---
+++ packages/pwed-cdk/pwed-cdk-0.0.54.tar.gz/pwed-cdk-0.0.54/src/pwed_cdk/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "pwed-cdk", "0.0.54", __name__[0:-6], "pwed-cdk@0.0.54.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
