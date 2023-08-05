b46e843b92b2ecc30753d6918194aac2d98bfda6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:41 +0000

    Add package pyfl/0.0.9/pyfl-0.0.9-py3-none-any.whl

---
+++ packages/pyfl/pyfl-0.0.9-py3-none-any.whl/pyfl/helper.py
@@ -0,0 +1,15 @@
+import requests
+
+class Helper():
+    victoria = "victoria"
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def make_raw_api_call(endpoint):
+        r = requests.get("https://api.tfl.gov.uk/" + endpoint)
+        return r.json()
+
+    #@staticmethod
+    #def victoria():
+    #    return "victoria"
---
+++ packages/pyfl/pyfl-0.0.9-py3-none-any.whl/pyfl/tube.py
@@ -0,0 +1,14 @@
+# local imports
+from .errors import *
+from .helper import Helper
+
+class LU():
+    """ Line class"""
+    def __init__(self):
+        """ Initialise the Line class """
+        pass
+
+    def get_line_status(self, line:str):
+        """ Get the status of a line """
+        endpoint = f"/line/{line}/status"
+        return Helper.make_raw_api_call(endpoint)
