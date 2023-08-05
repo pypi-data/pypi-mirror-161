d15dbb2e8fcb85991616dfdbb2250e4c00d3e85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:23 +0000

    Add package fake_user_agent/0.1.1/fake_user_agent-0.1.1.tar.gz

---
+++ packages/fake_user_agent/fake_user_agent-0.1.1.tar.gz/fake_user_agent-0.1.1/fake_user_agent/log.py
@@ -0,0 +1,8 @@
+"""Sets up logger."""
+
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,  # set root logger's level, applying to all its child loggers unless their levels are specifically set
+)
+
