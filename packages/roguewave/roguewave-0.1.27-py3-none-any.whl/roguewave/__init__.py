f4d1f1ff31e7cd94565e0794201defd3d1e37fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:52 +0000

    Add package roguewave/0.1.27/roguewave-0.1.27-py3-none-any.whl

---
+++ packages/roguewave/roguewave-0.1.27-py3-none-any.whl/roguewave/__init__.py
@@ -0,0 +1,41 @@
+import logging
+import sys
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+logger.addHandler(logging.NullHandler())
+
+# External API tools
+
+from .modeldata import sofarspectralapi
+from roguewave.modeldata.sofarspectralapi import load_sofar_spectral_file
+
+# Wave spectrum
+
+## object definition
+from .wavespectra.spectrum1D import WaveSpectrum1D
+from .wavespectra.spectrum2D import WaveSpectrum2D
+from .wavespectra import spectrum1D, spectrum2D
+
+## estimators
+from .wavespectra.estimators import convert_to_1d_spectrum,convert_to_2d_spectrum
+
+## save/load
+from .wavespectra.io import load_spectrum, save_spectrum # Depricated
+from .io.io import save,load
+
+## Partitioning
+from .wavespectra.partitioning.wavefields import (
+    get_bulk_partitions_from_spectral_partitions,
+    get_spectral_partitions_from_2dspectra)
+from .wavespectra.partitioning.observations import \
+    get_bulk_partitions_from_observations, \
+    get_spectral_partitions_from_observations
+
+def set_log_to_console(level=logging.INFO):
+    logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+    logger.setLevel(level)
+
+def set_log_to_file(filename, level=logging.INFO):
+    logger.addHandler(logging.FileHandler(filename))
+    logger.setLevel(level)
---
+++ packages/roguewave/roguewave-0.1.27-py3-none-any.whl/roguewave/modeldata/__init__.py
@@ -0,0 +1,7 @@
+from .keygeneration import generate_lead_keys_and_valid_times, \
+    generate_forecast_keys_and_valid_times
+
+from roguewave.modeldata.modelinformation import  \
+    list_available_variables, available_models
+
+from .pointdata import extract_along_spotter_tracks
