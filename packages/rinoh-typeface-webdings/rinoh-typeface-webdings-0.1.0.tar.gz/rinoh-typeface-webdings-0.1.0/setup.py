da6e7a32da615e5bc54c0ebb68e96313e887cd61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:10 +0000

    Add package rinoh-typeface-webdings/0.1.0/rinoh-typeface-webdings-0.1.0.tar.gz

---
+++ packages/rinoh-typeface-webdings/rinoh-typeface-webdings-0.1.0.tar.gz/rinoh-typeface-webdings-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from rinoh_mscorefontinstaller import setup
+
+
+setup(
+    name='Webdings',
+    version='0.1.0',
+    archive='webdin32.exe',
+    sha256sum='64595b5abc1080fba8610c5c34fab5863408e806aafe84653ca8575bed17d75a',
+    setup_file=__file__
+)
