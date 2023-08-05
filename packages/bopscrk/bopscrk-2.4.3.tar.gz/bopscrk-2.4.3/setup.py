a316404ab718b2a160e6b5c72396bffa7f039159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:44 +0000

    Add package bopscrk/2.4.3/bopscrk-2.4.3.tar.gz

---
+++ packages/bopscrk/bopscrk-2.4.3.tar.gz/bopscrk-2.4.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# https://github.com/r3nt0n/bopscrk
+
+
+from setuptools import setup, find_packages
+
+# Read project description
+with open('README.md', 'r') as f:
+    long_desc = f.read()
+
+setup(
+    name='bopscrk',
+    author='r3nt0n',
+    author_email='r3nt0n@protonmail.com',
+    url='https://github.com/r3nt0n/bopscrk',
+    version='2.4.3',
+    license='GNU General Public License v3.0',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    package_data={
+        # If any package contains *.ini files, include them
+        '': ['*.cfg'],
+    },
+    #packages=['modules',],
+    #packages=find_packages(),
+    packages=['bopscrk', 'bopscrk.modules', 'bopscrk.modules.lyricpass'],
+    #scripts=['bopscrk/bopscrk.py'],
+    install_requires=['requests'],
+    entry_points = {
+        'console_scripts':[
+            'bopscrk = bopscrk.modules.main:run'
+        ]
+    }
+)
