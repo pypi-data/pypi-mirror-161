dfa25f0a11aee12f8c91715f2dbb5d00c0b4b85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:25 +0000

    Add package DDPCL/1.0.0.2/DDPCL-1.0.0.2.tar.gz

---
+++ packages/DDPCL/DDPCL-1.0.0.2.tar.gz/DDPCL-1.0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DDPCL',
+    version='1.0.0.2',
+    license='MIT',
+    author="Anthony SIAMPIRINGUE",
+    author_email='anthony.siampiringue@epita.fr',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='http://pypi.python.org/pypi/DDPCL/',
+    keywords='drift detection CV bayesian recognition autolabeling',
+    install_requires=[
+          'opencv-python',
+          'pandas',
+          'numpy',
+          'scipy',
+          'scikit-learn',
+          'shutils',
+          'uuid',
+          'pillow',
+          'face_recognition',
+          'matplotlib'
+      ],
+
+)
