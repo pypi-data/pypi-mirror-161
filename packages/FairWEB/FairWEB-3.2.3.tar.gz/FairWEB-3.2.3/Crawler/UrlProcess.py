3501a1469205cb1867d18fbde778cfd4746e3ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:58 +0000

    Add package FairWEB/3.2.3/FairWEB-3.2.3.tar.gz

---
+++ packages/FairWEB/FairWEB-3.2.3.tar.gz/FairWEB-3.2.3/Crawler/UrlProcess.py
@@ -0,0 +1,12 @@
+from FusedDL import FusedDownloader
+from FLog.LOGGER import Log
+Log = Log("FWEB.Crawler.ArchiveCrawler_v2")
+
+
+# -> 2. Download Article
+def attempt_download(url, saveToDB=True):
+    Log.i("Attempting Archive Download.")
+    if saveToDB:
+        return FusedDownloader.download_v2(url)
+    else:
+        return FusedDownloader.download(url)
