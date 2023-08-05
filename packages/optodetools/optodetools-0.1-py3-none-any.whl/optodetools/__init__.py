5125fe6b59b7f408b2bab1673b9bd31558e29b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:31 +0000

    Add package optodetools/0.1/optodetools-0.1-py3-none-any.whl

---
+++ packages/optodetools/optodetools-0.1-py3-none-any.whl/optodetools/__init__.py
@@ -0,0 +1,11 @@
+"""
+package for correcting optode response time error
+"""
+
+from __future__ import absolute_import
+from .core import *
+
+
+__author__ = ['Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>']
+
+__version__ = '0.1'
