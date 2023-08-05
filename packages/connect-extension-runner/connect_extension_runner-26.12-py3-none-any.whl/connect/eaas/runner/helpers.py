eaa7de180418fdcb2df69f146e86ea5f867c9403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:04 +0000

    Add package connect-extension-runner/26.12/connect_extension_runner-26.12-py3-none-any.whl

---
+++ packages/connect-extension-runner/connect_extension_runner-26.12-py3-none-any.whl/connect/eaas/runner/helpers.py
@@ -0,0 +1,132 @@
+#
+# This file is part of the Ingram Micro CloudBlue Connect EaaS Extension Runner.
+#
+# Copyright (c) 2021 Ingram Micro. All Rights Reserved.
+#
+import os
+import logging
+import subprocess
+import sys
+from uuid import uuid4
+
+from connect.client import ClientError, ConnectClient
+
+import requests
+from pkg_resources import (
+    DistributionNotFound,
+    get_distribution,
+)
+
+from connect.eaas.runner.constants import (
+    BACKGROUND_TASK_MAX_EXECUTION_TIME,
+    INTERACTIVE_TASK_MAX_EXECUTION_TIME,
+    ORDINAL_SUFFIX,
+    PYPI_EXTENSION_RUNNER_URL,
+    SCHEDULED_TASK_MAX_EXECUTION_TIME,
+)
+
+logger = logging.getLogger('connect.eaas')
+
+
+def get_container_id():
+    result = subprocess.run(
+        ['cat', '/proc/1/cpuset'],
+        capture_output=True,
+        stdin=subprocess.DEVNULL,
+        start_new_session=True,
+    )
+    try:
+        result.check_returncode()
+    except subprocess.CalledProcessError:
+        return str(uuid4())
+
+    _, container_id = result.stdout.decode()[:-1].rsplit('/', 1)
+    if len(container_id) == 64:
+        return container_id
+
+    result = subprocess.run(
+        ['grep', 'overlay', '/proc/self/mountinfo'],
+        capture_output=True,
+        stdin=subprocess.DEVNULL,
+        start_new_session=True,
+    )
+    try:
+        result.check_returncode()
+        mount = result.stdout.decode()
+        start_idx = mount.index('upperdir=') + len('upperdir=')
+        end_idx = mount.index(',', start_idx)
+        dir_path = mount[start_idx:end_idx]
+        _, container_id, _ = dir_path.rsplit('/', 2)
+        if len(container_id) != 64:
+            return str(uuid4())
+        return container_id
+    except (subprocess.CalledProcessError, ValueError):
+        return str(uuid4())
+
+
+def get_environment():
+    return {
+        'api_key': os.getenv('API_KEY'),
+        'environment_id': os.getenv('ENVIRONMENT_ID'),
+        'instance_id': os.getenv('INSTANCE_ID', get_container_id()),
+        'ws_address': os.getenv('SERVER_ADDRESS', 'api.cnct.info'),
+        'api_address': os.getenv('API_ADDRESS', os.getenv('SERVER_ADDRESS', 'api.cnct.info')),
+        'background_task_max_execution_time': int(os.getenv(
+            'BACKGROUND_TASK_MAX_EXECUTION_TIME', BACKGROUND_TASK_MAX_EXECUTION_TIME,
+        )),
+        'interactive_task_max_execution_time': int(os.getenv(
+            'INTERACTIVE_TASK_MAX_EXECUTION_TIME', INTERACTIVE_TASK_MAX_EXECUTION_TIME,
+        )),
+        'scheduled_task_max_execution_time': int(os.getenv(
+            'SCHEDULED_TASK_MAX_EXECUTION_TIME', SCHEDULED_TASK_MAX_EXECUTION_TIME,
+        )),
+    }
+
+
+def get_version():
+    try:
+        return get_distribution('connect-extension-runner').version
+    except DistributionNotFound:
+        return '0.0.0'
+
+
+def to_ordinal(val):
+    if val > 14:
+        return f"{val}{ORDINAL_SUFFIX.get(int(str(val)[-1]), 'th')}"
+    return f"{val}{ORDINAL_SUFFIX.get(val, 'th')}"
+
+
+def get_connect_version():
+    env = get_environment()
+    url = f'https://{env["api_address"]}/public/v1'
+
+    connect_client = ConnectClient(env['api_key'], endpoint=url)
+    try:
+        connect_client.accounts.all().first()
+    except ClientError as ce:
+        if ce.status_code in [401, 403]:
+            logger.error(f'API Key is not valid: {ce}')
+            sys.exit(2)
+
+        logger.error(f'Cannot check the current EaaS Runner version: {ce}')
+        sys.exit(1)
+
+    return connect_client.response.headers['Connect-Version']
+
+
+def get_pypi_runner_minor_version(major_version):
+    res = requests.get(PYPI_EXTENSION_RUNNER_URL)
+    if res.status_code != 200:
+        logger.error(
+            f'Cannot check the current EaaS Runner version: {res.text}.',
+        )
+        sys.exit(1)
+
+    content = res.json()
+    tags = [
+        int(version.split('.')[1])
+        for version in content['releases'] if version.startswith(f'{major_version}.')
+    ]
+    if tags:
+        return str(max(tags))
+    return content['info']['version'].split('.')[1]
