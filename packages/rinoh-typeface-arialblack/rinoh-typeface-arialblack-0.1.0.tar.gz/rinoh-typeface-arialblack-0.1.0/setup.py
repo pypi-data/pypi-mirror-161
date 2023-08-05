16174d2749014efa73883fb1af701887c3ab0fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:08 +0000

    Add package rinoh-typeface-arialblack/0.1.0/rinoh-typeface-arialblack-0.1.0.tar.gz

---
+++ packages/rinoh-typeface-arialblack/rinoh-typeface-arialblack-0.1.0.tar.gz/rinoh-typeface-arialblack-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from rinoh_mscorefontinstaller import setup
+
+
+setup(
+    name='Arial Black',
+    version='0.1.0',
+    archive='arialb32.exe',
+    sha256sum='a425f0ffb6a1a5ede5b979ed6177f4f4f4fdef6ae7c302a7b7720ef332fec0a8',
+    setup_file=__file__
+)
