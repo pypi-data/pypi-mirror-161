5b02022c0d773266c589a8295b09ff23cc2a1417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:04 +0000

    Add package PlumedToHTML/0.33/PlumedToHTML-0.33.tar.gz

---
+++ packages/PlumedToHTML/PlumedToHTML-0.33.tar.gz/PlumedToHTML-0.33/PlumedToHTML/PlumedLexer.py
@@ -0,0 +1,73 @@
+from pygments.lexer import RegexLexer, bygroups, include
+from pygments.token import Text, Comment, Literal, Keyword, Name, Generic, String
+
+class PlumedLexer(RegexLexer):
+    name = 'plumed'
+    aliases = ['plumed']
+    filenames = ['*.plmd']
+
+    tokens = {
+        'defaults' : [
+            # Deals with blank space
+            (r'\s+$', Text),
+            # Deals with all comments
+            (r'#.*?$', Comment),
+            # Deals with incomplete inputs 
+            (r'__FILL__', Literal),  
+            # Find LABEL=lab
+            (r'((?i)LABEL)(=)(\S+\b)', bygroups(Name.Attribute, Text, String.Double)),
+            # Find special replica syntax with brackets
+            (r'(\w+)(=)(@replicas:)((?s)\{.*?\})', bygroups(Name.Attribute, Text, Name.Constant, Generic)),  
+            # Find special replica syntax without brackets
+            (r'(\w+)(=)(@replicas:)(\S+\b)', bygroups(Name.Attribute, Text, Name.Constant, Generic)),
+            # Find KEYWORD with brackets around value
+            (r'(\w+)(=)((?s)\{.*?\})', bygroups(Name.Attribute, Text, Generic)),
+            # Find KEYWORD=whatever 
+            (r'(\w+)(=)(\S+\b)', bygroups(Name.Attribute, Text, Generic))
+         ],
+        'root': [
+            # Deals with blank lines
+            (r'^\n', Text.Whitespace),
+            # Find ENDPLUMED and set everything after it to a comment
+            (r'(^\s*ENDPLUMED)((?s).*\Z)', bygroups(Keyword, Comment)),
+            # Find the start of shortcuts
+            (r'#SHORTCUT.*?\r?\n',Comment.Preproc),
+            # Find the start of a shortcut with a nested default
+            (r'#NODEFAULT.*?\r?\n',Comment.Special),
+            # Find the start of a default section
+            (r'#DEFAULT.*?\r?\n',Comment.Special),
+            # Find the end of a default section
+            (r'#ENDDEFAULT.*?\r?\n',Comment.Special),
+            # Find the middle of shortcuts
+            (r'#EXPANSION.*?\r?\n',Comment.Special),
+            # Find the end of shortcuts
+            (r'#ENDEXPANSION.*?\r?\n',Comment.Special),
+            # Find vimsyntax expression
+            (r'#\s*vim:\s*ft=plumed',Literal),
+            # Include all the default stuff
+            include('defaults'), 
+            # Find label: ACTION
+            (r'(.+)(:\s+)(\S+\b)', bygroups(String, Text, Keyword)),
+            # Find label: ... \n ACTION  
+            (r'(.+)(:\s+\.\.\.\s*$\s*)(\S+\b)', bygroups(String, Text, Keyword), 'continuation'),
+            # Find ... for start of continuation
+            (r'\.\.\.', Text, 'continuation'),
+            # Find ACTION at start of line
+            (r'^\s*\w+\b',Keyword),
+            # Find FLAG anywhere on line
+            (r'\w+\b',Name.Attribute),
+            # Find any left over white space
+            (r'\s+',Text)
+        ],
+        'continuation' : [
+            include('defaults'),
+            # Find FLAG which can now be anywhere on line or at start of line in continuation
+            (r'\w+\b', Name.Attribute),
+            # Find any left over white space 
+            (r'\s+', Text),
+            # Find ... ACTION as end of continuation
+            (r'(\.\.\.)(.+$)', bygroups(Text, Text), '#pop'),
+            # Find ... as end of continuation
+            (r'\.\.\.', Text, '#pop')
+        ]
+    }
