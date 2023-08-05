f4f3ceb38be0db63017bd63e3b3104c574b5b7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:03 +0000

    Add package dolistparser/0.2.1/dolistparser-0.2.1.tar.gz

---
+++ packages/dolistparser/dolistparser-0.2.1.tar.gz/dolistparser-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dolistparser', 'dolistparser.parsers']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.9.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'dolistparser',
+    'version': '0.2.1',
+    'description': 'Parse to-dos from the comment',
+    'long_description': None,
+    'author': 'yunjae',
+    'author_email': 'yunjae.oh.nl@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
