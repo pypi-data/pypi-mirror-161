4e833ec86b455e99d7aa81a8f7e90a6662b2b4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:40 +0000

    Add package mctinctools/0.1.1/mctinctools-0.1.1.tar.gz

---
+++ packages/mctinctools/mctinctools-0.1.1.tar.gz/mctinctools-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['mctinctools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytest>=7.1.2,<8.0.0']
+
+setup_kwargs = {
+    'name': 'mctinctools',
+    'version': '0.1.1',
+    'description': 'Common tools for our organization.',
+    'long_description': '',
+    'author': 'forums34',
+    'author_email': 'greg.wendel@hey.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/forums34/mctinctools',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
