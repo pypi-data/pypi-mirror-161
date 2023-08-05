37d3c7b78aaad4ddd8e6e066c1a03cc8a5e35bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:00 +0000

    Add package ceveri/1.0.1/ceveri-1.0.1.tar.gz

---
+++ packages/ceveri/ceveri-1.0.1.tar.gz/ceveri-1.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "Natural Language :: Turkish",
+    "Operating System :: Microsoft :: Windows :: Windows 10",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Topic :: Text Processing"
+]
+
+setup(
+    name="ceveri",
+    version="1.0.1",
+    description="Google Cloud API ile veri yerelleştirmesi amacıyla çeviri işlemleri gerçekleştiren bir kütüphane.",
+    long_description=open("README.txt").read() + "\n\n" + open("CHANGELOG.txt").read(),
+    url="https://ceveri.readthedocs.io",
+    author="Arda Uzunoğlu",
+    author_email="ardauzunogluarda@gmail.com",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="",
+    packages=find_packages(),
+    install_requires=[""]
+)
