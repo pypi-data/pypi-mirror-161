66530e9ad63f470d54bb47e64a3d7661abe57c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:17 +0000

    Add package pymlog/0.0.22/pymlog-0.0.22-py3-none-any.whl

---
+++ packages/pymlog/pymlog-0.0.22-py3-none-any.whl/mlog/__init__.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+from .mlog import start, get
+from .mlog import MLOG_DB, SQL_CREATE_RUNS_TABLE, SQL_CREATE_LOGS_TABLE
+
+
+MLOG_DIR.mkdir(parents=True, exist_ok=True)
+
+con = sqlite3.connect(MLOG_DB)
+
+with con:
+    con.execute(SQL_CREATE_RUNS_TABLE)
+    con.execute(SQL_CREATE_LOGS_TABLE)
+
+con.close()
