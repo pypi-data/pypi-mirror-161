4ed3ef5bd6cb1b69f6ec718c5cd524760605103d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:44 +0000

    Add package uw-it-build-fingerprinter/0.2.2/uw-it-build-fingerprinter-0.2.2.tar.gz

---
+++ packages/uw-it-build-fingerprinter/uw-it-build-fingerprinter-0.2.2.tar.gz/uw-it-build-fingerprinter-0.2.2/fingerprinter/models.py
@@ -0,0 +1,68 @@
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel, Field
+
+
+class BuildArgConfig(BaseModel):
+    class Config:
+        allow_population_by_field_name = True
+
+    arg: str
+    sources: List[str] = ['cli', 'env']
+
+
+class GlobalDockerConfig(BaseModel):
+    class Config:
+        allow_population_by_field_name = True
+
+    repository: Optional[str]
+    context: str = '.'
+    dockerfile: str = 'Dockerfile'
+    build_args: List[BuildArgConfig] = Field(default_factory=list, alias='build-args')
+    app_name: Optional[str] = Field(None, alias='app-name')
+
+
+class TargetDockerConfig(BaseModel):
+    class Config:
+        allow_population_by_field_name = True
+
+    dockerfile: Optional[str] = None
+    target: Optional[str] = None
+    build_args: List[BuildArgConfig] = Field(default_factory=list, alias='build-args')
+
+
+class FingerprintTarget(BaseModel):
+    class Config:
+        allow_population_by_field_name = True
+
+    depends_on: List[str] = Field(default_factory=list, alias='depends-on')
+
+    # All directory paths are recursive. Every element is a glob
+    include_paths: List[str] = Field(default_factory=list, alias='include-paths')
+    docker: TargetDockerConfig = TargetDockerConfig()
+    build_target: bool = Field(True, alias='build-target')
+
+
+class BuildConfig(BaseModel):
+    class Config:
+        allow_population_by_field_name = True
+    ignore_paths: List[str] = Field(default_factory=list, alias='ignore-paths')
+    targets: Dict[str, FingerprintTarget]
+    docker: GlobalDockerConfig = GlobalDockerConfig()
+
+
+class TargetOutput(BaseModel):
+    class Config:
+        allow_population_by_field_name = True
+    name: str
+    dockerfile: Optional[str]
+    docker_target: Optional[str] = Field(None, alias='dockerTarget')
+    fingerprint: str
+    docker_command: Optional[str] = Field(None, alias='dockerCommand')
+    docker_tag: Optional[str] = Field(None, alias='dockerTag')
+
+    @property
+    def docker_build_command(self) -> str:
+        return (
+            f"docker build"
+        )
