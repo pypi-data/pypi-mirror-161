ed84c18cea2fd2ca7e110f26d41294da136a44a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:14 +0000

    Add package aioopenexchangerates/0.2.0/aioopenexchangerates-0.2.0.tar.gz

---
+++ packages/aioopenexchangerates/aioopenexchangerates-0.2.0.tar.gz/aioopenexchangerates-0.2.0/src/aioopenexchangerates/exceptions.py
@@ -0,0 +1,13 @@
+"""Provide exceptions for the Open Exchange Rates API."""
+
+
+class OpenExchangeRatesError(Exception):
+    """Represent an error for the Open Exchange Rates API."""
+
+
+class OpenExchangeRatesClientError(OpenExchangeRatesError):
+    """Represent a client error."""
+
+
+class OpenExchangeRatesAuthError(OpenExchangeRatesClientError):
+    """Represent an authentication error."""
