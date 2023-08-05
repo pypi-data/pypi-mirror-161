55661771c3a039e2f3f6d8b1f0934e99fa62647a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:36 +0000

    Add package proteus-preprocessing/0.1.9/proteus_preprocessing-0.1.9-py3-none-any.whl

---
+++ packages/proteus-preprocessing/proteus_preprocessing-0.1.9-py3-none-any.whl/preprocessing/modular/egrid.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def extract_logic(egrid):
+    logic = egrid.export_actnum().numpyCopy()
+    return logic
+
+
+def preprocess(egrid):
+    """Extracts logic and grid dimension information from an EclGrid object
+
+    Args:
+        egrid (EclGrid): the EclGrid object
+
+    Returns:
+        {np.array, np.array}: a dictionary with the logic and dimension numpy arrays
+    """
+    logic = extract_logic(egrid)
+
+    return {"logic": logic}
