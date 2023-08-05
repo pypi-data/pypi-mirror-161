dde51fa7d9ef45fd292abd0859d6d867856f350f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:57 +0000

    Add package refnx/0.1.30/refnx-0.1.30-cp39-cp39-macosx_10_9_universal2.whl

---
+++ packages/refnx/refnx-0.1.30-cp39-cp39-macosx_10_9_universal2.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.30'
+version = '0.1.30'
+full_version = '0.1.30'
+git_revision = 'f61d6b06c46512d4980759314c366cb07c5ab123'
+release = True
+if not release:
+    version = full_version
