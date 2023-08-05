2b81a0345b84abb033608191518c9ea85409a233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:09 +0000

    Add package rinoh-typeface-verdana/0.1.0/rinoh-typeface-verdana-0.1.0.tar.gz

---
+++ packages/rinoh-typeface-verdana/rinoh-typeface-verdana-0.1.0.tar.gz/rinoh-typeface-verdana-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from rinoh_mscorefontinstaller import setup
+
+
+setup(
+    name='Verdana',
+    version='0.1.0',
+    archive='verdan32.exe',
+    sha256sum='c1cb61255e363166794e47664e2f21af8e3a26cb6346eb8d2ae2fa85dd5aad96',
+    setup_file=__file__
+)
