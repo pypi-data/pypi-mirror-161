cdba802c09b124eb191b961ec3c5fc0f55df698e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:41 +0000

    Add package pydeflate/1.1.10/pydeflate-1.1.10.tar.gz

---
+++ packages/pydeflate/pydeflate-1.1.10.tar.gz/pydeflate-1.1.10/pydeflate/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+class Paths:
+    def __init__(self, project_dir):
+        self.project_dir = project_dir
+
+    @property
+    def data(self):
+        return os.path.join(self.project_dir, "pydeflate", ".pydeflate_data")
+
+
+PATHS = Paths(os.path.dirname(os.path.dirname(__file__)))
