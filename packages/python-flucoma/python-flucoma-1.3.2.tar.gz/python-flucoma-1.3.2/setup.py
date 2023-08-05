9501eaeeb8c62481fa7854d22f2d6415673ea82f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:50 +0000

    Add package python-flucoma/1.3.2/python-flucoma-1.3.2.tar.gz

---
+++ packages/python-flucoma/python-flucoma-1.3.2.tar.gz/python-flucoma-1.3.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from pathlib import Path
+
+readme = Path("./README.md").resolve()
+
+with open(readme, encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="python-flucoma",
+    version="1.3.2",
+    author="James Bradbury",
+    url="https://github.com/jamesb93/python-flucoma",
+    author_email="reacoma@jamesbradbury.net",
+    description="Loose bindings to the FluCoMa command line",
+    license="BSD",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages=["flucoma"],
+    install_requires=[
+        "SoundFile",
+        "numpy",
+        "scipy",
+    ],
+)
