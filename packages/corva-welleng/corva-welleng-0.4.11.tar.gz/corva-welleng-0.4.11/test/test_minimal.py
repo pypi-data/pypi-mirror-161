07070111be7c78e6e80612ce85921b9b130a3adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:07 +0000

    Add package corva-welleng/0.4.11/corva-welleng-0.4.11.tar.gz

---
+++ packages/corva-welleng/corva-welleng-0.4.11.tar.gz/corva-welleng-0.4.11/test/test_minimal.py
@@ -0,0 +1,25 @@
+
+"""
+test_minimal.py
+--------------
+Test things that should work with a *minimal* welleng install.
+"""
+import unittest
+
+import welleng as we
+from welleng.error import ISCWSAErrorModel
+
+
+class MinimalTest(unittest.TestCase):
+
+    def test_survey(self):
+        survey = we.survey.interpolate_survey(
+            survey=we.survey.Survey(
+                md=[0, 500, 1000, 2000, 3000],
+                inc=[0, 0, 30, 90, 90],
+                azi=[90, 90, 90, 135, 180],
+                error_model=ISCWSAErrorModel.Rev5.value
+            ),
+            step=30.
+        )
+        return survey
