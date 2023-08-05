407ed6e35be18744e33ae54428f9dbfa54f5664e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:10 +0000

    Add package nonebot-plugin-smart-reply/0.0.4/nonebot_plugin_smart_reply-0.0.4.tar.gz

---
+++ packages/nonebot-plugin-smart-reply/nonebot_plugin_smart_reply-0.0.4.tar.gz/nonebot_plugin_smart_reply-0.0.4/nonebot_plugin_smart_reply/__init__.py
@@ -0,0 +1,64 @@
+from nonebot.plugin.on import on_message,on_notice
+from nonebot.rule import to_me
+from nonebot.adapters.onebot.v11 import (
+    GroupMessageEvent,
+    Message,
+    MessageEvent,
+    PokeNotifyEvent,
+    MessageSegment
+)
+from .utils import *
+
+# 优先级99, 条件: 艾特bot就触发
+ai = on_message(rule=to_me(), priority=99,block=False)
+# 优先级1, 不会向下阻断, 条件: 戳一戳bot触发
+poke_ = on_notice(rule=to_me(),block=False)
+
+
+@ai.handle()
+async def _(event: MessageEvent):
+    # 获取消息文本
+    msg = str(event.get_message())
+    # 去掉带中括号的内容(去除cq码)
+    msg = re.sub(r"\[.*?\]", "", msg)
+    # 如果是光艾特bot(没消息返回)或者打招呼的话,就回复以下内容
+    if (not msg) or msg.isspace() or msg in [
+        "你好啊",
+        "你好",
+        "在吗",
+        "在不在",
+        "您好",
+        "您好啊",
+        "你好",
+        "在",
+    ]:
+        await ai.finish(Message(random.choice(hello__reply)))
+    # 获取用户nickname
+    if isinstance(event, GroupMessageEvent):
+        nickname = event.sender.card or event.sender.nickname
+    else:
+        nickname = event.sender.nickname
+    # 从字典里获取结果
+    result = await get_chat_result(msg,  nickname)
+    # 如果词库没有结果，则调用qingyunke获取智能回复
+    if result == None:
+        url = f"http://api.qingyunke.com/api.php?key=free&appid=0&msg={msg}"
+        message = await get_reply(url)
+        await ai.finish(message=message)
+    await ai.finish(Message(result))
+
+
+@poke_.handle()
+async def _poke_event(event: PokeNotifyEvent):
+    if event.is_tome:
+        # 50%概率回复莲宝的藏话
+        if random.random() < 0.5:
+            # 发送语音需要配置ffmpeg, 这里try一下, 不行就随机回复poke__reply的内容
+            try:
+                await poke_.send(MessageSegment.record(Path(aac_file_path)/random.choice(aac_file_list)))
+            except:
+                await poke_.send(message=f"{random.choice(poke__reply)}")
+        # 随机回复poke__reply的内容
+        else:
+            await poke_.send(message=f"{random.choice(poke__reply)}")
+
