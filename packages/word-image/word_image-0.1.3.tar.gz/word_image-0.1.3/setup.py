37978220ce425b6ba239be4f686d6935678dc8e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:04 +0000

    Add package word-image/0.1.3/word_image-0.1.3.tar.gz

---
+++ packages/word-image/word_image-0.1.3.tar.gz/word_image-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from os.path import abspath, dirname, join
+
+README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
+
+setup(
+    name="word_image",
+    version="0.1.3",
+    license="MIT",
+    packages=find_packages(),
+    description="Generate images of words with configurable augmentation options",
+    long_description=README_MD,
+    long_description_content_type="text/markdown",
+    url="https://github.com/econser/word_image",
+    download_url="https://github.com/econser/word_image/archive/refs/tags/v_0_1_3.tar.gz",
+    install_requires=['numpy', 'imgaug', 'Pillow', 'matplotlib'],
+    author="econser",
+    author_email="econser@gmail.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python :: 3 :: Only"
+    ],
+    keywords="contrastive learning, computer vision, image augmentation"
+)
---
+++ packages/word-image/word_image-0.1.3.tar.gz/word_image-0.1.3/word_image/__init__.py
@@ -0,0 +1,1 @@
+from word_image.word_image import WordSet
