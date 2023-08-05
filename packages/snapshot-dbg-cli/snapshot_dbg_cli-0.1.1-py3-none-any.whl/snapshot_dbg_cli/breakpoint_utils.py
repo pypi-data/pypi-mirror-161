a9a2ab5f3a63c58e863720ea6e58b2f5e1a3d793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:45 +0000

    Add package snapshot-dbg-cli/0.1.1/snapshot_dbg_cli-0.1.1-py3-none-any.whl

---
+++ packages/snapshot-dbg-cli/snapshot_dbg_cli-0.1.1-py3-none-any.whl/snapshot_dbg_cli/breakpoint_utils.py
@@ -0,0 +1,170 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This module provides a variety of utilities related to breakpoint processing.
+
+These utilities are useful in multiple snapshot and logpoint commands.
+"""
+
+import datetime
+import re
+
+# Regex that can be used to validate a user inputted location which should be in
+# the format file:line.
+LOCATION_REGEX = '^[^:]+:[1-9][0-9]*$'
+
+# Impose some 'reasonable' max, here we choose 2^31-1, won't cause any overflows
+# anywhere and is more than enough for a file's line number.
+MAX_LINE_NUMBER = 2147483647
+
+LOCATION_ERROR_MSG = ('Location must be in the format file:line, with the '
+                      f'maximum line number being {MAX_LINE_NUMBER}')
+
+
+# Returns dict containing {'path': string, 'line:' int} on success.
+# Returns None on failure (ie invalid input).
+def parse_and_validate_location(file_line):
+  match = re.search(LOCATION_REGEX, file_line)
+
+  if match is None:
+    return None
+
+  parsed_value = file_line.split(':')
+
+  line = int(parsed_value[1])
+
+  if line > MAX_LINE_NUMBER:
+    return None
+
+  return {'path': parsed_value[0], 'line': line}
+
+
+def transform_location_to_file_line(location):
+  """Returns a location in the format of 'file:line'.
+
+  Parameters:
+    location (dict): Location in dict form with 'path' and 'line' fields.
+
+  Returns:
+    location (string): The location in the format 'file:line'
+  """
+  if 'path' not in location or 'line' not in location:
+    return None
+
+  return f"{location['path']}:{location['line']}"
+
+
+# Returns the id to use when creating a new breakpoint.
+# To note, we use the format 'b_<unix epoc seconds>'
+#
+# To note, this ensures the ID cannot be interpreted as an integer. This is
+# specifically done for the following reason:
+
+
+def convert_unix_msec_to_rfc3339(unix_msec):
+  """Converts a Unix timestamp represented in milliseconds since the epoch to an
+
+  RFC3339 string representation.
+
+  Args:
+    unix_msec: The Unix timestamp, represented in milliseconds since the epoch.
+
+  Returns:
+    An RFC3339 encoded timestamp string in format: "%Y-%m-%dT%H:%M:%S.%fZ".
+  """
+  try:
+    seconds = unix_msec / 1000
+    msec = unix_msec % 1000
+    timestamp = seconds
+    dt = datetime.datetime.fromtimestamp(timestamp, tz=datetime.timezone.utc)
+    return dt.strftime(f'%Y-%m-%dT%H:%M:%S.{msec:03}000') + 'Z'
+  except (OverflowError, OSError, TypeError, ValueError):
+    # By using 0, we'll still get the expected formatted string, and the value
+    # will be '1970-01-01...', which visually will be recognizable as beginning
+    # of epoch and that the value was not known.
+    return convert_unix_msec_to_rfc3339(0)
+
+
+def set_converted_timestamps(bp):
+  conversions = [['createTime', 'createTimeUnixMsec'],
+                 ['finalTime', 'finalTimeUnixMsec']]
+
+  for c in conversions:
+    if c[0] not in bp and c[1] in bp:
+      bp[c[0]] = convert_unix_msec_to_rfc3339(bp[c[1]])
+
+  return bp
+
+
+# Returns None if there's an issue
+def normalize_breakpoint(bp, bpid=None):
+  """Validates and normalizes a breakpoint.
+
+  This method ensures all required and expected fields are set. If any required
+  field is not set, and cannot be filled in, None will be returned.
+
+  If a breakpoint is returned, the following fields are guaranteed to
+  be populated:
+
+  id
+  location
+    path
+    line
+  action
+  isFinalState
+  createTime
+  createTimeUnixMsec
+  finalTime
+  finalTimeUnixMsec
+  userEmail
+
+  Returns:
+    The normalized breakpoint on success, None on failure.
+  """
+  if type(bp) is not dict:
+    return None
+
+  if 'id' not in bp and bpid is not None:
+    bp['id'] = bpid
+
+  required_fields = ['id', 'location']
+  for f in required_fields:
+    if f not in bp:
+      return None
+
+  required_location_fields = ['path', 'line']
+  for f in required_location_fields:
+    if f not in bp['location']:
+      return None
+
+  if 'action' not in bp:
+    bp['action'] = 'CAPTURE'
+
+  if 'isFinalState' not in bp:
+    bp['isFinalState'] = False
+
+  if 'createTimeUnixMsec' not in bp:
+    # Assuming everything is working correctly the createTimeUnixMsec value
+    # should be present, if not initialize it to 0 so it's set to something,
+    # but it's clear the time was not actually known.
+    bp['createTimeUnixMsec'] = 0
+
+  if 'finalTimeUnixMsec' not in bp and bp['isFinalState']:
+    bp['finalTimeUnixMsec'] = 0
+
+  if 'userEmail' not in bp:
+    bp['userEmail'] = 'unknown'
+
+  set_converted_timestamps(bp)
+
+  return bp
---
+++ packages/snapshot-dbg-cli/snapshot_dbg_cli-0.1.1-py3-none-any.whl/snapshot_dbg_cli/delete_snapshots_command.py
@@ -0,0 +1,138 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This module contains the support for the delete_snapshots command.
+
+The delete_snapshots command is used to delete snapshots from a debug target
+(debuggee).
+"""
+
+from snapshot_dbg_cli.exceptions import SilentlyExitError
+from snapshot_dbg_cli import breakpoint_utils
+
+DESCRIPTION = """
+Used to delete snapshots from a debug target (debuggee). You are prompted for
+confirmation before any snapshots are deleted. To suppress confirmation, use the
+--quiet option.
+"""
+
+ID_HELP = """
+Zero or more snapshot IDs. The specified snapshots will be deleted. By default,
+If no snapshot IDs are specified, all active snapshots created by the user are
+selected for deletion.
+"""
+
+ALL_USERS_HELP = """
+If set, snapshots from all users will be deleted, rather than only snapshots
+created by the current user. This flag is not required when specifying the exact
+ID of a snapshot.
+"""
+
+INCLUDE_INACTIVE_HELP = """
+If set, also delete snapshots which have been completed. By default, only
+pending snapshots will be deleted. This flag is not required when specifying the
+exact ID of an inactive snapshot.
+"""
+
+QUIET_HELP = 'If set, suppresses user confirmation of the command.'
+
+SUMMARY_HEADERS = ['Status', 'Location', 'Condition', 'ID']
+
+
+def transform_to_snapshot_summary(snapshot):
+  # Match the fields from SUMMARY_HEADERS
+  return [
+      'COMPLETED' if snapshot['isFinalState'] else 'ACTIVE',
+      breakpoint_utils.transform_location_to_file_line(snapshot['location']),
+      snapshot['condition'] if 'condition' in snapshot else '', snapshot['id']
+  ]
+
+
+class DeleteSnapshotsCommand:
+  """This class implements the delete_snapshots command.
+
+  The register() method is called by the CLI startup code to install the
+  delete_snapshots command information, and the cmd() function will be invoked
+  if the delete_snapshots command was specified by the user.
+  """
+
+  def __init__(self):
+    pass
+
+  def register(self, args_subparsers, required_parsers, common_parsers):
+    parent_parsers = [
+        common_parsers.database_url, common_parsers.format,
+        common_parsers.debuggee_id
+    ]
+    parent_parsers += required_parsers
+    parser = args_subparsers.add_parser(
+        'delete_snapshots', description=DESCRIPTION, parents=parent_parsers)
+    parser.add_argument('ID', help=ID_HELP, nargs='*')
+    parser.add_argument('--all-users', help=ALL_USERS_HELP, action='store_true')
+    parser.add_argument(
+        '--include-inactive', help=INCLUDE_INACTIVE_HELP, action='store_true')
+    parser.add_argument('--quiet', help=QUIET_HELP, action='store_true')
+    parser.set_defaults(func=self.cmd)
+
+  def cmd(self, args, cli_services):
+    user_input = cli_services.user_input
+    user_output = cli_services.user_output
+    debugger_rtdb_service = cli_services.get_snapshot_debugger_rtdb_service()
+
+    debugger_rtdb_service.validate_debuggee_id(args.debuggee_id)
+
+    # This will be a list, if no IDs were specified it will be empty. If any IDs
+    # are specified those are the only ones that will be deleted.
+    snapshot_ids = args.ID
+
+    user_email = None if args.all_users is True else cli_services.account
+
+    snapshots = []
+
+    if snapshot_ids:
+      ids_not_found = []
+
+      for bp_id in snapshot_ids:
+        snapshot = debugger_rtdb_service.get_breakpoint(args.debuggee_id, bp_id)
+        if snapshot is None:
+          ids_not_found.append(bp_id)
+        else:
+          snapshots.append(snapshot)
+
+      if ids_not_found:
+        user_output.error(f"Snapshot ID not found: {', '.join(ids_not_found)}")
+        raise SilentlyExitError
+    else:
+      snapshots = debugger_rtdb_service.get_snapshots(args.debuggee_id,
+                                                      args.include_inactive,
+                                                      user_email)
+
+    if snapshots:
+      user_output.normal('This command will delete the following snapshots:\n')
+      values = list(map(transform_to_snapshot_summary, snapshots))
+      user_output.tabular(SUMMARY_HEADERS, values)
+      user_output.normal('\n')
+
+      if not args.quiet and not user_input.prompt_user_to_continue():
+        user_output.error('Delete aborted.')
+        return
+
+      debugger_rtdb_service.delete_breakpoints(args.debuggee_id, snapshots)
+
+    # The status is output regardless of the requested output format. It should
+    # go to stderr, and if json output is requested, that should end up on
+    # stdout.
+    user_output.normal(f'Deleted {len(snapshots)} snapshots.')
+
+    if args.format.is_a_json_value():
+      user_output.json_format(snapshots, pretty=args.format.is_pretty_json())
