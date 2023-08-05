88c5df0e6be31c2c73328614f3577c90b3a0c3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:34 +0000

    Add package themispy/0.0.8/themispy-0.0.8.tar.gz

---
+++ packages/themispy/themispy-0.0.8.tar.gz/themispy-0.0.8/themispy/project/__init__.py
@@ -0,0 +1,11 @@
+from themispy.project.utils import (
+    PROJECT_PATH, PROJECT_TITLE, 
+    build_path, split_filepath
+)
+
+__all__ = [
+    "PROJECT_PATH",
+    "PROJECT_TITLE",
+    "build_path",
+    "split_filepath"
+]
