8547242801a41fc7c08f05af5d5a234e884f9d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:50 +0000

    Add package audio-diffusion-pytorch/0.0.9/audio-diffusion-pytorch-0.0.9.tar.gz

---
+++ packages/audio-diffusion-pytorch/audio-diffusion-pytorch-0.0.9.tar.gz/audio-diffusion-pytorch-0.0.9/audio_diffusion_pytorch/__init__.py
@@ -0,0 +1,12 @@
+from .diffusion import (
+    Diffusion,
+    DiffusionInpainter,
+    DiffusionSampler,
+    KerrasSchedule,
+    LogNormalSampler,
+    SigmaSampler,
+    SigmaSchedule,
+    SpanBySpanComposer,
+)
+from .model import AudioDiffusionModel, Model1d
+from .modules import UNet1d
