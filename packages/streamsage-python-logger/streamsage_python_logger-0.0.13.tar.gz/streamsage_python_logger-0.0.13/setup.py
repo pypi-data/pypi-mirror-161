5d31e5d96a396c80f46361adc9b385bcb72f7884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:32 +0000

    Add package streamsage-python-logger/0.0.13/streamsage_python_logger-0.0.13.tar.gz

---
+++ packages/streamsage-python-logger/streamsage_python_logger-0.0.13.tar.gz/streamsage_python_logger-0.0.13/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(name="streamsage_python_logger",
+                 version="0.0.13",
+                 author="streamsage",
+                 description="python logger",
+                 author_email="dev@streamsage.io",
+                 install_requires=["fluent-logger==0.10.0",
+                                   "pydantic~=1.8.0",
+                                   "python-dotenv==0.19.1"],
+                 packages=setuptools.find_packages(),
+                 license="MIT")
