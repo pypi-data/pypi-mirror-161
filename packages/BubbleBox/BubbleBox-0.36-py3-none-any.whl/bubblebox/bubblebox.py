382090ef50654e6145a3184330b29d03e0099bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:13 +0000

    Add package BubbleBox/0.36/BubbleBox-0.36-py3-none-any.whl

---
+++ packages/BubbleBox/BubbleBox-0.36-py3-none-any.whl/bubblebox/bubblebox.py
@@ -0,0 +1,17 @@
+# Bubblebox 
+# Author: Audun Skau Hansen 2022
+
+import bubblebox.mdbox
+import bubblebox.binding_models
+import bubblebox.ising
+import bubblebox.lattice_models
+import bubblebox.showcase
+
+
+from bubblebox.mdbox import mdbox, box
+from bubblebox.binding_models import bindingbox
+from bubblebox.ising import isingbox
+from bubblebox.lattice_models import latticebox
+
+
+import numpy as np
