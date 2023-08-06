93d3e4565ab841fe8ab5694a906a15893583cc6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:08 +0000

    Add package gradio/3.1.3a5/gradio-3.1.3a5.tar.gz

---
+++ packages/gradio/gradio-3.1.3a5.tar.gz/gradio-3.1.3a5/gradio/templates/cdn/assets/index.43ae246e.js
@@ -0,0 +1,5 @@
+export { M as Component } from './index.64c4405d.js';
+
+const modes = ["static"];
+
+export { modes };
---
+++ packages/gradio/gradio-3.1.3a5.tar.gz/gradio-3.1.3a5/gradio/templates/frontend/assets/index.bd494b52.js
@@ -0,0 +1,5 @@
+export { M as Component } from './index.d7a55308.js';
+
+const modes = ["static"];
+
+export { modes };
