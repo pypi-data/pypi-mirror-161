f7f01b3b2f7a4b655d47708351613d76ea356658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:42 +0000

    Add package azure-cli/2.39.0/azure_cli-2.39.0-py3-none-any.whl

---
+++ packages/azure-cli/azure_cli-2.39.0-py3-none-any.whl/azure/cli/command_modules/feedback/_help.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from knack.help_files import helps  # pylint: disable=unused-import
+# pylint: disable=line-too-long, too-many-lines
+
+helps['feedback'] = """
+type: command
+short-summary: Send feedback to the Azure CLI Team.
+long-summary: >-
+    This command is interactive. If possible, it launches the default
+    web browser to open GitHub issue creation page with the body auto-generated and pre-filled.
+    You will have a chance to edit the issue body before submitting it.
+"""
