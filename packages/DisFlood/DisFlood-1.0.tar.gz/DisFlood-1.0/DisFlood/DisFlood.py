898e3d82077b5a9c08f5c08ae8508c7b04550a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:53 +0000

    Add package DisFlood/1.0/DisFlood-1.0.tar.gz

---
+++ packages/DisFlood/DisFlood-1.0.tar.gz/DisFlood-1.0/DisFlood/DisFlood.py
@@ -0,0 +1,28 @@
+﻿from discontrol.Client import Client
+from discontrol.Message import Message
+import time
+class DisFlood:
+    def __init__(self,client : Client):
+        self.Client : Client = client
+        self.ChannelId : int = 0
+        self.Interval : float = 0
+        self.CountMessagesForInterval : int = 10
+        self.CountMessages : int = 0
+    def SetChannel(self,ChannelId : int):
+        self.ChannelId = ChannelId
+    def SetInterval(self,Interval : float):
+        self.Interval = Interval
+    def SetCountMessages(self,CountMessages : int):
+        self.CountMessages = CountMessages
+    def SetCountMessagesForInterval(self, CountMessagesForInterval : int):
+        self.CountMessagesForInterval = CountMessagesForInterval
+    def Flood(self,Content : str,ReturnMessages : bool=False):
+        Messages = []
+        for i in range(0,self.CountMessages):
+            for ii in range(0,self.CountMessagesForInterval):
+                Messages.append(self.Client.send_message(self.ChannelId,Content,ReturnMessages)) 
+            time.sleep(self.Interval)
+        return Messages
+        
+
+
---
+++ packages/DisFlood/DisFlood-1.0.tar.gz/DisFlood-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='DisFlood',
+	version='1.0',
+	author='Lcvb_X',
+	author_email='podygka1990@gmail.com',
+	description='this library for flood to discord on discontrol',
+	packages=['DisFlood'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
