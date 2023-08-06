ad06b15d80d95d842a3a82f6160f010e073faf06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:19 +0000

    Add package themispy/0.0.14/themispy-0.0.14-py3-none-any.whl

---
+++ packages/themispy/themispy-0.0.14-py3-none-any.whl/themispy/azure/__init__.py
@@ -0,0 +1,12 @@
+from themispy.azure.tools import (
+    get_connection_string,
+    build_ingestion_path,
+    INGESTION_PATH
+)
+
+
+__all__ = [
+    "get_connection_string",
+    "build_ingestion_path",
+    "INGESTION_PATH"
+]
