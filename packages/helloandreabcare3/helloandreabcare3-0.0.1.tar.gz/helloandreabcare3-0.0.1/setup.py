6226b1127a4c96574593502bb614ebd4714c3626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:48 +0000

    Add package helloandreabcare3/0.0.1/helloandreabcare3-0.0.1.tar.gz

---
+++ packages/helloandreabcare3/helloandreabcare3-0.0.1.tar.gz/helloandreabcare3-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A basic hello package'
+
+# Setting up
+setup(
+    name="helloandreabcare3",
+    version=VERSION,
+    author="Andrew (Florian Dedov)",
+    author_email="<ablasi@bettercare.es>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
