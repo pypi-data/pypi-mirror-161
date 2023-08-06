12adca6a7554ace3a9a55f41d71766f85f4556e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:49 +0000

    Add package np-data-validator-2/0.1.21/np_data_validator_2-0.1.21.tar.gz

---
+++ packages/np-data-validator-2/np_data_validator_2-0.1.21.tar.gz/np_data_validator_2-0.1.21/np_data_validator_2/storage.py
@@ -0,0 +1,42 @@
+from .dataclasses import AssetValidationResult, ValidationManifest
+from .transformers import (
+    from_AssetValidationResult,
+    from_ValidationManifest,
+    to_AssetValidationResult,
+    to_ValidationManifest,
+)
+
+
+def get_validation_manifest(table, name: str) -> ValidationManifest:
+    data = table.find_one({"name": name}, {"_id": False})
+    return to_ValidationManifest(data)
+
+
+def put_validation_manifest(table, manifest: ValidationManifest):
+    return table.insert_one(from_ValidationManifest(manifest))
+
+
+def get_asset_validation_results(
+    table, lims_id: str, asset_type: str = None
+) -> AssetValidationResult:
+    query = {"lims_id": lims_id}
+    if asset_type is not None:
+        query["manifest.asset_type"] = asset_type
+    data = table.find(query, {"_id": False})
+    return [to_AssetValidationResult(doc) for doc in data]
+
+
+def put_asset_validation_result(table, result: AssetValidationResult):
+    return table.insert_one(from_AssetValidationResult(result))
+
+
+def update_asset_validation_result(table, lims_id: str, name: str, updates: dict):
+    return table.update_one(
+        {
+            "lims_id": lims_id,
+            "manifest.asset_type": name,
+        },
+        {
+            "$set": updates,
+        },
+    )
