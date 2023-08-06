ab1f30c3e5c231e17f17df02e00806a6164358e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:56 +0000

    Add package AutoBrewML/0.2/AutoBrewML-0.2.tar.gz

---
+++ packages/AutoBrewML/AutoBrewML-0.2.tar.gz/AutoBrewML-0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='AutoBrewML', 
+ version='0.2',
+ author="Sreeja Deb",
+ author_email="srde@microsoft.com",
+ description="With AutoBrewML Framework the time it takes to get production-ready ML models with great ease and efficiency highly accelerates.",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
