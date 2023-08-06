f6e45e65e813d30149e27ba8c910e5974f475186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:36 +0000

    Add package scraper-api/0.1.0/scraper_api-0.1.0-py3-none-any.whl

---
+++ packages/scraper-api/scraper_api-0.1.0-py3-none-any.whl/scraper/base.py
@@ -0,0 +1,32 @@
+"""
+Base class for all scrapers
+
+Examples:
+    >>> from scraper.base import AbstractScraper
+    >>> import requests
+    >>> class Scraper(AbstractScraper):
+    >>>     def scrape(self, url: str) -> str:
+    >>>         return requests.get(url).text
+    >>> scraper = Scraper()
+    >>> scraper.scrape("https://www.example.com/")
+
+In this example we define our Scraper derived from AbstractScraper
+"""
+from abc import ABCMeta, abstractmethod
+
+
+class AbstractScraper(metaclass=ABCMeta):
+    """
+    Interface of scraper class
+    """
+
+    @abstractmethod
+    def scrape(self, url: str) -> str:
+        """
+        Main method to start to scrape data from url
+        Args:
+            url: (str): url of web-site
+
+        Returns:
+            str: Text plain
+        """
