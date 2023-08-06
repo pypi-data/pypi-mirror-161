d5d43680fd787e5b47c40ed622792aac801e3939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:22 +0000

    Add package fxtrade/0.0.6/fxtrade-0.0.6.tar.gz

---
+++ packages/fxtrade/fxtrade-0.0.6.tar.gz/fxtrade-0.0.6/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fxtrade', 'fxtrade.algorithm', 'fxtrade.interface']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'fxtrade',
+    'version': '0.0.6',
+    'description': '',
+    'long_description': None,
+    'author': 'wsuzume',
+    'author_email': 'joshnobus@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
