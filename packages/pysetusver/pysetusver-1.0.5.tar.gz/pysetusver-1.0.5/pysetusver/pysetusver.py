6e6e8e2550694313bc4561e18452a3dcc75208ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:18 +0000

    Add package pysetusver/1.0.5/pysetusver-1.0.5.tar.gz

---
+++ packages/pysetusver/pysetusver-1.0.5.tar.gz/pysetusver-1.0.5/pysetusver/pysetusver.py
@@ -0,0 +1,10 @@
+import requests as r
+import time,os,wget,random
+def let_me_see_see(number,var="sort=r18&size=original&type=json"):
+    count = 0
+    data1 = r.get(F"https://moe.jitsu.top/img/?{var}&num={number}")
+    data2 = data1.json()
+    for data3 in data2["pic"]:
+        count += 1
+        wget.download(data3,out=os.path.join(os.getcwd(),str(count)+".jpg"))
+        time.sleep(random.randint(0.5,1.0))
