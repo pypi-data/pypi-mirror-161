c330baaa0abfcdf4ab8a5bd0a0865e3d56e4cb2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:08 +0000

    Add package alphaGrinder/0.0.2/alphaGrinder-0.0.2.tar.gz

---
+++ packages/alphaGrinder/alphaGrinder-0.0.2.tar.gz/alphaGrinder-0.0.2/alphaGrinder/__init__.py
@@ -0,0 +1,1 @@
+from .alphaSeeker import *
---
+++ packages/alphaGrinder/alphaGrinder-0.0.2.tar.gz/alphaGrinder-0.0.2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name = "alphaGrinder",
+	version = "0.0.2",
+	author = "Xi",
+	description = "common A Share alpha and risk factors",
+	packages = ["alphaGrinder"]
+	)
