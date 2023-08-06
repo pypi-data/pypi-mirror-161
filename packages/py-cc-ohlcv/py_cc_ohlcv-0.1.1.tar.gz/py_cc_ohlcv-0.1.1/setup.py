1ee048b6b82c9c40319362d275ba6b4fa7bd8ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:08 +0000

    Add package py-cc-ohlcv/0.1.1/py_cc_ohlcv-0.1.1.tar.gz

---
+++ packages/py-cc-ohlcv/py_cc_ohlcv-0.1.1.tar.gz/py_cc_ohlcv-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py_cc_ohlcv',
+    version='0.1.1',
+    license='MIT',
+    author="Max Gao",
+    author_email='gaamox@tutanota.com',
+    packages=find_packages(exclude=["examples"]),
+    url='https://github.com/gmax0/Cryptocurrency-OHLCV-Scraper',
+    keywords='ohlcv, cryptocurrency, historical, market, data',
+    install_requires=[
+        'pandas',
+        'requests'
+      ],
+)
