0c1ad31970f5a4208a86d7917908cd58fa68a46d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:41 +0000

    Add package password-manager-cli/0.0.1/password_manager_cli-0.0.1-py3-none-any.whl

---
+++ packages/password-manager-cli/password_manager_cli-0.0.1-py3-none-any.whl/src/menu/__init__.py
@@ -0,0 +1,2 @@
+from .index_menu import gen_index, index_menu
+from .start import start
---
+++ packages/password-manager-cli/password_manager_cli-0.0.1-py3-none-any.whl/src/menu/start.py
@@ -0,0 +1,16 @@
+from rich.align import Align
+from rich.console import Group
+from rich.rule import Rule
+
+from .index_menu import gen_index
+
+
+def start() -> tuple:
+
+    arrow_pos = [0]
+    group = Group(
+        Rule("MENU"),
+        Align(gen_index(arrow_pos, ""), "center"),
+    )
+
+    return group, arrow_pos
