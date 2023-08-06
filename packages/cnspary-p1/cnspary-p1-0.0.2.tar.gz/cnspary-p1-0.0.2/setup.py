74540d270c1114db74a25769570501766c3277bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:39 +0000

    Add package cnspary-p1/0.0.2/cnspary-p1-0.0.2.tar.gz

---
+++ packages/cnspary-p1/cnspary-p1-0.0.2.tar.gz/cnspary-p1-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cnspary-p1',
+    version='0.0.2',
+    author='cnspary',
+    author_email='cnspary@outlook.com',
+    description='MC-PackageOne',
+    packages=find_packages(),
+    python_requires='>=3.0'
+)
