04443c8d95715095ad6d080663b8235afa07f4e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:15 +0000

    Add package mtoml/1.1.1/mtoml-1.1.1.tar.gz

---
+++ packages/mtoml/mtoml-1.1.1.tar.gz/mtoml-1.1.1/mtoml/__init__.py
@@ -0,0 +1,9 @@
+#######################################################################
+# Copyright (c) 2022 Jordan Schaffrin                                 #
+#                                                                     #
+# This Source Code Form is subject to the terms of the Mozilla Public #
+# License, v. 2.0. If a copy of the MPL was not distributed with this #
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.            #
+#######################################################################
+
+from .mtoml import (mtomlc, get_dir, set_dir, load, is_loaded, save, save_all, get, set)
