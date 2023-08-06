9670bbb09c16834349932686d3c092a79c2d9bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:25 +0000

    Add package handle-distributions/0.2/handle_distributions-0.2.tar.gz

---
+++ packages/handle-distributions/handle_distributions-0.2.tar.gz/handle_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='handle_distributions',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      packages=['handle_distributions'],
+      author='Chamod perera',
+      author_email= 'chamodperera87@gmail.com',
+      zip_safe=False)
