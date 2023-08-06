5b800029c5f649040599fe0cf38d5e08a6689107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:04 +0000

    Add package ifood-ds-utils/0.1.0/ifood-ds-utils-0.1.0.tar.gz

---
+++ packages/ifood-ds-utils/ifood-ds-utils-0.1.0.tar.gz/ifood-ds-utils-0.1.0/ifood_ds_utils/main.py
@@ -0,0 +1,56 @@
+import click
+import os
+
+from colorama import Fore, init, Back,Style
+
+import importlib.metadata
+from directory_tree import display_tree
+
+__version__ = importlib.metadata.version("ifood_ds_utils")
+init()
+
+
+@click.command()
+def create_ifood_ds_template():
+    os.system("cookiecutter https://github.com/javierjdaza/ck-ds-project.git")
+
+@click.command()
+def welcome_to_the_library():
+
+    with open('./files/ascii-art.txt', 'r') as f:
+        for line in f:
+            click.echo(Fore.WHITE + line.rstrip())
+
+@click.command()
+@click.option('-dev', 'env_dev', is_flag=True, help='-dev <<s3://dev-ifood-ml-sagemaker/>>')
+@click.option('-prd', 'env_prd', is_flag=True, help='-prd <<s3://prd-ifood-sagemaker-output-ohio/>>')
+def upload_to_s3(env_dev,env_prd):
+    click.echo(Fore.YELLOW + f"Don't forget to log in:{Fore.RED} ifood-aws-login")
+    click.echo(Fore.YELLOW + "--"*25)
+    tree_string_rep = display_tree(os.getcwd(), string_rep = True)
+
+    click.echo(Fore.GREEN + tree_string_rep)
+    current_folder = os.path.basename(os.getcwd())
+
+    if env_dev:
+        answer = click.prompt(f'{Fore.RED}This will be your folder tree that will be uploaded to S3,\nin the route: {Fore.YELLOW}s3://dev-ifood-ml-sagemaker/{current_folder}\n{Fore.RED}all right?\nyes/no' + Fore.WHITE + '\n')
+        answer = answer.lower()
+        if answer == 'yes' or answer == 'y':
+            click.echo("os.system(f'aws s3 cp s3://dev-ifood-ml-sagemaker/{current_folder}/ ./  --recursive')")
+            # os.system(f'aws s3 cp s3://dev-ifood-ml-sagemaker/{current_folder}/ ./  --recursive')
+    else:
+        answer = click.prompt(f'{Fore.RED}This will be your folder tree that will be uploaded to S3,\nin the route: {Fore.YELLOW}s3://prd-ifood-sagemaker-output-ohio/{current_folder}\n{Fore.RED}all right?\nyes/no' + Fore.WHITE + '\n')
+        answer = answer.lower()
+        if answer == 'no' or answer == 'n':
+            click.echo("os.system(f'aws s3 cp s3://prd-ifood-sagemaker-output-ohio/{current_folder}/ ./  --recursive')")
+            # os.system(f'aws s3 cp s3://prd-ifood-sagemaker-output-ohio/{current_folder}/ ./  --recursive')
+
+@click.command()
+@click.option('-version','-V', 'version', is_flag=True, help='--v: Display Version of the package')
+def version(version:str):
+    if version:
+        click.echo(Fore.GREEN + f'ifood-ds-utils version: {__version__}')
+    else:
+        with open('./files/ascii-art.txt', 'r') as f:
+            for line in f:
+                click.echo(Fore.WHITE + line.rstrip())
---
+++ packages/ifood-ds-utils/ifood-ds-utils-0.1.0.tar.gz/ifood-ds-utils-0.1.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ifood_ds_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.1.3,<9.0.0',
+ 'colorama>=0.4.5,<0.5.0',
+ 'directory-tree>=0.0.2,<0.0.3',
+ 'ifood-aws-login>=0.0.1,<0.0.2']
+
+entry_points = \
+{'console_scripts': ['ifood-ds-usdsd = ifood_ds_utils.main:version',
+                     'template = ifood_ds_utils.main:create_ifood_ds_template',
+                     'upload-folder-s3 = ifood_ds_utils.main:upload_to_s3']}
+
+setup_kwargs = {
+    'name': 'ifood-ds-utils',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Javier Daza',
+    'author_email': 'javier.olivella@ifood.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
