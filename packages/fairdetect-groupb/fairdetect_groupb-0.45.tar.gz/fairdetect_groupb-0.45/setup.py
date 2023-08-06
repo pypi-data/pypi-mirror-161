64044dcb01ed60c2813a23d34b962820ca9bc2eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:49 +0000

    Add package fairdetect-groupb/0.45/fairdetect_groupb-0.45.tar.gz

---
+++ packages/fairdetect-groupb/fairdetect_groupb-0.45.tar.gz/fairdetect_groupb-0.45/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+install_requires = [
+    'dalex>=1.4.1'
+    
+]
+
+setup(name='fairdetect_groupb',
+      version='0.45',  # Development release
+      description='Library to identify bias in pre-trained models!',
+      url='https://github.com/dianisley/fairdetect_b',
+      author='GMBD_Group_B',
+      author_email='Carlos.BlazquezP@student.ie.edu',
+      license='MIT',
+          packages=['fairdetect_groupb'],
+      zip_safe=False,
+      install_requires=install_requires)
