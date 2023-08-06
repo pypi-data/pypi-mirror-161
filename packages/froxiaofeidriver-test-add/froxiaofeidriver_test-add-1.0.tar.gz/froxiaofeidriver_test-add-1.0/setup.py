139c00ad3b7b103ae23d33d43d9a394ea3b946fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:22 +0000

    Add package froxiaofeidriver-test-add/1.0/froxiaofeidriver_test-add-1.0.tar.gz

---
+++ packages/froxiaofeidriver-test-add/froxiaofeidriver_test-add-1.0.tar.gz/froxiaofeidriver_test-add-1.0/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import  setup
+
+
+packages = ['froxiaofeidriver_test']
+
+setup(name='froxiaofeidriver_test-add',version='1.0',author='feipip', packages=packages, package_dir={'requests': 'requests'},)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
