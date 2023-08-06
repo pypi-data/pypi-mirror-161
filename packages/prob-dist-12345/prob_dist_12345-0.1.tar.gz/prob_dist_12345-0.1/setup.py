771572fb7bcf030136882ad1b9e93c86f232863c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:05 +0000

    Add package prob-dist-12345/0.1/prob_dist_12345-0.1.tar.gz

---
+++ packages/prob-dist-12345/prob_dist_12345-0.1.tar.gz/prob_dist_12345-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prob_dist_12345',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
