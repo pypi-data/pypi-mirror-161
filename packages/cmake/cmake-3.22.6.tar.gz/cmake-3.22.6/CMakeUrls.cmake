e1b5fb95b7d383dc81d3a58a570fbb7e4198e89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:51 +0000

    Add package cmake/3.22.6/cmake-3.22.6.tar.gz

---
+++ packages/cmake/cmake-3.22.6.tar.gz/cmake-3.22.6/CMakeUrls.cmake
@@ -0,0 +1,26 @@
+
+#-----------------------------------------------------------------------------
+# CMake sources
+set(unix_source_url          "https://github.com/Kitware/CMake/releases/download/v3.22.6/cmake-3.22.6.tar.gz")
+set(unix_source_sha256       "73933163670ea4ea95c231549007b0c7243282293506a2cf4443714826ad5ec3")
+
+set(windows_source_url       "https://github.com/Kitware/CMake/releases/download/v3.22.6/cmake-3.22.6.zip")
+set(windows_source_sha256    "e878cdaf2a8a0aac5c8ec8d5e1df1c5abd0d307ea9b9be2008ac56e2e8077cfb")
+
+#-----------------------------------------------------------------------------
+# CMake binaries
+
+set(linux32_binary_url       "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256    "NA")
+
+set(linux64_binary_url       "https://github.com/Kitware/CMake/releases/download/v3.22.6/cmake-3.22.6-linux-x86_64.tar.gz")
+set(linux64_binary_sha256    "09e1b34026c406c5bf4d1b053eadb3a8519cb360e37547ebf4b70ab766d94fbc")
+
+set(macos10_10_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.22.6/cmake-3.22.6-macos10.10-universal.tar.gz")
+set(macos10_10_binary_sha256 "873d296000b2fbd5cd306a3455fddc254b485cad988c67bf4ee0ba4fd7a1e057")
+
+set(win32_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.22.6/cmake-3.22.6-windows-i386.zip")
+set(win32_binary_sha256      "058572b13af626e48cc8cba235c14491117b761354fb3c567b11c29835ff8283")
+
+set(win64_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.22.6/cmake-3.22.6-windows-x86_64.zip")
+set(win64_binary_sha256      "48bcc3e71e918b72e2682f9ca9d44dd6c416379071c1ecb530d0633374f91f15")
---
+++ packages/cmake/cmake-3.22.6.tar.gz/cmake-3.22.6/setup.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from distutils.text_file import TextFile
+
+from skbuild import setup
+
+# Add current folder to path
+# This is required to import versioneer in an isolated pip build
+# Prepending allows not to break on a non-isolated build when versioneer
+# is already installed (c.f. https://github.com/scikit-build/cmake-python-distributions/issues/171)
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+import versioneer  # noqa: E402
+
+with open('README.rst', 'r') as fp:
+    readme = fp.read()
+
+with open('HISTORY.rst', 'r') as fp:
+    history = fp.read().replace('.. :changelog:', '')
+
+
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        return TextFile(filename, file).readlines()
+
+
+test_requirements = parse_requirements('requirements-test.txt')
+
+setup(
+    name='cmake',
+
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+
+    author='Jean-Christophe Fillion-Robin',
+    author_email='jchris.fillionr@kitware.com',
+
+    package_dir={'': 'src'},
+    packages=['cmake'],
+
+    cmake_install_dir='src/cmake/data',
+
+    entry_points={
+        'console_scripts': [
+            'cmake=cmake:cmake', 'cpack=cmake:cpack', 'ctest=cmake:ctest'
+        ]
+    },
+
+    url='https://cmake.org/',
+    download_url='https://cmake.org/download',
+    project_urls={
+        "Documentation": "https://cmake-python-distributions.readthedocs.io/",
+        "Source Code": "https://github.com/scikit-build/cmake-python-distributions",
+        "Mailing list": "https://groups.google.com/forum/#!forum/scikit-build",
+        "Bug Tracker": "https://github.com/scikit-build/cmake-python-distributions/issues",
+    },
+
+
+    description='CMake is an open-source, cross-platform family of '
+                'tools designed to build, test and package software',
+
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
+
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: C',
+        'Programming Language :: C++',
+        'Programming Language :: Fortran',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Typing :: Typed',
+        ],
+
+    license='Apache 2.0',
+
+    keywords='CMake build c++ fortran cross-platform cross-compilation',
+
+    extras_require={"test": test_requirements},
+)
