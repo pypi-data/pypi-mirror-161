87c4165be5bf347a670bdeb4fc0d5c7778fa2fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:22 +0000

    Add package pyBasicUnityLikeECS/0.1.0/pyBasicUnityLikeECS-0.1.0.tar.gz

---
+++ packages/pyBasicUnityLikeECS/pyBasicUnityLikeECS-0.1.0.tar.gz/pyBasicUnityLikeECS-0.1.0/README.md
@@ -0,0 +1,5 @@
+### Basic Unity-like ECS in python:
+u can add, remove gameobjets from a scene and add, remove a component from a gameObject. u can also switch scene and each component has a start and update method.
+
+### How to use:
+there is an example in example.py :)
---
+++ packages/pyBasicUnityLikeECS/pyBasicUnityLikeECS-0.1.0.tar.gz/pyBasicUnityLikeECS-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pyBasicUnityLikeECS',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Entity Component System like the Unity Engine',
+    author='everyone',
+    license='MIT',
+    install_requires=[]
+)
