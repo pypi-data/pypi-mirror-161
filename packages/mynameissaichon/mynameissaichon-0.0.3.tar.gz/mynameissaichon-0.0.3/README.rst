530cad70fde99aa290565534008b308d14d73649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:35 +0000

    Add package mynameissaichon/0.0.3/mynameissaichon-0.0.3.tar.gz

---
+++ packages/mynameissaichon/mynameissaichon-0.0.3.tar.gz/mynameissaichon-0.0.3/README.rst
@@ -0,0 +1,33 @@
+แพ็คเกจนี้เป็นแพ็คเกจ python ที่แสดงข้อมูลเกี่ยวกับ สายชล
+=========================================================
+
+PyPi: https://pypi.org/project/mynameissaichon/
+
+สวัสดีครับ นี่คือแพ็คเกจที่แสดงเกี่ยวกับข้อมูลของ สายชล
+ท่านสามารถดูลิงค์เฟสบุ๊ค ยูทูป ได้
+
+วิธีติดตั้ง
+~~~~~~~~~~~
+
+เปิด CMD / Terminal
+
+.. code:: python
+
+   pip install mynameissaichon
+
+วิธีใช้งานแพ็คเพจนี้
+~~~~~~~~~~~~~~~~~~~~
+
+-  เปิด IDLE ขึ้นมาแล้วพิมพ์…
+
+.. code:: python
+
+   from mynameissaichon import Saichon
+
+   chon = Saichon() # ประกาศชื่อ class
+   chon.show_name() # โชว์ชื่อ
+   chon.show_youtube() # โชว์ลิ้งยูทูป
+   chon.about() # เกี่ยวกับ สายชล
+   chon.show_atr() # โชว์ภาพศิลปะ
+
+พัฒนาโดย: Saichon FB: https://www.facebook.com/Ymingt
---
+++ packages/mynameissaichon/mynameissaichon-0.0.3.tar.gz/mynameissaichon-0.0.3/setup.py
@@ -0,0 +1,38 @@
+﻿import io
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+HERE = dirname(abspath(__file__))
+LOAD_TEXT = lambda name: io.open(join(HERE, name), encoding='UTF-8').read()
+DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
+    'README.rst'
+])
+
+setup(
+  name = 'mynameissaichon',      
+  packages = ['mynameissaichon'], 
+  version = '0.0.3', 
+  license='MIT', 
+  description = 'Who is Saichon?',
+  long_description=DESCRIPTION,
+  author = 'Saichon',                 
+  author_email = 'chindo.minras@gmail.com',     
+  url = 'https://github.com/chindo33/mynameissaichon',  
+  download_url = 'https://github.com/chindo33/mynameissaichon',  
+  keywords = ['Saichon', 'ASCII ART'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Education',     
+    'Topic :: Utilities',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
