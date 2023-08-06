c4c0c6a36b3d1556654e896c5bbab0595d7cb9c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:12 +0000

    Add package beamprofiler/0.1.4/beamprofiler-0.1.4-py2.py3-none-any.whl

---
+++ packages/beamprofiler/beamprofiler-0.1.4-py2.py3-none-any.whl/beamprofiler/__init__.py
@@ -0,0 +1,19 @@
+"""
+`BeamProfiler` is a Python package for laser beam analysis and
+characterization according to ISO 13694, ISO 11145, and other non-ISO
+definitions commonly used in the industry.
+"""
+
+
+__author__ = """Wagno Alves Braganca Junior"""
+__email__ = 'wagnojunior@gmail.com'
+__version__ = '0.1.4'
+
+
+# # =============================================================================
+# # Imports
+# # =============================================================================
+# from src.beamprofiler import analysis
+# from src.beamprofiler import beam
+
+# __all__ = ['analysis']
