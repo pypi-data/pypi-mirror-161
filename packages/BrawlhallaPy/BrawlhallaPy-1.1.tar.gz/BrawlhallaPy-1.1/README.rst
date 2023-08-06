c1f6798515f26cd5cfabf5451cd19627852ba8d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:04 +0000

    Add package BrawlhallaPy/1.1/BrawlhallaPy-1.1.tar.gz

---
+++ packages/BrawlhallaPy/BrawlhallaPy-1.1.tar.gz/BrawlhallaPy-1.1/README.rst
@@ -0,0 +1,8 @@
+# Brawlhalla.py
+Brawlhalla python package to check player ranking
+
+# DOCUMENTATION
+https://hexye.gitbook.io/brawlhallapy/
+
+# GitHub Repo
+https://github.com/HexyeDEV/Brawlhalla.py
---
+++ packages/BrawlhallaPy/BrawlhallaPy-1.1.tar.gz/BrawlhallaPy-1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='BrawlhallaPy',
+    version='1.1',
+    package_dir={'': "BrawlhallaPy"},
+    packages=setuptools.find_packages(where="src"),
+    license="GNU General Public License v3.0",
+    author='HexyeDEV',
+    author_email='dragonsale22@gmail.com',
+    url='https://github.com/HexyeDEV/Brawlhalla.py',
+    description="A python package to get brawlhalla players ranks",
+    long_description=open("./README.rst", "r").read(),
+        classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+        python_requires=">=3.6",
+    )
