1e35c7107f419da091faaf13bae145349fb78180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:42 +0000

    Add package gungor-package/0.0.4/gungor_package-0.0.4-py3-none-any.whl

---
+++ packages/gungor-package/gungor_package-0.0.4-py3-none-any.whl/gungor_package/AnExamplePackageForTheBook.py
@@ -0,0 +1,15 @@
+#######################################################################################
+#
+#   An Example Package For The Book
+#	Developper:	Abdulkadir GÜNGÖR (abdulkadir_gungor@outlook.com)
+#	Date:	08/2022
+#	All Rights Reserved (Tüm Hakları Saklıdır)
+#
+#######################################################################################
+
+def MerhabaDe():
+    print(end="\r\n")
+    print("\t[An Example Package For The Book]")
+    print("\t[Kitap İçin Örnek Bir Paket Uygulaması]\r\n")
+    print("\tHerkese Merhaba!")
+    print()
---
+++ packages/gungor-package/gungor_package-0.0.4-py3-none-any.whl/gungor_package/__init__.py
@@ -0,0 +1,10 @@
+#######################################################################################
+#
+#   An Example Package For The Book
+#	Developper:	Abdulkadir GÜNGÖR (abdulkadir_gungor@outlook.com)
+#	Date:	08/2022
+#	All Rights Reserved (Tüm Hakları Saklıdır)
+#
+#######################################################################################
+
+from gungor_package.AnExamplePackageForTheBook import MerhabaDe
