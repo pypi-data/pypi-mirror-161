e61a15bd8c641f8aaaca0b9c638578efe8dc81a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:03 +0000

    Add package nonebot-plugin-report/0.1.1/nonebot_plugin_report-0.1.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-report/nonebot_plugin_report-0.1.1-py3-none-any.whl/nonebot_plugin_report/config.py
@@ -0,0 +1,9 @@
+from typing import Optional, List
+from pydantic import BaseSettings, Extra
+
+
+class Config(BaseSettings, extra=Extra.ignore):
+    report_token: Optional[str] = None
+    report_route: Optional[str] = '/report'
+    environment: str
+    superusers: List[str]
