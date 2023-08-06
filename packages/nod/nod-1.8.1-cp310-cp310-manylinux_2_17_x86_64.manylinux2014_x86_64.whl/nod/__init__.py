4d49e91a5d128cbc23d52582db52a93a68fccb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:49 +0000

    Add package nod/1.8.1/nod-1.8.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/nod/nod-1.8.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/nod/__init__.py
@@ -0,0 +1,13 @@
+from _nod import (
+    open_disc_from_image,
+    DiscBase,
+    DiscBuilderGCN,
+    Partition,
+    PartReadStream,
+    DolHeader,
+    ExtractionContext,
+    ProgressCallback,
+)
+from . import version
+
+VERSION = version.version
