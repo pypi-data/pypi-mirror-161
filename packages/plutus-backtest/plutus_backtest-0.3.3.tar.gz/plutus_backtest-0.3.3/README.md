625d63c12723bfe0de3fd09562941a3472c9c286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:50 +0000

    Add package plutus-backtest/0.3.3/plutus_backtest-0.3.3.tar.gz

---
+++ packages/plutus-backtest/plutus_backtest-0.3.3.tar.gz/plutus_backtest-0.3.3/README.md
@@ -0,0 +1,296 @@
+# plutus_backtest
+
+[![PyPI](https://img.shields.io/pypi/v/plutus-backtest)](https://pypi.org/project/plutus-backtest/)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/oanda-backtest)](https://pypi.org/project/plutus-backtest/)
+[![Downloads](https://pepy.tech/badge/plutus-backtest)](https://pepy.tech/project/plutus-backtest)
+
+## Description:
+This project has been performed for the purpose of local backtests of financial strategies. The package contains various indicators and tools 
+allowing users obtaining exact results of their strategies over a certain period of time. The users are also able to pick 
+endless amount of trading instruments and set criteria such as long or short positioning. Beside that optional stop loss and take profit
+signals are available not only as general limit level for entire portfolio but can be also applied for each instrument individually.
+Another optional tool available is weights factor distribution which is oriented to assign weights according to the provided values. 
+In addition, the package allows to create full html report containing varius graphs and indicators.
+
+<br />
+
+Tickers for analysis are available on [Yahoo Finance page](https://finance.yahoo.com/).
+
+## Installation: 
+* Dependency: **pandas**, **numpy**, **plotly**, **yfinance**, **dash**, **tabulate**
+* Install from pypi:
+```
+pip install plutus_backtest
+```
+* Verified in Python:
+
+```python
+from plutus.backtest import execution
+```
+
+## Examples: 
+
+Function "execution" contains below parameters:<br />
+```
+asset: str or list or series
+   Instruments taken into the consideration for the backtest.
+o_day: list of str or timestamps or series
+   Day/Days of the position opening.
+c_day: list of str or timestamps or series
+   Day/Days of the position closing.
+weights_factor: list of int or float or array-like or series default None
+   Optional list of factors which will be considered to define the weights for taken companies. By default
+   all weights are distributed equally, however if the list of factors provided the backtest will maximize
+   the weights towards the one with max weight factor. Negative weight factor will be considered as short selling.
+take_profit: list of float or int or series default None
+   List of values determining the level till a particular stock shall be traded.
+stop_loss: list of float or int or series default None
+   List of values determining the level till a particular stock shall be traded.
+benchmark: str default None
+   A benchmark ticker for comparison with portfolio performance
+price_period_relation: str default 'O-C'
+   Instruct what part of the trading day a position shall be opened,
+   and what part of trading day it shall be closed.
+   Possible relations:
+   O-C / Open to Close prices
+   C-O / Close to Open prices
+   C-C / Close to Close prices
+   O-O / Open to Open prices
+   "Open" - the price at which a security first trades upon the opening of an exchange on a trading day.
+   "Close" - value of the last transacted price in a security before the market officially closes.
+full_report: bool, optional, default False
+   Generates full report in your browser.
+major_sample: int or None, optional, default 10
+   Based on duration of the trading period as well as weights factor of the asset.
+   In order to make understandable visualisation in full report graphs such as weights changes and
+   weights distribution, major sample is used which will focus to provide info regarding main provided
+   assets. Can be changed to any int. If value is None the backtest will consider all assets as major
+   ones.
+```
+
+<br />
+
+A short and fast way to run a single backtest would be:
+
+<br />
+
+```python
+from plutus.backtest import execution
+
+bt = execution(asset=["AAPL", "BTC-USD", "GC=F"], o_day=["2021-08-01", "2021-08-03", "2021-09-05"],
+               c_day=["2021-09-01", "2021-10-04", "2022-03-12"])
+
+```
+
+<br />
+
+As a result a statistical table as well as graphical representation of the portfolio accumulated return will appear.
+
+<br />
+
+![1](https://user-images.githubusercontent.com/83161286/180781862-55642708-02da-4d65-8d9d-2aa9743ed4cb.png)
+
+
+<br />
+
+In order to access dataframe with portfolio daily changes and weights distribution, use:
+
+<br />
+
+```python
+from plutus.backtest import execution
+
+bt, portfolio_daily_changes, portfolio_weights = execution(asset=["AAPL", "TWTR", "GC=F"], 
+                                                  o_day=["2021-08-01", "2021-08-03", "2021-09-05"],
+                                                  c_day=["2021-09-01", "2021-10-04", "2022-03-12"])
+
+portfolio_daily_changes.head()
+```
+<br />
+
+The result will appear as following (all values are in %):
+
+<br />
+
+![port fin head](https://user-images.githubusercontent.com/83161286/178458212-9cd51033-707c-476c-a9ed-94c35255bb69.png)
+
+<br />
+
+If the user would like to compare performance of of the portfolio with any other instrument a parameter "benchmark" shall be called:
+
+<br />
+
+```python
+from plutus.backtest import execution
+
+bt = execution(asset=["AAPL", "TWTR", "FB"], o_day=["2021-08-01", "2021-08-03", "2021-09-05"],
+               c_day=["2021-09-01", "2021-10-04", "2022-03-12"], benchmark= ['^GSPC'])
+
+```
+<br />
+
+Above example will additionaly plot a S&P 500 index performance (accumulated return from same period as the portfolio) [grey line] on the accumulated graph:
+
+<br />
+
+![2](https://user-images.githubusercontent.com/83161286/180781931-4cc4db55-0932-4f34-9854-abf0ea3af8ab.png)
+
+<br />
+
+"Full report" is an optional parameter which allows users users to observe additional graphs frames and indicators:
+
+<br />
+
+```python
+from plutus.backtest import execution
+
+bt = execution(asset=["AAPL", "F", "MS"], 
+              o_day=["2020-08-01", "2020-07-15", "2020-08-20"],
+              c_day=["2021-09-01", "2021-09-01", "2021-09-15"], full_report = True)
+
+
+```
+<br />
+
+Above script will generate a link to Dash app with report: 
+
+<br />
+
+![image](https://user-images.githubusercontent.com/83119547/175771957-67729f53-ef8e-43a1-b712-af079c401b1d.png)
+
+<br />
+
+By clicking it, and it will redirect to a new tab.
+
+<br />
+
+![127 0 0 1_8050_ (6)](https://user-images.githubusercontent.com/83161286/180781981-ccd2e0ee-5a11-4e24-b871-35723b16a9ab.png)
+
+<br />
+
+If the user didn't specify weights of particular assets in the portfolio (using **weights_factor** parameter), % allocation will be distributed equally (in selected period of time) and shown in the last plot called **Weights rebalancing**.
+
+<br />
+
+![127 0 0 1_8050_ (7)](https://user-images.githubusercontent.com/83161286/180782027-47f81c54-820f-40a7-8517-5369c421ed43.png)
+
+<br />
+
+```python
+from plutus.backtest import execution
+
+bt = execution(asset=["AAPL", "F", "MS"],
+              o_day=["2020-08-01", "2020-07-15", "2020-08-20"],
+              c_day=["2021-09-01", "2021-09-01", "2021-09-15"],
+              weights_factor = [50, 40, 10], full_report = True)
+
+```
+
+<br />
+
+In case of specifying % of portfolio allocation for each asset (AAPL = 50%, F = 40%, MS = 10% from above example) above plots will be adjusted.
+Example of Weights distribution plot:
+
+<br />
+
+![127 0 0 1_8050_ (8)](https://user-images.githubusercontent.com/83161286/180782082-dc5b9c44-2929-4485-a2b1-ab8774e5de5f.png)
+
+<br />
+
+No need to include weights that will sum up to 100% (but it is recommended). Code calculates % based on **value / total of absolute values**. For example:
+
+<br />
+
+```python
+from plutus.backtest import execution
+
+bt = execution(asset=["AAPL", "F", "MS"],
+              o_day=["2020-08-01", "2020-07-15", "2020-08-20"],
+              c_day=["2021-09-01", "2021-09-01", "2021-09-15"],
+              weights_factor = [35, 140, -21], full_report = True)
+```
+
+<br />
+
+weights_factor total is 196 [35 + 140 + 21]. <br />
+AAPL: 35 / 196 = ~17%<br />
+F: 140 / 196 = ~71.4%<br />
+MS: |21| / 196 = ~10.7%
+
+Keep in mind that weights factor with "-" sign will indicate short selling for a particular asset
+
+<br />
+
+![127 0 0 1_8050_ (9)](https://user-images.githubusercontent.com/83161286/180782138-ac83e459-34dd-4919-bb99-fa0e30a6c3d4.png)
+
+<br />
+
+More complex approach would be assigning weights factor/stop loss/ take profit indicators:
+
+<br />
+
+```python
+from plutus.backtest import execution
+
+bt = execution(asset = ["AAPL", "BTC-USD","GC=F"], 
+              o_day = ["2021-08-01", "2021-07-15", "2021-08-20"],
+              c_day = ["2021-09-01", "2021-09-01","2021-09-15"], 
+              weights_factor = [10, -5, 35], 
+              stop_loss = [0.8, 0.9, 0.95], 
+              take_profit = [1.1, 1.2, 1.05], full_report = True)
+
+```
+
+<br />
+
+In this case the weights will not be distributed equally. "AAPL"  will have 20% of the total portofolio BTC-USD - 10% and 
+"GC=F" will have 70%. The negative sign in the weights factor will mean short selling, therefore first "AAPL" and "GC=F" instruments are in long position and 
+"BTC-USD" is in the short.
+
+<br />
+
+Stop loss and take profit shall be interpreted as "AAPL" has 20% of stop loss and 10% of take profit, "BTC-USD" has 10% of stop loss and 20% of take profit, "GC=F" 5% of stop loss and 5% of take profit. As result accumulative graph will look as:
+
+<br />
+
+![127 0 0 1_8050_ (10)](https://user-images.githubusercontent.com/83161286/180782181-02157e75-b9f2-4828-8724-0443066620e7.png)
+
+<br />
+
+In the moment when one of the securities reaching its stop loss or take profit, the trade will be automatically stopped and the weights will be reassigned respectively to the left assets.
+
+<br />
+
+In case of users need to test one instrument but several times with different timelines, the package will interpret it as:
+
+<br />
+
+```python
+from plutus.backtest import execution
+
+bt, portfolio_daily_changes, pprtfolio_weights = execution(asset = ["AMZN", "AMZN","AMZN"], 
+              o_day = ["2021-08-01", "2021-09-01", "2021-10-01"],
+              c_day = ["2021-08-15", "2021-09-15","2021-10-15"])
+
+
+```
+
+<br />
+
+![multiindex](https://user-images.githubusercontent.com/83161286/178458263-d0d4253a-5358-4876-862f-995fc8aaf134.png)
+
+<br />
+
+Each time when one asset is repeating the backtest will unite it under one comon ticker name, the same corrections will happen on the graphs. 
+It's worth to mention that due to data limitation the code uses close price for the analysis of the securities. 
+
+<br />
+
+
+
+## Support:
+Please [open an issue](https://github.com/witmul/backt/issues/new) for support.<br />
+With additional questions please reachout to autors directly:
+- [witmul](mailto:witalijmulawa@gmail.com)
+- [IlliaBoloto](mailto:ils.boloto96@gmail.com)
+
---
+++ packages/plutus-backtest/plutus_backtest-0.3.3.tar.gz/plutus_backtest-0.3.3/plutus/calculations.py
@@ -0,0 +1,376 @@
+import pandas as pd
+import yfinance as yf
+import numpy as np
+from datetime import timedelta, datetime
+import re
+import workingdays
+from plutus.commissions import commission
+
+def _date_plus_one(d):
+    str_date = d
+    if type(str_date) == str:
+        date = datetime.strptime(str_date, "%Y-%m-%d")
+        modified_date = date + timedelta(days=1)
+        back_to_str = datetime.strftime(modified_date, "%Y-%m-%d")
+        return back_to_str
+    else:
+        modified_date = str_date + timedelta(days=1)
+        back_to_str = datetime.strftime(modified_date, "%Y-%m-%d")
+        return back_to_str
+
+def _security_list(asset, start, end, weights_factor, take_profit, stop_loss):
+
+    """
+    :return:
+        DataFrame with all input data.
+    """
+
+    if isinstance(asset, pd.core.series.Series):
+        asset = asset.values
+    if isinstance(weights_factor, pd.core.series.Series):
+        weights_factor = weights_factor.values
+    if isinstance(start, pd.core.series.Series):
+        start = start.values
+    if isinstance(end, pd.core.series.Series):
+        end = end.values
+    if isinstance(asset, pd.core.series.Series):
+        asset = asset.values
+    if isinstance(take_profit, pd.core.series.Series):
+        take_profit = take_profit.values
+    if isinstance(stop_loss, pd.core.series.Series):
+        stop_loss = stop_loss.values
+    if isinstance(start, np.ndarray):
+        ls = []
+        for x in range(len(start)):
+            ls.append(str(start[x])[:10])
+        start = ls
+    if isinstance(end, np.ndarray):
+        ls = []
+        for x in range(len(end)):
+            ls.append(str(end[x])[:10])
+        end = ls
+    df = pd.DataFrame(
+        {"company": asset, "start day": start, "end day": end, "weights factor": weights_factor,
+         "take profit": take_profit, "stop loss": stop_loss})
+    df = df.set_index(df['company'])
+    for x in range(len(df.index)):
+        if df.iloc[x, 3] < 0 and df.iloc[x, 4] != np.inf and df.iloc[x, 5] != -np.inf:
+            a = df.iloc[x, 5]
+            b = df.iloc[x, 4]
+            df.iloc[x, 4] = (1 - a) + 1
+            df.iloc[x, 5] = 1 - (b - 1)
+
+        if df.iloc[x, 3] < 0 and df.iloc[x, 4]!= np.inf and df.iloc[x,5]== -np.inf:
+            b = df.iloc[x, 4]
+            df.iloc[x, 4] = 100500
+            df.iloc[x, 5] = 1 - (b - 1)
+
+        if df.iloc[x, 3] < 0 and df.iloc[x, 4]== np.inf and df.iloc[x,5]!= -np.inf:
+            a = df.iloc[x, 5]
+            df.iloc[x, 4] = (1 - a) + 1
+            df.iloc[x, 5] = -100500
+    df = df.replace(100500, np.inf)
+    df = df.replace(-100500, -np.inf)
+
+    security_list = df
+    return security_list
+
+def _consolidated_table_detailed(security_list, asset,
+                                 start, end, weights_factor,
+                                 take_profit, stop_loss, p_p_n, p_p_p,
+                                 only_working_days):
+
+    if isinstance(weights_factor, pd.core.series.Series):
+        weights_factor = weights_factor.values
+
+    # check if companies are dublicatted in a list
+    df_original = security_list
+    fq = pd.DataFrame(index=asset, data=start)
+    v = fq.groupby(fq.index) \
+        .cumcount()
+
+    # doubled companies will have suffix
+    list_new = []
+    for x in range(len(v.values)):
+        if v.iloc[x] != 0:
+            a2 = v.index[x] + str(v.iloc[x])
+            list_new.append(a2)
+        else:
+            list_new.append(v.index[x])
+
+    #load prices from yahoo
+    min_date = min(security_list['start day'])
+    max_date = max(security_list['end day'])
+    data = asset.copy()
+    if type(data)!=list:
+        data = list(data.values)
+    ydata = yf.download(data, start=_date_plus_one(min_date),
+                        end=_date_plus_one(max_date), progress=False)
+    # Removing non working days
+    work_days = []
+
+    for x in ydata.index:
+        work_day = workingdays.is_workingday(x)
+        if work_day == True:
+            work_days.append(x)
+
+    if ydata.empty:
+        raise ValueError('No trading days were provided, perhaps days off or holidays have been given or non existing tickers')
+
+    initial_df = pd.DataFrame()
+
+    #check if column names are Multiindex
+    if not isinstance(ydata.columns, pd.MultiIndex):
+    #select desired start / end dates for selected companies
+        for b_d, s_d, new_com in zip(df_original["start day"], df_original["end day"], list_new):
+            data = ydata.loc[b_d:s_d].copy()
+            data['Ticker'] = new_com
+            initial_df = pd.concat([initial_df, data])
+    else:
+        for com, b_d, s_d, new_com in zip(df_original["company"], pd.to_datetime(df_original["start day"]),
+                                          pd.to_datetime(df_original["end day"]), list_new):
+            q1 = ydata.iloc[:, ydata.columns.get_level_values(1) == com]
+            q1.columns = q1.columns.droplevel(1)
+            data = q1.loc[b_d:s_d]
+            data = data.copy()
+            data['Ticker'] = new_com
+            initial_df = pd.concat([initial_df, data])
+    special_assets =  set(initial_df.loc[~initial_df.index.isin(work_days)].dropna()['Ticker'].values)
+    raw_ydata = initial_df.loc[work_days]
+    left_over = raw_ydata[raw_ydata.isna().any(axis=1)].index.tolist()
+    work_days = [item for item in work_days if item not in left_over]
+    if only_working_days == True:
+        initial_df = raw_ydata.dropna()
+    df_close = initial_df[["Ticker", p_p_n]]
+    df_close.columns = ['ticker', 'close_price']
+    df_open = initial_df[["Ticker", p_p_p]]
+    df_open.columns = ['ticker', 'open']
+    open_price = df_open.groupby('ticker').first()
+    em1 = pd.DataFrame()
+    em2 = pd.DataFrame()
+
+    for x in open_price.index:
+        get_open = open_price.loc[x]
+        get_end = df_close[df_close['ticker'] == x]
+        fake_df = pd.DataFrame(index=get_end.index, columns=['ticker', 'close_price'])
+        fake_df = pd.DataFrame(fake_df.iloc[0]).T
+        fake_df.iloc[0, 0] = x
+        fake_df.iloc[0, 1] = get_open.values[0]
+        merged_df = pd.concat([fake_df, get_end])
+        merged_df['daily_change'] = merged_df['close_price'].pct_change()
+        merged_df = merged_df.iloc[1:]
+        aux_df = merged_df[['ticker', 'close_price']]
+        work_df = merged_df[['ticker', 'daily_change']]
+        em1 = pd.concat([em1, aux_df])
+        em2 = pd.concat([em2, work_df])
+    dc = em2.pivot_table(index=em2.index, columns='ticker', values='daily_change')
+
+    asset = list_new
+
+    # rerunning security_list again using dublicate adjusted tickers
+    security_list = _security_list(asset=asset,
+                                   start=start,
+                                   end=end,
+                                   weights_factor=weights_factor,
+                                   take_profit=take_profit,
+                                   stop_loss=stop_loss)
+
+    if dc.columns.tolist() != asset:
+        l1 = dc.columns.tolist()
+        l2 = asset
+        res = [x for x in l1 + l2 if x not in l1 or x not in l2]
+        asset = l1
+        security_list = security_list.drop(res)
+        b_day = security_list['start day'].values
+        s_day = security_list['end day'].values
+        weights_factor = security_list['weights factor'].values
+        sl = security_list['stop loss'].values
+        tp = security_list['take profit'].values
+        if res ==[]:
+            pass
+        else:
+            print(f"\nNo price data found for {res} therefore was deleted from provided inputs.\n")
+
+    dc = dc[security_list['company']]
+    dc = dc.replace([np.inf, -np.inf], np.nan)
+    dc = dc.fillna(0)
+    dc = dc.apply(pd.to_numeric)
+    aux = em1.pivot_table(index=em2.index, columns='ticker', values='close_price')
+    aux = aux[security_list['company']]
+    aux = aux.replace([np.inf, -np.inf], np.nan)
+    aux = aux.fillna(0)
+    aux = aux.apply(pd.to_numeric)
+    auxiliar_df = aux.copy()
+    detailed_return = dc.copy()
+
+    return detailed_return, auxiliar_df, security_list, weights_factor, df_close, work_days, special_assets
+
+def _portfolio_construction(detailed_return, security_list, auxiliar_df, weights_factor,
+                            major_sample, weekdays,only_working_days,
+                            non_working_days_rebalance,broker_commission, special_assets):
+    """
+    :return:
+        Full constructed portfolio, including position length, weights factor, stop loss & take profit.
+    """
+    # Initial weights
+    if non_working_days_rebalance == False:
+        non_traded_day = []
+        for x in auxiliar_df.index.tolist():
+            if x not in weekdays:
+                non_traded_day.append(x)
+        #ranging active assets
+        proxy_dic = {}
+        for x in auxiliar_df.columns:
+            activity =auxiliar_df[x][auxiliar_df[x] > 0]
+            proxy_dic[x]= activity.index[0], activity.index[-1]
+            auxiliar_df[x].loc[proxy_dic[x][0]:proxy_dic[x][-1]] = 1
+
+    binary_weights = auxiliar_df / auxiliar_df
+    binary_weights.fillna(value=0, inplace=True)
+    fac_summing = np.sum(abs(np.array(weights_factor)))
+    dist = np.array(weights_factor) / fac_summing
+    dist_df = pd.DataFrame(index=security_list['company'], data=weights_factor).T
+    weights_df = binary_weights * dist
+    # Rebalancing over 100% wieghts
+    for z in weights_df.index:
+        if abs(weights_df.loc[z]).sum() != 1:
+            act = weights_df.loc[z][weights_df.loc[z] != 0]
+            act_df = dist_df[act.index]
+            new_dist = np.array(act_df) / np.sum(abs(np.array(act_df)))
+            new_dist_frame = pd.DataFrame(columns=act.index, data=new_dist).T
+            for i in new_dist_frame.index:
+                weights_df.loc[z, i] = float(new_dist_frame.loc[i].values)
+    #Weights as per sl/tp
+    accu = (detailed_return + 1).cumprod()
+    dic_longs = {}
+    dic_shorts = {}
+    for x in accu.columns:
+        q1 = accu[x]
+        for y in q1:
+            if y > security_list.loc[x, 'take profit'] and y!=0:
+                q1.iloc[q1.values.tolist().index(y) + 1:] = 0
+                if security_list.loc[x, 'weights factor'] > 0:
+                    dic_longs[x] = accu.index[q1.values.tolist().index(y)], round(q1.iloc[q1.values.tolist().index(y)],3),'Take profit'
+                else:
+                    dic_shorts[x] = accu.index[q1.values.tolist().index(y)], round(q1.iloc[q1.values.tolist().index(y)],3), 'Stop loss'
+            if y < security_list.loc[x, 'stop loss'] and y!=0:
+                q1.iloc[q1.values.tolist().index(y) + 1:] = 0
+                if security_list.loc[x, 'weights factor'] > 0:
+                    dic_shorts[x] = accu.index[q1.values.tolist().index(y)], round(q1.iloc[q1.values.tolist().index(y)],3), 'Stop loss'
+                else:
+                    dic_longs[x] = accu.index[q1.values.tolist().index(y)], round(q1.iloc[q1.values.tolist().index(y)],3), 'Take profit'
+    aux_table_2 = accu * binary_weights
+    new_binary_weights = aux_table_2 / aux_table_2
+    new_binary_weights.fillna(value=0, inplace=True)
+    weights_df = new_binary_weights * dist
+
+    stop_loss_assets = dic_shorts
+    take_profit_assets = dic_longs
+
+    for z in weights_df.index:
+        if abs(weights_df.loc[z]).sum() != 1:
+            act = weights_df.loc[z][weights_df.loc[z] != 0]
+            act_df = dist_df[act.index]
+            new_dist = np.array(act_df) / np.sum(abs(np.array(act_df)))
+            new_dist_frame = pd.DataFrame(columns=act.index, data=new_dist).T
+            for i in new_dist_frame.index:
+                weights_df.loc[z, i] = float(new_dist_frame.loc[i].values)
+
+    port_performance = weights_df * detailed_return
+    deluted_port_performance= commission(port_performance,  special_assets, weekdays,
+                                 only_working_days, non_working_days_rebalance,broker_commission, weights_df)
+    px = port_performance - deluted_port_performance
+    times_commission_paid = px.replace(0, np.nan).count().sum()
+    port_performance['Sum'] = port_performance.sum(axis=1)
+    port_performance['Sum'] = port_performance['Sum'] + 1
+    port_performance['Accumulation'] = (port_performance['Sum'].cumprod() - 1) * 100
+    deluted_port_performance['Sum'] = deluted_port_performance.sum(axis=1)
+    deluted_port_performance['Sum'] = deluted_port_performance['Sum'] + 1
+    deluted_port_performance['Accumulation'] = (deluted_port_performance['Sum'].cumprod() - 1) * 100
+    commission_loss = (deluted_port_performance- port_performance)['Accumulation'].iloc[-1]
+    port_performance = deluted_port_performance.copy()
+    #Weights changes
+    #Creating table of rebalance of weights
+    a1 = (abs(weights_df.diff()).sum(axis=1) != 0)
+    a1.iloc[0] = True
+    a1 = a1.astype(int)
+    a2 = a1 * port_performance['Sum'].cumprod()
+    a2.iloc[0] = 1
+    a2 = a2.replace(to_replace=0, method='ffill')
+    if a1.iloc[-1]==0:
+        a1.iloc[-1]=1
+    rebalance_days = a1[a1!=0].index
+    # Adopting the weights to current return
+    b1 = (a2.values * weights_df.T).T
+    q1 = dist.copy()
+    q1[q1>0]=1
+    q1[q1<0]=-1
+    detailed_return_orinted= detailed_return * q1
+    ef = pd.DataFrame()
+    # Calculating separated cumprod according to rebalncing days
+    for x in range(len(rebalance_days)):
+        if x!=len(rebalance_days)-1:
+            m1 = (detailed_return_orinted.loc[rebalance_days[x]:rebalance_days[x +1]]+1).cumprod()
+        else:
+            m1 = (detailed_return_orinted.loc[rebalance_days[x]:]+1).cumprod()
+        ef = pd.concat([ef,m1])
+    ef = ef[~ef.index.duplicated(keep='first')]
+    fff = ef* abs(b1)
+    fff['Accumulation'] = port_performance['Accumulation'].values /100 +1
+    #Clearing results due to diff price realtions
+    df_deluted = (fff[fff.columns[:-1]].T * (fff['Accumulation']/ fff[fff.columns[:-1]].sum(axis=1)).replace(np.inf, 1).values).T
+    df_deluted['Accu'] = port_performance['Accumulation'].values /100 +1
+    capitlised_weights_distribution = df_deluted.copy()
+
+    q1 = port_performance.index[0] - timedelta(days=1)  # starting from 0%
+    port_performance.loc[q1] = [0] * len(port_performance.columns)
+    port_performance = port_performance.sort_index()
+    ##### Grouping indexies #####
+    port_performance.columns = [re.sub('[0-9]', '', i) for i in port_performance.columns]
+    final_portfolio = port_performance.T.groupby(level=[0], sort=False).sum().T
+
+    weights_df.columns = [re.sub('[0-9]', '', i) for i in weights_df.columns]
+    portfolio_weights = weights_df.T.groupby(level=[0], sort=False).sum().T
+
+    capitlised_weights_distribution.columns = [re.sub('[0-9]', '', i) for i in capitlised_weights_distribution.columns]
+    weights_changes = capitlised_weights_distribution.T.groupby(level=[0], sort=False).sum().T
+
+    if major_sample == None:
+        top_assets = portfolio_weights.columns.tolist()
+    else:
+        top_assets= abs(portfolio_weights).sum(axis=0).nlargest(major_sample).index.tolist()
+    return final_portfolio, portfolio_weights, weights_changes, stop_loss_assets,\
+           take_profit_assets, top_assets ,times_commission_paid, commission_loss
+
+def _stats(final_portfolio, times_commission_paid, commission_loss):
+    obj = final_portfolio
+    pdr0 = obj['Sum']- 1
+    pdr= pdr0.iloc[1:].copy()
+    port_mean = pdr.mean()
+    port_mean_pct = port_mean * 100
+    port_std = pdr.std()
+    LPM_0 = len(pdr[pdr < 0]) / len(pdr)
+    LPM_1 = pdr.clip(upper=0).mean()
+    LPM_2 = pdr.clip(upper=0).std()
+    if LPM_0 == 0:
+        LPM_0 = 0.01
+    topless_pdr = pdr[pdr < port_std]
+    botless_prd = topless_pdr[topless_pdr > -port_std]
+    inner_mean = botless_prd.mean()
+    obj_only_stocks = obj.drop(columns=['Sum', 'Accumulation'])
+    stocks_mean = obj_only_stocks.mean()
+    top_per = stocks_mean.nlargest(1)
+    worst_per = stocks_mean.nsmallest(1)
+    trade_length = len(pdr) - 1
+    VaR_95 = -1.65 * port_std * np.sqrt(trade_length)
+    VaR_99 = -2.33 * port_std * np.sqrt(trade_length)
+    CVaR = LPM_1 / LPM_0
+    list_1 = ['Mean', 'Std', 'Downside prob', 'Downside mean', 'Downside std', 'Investment period', 'VaR_95',
+              'VaR_99', 'CVaR', 'Times commission paid', 'Commission loss']
+    list_2 = [port_mean, port_std, LPM_0, LPM_1, LPM_2, trade_length, VaR_95, VaR_99,
+              CVaR, times_commission_paid,commission_loss]
+    frame = pd.DataFrame({'Indicators': list_1, 'Values': list_2})
+    #stat_frame = frame
+    #frame = frame.to_string(index=False)
+
+    return frame
