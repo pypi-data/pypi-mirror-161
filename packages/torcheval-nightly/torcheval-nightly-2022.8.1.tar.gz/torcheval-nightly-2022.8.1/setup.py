60766f3362c2f16bea1b2472fca2b04962161e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:05 +0000

    Add package torcheval-nightly/2022.8.1/torcheval-nightly-2022.8.1.tar.gz

---
+++ packages/torcheval-nightly/torcheval-nightly-2022.8.1.tar.gz/torcheval-nightly-2022.8.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="torcheval-nightly",
+    version="2022.8.1",
+    author="torcheval team",
+    author_email="yicongd@fb.com",
+    description="A package for evaluating model performance",
+    url="https://github.com/facebookresearch/torcheval",
+    licsense="BSD-3",
+    python_requires=">=3.7",
+    zip_safe=True,
+    classifiers=[
+            "Development Status :: 2 - Pre-Alpha",
+    ]
+)
