6ec3a8be4dcdf0d0c25c1d4211f6c0b4c8c04513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:11 +0000

    Add package zzaTest/0.0.1/zzaTest-0.0.1.tar.gz

---
+++ packages/zzaTest/zzaTest-0.0.1.tar.gz/zzaTest-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="zzaTest",
+    version="0.0.1",
+    license="MIT Licence",
+
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
