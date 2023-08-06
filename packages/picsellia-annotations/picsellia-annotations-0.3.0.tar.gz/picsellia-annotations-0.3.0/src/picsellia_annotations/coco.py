79d9d8dbc991522bdc408aa8657c68ac7d514a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:04 +0000

    Add package picsellia-annotations/0.3.0/picsellia-annotations-0.3.0.tar.gz

---
+++ packages/picsellia-annotations/picsellia-annotations-0.3.0.tar.gz/picsellia-annotations-0.3.0/src/picsellia_annotations/coco.py
@@ -0,0 +1,94 @@
+from typing import Dict, List, Tuple, Union
+
+from pydantic import BaseModel
+
+
+class Info(BaseModel):
+    year: str = None
+    version: str = None
+    description: str = None
+    contributor: str = None
+    url: str = None
+    date_created: str = None
+
+
+class Image(BaseModel):
+    id: int
+    file_name: str
+    width: int = None
+    height: int = None
+    license: int = None
+    flickr_url: str = None
+    coco_url: str = None
+    date_captured: str = None
+
+
+class License(BaseModel):
+    id: int
+    name: str
+    url: str = None
+
+
+class Category(BaseModel):
+    id: int
+    name: str
+    supercategory: str = None
+
+
+class Annotation(BaseModel):
+    id: int
+    image_id: int
+    category_id: int
+    bbox: Union[Tuple[int, int, int, int], List]
+    segmentation: Union[List[int], Dict]
+    score: float = 0.0
+    iscrowd: int = 0
+
+    def area(self) -> float:
+        if self.bbox is None or self.bbox == []:
+            raise ValueError("This annotation has no bbox, so it does not have area")
+        return self.bbox[2] * self.bbox[3]
+
+    def is_rle(self) -> bool:
+        return (
+            self.segmentation is not None
+            and isinstance(self.segmentation, Dict)
+            and self.segmentation != {}
+        )
+
+    def is_polygon(self) -> bool:
+        return (
+            not self.is_rle()
+            and self.segmentation is not None
+            and isinstance(self.segmentation, List)
+            and self.segmentation != []
+        )
+
+    def is_rectangle(self) -> bool:
+        return (
+            not self.is_rle()
+            and not self.is_polygon()
+            and (
+                self.segmentation is None
+                or self.segmentation == {}
+                or self.segmentation == []
+            )
+            and not self.bbox == []
+        )
+
+    def polygon_to_list_coordinates(self) -> List[List[int]]:
+        if not self.is_polygon() or len(self.segmentation) % 2 != 0:
+            raise ValueError("This is not a polygon")
+
+        return [
+            [self.segmentation[k], self.segmentation[k + 1]]
+            for k in range(0, len(self.segmentation), 2)
+        ]
+
+
+class COCOFile(BaseModel):
+    info: Info = Info()
+    licenses: List[License] = []
+    categories: List[Category] = []
+    images: List[Image]
+    annotations: List[Annotation]
