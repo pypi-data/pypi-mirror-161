52e80bcd99337ed468cd9dfd32f0903491393d74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:32 +0000

    Add package django-df-auth/0.2/django-df-auth-0.2.tar.gz

---
+++ packages/django-df-auth/django-df-auth-0.2.tar.gz/django-df-auth-0.2/df_auth/djangoflow/settings.py
@@ -0,0 +1,9 @@
+DF_AUTH_APPS = [
+    "rest_framework_simplejwt",
+    "django_otp",
+    "django_otp.plugins.otp_email",
+    "django_otp.plugins.otp_totp",
+    "django_otp.plugins.otp_static",
+    "otp_twilio",
+    "social_django",
+]
