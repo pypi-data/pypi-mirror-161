a4b85113389f47c0b4344ba0c1978fe105e1152a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:19 +0000

    Add package pytest-httpserver/1.0.5/pytest_httpserver-1.0.5.tar.gz

---
+++ packages/pytest-httpserver/pytest_httpserver-1.0.5.tar.gz/pytest_httpserver-1.0.5/pytest_httpserver/__init__.py
@@ -0,0 +1,27 @@
+"""
+This is package provides the main API for the pytest_httpserver package.
+
+"""
+__all__ = [
+    "HTTPServer",
+    "HTTPServerError",
+    "Error",
+    "NoHandlerError",
+    "WaitingSettings",
+    "HeaderValueMatcher",
+    "RequestHandler",
+    "URIPattern",
+    "URI_DEFAULT",
+    "METHOD_ALL",
+]
+
+from .httpserver import METHOD_ALL
+from .httpserver import URI_DEFAULT
+from .httpserver import Error
+from .httpserver import HeaderValueMatcher
+from .httpserver import HTTPServer
+from .httpserver import HTTPServerError
+from .httpserver import NoHandlerError
+from .httpserver import RequestHandler
+from .httpserver import URIPattern
+from .httpserver import WaitingSettings
