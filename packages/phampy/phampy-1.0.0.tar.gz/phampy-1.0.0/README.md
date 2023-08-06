a0799030da70e33062c11258138c8a637bfa28d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:32 +0000

    Add package phampy/1.0.0/phampy-1.0.0.tar.gz

---
+++ packages/phampy/phampy-1.0.0.tar.gz/phampy-1.0.0/README.md
@@ -0,0 +1,12 @@
+# phampy - Python Libraries
+
+## Features
+- inputs
+## Sample Usage
+
+## Questions?
+Please feel free to contact me if you have any questions or comments.
+Email: haducloc13@gmail.com
+
+## License
+This code is distributed under the terms and conditions of the [MIT license](LICENSE).
---
+++ packages/phampy/phampy-1.0.0.tar.gz/phampy-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="phampy",
+    version="1.0.0",
+    author="Loc Ha",
+    author_email='haducloc13@gmail.com',
+    description="Python Libraries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haducloc/phampy",
+    packages=setuptools.find_packages('src'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8.0',
+    py_modules=["phampy"],
+    package_dir={'':'src'},
+    install_requires=[]
+)
