7fa1e74faca4fa5abf6ce2ebc0f1f4724b4a3517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:58 +0000

    Add package leeger/0.6.0/leeger-0.6.0.tar.gz

---
+++ packages/leeger/leeger-0.6.0.tar.gz/leeger-0.6.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setuptools.setup(
+    name="leeger",
+    version="0.6.0",
+    author="Joey Greco",
+    author_email="joeyagreco@gmail.com",
+    description="Instant stats for your fantasy football league.",
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    license=license,
+    packages=setuptools.find_packages(exclude=("test", "docs")),
+    install_requires=["numpy",
+                      "setuptools",
+                      "openpyxl",
+                      "espn-api",
+                      "yahoofantasy",
+                      "sleeper"]
+)
---
+++ packages/leeger/leeger-0.6.0.tar.gz/leeger-0.6.0/test/test_model/test_league/test_Matchup.py
@@ -0,0 +1,54 @@
+import unittest
+
+from leeger.enum.MatchupType import MatchupType
+from leeger.exception.InvalidMatchupFormatException import InvalidMatchupFormatException
+from leeger.model.league.Matchup import Matchup
+
+
+class TestMatchup(unittest.TestCase):
+    def test_matchup_init(self):
+        matchup = Matchup(
+            teamAId="teamAId",
+            teamBId="teamBId",
+            teamAScore=1.1,
+            teamBScore=2.2,
+            matchupType=MatchupType.PLAYOFF,
+            teamAHasTiebreaker=True,
+            teamBHasTiebreaker=False
+        )
+
+        self.assertEqual("teamAId", matchup.teamAId)
+        self.assertEqual("teamBId", matchup.teamBId)
+        self.assertEqual(1.1, matchup.teamAScore)
+        self.assertEqual(2.2, matchup.teamBScore)
+        self.assertEqual(MatchupType.PLAYOFF, matchup.matchupType)
+        self.assertTrue(matchup.teamAHasTiebreaker)
+        self.assertFalse(matchup.teamBHasTiebreaker)
+
+    def test_matchup_init_defaultValues(self):
+        matchup = Matchup(
+            teamAId="teamAId",
+            teamBId="teamBId",
+            teamAScore=1.1,
+            teamBScore=2.2
+        )
+
+        self.assertEqual("teamAId", matchup.teamAId)
+        self.assertEqual("teamBId", matchup.teamBId)
+        self.assertEqual(1.1, matchup.teamAScore)
+        self.assertEqual(2.2, matchup.teamBScore)
+        self.assertEqual(MatchupType.REGULAR_SEASON, matchup.matchupType)
+        self.assertFalse(matchup.teamAHasTiebreaker)
+        self.assertFalse(matchup.teamBHasTiebreaker)
+
+    def test_matchup_init_aAndBHaveTieBreakers_raisesException(self):
+        with self.assertRaises(InvalidMatchupFormatException) as context:
+            Matchup(
+                teamAId="teamAId",
+                teamBId="teamBId",
+                teamAScore=1.1,
+                teamBScore=2.2,
+                teamAHasTiebreaker=True,
+                teamBHasTiebreaker=True
+            )
+        self.assertEqual("Team A and Team B cannot both have the tiebreaker.", str(context.exception))
