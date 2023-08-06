fb2d28d022d7233095a860ccffdae38935ca21f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:49 +0000

    Add package whad/1.0.0/whad-1.0.0.tar.gz

---
+++ packages/whad/whad-1.0.0.tar.gz/whad-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='whad',
+    version='1.0.0',
+    description='Wireless Hacking Devices unified tools.',
+    url='https://github.com/virtualabs/whad',
+    author='virtualabs',
+    author_email='virtualabs@gmail.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux'
+    ],
+    python_requires='>=3.5',
+)
