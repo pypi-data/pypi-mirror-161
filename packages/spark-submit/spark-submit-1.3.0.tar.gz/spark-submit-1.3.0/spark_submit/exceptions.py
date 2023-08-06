6d7984d22db9945c74e1dad3dc6742f69c17fe34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:15 +0000

    Add package spark-submit/1.3.0/spark-submit-1.3.0.tar.gz

---
+++ packages/spark-submit/spark-submit-1.3.0.tar.gz/spark-submit-1.3.0/spark_submit/exceptions.py
@@ -0,0 +1,9 @@
+"""Exceptions raised by SparkJob class"""
+
+
+class SparkSubmitError(Exception):
+    """Raised if spark-submit command fails"""
+
+
+class SparkJobKillError(Exception):
+    """Raised if spark-submit command fails"""
