f49d6ef4e84c82bb31c56e609a6ea67489066c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:23 +0000

    Add package adachi-resource-assistant/2.8/adachi_resource_assistant-2.8.tar.gz

---
+++ packages/adachi-resource-assistant/adachi_resource_assistant-2.8.tar.gz/adachi_resource_assistant-2.8/adachi_resource_assistant/__init__.py
@@ -0,0 +1,5 @@
+from adachi_resource_assistant.names import characters
+from adachi_resource_assistant.utils import exploreDir, findImages, fillImage, png2Webp
+
+__version__ = "2.8"
+__all__ = ["characters", "exploreDir", "findImages", "fillImage", "png2Webp"]
