f9eca0f96334b5854c61c12d55a242e153ece814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:49 +0000

    Add package streamlit-bias-rank-component/0.0.1/streamlit-bias-rank-component-0.0.1.tar.gz

---
+++ packages/streamlit-bias-rank-component/streamlit-bias-rank-component-0.0.1.tar.gz/streamlit-bias-rank-component-0.0.1/bias_rank_component/frontend/build/precache-manifest.cc24608f61aac9ed71ab1eb92764cb2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03efb61d915eac06df5a9e03d2f247a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d846d80a3abd391d2b36",
+    "url": "./static/js/2.da1f79ce.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.da1f79ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9132f22b749a1e3cdb",
+    "url": "./static/js/main.f648ff23.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
