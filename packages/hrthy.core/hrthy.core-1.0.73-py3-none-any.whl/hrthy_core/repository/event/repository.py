1966a11e0aa47623c7cec39bff457f71cba2109b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:04 +0000

    Add package hrthy.core/1.0.73/hrthy.core-1.0.73-py3-none-any.whl

---
+++ packages/hrthy.core/hrthy.core-1.0.73-py3-none-any.whl/hrthy_core/repository/event/repository.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy import false
+from sqlalchemy.orm import Session, Query
+
+from hrthy_core.events.events.base_event import BaseEvent
+from hrthy_core.models.base_model import BaseEventModel
+from hrthy_core.repository.event.repository_abstract import BaseEventRepositoryAbstract
+
+
+class BaseEventRepository(BaseEventRepositoryAbstract):
+    MODEL = BaseEventModel
+
+    def __init__(self, db: Session):
+        super().__init__()
+        self.db = db
+
+    def get_first_event_to_send(self) -> Optional[BaseEventModel]:
+        query: Query = self.db.query(self.MODEL) \
+            .filter(self.MODEL.sent == false()) \
+            .order_by(self.MODEL.created_at) \
+            .with_for_update()
+        return query.first()
+
+    def _send_event(self, topic: str, even_to_send: BaseEvent) -> BaseEventModel:
+        event = self.MODEL()
+        event.id = even_to_send.payload.id
+        event.topic = topic
+        event.event_type = even_to_send.type
+        event.event = even_to_send.json()
+        event.created_at = datetime.utcnow()
+        event.sent = False
+        event.sent_at = None
+        self.db.add(event)
+        return event
+
+    def set_event_as_sent(self, event: BaseEventModel) -> BaseEventModel:
+        event.sent = True
+        event.sent_at = datetime.utcnow()
+        self.db.add(event)
+        return event
