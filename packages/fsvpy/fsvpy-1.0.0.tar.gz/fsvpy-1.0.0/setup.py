dd70030d1da98682e606ac985b601f35156e6201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:44 +0000

    Add package fsvpy/1.0.0/fsvpy-1.0.0.tar.gz

---
+++ packages/fsvpy/fsvpy-1.0.0.tar.gz/fsvpy-1.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from setuptools import setup
+
+setup(
+   name='fsvpy',
+   version='0.1',
+   description='streak finding module',
+   long_description='Efficient algorithm for generalized streak finding for velocimetry applications.',
+   author='Michelle Driscoll',
+   author_email='michelle.driscoll@northwestern.edu',
+   url="",
+   packages = setuptools.find_packages(),
+   install_requires=['pims', 'matplotlib', 'pandas', 'numpy', 'scikit-image', 'networkx'] #external packages as dependencies
+)
