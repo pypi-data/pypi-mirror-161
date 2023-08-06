c075fd889a965ce6fddb86327d971bcb61865657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:17 +0000

    Add package gradioWrapper/0.0.7/gradioWrapper-0.0.7.tar.gz

---
+++ packages/gradioWrapper/gradioWrapper-0.0.7.tar.gz/gradioWrapper-0.0.7/gradioWrapper/__init__.py
@@ -0,0 +1,1 @@
+from .compiler import register, GradioCompiler, tabularGradio, functionalCompiler
---
+++ packages/gradioWrapper/gradioWrapper-0.0.7.tar.gz/gradioWrapper-0.0.7/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+
+
+VERSION = '0.0.7'
+DESCRIPTION = 'A basic gradio class, class function, and functional decorator'
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+
+# Setting up
+setup(
+    name="gradioWrapper",
+    version=VERSION,
+    author="Luca Vivona",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    url="https://github.com/LVivona/GradioWrapper",
+    install_requires=['gradio'],
+    keywords=['python', 'sockets', 'artificial intelligence', 'machine learning', 'visualization', 'wrapper', 'decorator', 'testing'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
