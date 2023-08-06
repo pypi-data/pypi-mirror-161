92f182b7d8e512c850bc8244b69b167c57b9a185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:33 +0000

    Add package wolne-lektury/0.1.0/wolne_lektury-0.1.0-py3-none-any.whl

---
+++ packages/wolne-lektury/wolne_lektury-0.1.0-py3-none-any.whl/urls.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 23 17:13:19 2022
+
+@author: krzysztof
+
+"""
+
+# DAISY: https://en.wikipedia.org/wiki/Digital_Accessible_Information_System
+
+MAIN = "https://wolnelektury.pl"
+API = f"{MAIN}/api"
+
+BOOKS       = "books"
+AUDIOBOOKS  = "audiobooks"
+DAISY       = "daisy"
+AUTHORS     = "authors"
+EPOCHS      = "epochs"
+GENRES      = "genres"
+KINDS       = "kinds"
+THEMES      = "themes"
+COLLECTIONS = "collections"
+
+MEDIA = "media"
+
+BOOK      = "book"
+AUDIOBOOK = "audiobook"
+
+TXT  = "txt"
+PDF  = "pdf"
+EPUB = "epub"
+MOBI = "mobi"
+FB2  = "fb2"
+
+MP3 = "mp3"
+OGG = "ogg" # Ogg Vorbis
+
