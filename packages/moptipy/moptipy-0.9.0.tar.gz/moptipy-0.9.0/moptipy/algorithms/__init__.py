146066acbd1c03c4d3bee51ecefc02d05336843d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:06 +0000

    Add package moptipy/0.9.0/moptipy-0.9.0.tar.gz

---
+++ packages/moptipy/moptipy-0.9.0.tar.gz/moptipy-0.9.0/moptipy/algorithms/__init__.py
@@ -0,0 +1,7 @@
+"""
+In this package, we provide implementations of metaheuristic algorithms.
+
+The black-box methods are given directly in this package, more specialized
+algorithms will be placed in sub-packages corresponding to their requirements
+for the search- and solution space.
+"""
