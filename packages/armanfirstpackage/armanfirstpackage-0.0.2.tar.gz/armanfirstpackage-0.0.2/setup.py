6ae3d8438d761c69bb353801dcde5abab0889d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:37 +0000

    Add package armanfirstpackage/0.0.2/armanfirstpackage-0.0.2.tar.gz

---
+++ packages/armanfirstpackage/armanfirstpackage-0.0.2.tar.gz/armanfirstpackage-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package'
+
+# Setting up
+setup(
+    # the name must match the folder name 'armanfirstpackage'
+    name="armanfirstpackage",
+    version=VERSION,
+    author="Md. Arman Hossain",
+    author_email="shanto377@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
+)
