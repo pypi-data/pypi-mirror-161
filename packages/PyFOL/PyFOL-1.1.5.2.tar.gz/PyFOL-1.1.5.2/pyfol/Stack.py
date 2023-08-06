184937385eded34f67fda3eeff25bca142ba51dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:25 +0000

    Add package PyFOL/1.1.5.2/PyFOL-1.1.5.2.tar.gz

---
+++ packages/PyFOL/PyFOL-1.1.5.2.tar.gz/PyFOL-1.1.5.2/pyfol/Stack.py
@@ -0,0 +1,30 @@
+class Stack():
+    def __init__(self):
+        self.top = 0
+        self.stacklst = []
+        
+    def isempty(self):
+        return self.top==0
+    
+    def height(self):
+        return self.top
+    
+    def push(self,value):
+        self.stacklst.append(value)
+        self.top += 1
+        
+    def pop(self):
+        self.stacklst.pop()
+        
+    def show(self):
+        return self.stacklst
+        
+# stack = Stack()
+# print(stack.isempty())
+# stack.push(123)
+# print(stack.isempty())
+# print(stack.height())
+# stack.push(456)
+# stack.push(789)
+# stack.pop()
+# print(stack.stacklst)
