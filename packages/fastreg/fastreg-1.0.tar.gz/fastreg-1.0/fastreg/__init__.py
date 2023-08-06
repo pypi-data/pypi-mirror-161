0dfb49957d17fda27bdd346f0edcb81b34f39eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:52 +0000

    Add package fastreg/1.0/fastreg-1.0.tar.gz

---
+++ packages/fastreg/fastreg-1.0.tar.gz/fastreg-1.0/fastreg/__init__.py
@@ -0,0 +1,18 @@
+from . import tools
+from . import formula
+from . import linear
+from . import testing
+
+from .formula import (
+    Factor, Term, Formula, Real, Categ, Demean, Binned, Custom,
+    I, R, C, D, B, robust_eval, factor, design_matrix, design_matrices
+)
+from .linear import ols
+from .testing import dataset
+
+try:
+    from .general import (
+        glm, logit, poisson, negbin, zinf_poisson, zinf_negbin, gols
+    )
+except:
+    pass
