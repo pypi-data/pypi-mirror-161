aa5e37541ddc3bc2bd1de31e6cc983b384bb325e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:26 +0000

    Add package Package-for-internship/0.0.1/Package%20for%20internship-0.0.1.tar.gz

---
+++ packages/Package-for-internship/Package%20for%20internship-0.0.1.tar.gz/Package for internship-0.0.1/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 12:29:56 2022
+
+@author: Lenovo
+"""
+import setuptools
+
+setuptools.setup(
+    name="Package for internship",
+    version="0.0.1",
+    author="Kenneth Paulsen",
+    author_email="kennethpaulsen1997@gmail.com",
+    description="Easy package for internship test",
+    packages=["cmc_dataeng_internship_kenneth_paulsen"]
+)
