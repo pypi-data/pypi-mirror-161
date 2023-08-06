1e51a51a42e7fb138e260fec1473f7126983d988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:23 +0000

    Add package password-manager-cli/0.1.6/password-manager-cli-0.1.6.tar.gz

---
+++ packages/password-manager-cli/password-manager-cli-0.1.6.tar.gz/password-manager-cli-0.1.6/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['src', 'src.data', 'src.menu', 'src.utils']
+
+package_data = \
+{'': ['*'], 'src.data': ['personal_data/*']}
+
+install_requires = \
+['SQLAlchemy>=1.4.39,<2.0.0',
+ 'argon2-cffi>=21.3.0,<22.0.0',
+ 'click>=8.1.3,<9.0.0',
+ 'cryptography>=37.0.4,<38.0.0',
+ 'python-dotenv>=0.20.0,<0.21.0',
+ 'rich>=12.5.1,<13.0.0']
+
+entry_points = \
+{'console_scripts': ['mankey = src.main:main']}
+
+setup_kwargs = {
+    'name': 'password-manager-cli',
+    'version': '0.1.6',
+    'description': 'Password Manager CLI',
+    'long_description': None,
+    'author': 'gbPagano',
+    'author_email': 'guilhermebpagano@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/password-manager-cli/password-manager-cli-0.1.6.tar.gz/password-manager-cli-0.1.6/src/menu/__init__.py
@@ -0,0 +1,2 @@
+from .index_menu import gen_index, index_menu
+from .new_password import new_pwd
