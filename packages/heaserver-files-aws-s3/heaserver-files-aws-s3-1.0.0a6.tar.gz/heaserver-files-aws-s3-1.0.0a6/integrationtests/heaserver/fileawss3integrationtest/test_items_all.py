afc4d711c332b291b06d501f4eaf42ec378ba252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:28 +0000

    Add package heaserver-files-aws-s3/1.0.0a6/heaserver-files-aws-s3-1.0.0a6.tar.gz

---
+++ packages/heaserver-files-aws-s3/heaserver-files-aws-s3-1.0.0a6.tar.gz/heaserver-files-aws-s3-1.0.0a6/integrationtests/heaserver/fileawss3integrationtest/test_items_all.py
@@ -0,0 +1,18 @@
+from .fileawss3testcase import AWSS3FileTestCase
+from heaserver.service.testcase.mixin import DeleteMixin, GetAllMixin, GetOneMixin, PutMixin
+
+
+class TestDeleteFile(AWSS3FileTestCase, DeleteMixin):
+    pass
+
+
+class TestGetFiles(AWSS3FileTestCase, GetAllMixin):
+    pass
+
+
+class TestGetFile(AWSS3FileTestCase, GetOneMixin):
+    pass
+
+
+class TestPutFile(AWSS3FileTestCase, PutMixin):
+    pass
