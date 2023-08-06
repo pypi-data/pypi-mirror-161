55e6054f4e68211d857afe102d9e433e88f9aed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:14 +0000

    Add package py-optional-chain/0.0.2/py-optional-chain-0.0.2.tar.gz

---
+++ packages/py-optional-chain/py-optional-chain-0.0.2.tar.gz/py-optional-chain-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+import os
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = lib_folder + '/requirements.txt'
+install_requires = []  # Here we'll get: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
+setuptools.setup(
+    name='py-optional-chain',
+    version='0.0.2',
+    description='Optional Chain like in Java',
+    long_description='Optional Chain like in Java',
+    author='wysohn',
+    author_email='wysohn2002@naver.com',
+    python_requires='>=3.6',
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+)
