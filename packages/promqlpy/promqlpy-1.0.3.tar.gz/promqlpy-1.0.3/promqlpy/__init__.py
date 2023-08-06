02a1ef413710b6df6a765167c29e718b6d64abb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:21 +0000

    Add package promqlpy/1.0.3/promqlpy-1.0.3.tar.gz

---
+++ packages/promqlpy/promqlpy-1.0.3.tar.gz/promqlpy-1.0.3/promqlpy/__init__.py
@@ -0,0 +1,3 @@
+from .split import split_binary_op
+
+__all__ = ["split_binary_op"]
---
+++ packages/promqlpy/promqlpy-1.0.3.tar.gz/promqlpy-1.0.3/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['promqlpy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cffi>=1.15.1,<2.0.0', 'rich>=12.5.1,<13.0.0']
+
+entry_points = \
+{'console_scripts': ['alert-rule-explain = promqlpy.cli:main']}
+
+setup_kwargs = {
+    'name': 'promqlpy',
+    'version': '1.0.3',
+    'description': 'Python utils for parsing PromQL/MetricsQL.',
+    'long_description': None,
+    'author': 'laixintao',
+    'author_email': 'laixintaoo@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+from build import *
+build(setup_kwargs)
+
+setup(**setup_kwargs)
