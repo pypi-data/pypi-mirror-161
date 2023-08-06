007bfdbe15f35df5b07c7ef3203926c9d0eb6c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:51 +0000

    Add package mosaicml-experimental/0.0.2/mosaicml_experimental-0.0.2-py3-none-any.whl

---
+++ packages/mosaicml-experimental/mosaicml_experimental-0.0.2-py3-none-any.whl/experimental/__init__.py
@@ -0,0 +1,7 @@
+from experimental import algorithms
+from experimental.algorithms import register_all_algorithms
+
+__all__ = [
+    "algorithms",
+    "register_all_algorithms",
+]
---
+++ packages/mosaicml-experimental/mosaicml_experimental-0.0.2-py3-none-any.whl/experimental/algorithms/example_algorithm/__init__.py
@@ -0,0 +1,5 @@
+from experimental.algorithms.example_algorithm.example_algorithm import ExampleAlgorithm
+
+__all__ = [
+    "ExampleAlgorithm",
+]
