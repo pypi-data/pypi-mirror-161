eadd65a4cb125ee0a2d32febf3a09fb893b746ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:49 +0000

    Add package lucd/0.2.0/lucd-0.2.0.tar.gz

---
+++ packages/lucd/lucd-0.2.0.tar.gz/lucd-0.2.0/lucd/driver.py
@@ -0,0 +1,105 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import sys
+from termcolor import colored
+from colorama import init
+import platform
+import os
+from .check import get_installed_chrome_path
+from .base import Base
+from .clear import PreLaunch
+
+
+init()
+
+
+class Driver(Base, PreLaunch):
+    """
+    Create undetectable Chrome driver
+
+    Args:
+        headless (bool): Hide the browser's window, defualt = False
+        mute (bool): Mute the sound of the browser, defualt = False
+        profile (Path): The absolute path of the Chrome profile
+
+    Returns
+        None
+    """
+
+    def create(self):
+        path = get_installed_chrome_path()
+        if path is not None:
+            options = webdriver.ChromeOptions()
+            options.headless = self.headless
+            if self.profile:
+                options.add_argument(r"--user-data-dir=%s" % self.profile)
+
+            options.add_argument("start-maximized")
+            options.add_experimental_option(
+                "excludeSwitches", ["enable-automation", 'enable-logging']
+            )
+            options.add_argument("--disable-blink-features=AutomationControlled")
+            options.add_argument(f"user-agent={self.generate_user_agent()}")
+            options.add_argument("--no-sandbox")
+            if self.mute:
+                options.add_argument("--mute-audio")
+            try:
+                driver = webdriver.Chrome(service=Service(path), options=options)
+            except TypeError:
+                driver = webdriver.Chrome(executable_path=path, options=options)
+            except Exception as error:
+                sys.exit(colored("[-] ", "red") + f" {error}")
+            else:
+                driver = self.driver_navigator(driver)
+                driver = self.replace_user_agent(driver)
+                driver.get("https://selmi.tech")
+                return driver
+        else:
+            sys.exit(
+                colored("Make sure you have chrome installed on your machine!", "red")
+            )
+
+
+class Remote(Base):
+    def open_new_cmd_and_run_command(self, port):
+        """
+        This function opens a new cmd window and runs the command
+         that runs the Chrome driver in the debugging mode
+        """
+        system = platform.system()
+        if not self.profile:
+            cmd = f"chrome.exe -remote-debugging-port={port}"
+        else:
+            cmd = f'chrome.exe --remote-debugging-port={port} --user-data-dir="{self.profile}"'
+        if system == 'Windows':
+            os.system('start cmd /k ' + cmd)
+        else:
+            google = self.whereis_google_chrome()
+            if not google:
+                raise Exception("whereis: Google Chrome is not installed")
+            if not self.profile:
+                cmd = f'{google} --remote-debugging-port={port}'
+            else:
+                cmd = f'{google} --remote-debugging-port={port} --user-data-dir="{self.profile}"'
+            os.system(f'gnome-terminal -- bash -c "{cmd}"')
+
+    def create(self, control_existing_instance: bool = False, port: int = 9222):
+        path = get_installed_chrome_path()
+        if path is not None:
+            options = webdriver.ChromeOptions()
+            if not control_existing_instance:
+                self.open_new_cmd_and_run_command(
+                    port=port
+                )
+            options.add_experimental_option("debuggerAddress", f"127.0.0.1:{port}")
+            try:
+                driver = webdriver.Chrome(service=Service(path), options=options)
+            except TypeError:
+                driver = webdriver.Chrome(executable_path=path, options=options)
+            except Exception as error:
+                sys.exit(colored("[-] ", "red") + f" {error}")
+            else:
+                driver.get("https://selmi.tech")
+                return driver
+        else:
+            sys.exit(colored("Make sure you have chrome installed on your machine!", "red"))
---
+++ packages/lucd/lucd-0.2.0.tar.gz/lucd-0.2.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import setuptools
+
+VERSION = '0.2.0' 
+DESCRIPTION = 'Create Local Undetectable Chrome Driver'
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+        name="lucd", 
+        version=VERSION,
+        author="Selmi Abderrahim",
+        author_email="contact@selmi.tech",
+        description=DESCRIPTION,
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url='https://github.com/SelmiAbderrahim/CreateUndetectableChromeDriver',
+        install_requires=[
+            "beautifulsoup4==4.11.1",
+            "colorama==0.4.4",
+            "requests==2.27.1",
+            "termcolor==1.1.0",
+            "lxml==4.9.1",
+            "selenium==4.1.5",
+            "fake-useragent==0.1.11",
+        ],
+        packages=setuptools.find_packages(),
+        keywords=['python', 'undetectable', "chrome", "selenium", "webdriver"],
+        classifiers= [
+            "Development Status :: 4 - Beta",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Microsoft :: Windows",
+        ],
+        license='MIT',
+)
