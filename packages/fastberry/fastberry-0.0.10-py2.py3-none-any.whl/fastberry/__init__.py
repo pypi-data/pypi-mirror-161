b25c768cc33523c2a9b753920b48245dbb23a9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:22 +0000

    Add package fastberry/0.0.10/fastberry-0.0.10-py2.py3-none-any.whl

---
+++ packages/fastberry/fastberry-0.0.10-py2.py3-none-any.whl/fastberry/__init__.py
@@ -0,0 +1,20 @@
+"""
+    Fastberry Main (Core-Methods)
+"""
+
+from starlette.middleware.base import BaseHTTPMiddleware as BaseMiddleware
+from strawberry.extensions import Extension as BaseExtension
+from strawberry.permission import BasePermission
+
+from . import config
+
+# from .config import Settings
+from .config import Settings as Fastberry
+from .config.extras.gql import GQL
+
+# from .graphql import BaseType
+from .schema import Schema
+from .types import Model
+
+Text = Model.text
+JSON = Model.json
