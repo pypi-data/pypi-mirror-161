aec1fa6fde5232761c35e941503d2ccb9dd12b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:18 +0000

    Add package drugintfinder/0.2.3/drugintfinder-0.2.3.tar.gz

---
+++ packages/drugintfinder/drugintfinder-0.2.3.tar.gz/drugintfinder-0.2.3/tests/test_ranker.py
@@ -0,0 +1,27 @@
+"""Tests for `Ranker` class."""
+import pandas as pd
+
+from drugintfinder.ranker import Ranker
+from .constants import MAPT, PHOSPHORYLATION
+
+
+ranker = Ranker(name=MAPT, pmods=[PHOSPHORYLATION], print_sql=True)
+
+
+class TestRanker:
+    """Tests for the Ranker class."""
+
+    def test_rank(self):
+        """Test the find_interactors method."""
+        ranker.rank()
+        summary = ranker.summarize()
+
+        assert isinstance(summary, pd.DataFrame)
+        assert len(summary) > 850
+
+        expected_cols = ['Drug', 'Target', 'Synergizes', 'Number of BioAssays for Target',
+                         'Number of Causal Edges for Target', 'Drug Patent Ongoing',
+                         'Generic Version of Drug Available', 'Number of Drug Targets']
+        assert all([col in summary.columns for col in expected_cols])
+
+# TODO finish writing tests
