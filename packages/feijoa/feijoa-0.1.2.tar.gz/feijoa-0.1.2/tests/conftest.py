45dce640c906ff90e206408dc16fc52067ea146e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:56 +0000

    Add package feijoa/0.1.2/feijoa-0.1.2.tar.gz

---
+++ packages/feijoa/feijoa-0.1.2.tar.gz/feijoa-0.1.2/tests/conftest.py
@@ -0,0 +1,24 @@
+import glob
+import logging
+import os
+from itertools import chain
+
+import pytest
+
+excluded_loggers = (
+    'numba',
+    'matplotlib',
+)
+
+for log_name in excluded_loggers:
+    other_log = logging.getLogger(log_name)
+    other_log.setLevel(logging.WARNING)
+
+
+@pytest.fixture(autouse=True)
+def cleanup():
+    yield
+    for f in chain(glob.glob("*.json"), glob.glob("*.yaml"),
+                   glob.glob("*.db")):
+        os.remove(f)
+
