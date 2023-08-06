fdfe386b120d507c9e34e52df3b47ae3e4471941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:16 +0000

    Add package libvcs/0.14.0a8/libvcs-0.14.0a8-py3-none-any.whl

---
+++ packages/libvcs/libvcs-0.14.0a8-py3-none-any.whl/libvcs/exc.py
@@ -0,0 +1,43 @@
+"""Exceptions for libvcs.
+
+If you see this, we're publishing to S3 automatically
+
+"""
+from typing import List, Optional, Union
+
+
+class LibVCSException(Exception):
+    """Standard exception raised by libvcs."""
+
+
+class CommandError(LibVCSException):
+    """This exception is raised on non-zero return codes."""
+
+    def __init__(
+        self,
+        output: str,
+        returncode: Optional[int] = None,
+        cmd: Optional[Union[str, List[str]]] = None,
+    ) -> None:
+        self.returncode = returncode
+        self.output = output
+        if cmd:
+            if isinstance(cmd, list):
+                cmd = " ".join(cmd)
+            self.cmd = cmd
+
+    def __str__(self) -> str:
+        message = self.message.format(returncode=self.returncode, cmd=self.cmd)
+        if len(self.output.strip()):
+            message += "\n%s" % self.output
+        return message
+
+    message = "Command failed with code {returncode}: {cmd}"
+
+
+class CommandTimeoutError(CommandError):
+    """CommandError which gets raised when a subprocess exceeds its timeout."""
+
+
+class InvalidVCS(LibVCSException):
+    """Invalid VCS."""
