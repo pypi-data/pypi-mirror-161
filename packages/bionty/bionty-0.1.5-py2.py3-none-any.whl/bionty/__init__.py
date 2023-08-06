750b53edfdfd14a60a95b4e9b06fcc4bbb218072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:13 +0000

    Add package bionty/0.1.5/bionty-0.1.5-py2.py3-none-any.whl

---
+++ packages/bionty/bionty-0.1.5-py2.py3-none-any.whl/bionty/__init__.py
@@ -0,0 +1,49 @@
+"""Bionty: mapping and standardizing biological entities.
+
+Import the package::
+
+   import bionty as bt
+
+The base model for every entity class is:
+
+.. autosummary::
+   :toctree: .
+
+   EntityTable
+
+Entities:
+
+.. autosummary::
+   :toctree: .
+
+   Species
+   Gene
+   Protein
+   CellType
+   Disease
+
+Developer API:
+
+.. autosummary::
+   :toctree: .
+
+   dev
+
+"""
+
+__version__ = "0.1.5"
+
+
+# dynamic classes
+from ._gene import Gene
+from ._species import Species
+from ._protein import Protein
+from ._celltype import CellType
+from ._disease import Disease
+
+# tools
+from ._normalize import NormalizeColumns
+from ._table import EntityTable
+
+# dev
+from .dev import lookup
