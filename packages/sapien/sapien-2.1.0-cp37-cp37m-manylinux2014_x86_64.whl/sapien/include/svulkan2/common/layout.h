bd6659c2e741e98af7c2d6136ee58559591168dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:12 +0000

    Add package sapien/2.1.0/sapien-2.1.0-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/sapien/sapien-2.1.0-cp37-cp37m-manylinux2014_x86_64.whl/sapien/include/svulkan2/common/layout.h
@@ -0,0 +1,208 @@
+#pragma once
+#include "svulkan2/common/vk.h"
+
+#include <algorithm>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace svulkan2 {
+
+enum DataType {
+  eSTRUCT,
+  eINT,
+  eINT2,
+  eINT3,
+  eINT4,
+  eINT44,
+  eUINT,
+  eUINT2,
+  eUINT3,
+  eUINT4,
+  eUINT44,
+  eFLOAT,
+  eFLOAT2,
+  eFLOAT3,
+  eFLOAT4,
+  eFLOAT44,
+  eUNKNOWN
+};
+
+inline std::string DataTypeToString(DataType type) {
+  switch (type) {
+  case eSTRUCT:
+    return "struct";
+  case eINT:
+    return "int";
+  case eINT2:
+    return "int2";
+  case eINT3:
+    return "int3";
+  case eINT4:
+    return "int4";
+  case eINT44:
+    return "int44";
+  case eUINT:
+    return "uint";
+  case eUINT2:
+    return "uint2";
+  case eUINT3:
+    return "uint3";
+  case eUINT4:
+    return "uint4";
+  case eUINT44:
+    return "uint44";
+  case eFLOAT:
+    return "float";
+  case eFLOAT2:
+    return "float2";
+  case eFLOAT3:
+    return "float3";
+  case eFLOAT4:
+    return "float4";
+  case eFLOAT44:
+    return "float44";
+  case eUNKNOWN:
+    return "unknown";
+  }
+  throw std::runtime_error("invalid data type");
+}
+
+inline uint32_t GetDataTypeSize(DataType type) {
+  switch (type) {
+  case eSTRUCT:
+    return 0;
+  case eINT:
+    return 4;
+  case eINT2:
+    return 8;
+  case eINT3:
+    return 12;
+  case eINT4:
+    return 16;
+  case eINT44:
+    return 64;
+  case eUINT:
+    return 4;
+  case eUINT2:
+    return 8;
+  case eUINT3:
+    return 12;
+  case eUINT4:
+    return 16;
+  case eUINT44:
+    return 64;
+  case eFLOAT:
+    return 4;
+  case eFLOAT2:
+    return 8;
+  case eFLOAT3:
+    return 12;
+  case eFLOAT4:
+    return 16;
+  case eFLOAT44:
+    return 64;
+  case eUNKNOWN:
+    return 0;
+  }
+  throw std::runtime_error("invalid data type");
+}
+
+struct SpecializationConstantLayout {
+  struct Element {
+    std::string name{};
+    uint32_t id{0};
+    DataType dtype{};
+    union {
+      int intValue;
+      float floatValue;
+    };
+  };
+  std::unordered_map<std::string, SpecializationConstantLayout::Element>
+      elements;
+  std::vector<SpecializationConstantLayout::Element> getElementsSorted() const;
+};
+
+struct CombinedSamplerLayout {
+  struct Element {
+    std::string name{};
+    uint32_t binding{0};
+    uint32_t set{0};
+  };
+  std::unordered_map<std::string, CombinedSamplerLayout::Element> elements;
+  std::vector<CombinedSamplerLayout::Element> getElementsSorted() const;
+};
+
+struct InputDataLayout {
+  struct Element {
+    std::string name{};
+    uint32_t location{0};
+    DataType dtype{eFLOAT};
+    inline uint32_t getSize() const { return GetDataTypeSize(dtype); }
+    inline bool operator==(Element const &other) const {
+      return name == other.name && location == other.location &&
+             dtype == other.dtype;
+    }
+  };
+
+  std::unordered_map<std::string, InputDataLayout::Element> elements;
+
+  std::vector<InputDataLayout::Element> getElementsSorted() const;
+  uint32_t getSize() const;
+
+  std::vector<vk::VertexInputBindingDescription>
+  computeVertexInputBindingDescriptions() const;
+  std::vector<vk::VertexInputAttributeDescription>
+  computeVertexInputAttributesDescriptions() const;
+
+  bool operator==(InputDataLayout const &other) const;
+  bool operator!=(InputDataLayout const &other) const;
+};
+
+struct OutputDataLayout {
+  struct Element {
+    std::string name{};
+    uint32_t location{0};
+    DataType dtype{eFLOAT};
+  };
+
+  std::unordered_map<std::string, OutputDataLayout::Element> elements;
+
+  std::vector<OutputDataLayout::Element> getElementsSorted() const;
+};
+
+struct StructDataLayout {
+  struct Element {
+    std::string name{};
+    uint32_t size{0};
+    uint32_t offset{0};
+    uint32_t arrayDim{0};
+    DataType dtype{eFLOAT};
+    std::shared_ptr<StructDataLayout> member;
+    bool operator==(Element const &other) const;
+  };
+
+  uint32_t size;
+  std::unordered_map<std::string, StructDataLayout::Element> elements;
+  std::vector<StructDataLayout::Element const *> getElementsSorted() const;
+
+  bool operator==(StructDataLayout const &other) const;
+  bool operator!=(StructDataLayout const &other) const;
+  uint32_t getAlignedSize(uint32_t alignment) const;
+};
+
+inline void strided_memcpy(void *target, void *source, size_t chunk_size,
+                           size_t chunks, size_t stride) {
+  char *target_ = reinterpret_cast<char *>(target);
+  char *source_ = reinterpret_cast<char *>(source);
+
+  for (size_t i = 0; i < chunks; ++i) {
+    std::memcpy(target_, source_, chunk_size);
+    target_ += stride;
+    source_ += chunk_size;
+  }
+}
+
+}; // namespace svulkan2
---
+++ packages/sapien/sapien-2.1.0-cp37-cp37m-manylinux2014_x86_64.whl/sapien/include/svulkan2/shader/base_parser.h
@@ -0,0 +1,172 @@
+#pragma once
+#include "glsl_compiler.h"
+#include "reflect.h"
+#include "svulkan2/common/err.h"
+#include "svulkan2/common/fs.h"
+#include "svulkan2/common/layout.h"
+#include <future>
+#include <optional>
+#include <map>
+
+namespace svulkan2 {
+namespace shader {
+
+struct SpecializationConstantValue {
+  DataType dtype;
+  union {
+    int intValue;
+    float floatValue;
+  };
+};
+
+enum UniformBindingType {
+  eScene,
+  eObject,
+  eCamera,
+  eMaterial,
+  eTextures,
+  eLight,
+  eNone,
+  eUnknown
+};
+
+struct DescriptorSetDescription {
+  struct Binding {
+    std::string name;
+    vk::DescriptorType type;
+    int dim;
+    int arraySize;
+    uint32_t arrayIndex; // index in the buffers/samplers vector
+  };
+  UniformBindingType type{eUnknown};
+  std::vector<std::shared_ptr<StructDataLayout>> buffers;
+  std::vector<std::string> samplers;
+  std::map<uint32_t, Binding> bindings;
+
+  DescriptorSetDescription merge(DescriptorSetDescription const &other) const;
+};
+
+DescriptorSetDescription
+getDescriptorSetDescription(spirv_cross::Compiler &compiler,
+                            uint32_t setNumber);
+
+inline std::string
+getOutTextureName(std::string variableName) { // remove "out" prefix
+  if (variableName.substr(0, 3) != "out") {
+    throw std::runtime_error("Output texture must start with \"out\"");
+  }
+  return variableName.substr(3, std::string::npos);
+}
+
+inline static std::string
+getInTextureName(std::string variableName) { // remove "sampler" prefix
+  if (variableName.substr(0, 7) != "sampler") {
+    throw std::runtime_error("Input texture must start with \"sampler\"");
+  }
+  return variableName.substr(7, std::string::npos);
+}
+
+std::shared_ptr<InputDataLayout>
+parseInputData(spirv_cross::Compiler &compiler);
+std::shared_ptr<InputDataLayout>
+parseVertexInput(spirv_cross::Compiler &compiler);
+
+std::shared_ptr<OutputDataLayout>
+parseOutputData(spirv_cross::Compiler &compiler);
+std::shared_ptr<OutputDataLayout>
+parseTextureOutput(spirv_cross::Compiler &compiler);
+
+bool hasUniformBuffer(spirv_cross::Compiler &compiler, uint32_t bindingNumber,
+                      uint32_t setNumber);
+
+std::shared_ptr<StructDataLayout> parseBuffer(spirv_cross::Compiler &compiler,
+                                              uint32_t bindingNumber,
+                                              uint32_t setNumber);
+std::shared_ptr<StructDataLayout> parseBuffer(spirv_cross::Compiler &compiler,
+                                              spirv_cross::Resource &resource);
+std::shared_ptr<StructDataLayout>
+parseCameraBuffer(spirv_cross::Compiler &compiler, uint32_t bindingNumber,
+                  uint32_t setNumber);
+
+std::shared_ptr<StructDataLayout>
+parseMaterialBuffer(spirv_cross::Compiler &compiler, uint32_t bindingNumber,
+                    uint32_t setNumber);
+std::shared_ptr<StructDataLayout>
+parseObjectBuffer(spirv_cross::Compiler &compiler, uint32_t bindingNumber,
+                  uint32_t setNumber);
+std::shared_ptr<StructDataLayout>
+parseSceneBuffer(spirv_cross::Compiler &compiler, uint32_t bindingNumber,
+                 uint32_t setNumber);
+std::shared_ptr<StructDataLayout>
+parseLightSpaceBuffer(spirv_cross::Compiler &compiler, uint32_t bindingNumber,
+                      uint32_t setNumber);
+
+std::shared_ptr<SpecializationConstantLayout>
+parseSpecializationConstant(spirv_cross::Compiler &compiler);
+
+class BaseParser {
+
+protected:
+  std::string mName;
+  std::vector<uint32_t> mVertSPVCode;
+  std::vector<uint32_t> mFragSPVCode;
+  std::vector<uint32_t> mGeomSPVCode;
+  vk::UniquePipelineLayout mPipelineLayout;
+  float mResolutionScale{1.f};
+
+  bool mAlphaBlend{};
+
+public:
+  void loadGLSLFiles(std::string const &vertFile, std::string const &fragFile,
+                     std::string const &geomFile = "");
+  void loadSPVFiles(std::string const &vertFile, std::string const &fragFile);
+  void loadSPVCode(std::vector<uint32_t> const &vertCode,
+                   std::vector<uint32_t> const &fragCode);
+
+  std::future<void> loadGLSLFilesAsync(std::string const &vertFile,
+                                       std::string const &fragFile,
+                                       std::string const &geomFile = "");
+
+  vk::PipelineLayout getPipelineLayout() const { return mPipelineLayout.get(); }
+
+  virtual std::shared_ptr<OutputDataLayout> getTextureOutputLayout() const = 0;
+  virtual std::vector<std::string> getColorRenderTargetNames() const = 0;
+  virtual std::optional<std::string> getDepthRenderTargetName() const {
+    return {};
+  };
+
+  virtual vk::RenderPass getRenderPass() const = 0;
+  virtual vk::Pipeline getPipeline() const = 0;
+  virtual std::vector<UniformBindingType> getUniformBindingTypes() const;
+
+  /** name of textures used in the texture descriptor set (only in
+   * deferred-like passes) */
+  virtual std::vector<std::string> getInputTextureNames() const;
+
+  inline void enableAlphaBlend(bool enable) { mAlphaBlend = enable; }
+  inline void setName(std::string const &name) { mName = name; }
+  inline std::string getName() const { return mName; }
+
+  virtual vk::Pipeline createGraphicsPipeline(
+      vk::Device device, std::vector<vk::Format> const &colorFormats,
+      vk::Format depthFormat, vk::CullModeFlags cullMode,
+      vk::FrontFace frontFace,
+      std::vector<std::pair<vk::ImageLayout, vk::ImageLayout>> const
+          &colorTargetLayouts,
+      std::pair<vk::ImageLayout, vk::ImageLayout> const &depthLayout,
+      std::vector<vk::DescriptorSetLayout> const &descriptorSetLayouts,
+      std::map<std::string, SpecializationConstantValue> const
+          &specializationConstantInfo) = 0;
+
+  virtual std::vector<DescriptorSetDescription>
+  getDescriptorSetDescriptions() const = 0;
+
+  inline float getResolutionScale() const { return mResolutionScale; }
+
+  virtual ~BaseParser() = default;
+
+protected:
+  virtual void reflectSPV() = 0;
+};
+} // namespace shader
+} // namespace svulkan2
