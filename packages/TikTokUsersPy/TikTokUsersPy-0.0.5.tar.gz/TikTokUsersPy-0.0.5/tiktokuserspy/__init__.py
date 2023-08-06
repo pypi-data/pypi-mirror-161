8cc30981ef9222dcdcfcc3816199988bc88d5d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:03 +0000

    Add package TikTokUsersPy/0.0.5/TikTokUsersPy-0.0.5.tar.gz

---
+++ packages/TikTokUsersPy/TikTokUsersPy-0.0.5.tar.gz/TikTokUsersPy-0.0.5/tiktokuserspy/__init__.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from TikTokUsersPy.main import User
+from TikTokUsersPy.main import Video
+from TikTokUsersPy.main import TikTokScraper
+"""
+TikTokUsersPy
+"""
+
+__version__ = "0.0.5"
+__author__ = 'Jason Bodzy'
