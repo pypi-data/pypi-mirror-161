fc4d35d0f2e10c5ccd170e6308c55a83e1945ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:03 +0000

    Add package satellite-images-fusion/1.1.0/satellite_images_fusion-1.1.0.tar.gz

---
+++ packages/satellite-images-fusion/satellite_images_fusion-1.1.0.tar.gz/satellite_images_fusion-1.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'satellite_images_fusion',
+    packages = ['satellite_images_fusion', 'satellite_images_fusion.algorithms', 'satellite_images_fusion.metrics', 'satellite_images_fusion.utils'],
+    version = '1.1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Package for satellite image fusion',
+    author = 'ParallUD',
+    author_email = 'aorestrepor@correo.udistrital.edu.co',
+    url = 'https://github.com/AndresRestrepoRodriguez/satellite_images_fusion',
+    download_url = 'https://github.com/AndresRestrepoRodriguez/satellite_images_fusion/archive/refs/tags/1.1.0.tar.gz',
+    keywords = ['fusion', 'satellite image'],
+    classifiers = [],
+)
