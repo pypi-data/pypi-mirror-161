1fb67c7f65e2aa846129e22e16996d73219722c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:28 +0000

    Add package approvaltests/5.4.0/approvaltests-5.4.0.tar.gz

---
+++ packages/approvaltests/approvaltests-5.4.0.tar.gz/approvaltests-5.4.0/setup.py
@@ -0,0 +1,49 @@
+import os
+import re
+from pathlib import Path
+
+from setuptools import setup, find_packages #type: ignore
+
+HERE = Path(__file__).parent
+_version_file_contents = (HERE / "approvaltests" / "version.py").read_text()
+matched = re.search(r'"(.*)"', _version_file_contents)
+VERSION = matched.group(1) if matched is not None else "UNKNOWN VERSION"
+
+with open(HERE / 'requirements.prod.required.txt') as f:
+    required = f.read().splitlines()
+with open(HERE / 'requirements.prod.extras.txt') as f:
+    required += f.read().splitlines()
+
+print(f"required={required}")
+
+setup(
+    name="approvaltests",
+    version=VERSION,
+    description="Assertion/verification library to aid testing",
+    author="ApprovalTests Contributors",
+    author_email="llewellyn.falco@gmail.com",
+    url="https://github.com/approvals/ApprovalTests.Python",
+    python_requires=">=3.6.1",
+    packages=find_packages(exclude=["tests*"]),
+    package_data={"approvaltests": ["reporters/reporters.json"]},
+    install_requires=required,
+    long_description=(HERE / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities",
+    ],
+)
