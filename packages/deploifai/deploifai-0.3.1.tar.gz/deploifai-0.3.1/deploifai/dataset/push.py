d1815dfe12b64a7ae56154cf9765f7bd910297fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:00 +0000

    Add package deploifai/0.3.1/deploifai-0.3.1.tar.gz

---
+++ packages/deploifai/deploifai-0.3.1.tar.gz/deploifai-0.3.1/deploifai/dataset/push.py
@@ -0,0 +1,18 @@
+import click
+
+from deploifai.context import DeploifaiContextObj, pass_deploifai_context_obj
+from deploifai.utilities.config.local_config import read_config_file
+from deploifai.core.data_storage import DataStorage
+
+
+@click.command()
+@pass_deploifai_context_obj
+def push(context: DeploifaiContextObj):
+    api = context.api
+    try:
+        configs = read_config_file()
+    except FileNotFoundError:
+        return
+
+    datastorage_handler = DataStorage(context=context, config=configs, api=api)
+    datastorage_handler.push()
