b7acb451bcad04b621d566d2c94737336f03c93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:32 +0000

    Add package cogwatch/3.0.1/cogwatch-3.0.1.tar.gz

---
+++ packages/cogwatch/cogwatch-3.0.1.tar.gz/cogwatch-3.0.1/cogwatch/__init__.py
@@ -0,0 +1,11 @@
+"""
+dpymenus -- Simplified menus for discord.py developers.
+"""
+
+__title__ = "cogwatch"
+__author__ = "Rob Wagner <rob@sombia.com>"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020-2022 Rob Wagner"
+__version__ = "3.0.1"
+
+from cogwatch.cogwatch import Watcher, watch
