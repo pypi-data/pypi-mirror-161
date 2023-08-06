e637ad79e68c3ec19dfec5a0121298e225c2a664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:30 +0000

    Add package python-la/0.95/python-la-0.95.tar.gz

---
+++ packages/python-la/python-la-0.95.tar.gz/python-la-0.95/README.md
@@ -0,0 +1,299 @@
+
+# py-la : Python linear algebra
+
+  
+
+## Field
+__Static methods:__
+```python
+create
+is_field
+```
+__Private methods:__
+```python
+__str__
+__eq__ virtual
+__contains__ virtual
+
+_generate_one virtual
+random
+```
+__Properties:__
+```python
+classOfInstance
+```
+__Other classes:__
+```python
+class  RationalField(Field)
+class  RealField(Field)
+class  ComplexField(Field)
+class  MatrixField(Field)
+
+Fields(Enum)
+```
+__Instances:__
+```python
+DefaultRationalField
+DefaultRealField
+DefaultComplexField
+```
+
+## Complex
+__Static methods:__
+```python
+random(min_val: float = -10, max_val: float = 10, value_func=random.randint) -> Complex:
+```
+__Private methods:__
+```python
+__add__
+__radd__
+__sub__
+__rsub__
+__neg__
+__mul__
+__rmul__
+__truediv__
+__rtruediv__
+__abs__ := norm
+__eq__
+__ne__
+__pow__
+__rpow__ TBD
+```
+__Properties::__
+```python
+conjugate
+norm
+```
+## Vector
+__Static methods:__
+```python
+random(min: float = -10, max: float = 10, degree: int = 10, def_value=None, f: Field = Field.DefaultRealField) -> Vector
+
+fromSize(size: int, default_value: Any = 0) -> Vector
+```
+__Private methods:__
+```python
+__add__
+__radd__
+__sub__
+__rsub__
+__neg__
+__mul__
+__rmul__
+__truediv__
+__rtruediv__
+__eq__
+__ne__
+__getitem__
+__iter__
+__len__
+
+almost_equal
+set
+norm
+dot
+toOrthonormal
+projection_onto
+copy
+```
+__Properties:__
+```python
+length
+adjoint TBD
+has_no_zero
+```
+
+## Span
+__Static methods:__
+```python
+spanField(field: Field) -> Span
+```
+__Private methods:__
+```python
+__add__
+__eq__
+__ne__
+__getitem__
+__iter__
+__len__
+__contains__
+
+contains
+append
+toOrthonormal
+projection_of
+random
+is_spanning
+```
+__Properties:__
+```python
+dim
+basis
+has_lineart_dependency
+is_orthogonal
+is_orthonormal
+```
+
+## Matrix
+__Static methods:__
+```python
+fromVector
+fromVectors
+fromSpan
+fromString
+random
+fromJordanBlocks TBD
+createJordanBlcok TBD
+id_matrix
+```
+__Private methods:__
+```python
+__add__
+__sub__
+__neg__
+__mul__
+__rmul__
+__truediv__
+__rtruediv__
+__eq__
+__ne__
+__getitem__
+__len__
+
+almost_equal
+inverse
+cofactor
+minor
+tarnspose
+reorgenize_rows
+guassian_elimination
+solve TBD
+get_eigen_space_of TBD
+algebraic_multiplicity TBD
+geometric_multiplicity TBD
+```
+__Properties:__
+```python
+kernel TBD
+image TBD
+rank
+determinant
+is_invertible
+is_square
+is_diagonialable TBD
+is_nilpotent TBD
+eigen_values TBD
+jordan_form TBD
+chain_basis TBD
+characteristic_polynomial TBD
+minimal_polynomial TBD
+```
+Partially implemented
+
+## Linear Transformation
+__Static methods:__
+```python
+isFuncLinearTransformation
+fromMatrix
+id
+```
+__Private methods:__
+```python
+__add__
+__radd__
+__sub__
+__rsub__
+__neg__
+__mul__
+__rmul__
+__truediv__
+__call__
+
+toMatrix TBD
+```
+__Properties:__
+```python
+
+```
+Partially implemented
+
+  
+## Calculable
+An interface for the following subclasses:
+### Polynomial
+* #### SimplePolynomial
+	__Static methods:__
+	```python
+	fromString
+	```
+	__Private methods:__
+	```python
+	__add__
+	__radd__
+	__sub__
+	__rsub__
+	__neg__
+	__mul__
+	__rmul__
+	__truediv__
+	__rtruediv__
+	__pow__
+	__eq__
+	__ne__
+	__call__
+	__len__
+
+	gcd_with TBD
+	```
+	__Properties:__
+	```python
+	roots
+	degree
+	```
+* #### PolinomialFraction
+	 __Static methods:__
+	```python
+
+	```
+	__Private methods:__
+	```python
+
+	```
+	__Properties:__
+	```python
+
+	```
+
+### Expression - TBD
+__Static methods:__
+```python
+
+```
+__Private methods:__
+```python
+
+```
+__Properties:__
+```python
+
+```
+
+## InnerProduct
+
+__Static methods:__
+```python
+isInnerProduct
+```
+__Private methods:__
+```python
+__call__
+```
+__Properties:__
+```python
+
+```
+__Instances:__
+```python
+StandardInnerProduct
+```
---
+++ packages/python-la/python-la-0.95.tar.gz/python-la-0.95/python_la/src/la1/Vector.py
@@ -0,0 +1,153 @@
+from __future__ import annotations
+from ..utils import almost_equal, isoneof
+from typing import Union, Any
+from .Field import Field, RealField, DefaultRealField
+from .Complex import Complex
+
+
+class Vector:
+
+    @staticmethod
+    def random(min: float = -10, max: float = 10, degree: int = 10,  def_value=None, f: Field = DefaultRealField) -> Vector:
+        return Vector([f.random(min, max) if def_value is None else def_value for _ in range(degree)])
+
+    @staticmethod
+    def fromSize(size: int, default_value: Any = 0) -> Vector:
+        return Vector([default_value for _ in range(size)])
+
+    def __init__(self, values: list[Any], field: Field = None) -> None:
+        self.__values = values
+        # TODO add default field detection
+        self.field = RealField(len(values)) if not field else field
+
+    @property
+    def length(self):
+        return len(self.__values)
+
+    @property
+    def conjugate(self) -> Vector:
+        return [v.conjugate if isinstance(v, Complex) else v for v in self]
+
+    @property
+    def has_no_zero(self) -> bool:
+        for v in self:
+            if v != 0:
+                return True
+        return False
+
+    def __str__(self) -> str:
+        result = "["
+        for v in self:
+            result += str(v) + ", "
+        return result[:-2]+"]"
+
+    def __add__(self, other: Vector) -> Vector:
+        if not isinstance(other, Vector):
+            raise TypeError("Vector can only be added to another Vector")
+        if self.field != other.field:
+            raise ValueError("Vectors must have the same field")
+        if len(self.__values) != len(other.__values):
+            raise ValueError("Vectors must have the same length")
+        return Vector([self[i] + other[i] for i in range(len(self))], self.field)
+
+    def __radd__(self, other) -> Vector:
+        return self.__add__(other)
+
+    def __sub__(self, other: Vector) -> Vector:
+        if not isinstance(other, Vector):
+            raise TypeError(
+                "Vector can only be subtracted from another Vector")
+        if self.field != other.field:
+            raise ValueError("Vectors must have the same field")
+        if len(self.__values) != len(other.__values):
+            raise ValueError("Vectors must have the same length")
+        return Vector([self.__values[i] - other.__values[i] for i in range(len(self.__values))], self.field)
+
+    def __rsub__(self, other) -> Vector:
+        return (-self) + other
+
+    def __neg__(self) -> Vector:
+        return Vector([-self.__values[i] for i in range(len(self.__values))], self.field)
+
+    def __mul__(self, num: float) -> Vector:
+        return Vector([num * self.__values[i] for i in range(len(self.__values))], self.field)
+
+    def __rmul__(self, num: float) -> Vector:
+        return self.__mul__(num)
+
+    def __truediv__(self, other) -> Vector:
+        if isoneof(other, [int, float, Complex]):
+            return self.__mul__(1/other)
+        raise ValueError("cant divide vector")
+
+    def __rtruediv__(self, num: float) -> Vector:
+        raise ValueError("cant divide by vector")
+
+    def __getitem__(self, index: int) -> Union[int, float, Complex]:
+        return self.__values[index]
+
+    def __iter__(self):
+        return iter(self.__values)
+
+    def __eq__(self, other: Vector) -> bool:
+        if not isoneof(other, [Vector, list]):
+            return False
+        if isinstance(other, Vector):
+            other = other.__values
+        if len(self) != len(other):
+            return False
+        return self.__values == other
+
+    def __ne__(self, other: Vector) -> bool:
+        return not (self == other)
+
+    def __len__(self) -> int:
+        return self.length
+
+    def almost_equal(self, other: Vector) -> bool:
+        if not isinstance(other, Vector):
+            raise TypeError("Vector can only be compared to another Vector")
+        if not self.field == other.field:
+            raise ValueError("Vectors must have the same field")
+        return all([almost_equal(self[i], other[i]) for i in range(len(self))])
+
+    def set(self, index, value) -> None:
+        self.__values[index] = value
+
+    def norm(self) -> float:
+        return sum([x ** 2 for x in self]) ** 0.5
+
+    def dot(self, other: Vector) -> Vector:
+        if not isinstance(other, Vector):
+            raise TypeError("Vector can only be multiplied by another Vector")
+        if self.field != other.field:
+            raise ValueError("Vectors must have the same field")
+        if len(self.__values) != len(other.__values):
+            raise ValueError("Vectors must have the same length")
+        return Vector([self.__values[i] * other.__values[i] for i in range(len(self.__values))])
+
+    def toOrthonormal(self) -> Vector:
+        # TODO change name to Normalize?
+        return self / self.norm()
+
+    def projection_onto(self, value) -> Vector:
+        """
+        return the projection of self onto value which can be another vector or a Span
+        """
+        from .Span import Span
+        if not isinstance(value, Vector) and not isinstance(value, Span):
+            raise TypeError("v must be of type Vector or Span")
+        is_span = isinstance(value, Span)
+        if not is_span and value.length != self.length:
+            raise ValueError("value must have the same length as self")
+        if is_span and value[0].length != self.length:
+            raise ValueError(
+                "the span's vectors must have the same length as self")
+        from ..la2.InnerProduct import StandardInnerProduct as sip
+        if not is_span:
+            return sip(self, value)/sip(value, value)*value
+        else:
+            return value.projection_of(self)
+
+    def copy(self) -> Vector:
+        return Vector(self.__values, self.field)
