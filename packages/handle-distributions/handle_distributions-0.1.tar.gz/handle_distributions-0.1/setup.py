488c57b6a1a285124c02d5115cf8d4a5741f0f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:24 +0000

    Add package handle-distributions/0.1/handle_distributions-0.1.tar.gz

---
+++ packages/handle-distributions/handle_distributions-0.1.tar.gz/handle_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='handle_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['handle_distributions'],
+      author='Chamod perera',
+      author_email= 'chamodperera87@gmail.com',
+      zip_safe=False)
