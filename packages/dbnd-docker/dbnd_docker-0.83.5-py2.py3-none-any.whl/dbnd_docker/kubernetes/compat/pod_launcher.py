d3b7e99e0a46a326e62d38ba752930a254e51a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:20 +0000

    Add package dbnd-docker/0.83.5/dbnd_docker-0.83.5-py2.py3-none-any.whl

---
+++ packages/dbnd-docker/dbnd_docker-0.83.5-py2.py3-none-any.whl/dbnd_docker/kubernetes/compat/pod_launcher.py
@@ -0,0 +1,10 @@
+from dbnd_airflow.constants import AIRFLOW_ABOVE_10
+
+
+if AIRFLOW_ABOVE_10:
+    from airflow.kubernetes.pod_launcher import PodStatus
+else:
+    from airflow.contrib.kubernetes.pod_launcher import PodStatus
+
+
+__all__ = ["PodStatus"]
