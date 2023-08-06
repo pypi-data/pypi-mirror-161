aa89ba2ca6bb9fea7a8e3d24959a2ec1d0e582ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:03 +0000

    Add package impy-array/2.1.2/impy_array-2.1.2-py3-none-any.whl

---
+++ packages/impy-array/impy_array-2.1.2-py3-none-any.whl/impy/arrays/__init__.py
@@ -0,0 +1,6 @@
+from .imgarray import ImgArray
+from .labeledarray import LabeledArray
+from .label import Label
+from .phasearray import PhaseArray
+from .specials import PropArray
+from .lazy import LazyImgArray
