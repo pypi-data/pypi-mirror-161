290cffa6b8668ed7e42035223d38d02350aef675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:37 +0000

    Add package AbeInterpreter/1.1/abeinterpreter-1.1.tar.gz

---
+++ packages/AbeInterpreter/abeinterpreter-1.1.tar.gz/abeinterpreter-1.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='AbeInterpreter',
+      version='1.0',
+      description='Interpreter the Abraham esolang',
+      author='Byron Himes',
+      author_email='TheByronHimes@gmail.com',
+      url='https://www.byronhimes.com/abraham',
+     )
