3eef4d470f25bf1ef01450ebf4bf2064cb3f8107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:28 +0000

    Add package zfun/2.0.0/zfun-2.0.0.tar.gz

---
+++ packages/zfun/zfun-2.0.0.tar.gz/zfun-2.0.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name="zfun",
+    version="2.0.0",
+    author="Haiyang Zheng",
+    author_email="wnfdsfy@gmail.com",
+    packages=setuptools.find_packages(),
+    url="https://github.com/zhenghy/zpy",
+    license="MIT",
+    description="个人常用函数库",
+    long_description=open("README.MD", "r").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "pymysql",
+        "dash_mantine_components",
+        "dash_tabulator",
+        "PyPDF2",
+        "winrm",
+        "sqlalchemy",
+        "fastapi",
+        "aiofiles",
+        "reportlab",
+        "pandas",
+    ],
+    keywords=["common", "function", "class"],
+    platforms="any",
+)
---
+++ packages/zfun/zfun-2.0.0.tar.gz/zfun-2.0.0/zfun/__init__.py
@@ -0,0 +1,39 @@
+__author__ = "zhenghy"
+__all__ = [
+    "nowf",
+    "mkdirf",
+    "mkscript",
+    "winCMD",
+    "pdfMerger",
+    "pdfPaste",
+    "text2Range",
+    "remove_upprintable_chars",
+    "humanChrName",
+    "MYSQL",
+    "webDoc",
+    "webPost",
+    "webTab",
+    "ctxIndex",
+    "dccfileSave",
+]
+
+from zfun.com import (
+    nowf,
+    mkdirf,
+    mkscript,
+    winCMD,
+    pdfMerger,
+    pdfPaste,
+    text2Range,
+    remove_upprintable_chars,
+)
+
+from zfun.bio import humanChrName
+from zfun.db import MYSQL
+from zfun.web import (
+    webDoc,
+    webPost,
+    webTab,
+    ctxIndex,
+    dccfileSave,
+)
