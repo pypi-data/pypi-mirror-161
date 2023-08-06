7b9dfc7dd8fcddff30d831df93c051f4979d2385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:38 +0000

    Add package moldyutilities/0.0.1/moldyutilities-0.0.1-py3-none-any.whl

---
+++ packages/moldyutilities/moldyutilities-0.0.1-py3-none-any.whl/moldyutilites/networkutilities.py
@@ -0,0 +1,2 @@
+def ping(ip):
+    ''' Should auto detect the OS instead of requiring it as an input. '''
---
+++ packages/moldyutilities/moldyutilities-0.0.1-py3-none-any.whl/moldyutilites/osutilities.py
@@ -0,0 +1,3 @@
+def create_folder():
+    ''' Check if the folder exists, and if it doesn't, create the folder. 
+    This should be OS agnostic. '''
