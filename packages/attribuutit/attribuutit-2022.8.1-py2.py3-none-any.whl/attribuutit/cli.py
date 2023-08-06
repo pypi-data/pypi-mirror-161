8a0b0493aa058acbf1f94a7b72d93f903ec87e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:07 +0000

    Add package attribuutit/2022.8.1/attribuutit-2022.8.1-py2.py3-none-any.whl

---
+++ packages/attribuutit/attribuutit-2022.8.1-py2.py3-none-any.whl/attribuutit/cli.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+"""Scan a tree of files and extract attributes of the underlying vector or raster data."""
+import json
+import pathlib
+import sys
+
+import typer
+
+import attribuutit.shp as shp
+import attribuutit.tif as tif
+import attribuutit.vpf as vpf
+
+app = typer.Typer()
+
+
+@app.command()
+def eject(name: str) -> int:
+    """Eject an example configuration in JSON format with stem equal to name."""
+    typer.echo(f'This will eject an example configuration as {name}.json')
+    return 0
+
+
+@app.command()
+def scan(name: str, dry_run: bool = False) -> int:
+    """Scan the file system based on the configuration given in name.json and persist the report eventually."""
+    if not dry_run:
+        typer.echo(f'Real run changing the world following the configuration {name}.json - later ...')
+        return 2
+
+    typer.echo(f'Dry run exposing the plan of execution when following the configuration {name}.json - later ...')
+    return 0
+
+
+@app.command()
+def inspect(path_str: str, dry_run: bool = False) -> int:
+    """Inspect the file at path and persist the report eventually."""
+    if not dry_run:
+        typer.echo(f'Real run inspecting file at {path_str}')
+        path = pathlib.Path(path_str)
+        suffix = path.suffix
+        if suffix == '.shp':
+            error, shape = shp.load(path)
+            if not error:
+                print(shp.summary(shape))
+            else:
+                print(error)
+        elif suffix == '.tif':
+            error, bitmap = tif.load(path)
+            if not error:
+                print(tif.summary(bitmap))
+            else:
+                print(error)
+        else:
+            with open(path, 'rb') as source:
+                vpf_artifact = vpf.Table(str(path), (b for b in source.read()))
+            print(json.dumps(vpf_artifact.table, indent=2))
+        return 0
+
+    typer.echo(f'Dry run exposing the plan of execution when inspecting file at {path_str}')
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(app())
