64a34264330997b665b4007f015cc1230e48990e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:58 +0000

    Add package gradio/3.1.3a4/gradio-3.1.3a4-py3-none-any.whl

---
+++ packages/gradio/gradio-3.1.3a4-py3-none-any.whl/gradio/templates/cdn/assets/index.c6ac6bf0.js
@@ -0,0 +1,5 @@
+export { M as Component } from './index.efc3106d.js';
+
+const modes = ["static"];
+
+export { modes };
---
+++ packages/gradio/gradio-3.1.3a4-py3-none-any.whl/gradio/templates/frontend/assets/index.64048ef0.js
@@ -0,0 +1,5 @@
+export { M as Component } from './index.20bd6624.js';
+
+const modes = ["static"];
+
+export { modes };
