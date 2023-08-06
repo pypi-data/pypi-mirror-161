f6c20cfb8cbb7eadef23b05342f777854f6448a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:20 +0000

    Add package giant-plugins/1.0.6/giant-plugins-1.0.6.tar.gz

---
+++ packages/giant-plugins/giant-plugins-1.0.6.tar.gz/giant-plugins-1.0.6/giant_plugins/content_width_video/models.py
@@ -0,0 +1,41 @@
+from cms.models import CMSPlugin
+from django.db import models
+from filer.fields.image import FilerImageField
+from mixins.models import VideoURLMixin
+
+
+class ContentWidthVideo(CMSPlugin, VideoURLMixin):
+    """
+    Represents a content width video object
+    """
+
+    image = FilerImageField(
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    title = models.CharField(max_length=128, blank=True)
+    caption = models.CharField(max_length=128)
+    alt_text = models.CharField(max_length=128, blank=True, default="")
+    display_image = models.BooleanField(default=True)
+
+    def __str__(self):
+        """
+        String representation of the object
+        """
+        return f"Content Width Video {self.pk}"
+
+    def get_video_id(self):
+        """
+        Return the youtube video ID
+        """
+        youtube_id = parse_qs(urlparse(self.youtube_url).query)["v"][0]
+        return youtube_id
+
+    def fallback_thumbnail(self):
+        """
+        Returns a fallback thumbnail for the video
+        """
+
+        return f"https://img.youtube.com/vi/{self.get_video_id()}/maxresdefault.jpg"
