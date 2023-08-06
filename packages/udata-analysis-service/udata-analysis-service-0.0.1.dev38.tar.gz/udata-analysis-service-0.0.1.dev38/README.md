8ee4b9687122992e61172a3227f96332670ad000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:03 +0000

    Add package udata-analysis-service/0.0.1.dev38/udata-analysis-service-0.0.1.dev38.tar.gz

---
+++ packages/udata-analysis-service/udata-analysis-service-0.0.1.dev38.tar.gz/udata-analysis-service-0.0.1.dev38/README.md
@@ -0,0 +1,50 @@
+# udata-analysis-service
+
+This service's purpose is to analyse udata datalake files to enrich the metadata, starting with CSVs.
+It uses csv-detective to detect the type and format of CSV columns by checking both headers and contents.
+
+## Installation
+
+Install **udata-analysis-service**:
+
+```shell
+pip install udata-analysis-service
+```
+
+Rename the `.env.sample` to `.env` and fill it with the right values.
+
+```shell
+REDIS_URL = redis://localhost:6381/0
+REDIS_HOST = localhost
+REDIS_PORT = 6381
+KAFKA_HOST = localhost
+KAFKA_PORT = 9092
+KAFKA_API_VERSION = 2.5.0
+MINIO_URL = https://object.local.dev/
+MINIO_USER = sample_user
+MINIO_PWD = sample_pwd
+ROWS_TO_ANALYSE_PER_FILE=500
+CSV_DETECTIVE_REPORT_BUCKET = benchmark-de
+CSV_DETECTIVE_REPORT_FOLDER = report
+TABLESCHEMA_BUCKET = benchmark-de
+TABLESCHEMA_FOLDER = schemas
+UDATA_INSTANCE_NAME=udata
+```
+
+## Usage
+
+Start the Kafka consumer:
+
+```shell
+udata-analysis-service consume
+```
+
+Start the Celery worker:
+
+```shell
+udata-analysis-service work
+```
+
+### Logging & Debugging
+The log level can be adjusted using the environment variable LOGLEVEL.
+For example, to set the log level to `DEBUG` when consuming Kafka messages, use `LOGLEVEL="DEBUG" udata-analysis-service consume`.
---
+++ packages/udata-analysis-service/udata-analysis-service-0.0.1.dev38.tar.gz/udata-analysis-service-0.0.1.dev38/tests/test_background_tasks.py
@@ -0,0 +1,67 @@
+import os
+
+import csv_detective
+import pytest
+
+from udata_analysis_service.background_tasks import manage_resource
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_manage_resource_send_produce_message(mocker):
+    mocker.patch.dict(
+        os.environ,
+        {
+            "CSV_DETECTIVE_REPORT_BUCKET": "detective-bucket",
+            "CSV_DETECTIVE_REPORT_FOLDER": "report",
+            "TABLESCHEMA_BUCKET": "tableschema-bucket",
+            "TABLESCHEMA_FOLDER": "tableschema-folder",
+            "KAFKA_HOST": "localhost",
+            "KAFKA_PORT": "9092",
+            "UDATA_INSTANCE_NAME": "udata",
+        },
+    )
+    mocker.patch("boto3.client")
+    routine = mocker.patch(
+        "udata_analysis_service.background_tasks.routine_minio"
+    )
+    produce = mocker.patch("udata_analysis_service.background_tasks.produce")
+
+    manage_resource(
+        dataset_id="dataset_id",
+        resource_id="resource_id",
+        resource_details={
+            "data_location": {"netloc": "netloc", "bucket": "bucket", "key": "key"},
+            "encoding": "UTF-8",
+            "delimiter": ",",
+        },
+        minio_user="minio_user",
+        minio_pwd="minio_pwd",
+    )
+
+    produce.assert_called_with(
+        "localhost:9092",
+        "udata.resource.analysed",
+        service="csvdetective",
+        key_id="resource_id",
+        document={
+            "data_location": {
+                "netloc": "netloc",
+                "bucket": "bucket",
+                "key": "key",
+            },
+            "report_location": {
+                "netloc": "netloc",
+                "bucket": "detective-bucket",
+                "key": "report/dataset_id/resource_id.json",
+            },
+            "tableschema_location": {
+                "netloc": "netloc",
+                "bucket": "tableschema-bucket",
+                "key": "tableschema-folder/dataset_id/resource_id",
+            },
+            "encoding": "UTF-8",
+            "delimiter": ",",
+        },
+        meta={"dataset_id": "dataset_id", "message_type": "resource.analysed"},
+    )
