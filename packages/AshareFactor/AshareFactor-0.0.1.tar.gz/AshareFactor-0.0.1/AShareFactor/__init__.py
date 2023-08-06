6114400c70d491cd71fcc5a51f435350c8339c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:51 +0000

    Add package AshareFactor/0.0.1/AshareFactor-0.0.1.tar.gz

---
+++ packages/AshareFactor/AshareFactor-0.0.1.tar.gz/AshareFactor-0.0.1/AShareFactor/__init__.py
@@ -0,0 +1,1 @@
+from .AshareFactor import *
---
+++ packages/AshareFactor/AshareFactor-0.0.1.tar.gz/AshareFactor-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name = "AshareFactor",
+	version = "0.0.1",
+	author = "Xi",
+	description = "common A Share alpha and risk factors",
+	packages = ["AShareFactor"]
+	)
