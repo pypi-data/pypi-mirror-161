a57989f5134b050dc1992584789cf029c6a2e49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:48 +0000

    Add package astromorphlib/0.2.1/astromorphlib-0.2.1.tar.gz

---
+++ packages/astromorphlib/astromorphlib-0.2.1.tar.gz/astromorphlib-0.2.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='astromorphlib',
+      version='0.2.1',
+      description='Python scripts to analyze the morphology of isolated/interacting galaxies',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='J. A. Hernandez-Jimenez',
+      author_email='joseaher@gmail.com',
+      url = "https://gitlab.com/joseaher/astromorphlib",
+      packages=['stat_lib'],
+      package_data = {'stat_lib':['Table_Arp_Madore_pairs_updated.txt','zero_points.fits']},
+      include_package_data =  True
+      )
