4f0b6aac904b7cc235d1ed72e6b81806b1eb60a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:55 +0000

    Add package movici-geo-query/1.1.1/movici_geo_query-1.1.1-cp39-cp39-win_amd64.whl

---
+++ packages/movici-geo-query/movici_geo_query-1.1.1-cp39-cp39-win_amd64.whl/movici_geo_query/__init__.py
@@ -0,0 +1,11 @@
+from .geo_query import GeoQuery, QueryResult
+from .geometry import PointGeometry, LinestringGeometry, OpenPolygonGeometry, ClosedPolygonGeometry
+
+__all__ = [
+    "GeoQuery",
+    "QueryResult",
+    "PointGeometry",
+    "LinestringGeometry",
+    "OpenPolygonGeometry",
+    "ClosedPolygonGeometry",
+]
