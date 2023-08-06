bef1754604966ec8be6b045afc19d4b254636b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:56 +0000

    Add package lewaa/0.0.1/lewaa-0.0.1.tar.gz

---
+++ packages/lewaa/lewaa-0.0.1.tar.gz/lewaa-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lewaa',
+      version='0.0.1',
+      description='Lewaa Dataframe',
+      packages=['lewaa'],
+      author='Abdul Qadeer',
+      author_email='imaqafridi@gmail.com',
+      zip_safe=False)
