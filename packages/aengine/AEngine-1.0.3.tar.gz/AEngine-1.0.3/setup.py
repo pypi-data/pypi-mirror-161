cc4ec4930a82892d2fbd90addf12505bc40d6368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:59 +0000

    Add package aengine/1.0.3/AEngine-1.0.3.tar.gz

---
+++ packages/aengine/AEngine-1.0.3.tar.gz/AEngine-1.0.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aengine']
+
+package_data = \
+{'': ['*'],
+ 'aengine': ['.git/*',
+             '.git/hooks/*',
+             '.git/info/*',
+             '.git/logs/*',
+             '.git/logs/refs/heads/*',
+             '.git/logs/refs/remotes/origin/*',
+             '.git/objects/66/*',
+             '.git/objects/80/*',
+             '.git/objects/d9/*',
+             '.git/objects/pack/*',
+             '.git/refs/heads/*',
+             '.git/refs/remotes/origin/*']}
+
+setup_kwargs = {
+    'name': 'aengine',
+    'version': '1.0.3',
+    'description': 'Console applications Engine',
+    'long_description': None,
+    'author': 'alex',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
