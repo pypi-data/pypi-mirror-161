f31dcbaa28b5647f67be40e02c28170588ea6606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:11 +0000

    Add package pysystemair/0.3.0/pysystemair-0.3.0.tar.gz

---
+++ packages/pysystemair/pysystemair-0.3.0.tar.gz/pysystemair-0.3.0/pysystemair/models.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import Optional, Awaitable
+from .const import REG_TYPE
+
+from box import Box
+
+class Callbacks(BaseModel):
+    holding_reg: Awaitable
+    input_reg: Awaitable
+    write_reg: Awaitable
+
+    class Config:
+        arbitrary_types_allowed = True
+
+class Register(BaseModel):
+    addr: int
+    value: Optional[float]=None
+    reg_type: REG_TYPE
+
+class InputRegister(Register):
+    reg_type: REG_TYPE = REG_TYPE.INPUT
+
+
+class HoldingRegister(Register):
+    reg_type: REG_TYPE = REG_TYPE.HOLDING
+
+## https://shop.systemair.com/upload/assets/SAVE_MODBUS_VARIABLE_LIST_20190116__REV__29_.PDF
+## registry values - 1 for some reason
+
+
+
+
