e2debd8abe43759caca14522e817fb529f6aea34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:30 +0000

    Add package mybox/0.0.21/mybox-0.0.21.tar.gz

---
+++ packages/mybox/mybox-0.0.21.tar.gz/mybox-0.0.21/mybox/package/shell.py
@@ -0,0 +1,49 @@
+import os
+import pwd
+from typing import Optional
+
+from ..fs import home
+from ..utils import run, run_output, with_os
+from .base import Package
+
+
+def get_current_linux_shell() -> str:
+    return pwd.getpwuid(os.getuid()).pw_shell
+
+
+def get_current_macos_shell() -> str:
+    return run_output("dscl", ".", "-read", home(), "UserShell").split(": ")[1]
+
+
+SHELLS_FILE = "/etc/shells"
+
+
+def get_all_shells() -> list[str]:
+    with open(SHELLS_FILE) as shells_file:
+        return [shell.strip() for shell in shells_file]
+
+
+class Shell(Package):
+    def __init__(self, shell: str, **kwargs) -> None:
+        self.shell = shell
+        super().__init__(**kwargs)
+
+    @property
+    def name(self) -> str:
+        return "_shell"
+
+    def get_remote_version(self) -> str:
+        return self.shell
+
+    @property
+    def local_version(self) -> Optional[str]:
+        return with_os(linux=get_current_linux_shell, macos=get_current_macos_shell)()
+
+    def install(self) -> None:
+        if not os.path.exists(self.shell):
+            raise ValueError(f"{self.shell} does not exist.")
+        if not os.access(self.shell, os.X_OK):
+            raise ValueError(f"{self.shell} is not executable.")
+        if self.shell not in get_all_shells():
+            run("sudo", "tee", "-a", SHELLS_FILE, input=self.shell.encode())
+        run("chsh", "-s", self.shell)
