220f0bd2a0ca82e098870f4fede7b1e7da1b8927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:16 +0000

    Add package apachetomcatscanner/1.3/apachetomcatscanner-1.3-py3-none-any.whl

---
+++ packages/apachetomcatscanner/apachetomcatscanner-1.3-py3-none-any.whl/apachetomcatscanner/utils/utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name          : utils.py
+# Author             : Podalirius (@podalirius_)
+# Date created       : 24 Jul 2022
+
+import re
+import requests
+
+
+
+
+
+
+
+
