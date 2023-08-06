54c2014513dfbae4dbced6641aaff8da61f8406a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:42 +0000

    Add package mac-pyxa/0.0.6/mac_pyxa-0.0.6-py3-none-any.whl

---
+++ packages/mac-pyxa/mac_pyxa-0.0.6-py3-none-any.whl/PyXA/__init__.py
@@ -0,0 +1,18 @@
+from .PyXA import (
+    current_application,
+    application,
+    running_applications,
+    open_url,
+    get_clipboard,
+    get_clipboard_strings,
+    set_clipboard,
+    run_applescript,
+)
+
+from .XABase import (
+    XAAlert, XAAlertStyle,
+    XAColorPicker, XAColorPickerStyle,
+    XASound,
+    XAURL,
+    AppleScript,
+)
