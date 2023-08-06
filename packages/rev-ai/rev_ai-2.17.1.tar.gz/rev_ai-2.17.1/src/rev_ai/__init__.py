b3a906cd901c08cd3645e27be41835bf4be86326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:52 +0000

    Add package rev-ai/2.17.1/rev_ai-2.17.1.tar.gz

---
+++ packages/rev-ai/rev_ai-2.17.1.tar.gz/rev_ai-2.17.1/src/rev_ai/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for rev_ai"""
+
+__version__ = '2.17.1'
+
+from .models import Job, JobStatus, Account, Transcript, Monologue, Element, MediaConfig, \
+    CaptionType, CustomVocabulary, TopicExtractionJob, TopicExtractionResult, Topic, Informant, \
+    SpeakerName, LanguageIdentificationJob, LanguageIdentificationResult, LanguageConfidence, \
+    SentimentAnalysisResult, SentimentValue, SentimentMessage, SentimentAnalysisJob, CustomerUrlData
