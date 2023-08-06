c4df7d0330f87656b069c81048b035cd73a2e636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:53 +0000

    Add package Mensajes-Julio/6.0/Mensajes_Julio-6.0-py3-none-any.whl

---
+++ packages/Mensajes-Julio/Mensajes_Julio-6.0-py3-none-any.whl/mensajes/hola/saludos.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def saludar():
+    print("Hola, te estoy saludando desde saludos.saludar")
+
+def prueba():
+    print("Esto es una prueba de la nueva version")
+
+def generar_array(numeros):
+    return np.arange(numeros)
+
+class Saludo():
+    def __init__(self):
+        print("Hola, te saludo desde el init de la clase Saludo")
+
+if __name__ =='__main__':
+    print(generar_array(5))
