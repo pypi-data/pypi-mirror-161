18977722a55bd1720330d7b4099b89dbefa51d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:57 +0000

    Add package polars-u64-idx/0.13.59/polars_u64_idx-0.13.59.tar.gz

---
+++ packages/polars-u64-idx/polars_u64_idx-0.13.59.tar.gz/polars_u64_idx-0.13.59/local_dependencies/polars-core/src/series/unstable.rs
@@ -0,0 +1,65 @@
+use crate::prelude::*;
+use std::convert::TryFrom;
+use std::marker::PhantomData;
+use std::ptr::NonNull;
+
+/// A wrapper type that should make it a bit more clear that we should not clone Series
+#[derive(Debug, Copy, Clone)]
+#[cfg(feature = "private")]
+pub struct UnstableSeries<'a> {
+    lifetime: PhantomData<&'a Series>,
+    // A series containing a single chunk ArrayRef
+    // the ArrayRef will be replaced by amortized_iter
+    // use with caution!
+    container: *mut Series,
+    // the ptr to the inner chunk, this saves some ptr chasing
+    inner: NonNull<ArrayRef>,
+}
+
+/// We don't implement Deref so that the caller is aware of converting to Series
+impl AsRef<Series> for UnstableSeries<'_> {
+    fn as_ref(&self) -> &Series {
+        unsafe { &*self.container }
+    }
+}
+
+impl AsMut<Series> for UnstableSeries<'_> {
+    fn as_mut(&mut self) -> &mut Series {
+        unsafe { &mut *self.container }
+    }
+}
+
+pub type ArrayBox = Box<dyn Array>;
+
+impl<'a> UnstableSeries<'a> {
+    pub fn new(series: &'a mut Series) -> Self {
+        let container = series as *mut Series;
+        let inner_chunk = series.array_ref(0);
+        UnstableSeries {
+            lifetime: PhantomData,
+            container,
+            inner: NonNull::new(inner_chunk as *const ArrayRef as *mut ArrayRef).unwrap(),
+        }
+    }
+
+    /// Creates a new `[UnsafeSeries]`
+    /// # Safety
+    /// Inner chunks must be from `Series` otherwise the dtype may be incorrect and lead to UB.
+    pub(crate) unsafe fn new_with_chunk(series: &'a mut Series, inner_chunk: &ArrayRef) -> Self {
+        UnstableSeries {
+            lifetime: PhantomData,
+            container: series,
+            inner: NonNull::new(inner_chunk as *const ArrayRef as *mut ArrayRef).unwrap(),
+        }
+    }
+
+    pub fn deep_clone(&self) -> Series {
+        let array_ref = unsafe { (*self.container).chunks() }[0].clone();
+        let name = unsafe { (*self.container).name() };
+        Series::try_from((name, array_ref)).unwrap()
+    }
+
+    pub fn swap(&mut self, array: ArrayRef) {
+        unsafe { *self.inner.as_mut() = array };
+    }
+}
---
+++ packages/polars-u64-idx/polars_u64_idx-0.13.59.tar.gz/polars_u64_idx-0.13.59/tests/test_schema.py
@@ -0,0 +1,38 @@
+import polars as pl
+
+
+def test_schema_on_agg() -> None:
+    df = pl.DataFrame({"a": ["x", "x", "y", "n"], "b": [1, 2, 3, 4]})
+
+    assert (
+        df.lazy()
+        .groupby("a")
+        .agg(
+            [
+                pl.col("b").min().alias("min"),
+                pl.col("b").max().alias("max"),
+                pl.col("b").sum().alias("sum"),
+                pl.col("b").first().alias("first"),
+                pl.col("b").last().alias("last"),
+            ]
+        )
+    ).schema == {
+        "a": pl.Utf8,
+        "min": pl.Int64,
+        "max": pl.Int64,
+        "sum": pl.Int64,
+        "first": pl.Int64,
+        "last": pl.Int64,
+    }
+
+
+def test_fill_null_minimal_upcast_4056() -> None:
+    df = pl.DataFrame({"a": [-1, 2, None]})
+    df = df.with_columns(pl.col("a").cast(pl.Int8))
+    assert df.with_column(pl.col(pl.Int8).fill_null(-1)).dtypes[0] == pl.Int8
+    assert df.with_column(pl.col(pl.Int8).fill_null(-1000)).dtypes[0] == pl.Int32
+
+
+def test_with_column_duplicates() -> None:
+    df = pl.DataFrame({"a": [0, None, 2, 3, None], "b": [None, 1, 2, 3, None]})
+    assert df.with_columns([pl.all().alias("same")]).columns == ["a", "b", "same"]
