746b859bfe8f4c8a63e6146386acfebaf9239268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:32 +0000

    Add package spgrep/0.2.8/spgrep-0.2.8.tar.gz

---
+++ packages/spgrep/spgrep-0.2.8.tar.gz/spgrep-0.2.8/tests/test_representation.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+from spgrep.irreps import enumerate_unitary_irreps, is_equivalent_irrep
+from spgrep.representation import (
+    frobenius_schur_indicator,
+    get_character,
+    get_intertwiner,
+    get_regular_representation,
+    project_to_irrep,
+)
+
+
+def test_get_character(C3v):
+    reg = get_regular_representation(C3v)
+    actual = get_character(reg)
+    expect = np.array([6, 0, 0, 0, 0, 0])
+    assert np.allclose(actual, expect)
+
+
+def test_intertwiner():
+    rep1 = np.array(
+        [
+            [[1.0 - 0.0j, 0.0 + 0.0j], [0.0 + 0.0j, 1.0 - 0.0j]],
+            [[0.0 + 0.0j, 1.0 - 0.0j], [1.0 - 0.0j, 0.0 + 0.0j]],
+            [[-0.0 - 1.0j, 0.0 + 0.0j], [0.0 + 0.0j, -0.0 + 1.0j]],
+            [[0.0 + 0.0j, -0.0 - 1.0j], [-0.0 + 1.0j, 0.0 + 0.0j]],
+        ]
+    )
+    rep2 = np.array(
+        [
+            [[1.0 - 0.0j, 0.0 + 0.0j], [0.0 + 0.0j, 1.0 - 0.0j]],
+            [[0.0 + 0.0j, 1.0 - 0.0j], [1.0 - 0.0j, 0.0 + 0.0j]],
+            [[-0.0 + 1.0j, 0.0 - 0.0j], [0.0 - 0.0j, 0.0 - 1.0j]],
+            [[0.0 - 0.0j, -0.0 + 1.0j], [0.0 - 1.0j, 0.0 - 0.0j]],
+        ]
+    )
+
+    intertwiner = get_intertwiner(rep1, rep2)
+    assert is_equivalent_irrep(get_character(rep1), get_character(rep2))
+    assert np.allclose(
+        np.einsum("kil,lj->kij", rep1, intertwiner),
+        np.einsum("il,klj->kij", intertwiner, rep2),
+    )
+
+
+def test_project_to_irrep(C3v):
+    reg = get_regular_representation(C3v)
+    irreps = enumerate_unitary_irreps(C3v)
+
+    count = 0
+    for irrep in irreps:
+        projected = project_to_irrep(reg, irrep)
+        count += len(projected)
+
+    assert count == sum(irrep.shape[1] for irrep in irreps)
+
+
+def test_frobenius_schur_indicator(C4):
+    irreps = enumerate_unitary_irreps(C4)
+    indicators = [frobenius_schur_indicator(irrep) for irrep in irreps]
+    assert sorted(indicators) == [0, 0, 1, 1]
