69ca630b0639eca0219855a0cb0ca4f77a185eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:51 +0000

    Add package census-consumer-complaint/0.1.0/census-consumer-complaint-0.1.0.tar.gz

---
+++ packages/census-consumer-complaint/census-consumer-complaint-0.1.0.tar.gz/census-consumer-complaint-0.1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+REMOVE_PACKAGE = "-e ."
+
+
+# def get_requirement_list(requirement_file_name=REQUIREMENT_FILE_NAME) -> list:
+#     try:
+#         requirement_list = None
+#         with open(requirement_file_name) as requirement_file:
+#             requirement_list = [requirement.replace("\n", "") for requirement in requirement_file]
+#             requirement_list.remove(REMOVE_PACKAGE)
+#         return requirement_list
+#     except Exception as e:
+#         raise e
+
+
+
+setup(
+    name="census-consumer-complaint",
+    license="MIT",
+    version="0.1.0",
+    description="Project has been completed.",
+    # packages=find_packages(),
+    # install_requires=['tfx==1.6.1', 'apache-beam[interactive]', 'apache-airflow']
+    author="Avnish Yadav",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['tfx', 'apache-beam[interactive]', 'apache-airflow']
+)
+
+
