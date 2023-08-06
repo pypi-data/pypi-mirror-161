f3cde99a3c150af0c0b5ea8c9787a8e83b365ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:42 +0000

    Add package pvim/3.9/pvim-3.9.tar.gz

---
+++ packages/pvim/pvim-3.9.tar.gz/pvim-3.9/pvim/welcome_message.py
@@ -0,0 +1,42 @@
+"""
+The welcome message. This is displayed when the editor opens without any files.
+"""
+from __future__ import unicode_literals
+from prompt_toolkit.formatted_text.utils import fragment_list_len
+
+import prompt_toolkit
+import pvim
+import platform
+import sys
+version = sys.version_info
+vim_version = pvim.__version__
+
+__all__ = (
+    'WELCOME_MESSAGE_TOKENS',
+    'WELCOME_MESSAGE_WIDTH',
+    'WELCOME_MESSAGE_HEIGHT',
+)
+
+WELCOME_MESSAGE_WIDTH = 36
+
+
+WELCOME_MESSAGE_TOKENS = [
+    ('class:title', 'vim - Pure Python Vi clone\n'),
+    ('', 'Still experimental\n\n'),
+    ('', 'version '), ('class:version', vim_version),
+        ('', ', prompt_toolkit '), ('class:version', prompt_toolkit.__version__),
+    ('', '\n'),
+    ('', 'type :q'),
+    ('class:key', '<Enter>'),
+    ('', '            to exit\n'),
+    ('', 'type :help'),
+    ('class:key', '<Enter>'),
+    ('', ' or '),
+    ('class:key', '<F1>'),
+    ('', ' for help\n\n'),
+    ('class:pythonversion', ' %s %i.%i.%i ' % (
+        platform.python_implementation(),
+        version[0], version[1], version[2])),
+]
+
+WELCOME_MESSAGE_HEIGHT = ''.join(t[1] for t in WELCOME_MESSAGE_TOKENS).count('\n') + 1
---
+++ packages/pvim/pvim-3.9.tar.gz/pvim-3.9/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+import pvim
+
+
+
+setup(
+    name='pvim',
+    author='Intel',
+    version=3.9,
+    description='Pure Python Vim Implementation',
+    packages=find_packages('.'),
+    install_requires = [
+        'prompt_toolkit>=2.0.0,<3.1.0',
+        'six',
+        'pyflakes',        # For Python error reporting.
+        'pygments',        # For the syntax highlighting.
+        'docopt',          # For command line arguments.
+    ],
+    entry_points={
+        'console_scripts': [
+            'vim = pvim.entry_points.run_pyvim:run',
+        ]
+    },
+)
