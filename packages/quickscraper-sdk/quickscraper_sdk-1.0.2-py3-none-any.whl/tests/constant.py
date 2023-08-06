3dd262c1313dcd74e4b9acd7f60e0f31bbd9bef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:59 +0000

    Add package quickscraper-sdk/1.0.2/quickscraper_sdk-1.0.2-py3-none-any.whl

---
+++ packages/quickscraper-sdk/quickscraper_sdk-1.0.2-py3-none-any.whl/tests/constant.py
@@ -0,0 +1,11 @@
+
+import os
+
+
+ACCESS_TOKEN = os.environ.get('QS_ACCESS_TOKEN') or 'DUMMY'
+SAMPLE_REQUEST_URL_FOR_HTML = 'http://httpbin.org/ip'
+SAMPLE_REQUEST_URL_FOR_CSV = 'https://cleartax.in/s/gst-state-code-jurisdiction'
+SAMPLE_REQUEST_URL_FOR_ZIP = 'https://www.amazon.in/s?i=electronics&rh=n:20631943031&fs=true&page=3'
+SAMPLE_REQUEST_URL_FOR_DOCX = 'http://httpbin.org/html'
+SAMPLE_REQUEST_URL_COUNTRY = 'http://ip-api.com/json'
+SAMPLE_REQUEST_URL_HEADER = 'https://httpbin.org/headers'
