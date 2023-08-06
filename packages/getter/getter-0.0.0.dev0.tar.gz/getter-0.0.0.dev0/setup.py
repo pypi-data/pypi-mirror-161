de7b79c4178f4e4a4281d2c1a831e6c49f5f7d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:09 +0000

    Add package getter/0.0.0.dev0/getter-0.0.0.dev0.tar.gz

---
+++ packages/getter/getter-0.0.0.dev0.tar.gz/getter-0.0.0.dev0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='getter',
+   version='0.0.0dev',
+   description='Under Development',
+   author='Nawaf Alqari',
+   author_email='nawafalqari13@gmail.com'
+)
