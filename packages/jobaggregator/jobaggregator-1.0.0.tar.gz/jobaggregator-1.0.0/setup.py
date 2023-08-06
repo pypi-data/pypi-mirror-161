47f0615f091bb809c4c2f6a5fdfd88e5ebadb77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:24 +0000

    Add package jobaggregator/1.0.0/jobaggregator-1.0.0.tar.gz

---
+++ packages/jobaggregator/jobaggregator-1.0.0.tar.gz/jobaggregator-1.0.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name='jobaggregator',
+    version='1.0.0',
+    author='portfolio-shop',
+    description='Common Python package for job aggregator project',
+    packages=['jobaggregator']
+)
