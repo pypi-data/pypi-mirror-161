8e498b33e57bb9345192adc09fcb797d4b900c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:41 +0000

    Add package pvim/2.9/pvim-2.9.tar.gz

---
+++ packages/pvim/pvim-2.9.tar.gz/pvim-2.9/pvim/entry_points/run_pyvim.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+vim: Pure Python Vim clone.
+Usage:
+    vim [-p] [-o] [-O] [-u <vimrc>] [<location>...]
+
+Options:
+    -p           : Open files in tab pages.
+    -o           : Split horizontally.
+    -O           : Split vertically.
+    -u <vimrc> : Use this .vimrc file instead.
+"""
+from __future__ import unicode_literals
+import docopt
+import os
+
+from pvim.editor import Editor
+from pvim.rc_file import run_rc_file
+
+__all__ = (
+    'run',
+)
+
+
+def run():
+    a = docopt.docopt(__doc__)
+    locations = a['<location>']
+    in_tab_pages = a['-p']
+    hsplit = a['-o']
+    vsplit = a['-O']
+    vimrc = a['-u']
+
+    # Create new editor instance.
+    editor = Editor()
+
+    # Apply rc file.
+    if vimrc:
+        run_rc_file(editor, vimrc)
+    else:
+        default_vimrc = os.path.expanduser('~/.vimrc')
+
+        if os.path.exists(default_vimrc):
+            run_rc_file(editor, default_vimrc)
+
+    # Load files and run.
+    editor.load_initial_files(locations, in_tab_pages=in_tab_pages,
+                              hsplit=hsplit, vsplit=vsplit)
+    editor.run()
+
+
+if __name__ == '__main__':
+    run()
