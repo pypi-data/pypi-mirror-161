7f65ff48027a51dc731bb35c4d0eac0e5ecc5415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:38 +0000

    Add package gs-aws-to-gcp-workload-identity/0.1.4/gs_aws_to_gcp_workload_identity-0.1.4-py3-none-any.whl

---
+++ packages/gs-aws-to-gcp-workload-identity/gs_aws_to_gcp_workload_identity-0.1.4-py3-none-any.whl/gs_aws_to_gcp_workload_identity/gcp_util.py
@@ -0,0 +1,40 @@
+import json
+import logging
+import requests
+
+from google.auth import crypt
+from google.auth import jwt
+
+logger = logging.getLogger(__name__)
+
+class GcpUtil:
+    def __init__(self) -> None:
+        pass
+
+    def call_gcp_url(self, body: dict, url: str, auth_token=None) -> dict:
+
+        headers = {
+            "content-type": "application/json; charset=utf-8",
+            "Accept": "application/json",
+        }
+
+        if auth_token is not None:
+            headers["Authorization"] = f"Bearer {auth_token}"
+
+        response = requests.post(
+            url,
+            json=body,
+            headers=headers,
+        )
+
+        if response.status_code != 200:
+            logger.fatal(f"Error getting token via {url}")
+            logger.error(response.text)
+            response.raise_for_status()
+
+        return response.json()
+
+
+    def parse_gcp_jwt_expiration(self, token) -> str:
+
+        return jwt.decode(token, certs=None, verify=False).get('exp')
