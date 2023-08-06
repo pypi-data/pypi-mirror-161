8685d89a5756619d6cb4a9a6613f1dfacc724f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:50 +0000

    Add package reflekt/0.2.9/reflekt-0.2.9.tar.gz

---
+++ packages/reflekt/reflekt-0.2.9.tar.gz/reflekt-0.2.9/reflekt/constants.py
@@ -0,0 +1,71 @@
+# SPDX-FileCopyrightText: 2022 Gregory Clunies <greg@reflekt-ci.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+TITLE_CASE_RE = r"[A-Z][a-z]+"
+CAMEL_CASE_RE = r"(^[a-z]|[A-Z])[a-z]*"
+SNAKE_CASE_RE = r"^[a-z._]+$"
+
+REFLEKT_TEMPLATE_COLUMNS = [
+    "source_schema",
+    "source_table",
+    "tracking_plan",
+    "event_text",
+    "call_type",
+]
+
+REFLEKT_RESERVED_COLUMNS = [
+    "user_id",
+    "anonymous_id",
+]
+
+PLANS = [
+    "segment",
+    "avo",
+    # "rudderstack",
+    # "snowplow",
+]
+
+PLAN_INIT_STRING = (
+    "\n[1] Segment Protocols"
+    "\n[2] Avo"
+    # "\n[3] Rudderstack"
+    # "\n[4] Snowplow"
+)
+
+PLAN_MAP = {
+    "1": "segment",
+    "2": "avo",
+    # "3": "rudderstack",
+    # "4": "snowplow",
+}
+
+CDPS = [
+    "segment",
+    # "rudderstack",
+    # "snowplow",
+]
+
+CDP_INIT_STRING = (
+    "\n[1] Segment"
+    # "\n[2] Rudderstack"
+    # "\n[3] Snowplow"
+)
+
+CDP_MAP = {
+    "1": "segment",
+    # "2": "rudderstack",
+    # "3": "snowplow",
+}
+
+WAREHOUSES = [
+    "snowflake",
+    "redshift",
+]
+
+WAREHOUSE_INIT_STRING = "\n[1] Snowflake" "\n[2] Redshift"
+
+WAREHOUSE_MAP = {
+    "1": "snowflake",
+    "2": "redshift",
+}
