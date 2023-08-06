907f98f7a557327d40f2cc1b003631fe2169614c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:23 +0000

    Add package cs-models/0.0.426/cs-models-0.0.426.tar.gz

---
+++ packages/cs-models/cs-models-0.0.426.tar.gz/cs-models-0.0.426/src/cs_models/resources/PatentSubsidiary/schemas.py
@@ -0,0 +1,15 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class PatentSubsidiaryResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    patent_id = fields.Integer(required=True)
+    subsidiary_id = fields.Integer(required=True)
+    score = fields.Float(required=True)
+    updated_at = fields.DateTime()
