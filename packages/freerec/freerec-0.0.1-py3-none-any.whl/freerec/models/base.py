a109c41656d22d9aa172ff675fac94ed813c40ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:51 +0000

    Add package freerec/0.0.1/freerec-0.0.1-py3-none-any.whl

---
+++ packages/freerec/freerec-0.0.1-py3-none-any.whl/freerec/models/base.py
@@ -0,0 +1,15 @@
+
+
+
+import torch.nn as nn
+
+from ..data.fields import Tokenizer
+
+__all__ = ['RecSysArch']
+
+class RecSysArch(nn.Module):
+
+    def __init__(self, tokenizer: Tokenizer) -> None:
+        super().__init__()
+
+        self.tokenizer = tokenizer
