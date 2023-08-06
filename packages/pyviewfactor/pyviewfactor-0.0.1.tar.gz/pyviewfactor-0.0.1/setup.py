6faba2be5a52d9f6a2b0d015c25ffa175b18e19a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:41 +0000

    Add package pyviewfactor/0.0.1/pyviewfactor-0.0.1.tar.gz

---
+++ packages/pyviewfactor/pyviewfactor-0.0.1.tar.gz/pyviewfactor-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+	name="pyviewfactor",
+	version="0.0.1",
+	author="Mateusz BOGDAN, Edouard WALTHER, Marc ALECIAN, Mina CHAPON",
+	description="A python library to calculate numerically exact radiation view factors between planar faces.",
+	packages=["pyviewfactor"]
+
+)
