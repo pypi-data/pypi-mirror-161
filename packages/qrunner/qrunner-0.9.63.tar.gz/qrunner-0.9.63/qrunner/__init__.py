8494418960c520974f1c5e34dfacb0c2a44d8467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:33 +0000

    Add package qrunner/0.9.63/qrunner-0.9.63.tar.gz

---
+++ packages/qrunner/qrunner-0.9.63.tar.gz/qrunner-0.9.63/qrunner/__init__.py
@@ -0,0 +1,7 @@
+from qrunner.running.runner import main
+from qrunner.core.api import TestCase
+from qrunner.utils.log import logger
+from qrunner.utils.decorate import *
+
+__version__ = "0.9.63"
+__description__ = "全栈自动化测试框架"
---
+++ packages/qrunner/qrunner-0.9.63.tar.gz/qrunner-0.9.63/qrunner/core/api/__init__.py
@@ -0,0 +1,6 @@
+# @Time    : 2022/2/22 9:33
+# @Author  : kang.yang@qizhidao.com
+# @File    : __init__.py.py
+from .case import TestCase
+
+__all__ = ['TestCase']
