92b2cc5ca162a1f50df1560ff123a54a11901f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:43 +0000

    Add package feedback-generation-nigula/0.0.4/feedback_generation_nigula-0.0.4.tar.gz

---
+++ packages/feedback-generation-nigula/feedback_generation_nigula-0.0.4.tar.gz/feedback_generation_nigula-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+description = "feedback_generation_nigula: the model for generating feedback comments  " \
+              " to the English sentences with pre-defined error-spans."
+long_description = description
+if os.path.exists("README.md"):
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
+setup(
+    name='feedback_generation_nigula',
+    packages = ['feedback_generation_nigula'],
+    version='0.0.4',
+    license='Apache',
+    author="Nikolay babakov",
+    author_email='bbkhse@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/skoltech-nlp/feedback_generation_nigula',
+    install_requires=[
+          'transformers', 'torch', 'spacy'
+      ],
+    data_files= [('terms',['feedback_generation_nigula/complex_content_termins.txt',
+                          'feedback_generation_nigula/complex_grammar_termins.txt'])]
+)
