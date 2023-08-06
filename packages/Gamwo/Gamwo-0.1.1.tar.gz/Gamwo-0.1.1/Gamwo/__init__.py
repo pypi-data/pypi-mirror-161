0681007ab833fcb55ab20e12a5719fb6f17be5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:41 +0000

    Add package Gamwo/0.1.1/Gamwo-0.1.1.tar.gz

---
+++ packages/Gamwo/Gamwo-0.1.1.tar.gz/Gamwo-0.1.1/Gamwo/__init__.py
@@ -0,0 +1,8 @@
+from gamwo.main import Gamwo
+from gamwo.entity import Entity
+from gamwo.line import Line
+from gamwo.sprite import Sprite
+from gamwo.text import Text
+from gamwo.timer import Timer
+
+__name__ == "Gamwo"
