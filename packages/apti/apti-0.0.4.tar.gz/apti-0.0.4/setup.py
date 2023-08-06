c982bfc8036931663f298dbe133af59960f96eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:40 +0000

    Add package apti/0.0.4/apti-0.0.4.tar.gz

---
+++ packages/apti/apti-0.0.4.tar.gz/apti-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='apti',
+    version='0.0.4',
+    decription='This will help you make robots',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Mikko Shogren',
+    author_email='mikkocoding@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords=['robot', 'window'],
+    packages=find_packages(),
+    install_requires=['pyautogui', 'opencv-python', 'pywhatkit', 'pynput', 'speechrecognition', 'keyboard', 'pyttsx3', 'googletrans', 'sklearn','pandas']
+)
