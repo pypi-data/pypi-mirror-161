a3b8c6236b949abc8dca6d9ca1a2287a95e944ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:12 +0000

    Add package bagbag/0.21.5/bagbag-0.21.5.tar.gz

---
+++ packages/bagbag/bagbag-0.21.5.tar.gz/bagbag-0.21.5/src/bagbag/Tools/Web.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request
+from flask import make_response
+from flask import abort, redirect
+from flask import render_template
+
+try:
+    from ..Thread import Thread
+except:
+    import sys 
+    sys.path.append("..")
+    from ..Thread import Thread
+
+class Response():
+    Make = make_response
+    Abort = abort
+    Redirect = redirect
+    Render = render_template
+
+class Web():
+    def __init__(self, host:str, port:int, name:str=__name__):
+        self.app = Flask(name)
+        
+        self.Route = self.app.route 
+        self.Request = request
+        self.Response = Response()
+
+        Thread(self.app.run, host, port)
