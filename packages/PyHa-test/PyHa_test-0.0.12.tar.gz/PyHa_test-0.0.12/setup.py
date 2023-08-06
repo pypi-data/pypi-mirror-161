a027445b3940b6eb363750d47c9b05d55c18e63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:52 +0000

    Add package PyHa-test/0.0.12/PyHa_test-0.0.12.tar.gz

---
+++ packages/PyHa-test/PyHa_test-0.0.12.tar.gz/PyHa_test-0.0.12/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="PyHa_test", 
+        version="0.0.12",
+        author="Jacob Ayers, Sean Perry, Sam Prestrelski, Vannessa Salgado",
+        #author_email="<youremail@email.com>",
+        description="A python package for automatically detecting species and comparing to ground truth",
+        #long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
