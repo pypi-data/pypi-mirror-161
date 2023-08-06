e59ad2c059db53be6215a7f5f12d57ae0186e8a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:27 +0000

    Add package ApiTestEngine/1.0.0/ApiTestEngine-1.0.0.tar.gz

---
+++ packages/ApiTestEngine/ApiTestEngine-1.0.0.tar.gz/ApiTestEngine-1.0.0/setup.py
@@ -0,0 +1,37 @@
+"""
+============================
+Author:柠檬班-木森
+Time:2020/7/16   16:20
+E-mail:3247119728@qq.com
+Company:湖南零檬信息技术有限公司
+============================
+"""
+from setuptools import setup, find_packages
+
+with open("readme.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='ApiTestEngine',
+    version='1.0.0',
+    author='MuSen',
+    author_email='musen_nmb@qq.com',
+    url='https://github.com/musen123/ApiTestEngine',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["Faker==13.4.0",
+                      "jsonpath==0.82",
+                      "PyMySQL==1.0.2",
+                      'requests==2.27.1',
+                      "requests-toolbelt==0.9.1",
+                      "rsa==4.8"
+                      ],
+    packages=find_packages(),
+    package_data={
+        "": ['*.md'],
+    },
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
