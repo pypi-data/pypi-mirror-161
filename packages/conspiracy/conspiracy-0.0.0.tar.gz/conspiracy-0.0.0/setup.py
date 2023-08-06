9a7095b1f0511c1e411d3aea353d02ff27df66f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:02 +0000

    Add package conspiracy/0.0.0/conspiracy-0.0.0.tar.gz

---
+++ packages/conspiracy/conspiracy-0.0.0.tar.gz/conspiracy-0.0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="conspiracy",
+    version="0.0.0",
+    license='MIT',
+    install_requires = ['numpy', 'colorama'],
+    author="Aaron Walsman",
+    author_email="aaronwalsman@gmail.com",
+    description='Terminal Plotter',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aaronwalsman/conspiracy",
+    packages=setuptools.find_packages(),
+)
