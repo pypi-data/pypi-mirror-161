47ece2dd3e767766dc73d12f01ca2efab3930218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:06 +0000

    Add package Latest-IDEarthquake/0.0.3/Latest_IDEarthquake-0.0.3.tar.gz

---
+++ packages/Latest-IDEarthquake/Latest_IDEarthquake-0.0.3.tar.gz/Latest_IDEarthquake-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Latest_IDEarthquake',
+    version='0.0.3',
+    packages=['gempaterkirni'],
+    url='https://github.com/RemoteWorkerIDN/Latest_IDEarthquake',
+    license='Development Status :: 4 - Beta',
+    author='danahiswara',
+    author_email='danahiswara.danas@gmail.com',
+    description='This package will scrape Indonesia\'s latest earthquake data from bmkg.go.id'
+)
