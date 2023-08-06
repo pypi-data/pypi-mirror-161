394038a733eefc170fcae90c91821841c6487a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:00 +0000

    Add package Mathics3/5.0.0/Mathics3-5.0.0.tar.gz

---
+++ packages/Mathics3/Mathics3-5.0.0.tar.gz/Mathics3-5.0.0/mathics/builtin/drawing/__init__.py
@@ -0,0 +1,14 @@
+"""
+Graphics, Drawing, and Images
+
+Showing something visually can be don in a number of ways:
+
+<ul>
+  <li>Starting with complete images and modifiying them. The 'Image' function is in this category.
+  <li>Use pre-defined 2D or 3D objects like 'Circle' and 'Cuboid' and place them in a coordiate space.
+  <li>Compute the points of the space using a function. This is done using functions like 'Plot' and 'ListPlot'.
+</ul>
+"""
+
+# This tells documentation how to sort this module
+sort_order = "mathics.builtin.graphing-drawing-and-images"
