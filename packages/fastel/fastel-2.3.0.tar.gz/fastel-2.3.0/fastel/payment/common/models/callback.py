3b0b906749ae749e817a6fb47ad64687c8e0c68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:18 +0000

    Add package fastel/2.3.0/fastel-2.3.0.tar.gz

---
+++ packages/fastel/fastel-2.3.0.tar.gz/fastel-2.3.0/fastel/payment/common/models/callback.py
@@ -0,0 +1,40 @@
+import time
+from typing import Any, Dict, Optional
+
+from pydantic import BaseModel
+
+
+class CallbackModel(BaseModel):
+    client_id: Optional[str]
+    payment_subtype: str
+    payment_status: str
+    payment_transaction_id: Optional[str]
+    payment_getnum_id: Optional[str]
+    payment_transaction_detail: Optional[Dict[str, Any]]
+    payment_redirect_detail: Optional[Dict[str, Any]]
+    payment_transaction_time: Optional[str]
+
+    total: int
+    order_id: str
+    pay_deadline: Optional[str]
+
+    code_no: Optional[str]
+
+    bank_code: Optional[str]
+    bank_account: Optional[str]
+
+    barcode_1: Optional[str]
+    barcode_2: Optional[str]
+    barcode_3: Optional[str]
+
+    @property
+    def stepfn_name(self) -> str:
+        return f"{self.order_id}_{self.payment_status}_{int(time.time())}"
+
+
+class LogisticCallbackModel(BaseModel):
+    logistic_status: str
+    logistic_type: str
+    logistic_subtype: str
+    logistic_id: str
+    detail: Dict[str, Any]
