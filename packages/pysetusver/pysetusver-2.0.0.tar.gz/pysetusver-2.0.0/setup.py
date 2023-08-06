102869e8483b5ff78bf53547cf8c5f4d2fbc2c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:18 +0000

    Add package pysetusver/2.0.0/pysetusver-2.0.0.tar.gz

---
+++ packages/pysetusver/pysetusver-2.0.0.tar.gz/pysetusver-2.0.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pysetusver",# 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
+    version="2.0.0",#程序版本
+    author="HANASE", # 项目作者
+    author_email="shenziqian66@163.com",#作者邮件
+    description="letmeseesee", # 项目的一句话描述
+    long_description=long_description,#加长版描述？
+    long_description_content_type="text/markdown",#描述使用Markdown
+    packages=setuptools.find_packages(),#无需修改
+    classifiers=[
+        "Programming Language :: Python :: 3",#使用Python3
+        "License :: OSI Approved :: Apache Software License",#开源协议
+        "Operating System :: OS Independent",
+    ],
+)
