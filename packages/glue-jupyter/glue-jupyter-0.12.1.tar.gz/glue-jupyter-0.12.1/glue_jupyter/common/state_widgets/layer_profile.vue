3bde9543ab78808ecbfc1c80ec991972a2827775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:05 +0000

    Add package glue-jupyter/0.12.1/glue-jupyter-0.12.1.tar.gz

---
+++ packages/glue-jupyter/glue-jupyter-0.12.1.tar.gz/glue-jupyter-0.12.1/glue_jupyter/common/state_widgets/layer_profile.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <div>
+            <glue-float-field label="line width" suffix="px" :value.sync="glue_state.linewidth" />
+        </div>
+        <div>
+            <v-select label="attribute" :items="attribute_items" v-model="attribute_selected" hide-details />
+        </div>
+    </div>
+</template>
