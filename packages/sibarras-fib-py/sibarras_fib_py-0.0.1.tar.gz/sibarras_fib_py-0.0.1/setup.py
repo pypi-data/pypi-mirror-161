92f715c91df1cd9c8bf4e8bf0bb7292922195bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:04 +0000

    Add package sibarras-fib-py/0.0.1/sibarras_fib_py-0.0.1.tar.gz

---
+++ packages/sibarras-fib-py/sibarras_fib_py-0.0.1.tar.gz/sibarras_fib_py-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name="sibarras_fib_py",
+    version="0.0.1",
+    author="Samuel Ibarra",
+    author_email="ing.samuelibarra@gmail.com",
+    description="Calculates a fibonacci number",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sibarras/sibarras-fib-py.git",
+    install_requires=[],
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    test_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'fib-number = sibarras_fib_py.cmd.fib_numb:fib_numb', 
+        ]
+    }
+)
