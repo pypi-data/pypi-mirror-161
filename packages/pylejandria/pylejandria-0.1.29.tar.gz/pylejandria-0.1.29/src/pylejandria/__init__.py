93311dd7f5faa0d4d882fddfd499203168c03ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:56 +0000

    Add package pylejandria/0.1.29/pylejandria-0.1.29.tar.gz

---
+++ packages/pylejandria/pylejandria-0.1.29.tar.gz/pylejandria-0.1.29/src/pylejandria/__init__.py
@@ -0,0 +1,22 @@
+#################################
+#   MADE BY: ARMANDO CHAPARRO   #
+#################################
+
+import sys
+
+sys.dont_write_bytecode = True
+
+from pylejandria import gui
+from pylejandria import math
+from pylejandria import module
+from pylejandria import tools
+from pylejandria import upload
+from pylejandria import constants
+# from pylejandria import auto
+
+MAFER = """
+It´s hard to forget
+someone who gave you
+so much to remember.
+I´ll always love you, Mafer.
+"""
