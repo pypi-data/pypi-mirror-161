289e3a91352edad516758c8f7d1fbf30681637f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:58 +0000

    Add package lndb_schema_core/0.2.1/lndb_schema_core-0.2.1-py2.py3-none-any.whl

---
+++ packages/lndb_schema_core/lndb_schema_core-0.2.1-py2.py3-none-any.whl/lndb_schema_core/__init__.py
@@ -0,0 +1,37 @@
+"""Core schema module.
+
+Import the package::
+
+   import lndb_schema_core
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   dobject
+   dtransform
+   dtransform_in
+   dtransform_out
+   jupynb
+   lndb_schema_core
+   track_do
+   track_do_type
+   user
+
+"""
+
+__version__ = "0.2.1"  # denote a pre-release for 0.1.0 with 0.1a1
+
+from . import id
+from ._core import (  # noqa
+    dobject,
+    dtransform,
+    dtransform_in,
+    dtransform_out,
+    jupynb,
+    lndb_schema_core,
+    track_do,
+    track_do_type,
+    user,
+)
