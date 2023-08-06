fba8e3d90db91932ad5828f80224fe84cc48c067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:50 +0000

    Add package AlanHesap/0.1.0/AlanHesap-0.1.0.tar.gz

---
+++ packages/AlanHesap/AlanHesap-0.1.0.tar.gz/AlanHesap-0.1.0/hesapla.py
@@ -0,0 +1,8 @@
+
+def daire(yaricap):
+    alan=3.14*yaricap*yaricap
+    return alan
+
+def kare(kenar):
+    return kenar*kenar
+
---
+++ packages/AlanHesap/AlanHesap-0.1.0.tar.gz/AlanHesap-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name="AlanHesap",
+    packages=["."],
+    version="0.1.0",
+    description="Alan hesaplama",
+    author="İlhan Cüvelek",
+    author_email="cuvelekilhan@gmail.com",
+    url="https://github.com/ilhancuvelek/mypypipackage",
+    keywords=["TAG1", "TAG2"],
+    classifiers=[]
+)
