4b22a059c2d44ec93c75b38e6f871199783620b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:49 +0000

    Add package AoE2ScenarioParser/0.1.42/AoE2ScenarioParser-0.1.42.tar.gz

---
+++ packages/AoE2ScenarioParser/AoE2ScenarioParser-0.1.42.tar.gz/AoE2ScenarioParser-0.1.42/AoE2ScenarioParser/sections/dependencies/dependency.py
@@ -0,0 +1,65 @@
+import math
+from uuid import UUID
+
+from AoE2ScenarioParser.scenarios.scenario_store import getters
+from AoE2ScenarioParser.sections.dependencies.dependency_action import DependencyAction
+from AoE2ScenarioParser.sections.retrievers.retriever import Retriever
+
+
+def refresh_targets(retriever_event, section, host_uuid):
+    for target in retriever_event.dependency_target.targets:
+        selected_retriever = select_retriever(target, section, host_uuid)
+        # selected_retriever = get_retriever_by_name(retriever_list, target[1])
+        # selected_retriever = section.retriever_map[target[1]]
+        execute_refresh_action(selected_retriever, section, host_uuid)
+
+
+def execute_refresh_action(retriever, section, host_uuid):
+    handle_retriever_dependency(retriever, "refresh", section, host_uuid)
+
+
+def handle_retriever_dependency(retriever: Retriever, state, section, host_uuid: UUID):
+    on_x = f'on_{state}'
+    if not hasattr(retriever, on_x):
+        return
+
+    retriever_event = getattr(retriever, on_x)  # construct, commit or refresh
+
+    action = retriever_event.dependency_action
+
+    if action == DependencyAction.REFRESH_SELF:
+        execute_refresh_action(retriever, section, host_uuid)
+    elif action == DependencyAction.REFRESH:
+        refresh_targets(retriever_event, section, host_uuid)
+    elif action in [DependencyAction.SET_VALUE, DependencyAction.SET_REPEAT]:
+        value = execute_dependency_eval(retriever_event, section, host_uuid)
+        if action == DependencyAction.SET_VALUE:
+            retriever.data = value
+        elif action == DependencyAction.SET_REPEAT:
+            retriever.datatype.repeat = value
+
+
+def execute_dependency_eval(retriever_event, section, host_uuid):
+    eval_code = retriever_event.dependency_eval.eval_code
+    eval_locals = retriever_event.dependency_eval.eval_locals
+    targets = retriever_event.dependency_target.targets
+
+    values = []
+    for target in targets:
+        # retriever_list = select_retriever_list(target, self_list, sections)
+        # values.append(get_retriever_by_name(retriever_list, target[1]).data)
+        values.append(select_retriever(target, section, host_uuid).data)
+
+    for index, target in enumerate(targets):
+        eval_locals[target[1]] = values[index]
+    eval_locals['math'] = math
+
+    return eval(eval_code, {}, eval_locals)
+
+
+def select_retriever(target, section, host_uuid):
+    if target[0] == "self":
+        return section.retriever_map[target[1]]
+    else:
+        sections = getters.get_sections(host_uuid)
+        return sections[target[0]].retriever_map[target[1]]
---
+++ packages/AoE2ScenarioParser/AoE2ScenarioParser-0.1.42.tar.gz/AoE2ScenarioParser-0.1.42/AoE2ScenarioParser/sections/retrievers/retriever_object_link.py
@@ -0,0 +1,166 @@
+from typing import Type, List
+from uuid import UUID
+
+from AoE2ScenarioParser import settings
+from AoE2ScenarioParser.helper.exceptions import UnsupportedAttributeError
+from AoE2ScenarioParser.helper.helper import exclusive_if
+from AoE2ScenarioParser.objects.aoe2_object import AoE2Object
+from AoE2ScenarioParser.objects.support.uuid_list import NO_UUID
+from AoE2ScenarioParser.scenarios.scenario_store import getters
+from AoE2ScenarioParser.sections.aoe2_file_section import AoE2FileSection
+from AoE2ScenarioParser.sections.dependencies.dependency import handle_retriever_dependency
+from AoE2ScenarioParser.sections.retrievers.support import Support
+
+
+class RetrieverObjectLink:
+    def __init__(
+            self,
+            variable_name: str,
+            section_name: str = None,
+            link: str = None,
+            support: Support = None,
+            process_as_object: Type[AoE2Object] = None,
+            retrieve_history_number: int = -1,
+            commit_callback=None,
+            destination_object=None,
+    ):
+        if not exclusive_if(link is not None, retrieve_history_number != -1):
+            raise ValueError("You must use exactly one of 'link' and the two 'retrieve...number' parameters.")
+
+        self.name: str = variable_name
+        self.section_name = section_name
+        self.link = link
+        self.support: Support = support
+        self.process_as_object: Type[AoE2Object] = process_as_object
+        self.retrieve_history_number: int = retrieve_history_number
+        self.commit_callback = commit_callback
+        self.destination_object = destination_object
+
+        self.splitted_link: List[str] = link.split('.') if link is not None else []
+
+    def construct(self, host_uuid, number_hist=None):
+        if number_hist is None:
+            number_hist = []
+
+        if self.retrieve_history_number != -1:
+            return number_hist[self.retrieve_history_number]
+        else:
+            sections = getters.get_sections(host_uuid)
+
+            # Retrieve value without using eval() -- Eval is slow
+            value = sections[self.section_name]
+            for index, item in enumerate(self.splitted_link):
+                if item.endswith("]"):
+                    # item[:-11] removes "[__index__]" from the key
+                    value = getattr(value, item[:-11])[number_hist[index]]
+                else:
+                    value = getattr(value, item)
+
+            if self.process_as_object:
+                return self.process_object_list(value, number_hist, host_uuid)
+            return value
+
+    def process_object_list(self, value_list, instance_number_history, host_uuid):
+        object_list = []
+        for index, struct in enumerate(value_list):
+            object_list.append(
+                self.process_as_object._construct(host_uuid, instance_number_history + [index])
+            )
+        return object_list
+
+    def commit(self, host_uuid: UUID, host_obj: AoE2Object):
+        # Object-only attributes for the ease of access of information.
+        # Not actually representing a value in the scenario file.
+        if self.retrieve_history_number != -1:
+            return
+
+        if host_uuid == NO_UUID:
+            raise ValueError(f"Invalid object commit. No UUID was set. Object class: {host_obj.__class__.__name__}")
+
+        number_hist = host_obj._instance_number_history
+
+        try:
+            # Get new value for receiver
+            value = getattr(host_obj, self.name)
+        except UnsupportedAttributeError:
+            return  # Not supported in current version.
+
+        if self.commit_callback is not None:
+            value = self.commit_callback(host_obj, self.name, value)
+
+        sections = getters.get_sections(host_uuid)
+        section = sections[self.section_name]
+
+        # Retrieve value without using eval() -- Eval is slow
+        retriever = None
+        file_section = section
+        for index, item in enumerate(self.splitted_link):
+            try:
+                if "[" in item:
+                    file_section = getattr(file_section, item[:-11])[number_hist[index]]
+                else:
+                    retriever = file_section.retriever_map[item]
+            except KeyError as e:
+                print(e)
+                # Maybe not supported in current version. if actually not supported -> ignore
+                if self.support is not None:
+                    if not self.support.supports(
+                            getters.get_scenario_version(host_uuid)):
+                        return
+                if settings.IGNORE_WRITING_ERRORS:
+                    return
+                raise e
+
+        if retriever is None:
+            raise ValueError("RetrieverObjectLink is unable to find retriever")
+
+        if self.process_as_object:
+            struct_datatype = retriever.datatype.var
+
+            prefix = "struct:"
+            if not struct_datatype.startswith(prefix):
+                raise ValueError(
+                    f"process_as_object isn't defined properly. Expected: '{prefix}...', got: '{struct_datatype}'"
+                )
+
+            struct_name = struct_datatype[len(prefix):]
+            struct_model = file_section.struct_models[struct_name]
+
+            RetrieverObjectLink.update_retriever_length(retriever, struct_model, len(value), host_uuid)
+            RetrieverObjectLink.commit_object_list(value, host_obj._instance_number_history)
+        else:
+            retriever.data = value
+
+        if hasattr(retriever, 'on_commit'):
+            handle_retriever_dependency(retriever, "commit", file_section, host_uuid)
+
+    @staticmethod
+    def commit_object_list(object_list, instance_number_history):
+        obj: AoE2Object
+        for index, obj in enumerate(object_list):
+            obj._instance_number_history = instance_number_history + [index]
+            obj.commit()
+
+    @staticmethod
+    def update_retriever_length(retriever, model, new_len, host_uuid):
+        try:
+            old_len = len(retriever.data)
+        except TypeError:  # retriever.data was not set before (list of 0 -> None)
+            old_len = 0
+            retriever.data = []
+
+        if new_len < old_len:
+            retriever.data = retriever.data[:new_len]
+        elif new_len > old_len:
+            retriever.data += [
+                AoE2FileSection.from_model(model, host_uuid, set_defaults=True)
+                for _ in range(new_len - old_len)
+            ]
+
+            if retriever.log_value:
+                retriever._print_value_update(f"[{model.name}] * {old_len}", f"[{model.name}] * {new_len}")
+
+    def __repr__(self):
+        return f"[RetrieverObjectLink] {self.name}: {self.section_name}.{self.link}" + \
+               (f"\n\t- Process as: {self.process_as_object.__name__}" if self.process_as_object else "") + \
+               (f"\n\t- Get Hist Number: {self.retrieve_history_number}" if self.retrieve_history_number >= 0 else "")
