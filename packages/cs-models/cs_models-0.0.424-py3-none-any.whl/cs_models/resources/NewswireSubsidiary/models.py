1157f75bf0d7caed4b9afb1e73a92f455de1b617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:58 +0000

    Add package cs-models/0.0.424/cs_models-0.0.424-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.424-py3-none-any.whl/cs_models/resources/NewswireSubsidiary/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    ForeignKey,
+)
+
+from ...database import Base
+
+
+class NewswireSubsidiaryModel(Base):
+    __tablename__ = "newswires_subsidiaries"
+
+    id = Column(Integer, primary_key=True)
+    news_id = Column(
+        Integer,
+        ForeignKey('newswires.id'),
+        nullable=False,
+    )
+    subsidiary_id = Column(
+        Integer,
+        ForeignKey('subsidiaries.id'),
+        nullable=False,
+    )
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
