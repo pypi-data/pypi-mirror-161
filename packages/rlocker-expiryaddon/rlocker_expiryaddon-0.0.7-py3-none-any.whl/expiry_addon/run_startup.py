683909d8d5b39aad4b22e873ab5ccf99ec5c9ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:58 +0000

    Add package rlocker-expiryaddon/0.0.7/rlocker_expiryaddon-0.0.7-py3-none-any.whl

---
+++ packages/rlocker-expiryaddon/rlocker_expiryaddon-0.0.7-py3-none-any.whl/expiry_addon/run_startup.py
@@ -0,0 +1,17 @@
+# This file loads up for ONE TIME once the application
+# is being recognized as an INSTALLED_APP (INSTALLED_ADDON)
+# Use this file to add code that should be executed first time
+
+import expiry_addon.constants as const
+from lockable_resource.action_manager import LRActionObjectsHandler, SUPPORTED_ACTIONS
+from expiry_addon.startup import lr_action_utils
+from expiry_addon.apps import ExpiryAddonConfig
+
+
+def main():
+    print(f"The Addon {ExpiryAddonConfig.name} is recognized!")
+    SUPPORTED_ACTIONS.extend(lr_action_utils.EXTENDED_SUPPORTED_ACTIONS)
+    a = LRActionObjectsHandler()
+    a.add_supported_action_object_pair(
+        key=const.ACTION_MODIFY_EXPIRY, value=lr_action_utils.LRActionModifyExpiry
+    )
