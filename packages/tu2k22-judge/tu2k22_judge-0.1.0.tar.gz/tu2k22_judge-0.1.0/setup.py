ec8a0e060f029cf9e701bba15e082a9dbba412c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:19:26 +0000

    Add package tu2k22-judge/0.1.0/tu2k22_judge-0.1.0.tar.gz

---
+++ packages/tu2k22-judge/tu2k22_judge-0.1.0.tar.gz/tu2k22_judge-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tu2k22_judge']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama>=0.4.5,<0.5.0',
+ 'pydantic>=1.9.1,<2.0.0',
+ 'requests>=2.28.1,<3.0.0',
+ 'typer>=0.6.1,<0.7.0']
+
+setup_kwargs = {
+    'name': 'tu2k22-judge',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Kushal Chordiya',
+    'author_email': 'kushal.chordiya@trilogy.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
