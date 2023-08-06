9ff12c86b0aee32ccb1d27bae1306b94e30738e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:35 +0000

    Add package SigProfilerAssignment/0.0.9/SigProfilerAssignment-0.0.9.tar.gz

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.9.tar.gz/SigProfilerAssignment-0.0.9/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.9'
+version = '0.0.9'
+Update = '1. Make consistent MutationType column 2. Update subgroups'
+    
+    
