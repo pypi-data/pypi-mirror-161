45a0ffaa982b6201cbc002b5f175e8db1b2dd55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:41 +0000

    Add package run-logger/0.1.3/run-logger-0.1.3.tar.gz

---
+++ packages/run-logger/run-logger-0.1.3.tar.gz/run-logger-0.1.3/run_logger/params.py
@@ -0,0 +1,36 @@
+from enum import Enum, auto
+from typing import Any, Iterable, Mapping, Tuple
+
+import numpy as np
+
+ParamChoice = Tuple[str, Iterable]
+
+
+def param_generator(params: Any):
+    if isinstance(params, Mapping):
+        if tuple(params.keys()) == ("",):
+            yield from param_generator(params[""])
+            return
+        if not params:
+            yield {}
+        else:
+            (key, value), *params = params.items()
+            for choice in param_generator(value):
+                for other_choices in param_generator(dict(params)):
+                    yield {key: choice, **other_choices}
+    elif isinstance(params, (list, tuple)):
+        for choices in params:
+            yield from param_generator(choices)
+    else:
+        yield params
+
+
+def param_sampler(params: Any, rng: np.random.Generator):
+    if isinstance(params, Mapping):
+        if tuple(params.keys()) == ("",):
+            return param_sampler(params[""], rng)
+        return {k: param_sampler(v, rng) for k, v in params.items()}
+    elif isinstance(params, (list, tuple)):
+        return param_sampler(params[rng.choice(len(params))], rng)
+    else:
+        return params
