884c39781ea0119ca52500deed8caf301c39765b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:49 +0000

    Add package rics/0.15.0/rics-0.15.0-py3-none-any.whl

---
+++ packages/rics/rics-0.15.0-py3-none-any.whl/rics/performance/__init__.py
@@ -0,0 +1,15 @@
+"""Performance testing utility."""
+
+from ._format_perf_counter import format_perf_counter, format_seconds
+from ._multi_case_timer import MultiCaseTimer
+from ._util import plot_run, to_dataframe
+from ._wrapper import run_multivariate_test
+
+__all__ = [
+    "MultiCaseTimer",
+    "run_multivariate_test",
+    "format_perf_counter",
+    "format_seconds",
+    "plot_run",
+    "to_dataframe",
+]
