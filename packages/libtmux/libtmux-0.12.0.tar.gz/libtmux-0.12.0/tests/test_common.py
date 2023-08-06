6ea0fe3a0ac310cb439bcd212ee6755ff3843fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:19:02 +0000

    Add package libtmux/0.12.0/libtmux-0.12.0.tar.gz

---
+++ packages/libtmux/libtmux-0.12.0.tar.gz/libtmux-0.12.0/tests/test_common.py
@@ -0,0 +1,213 @@
+"""Tests for utility functions in libtmux."""
+
+import re
+import sys
+from distutils.version import LooseVersion
+
+import pytest
+
+import libtmux
+from libtmux.common import (
+    TMUX_MAX_VERSION,
+    TMUX_MIN_VERSION,
+    get_libtmux_version,
+    get_version,
+    has_gt_version,
+    has_gte_version,
+    has_lt_version,
+    has_lte_version,
+    has_minimum_version,
+    has_version,
+    session_check_name,
+    tmux_cmd,
+    which,
+)
+from libtmux.exc import BadSessionName, LibTmuxException, TmuxCommandNotFound
+
+version_regex = re.compile(r"([0-9]\.[0-9])|(master)")
+
+
+def test_allows_master_version(monkeypatch):
+    def mock_tmux_cmd(param):
+        class Hi:
+            stdout = ["tmux master"]
+            stderr = None
+
+        return Hi()
+
+    monkeypatch.setattr(libtmux.common, "tmux_cmd", mock_tmux_cmd)
+
+    assert has_minimum_version()
+    assert has_gte_version(TMUX_MIN_VERSION)
+    assert has_gt_version(TMUX_MAX_VERSION), "Greater than the max-supported version"
+    assert (
+        "%s-master" % TMUX_MAX_VERSION == get_version()
+    ), "Is the latest supported version with -master appended"
+
+
+def test_allows_next_version(monkeypatch):
+    TMUX_NEXT_VERSION = str(float(TMUX_MAX_VERSION) + 0.1)
+
+    def mock_tmux_cmd(param):
+        class Hi:
+            stdout = [f"tmux next-{TMUX_NEXT_VERSION}"]
+            stderr = None
+
+        return Hi()
+
+    monkeypatch.setattr(libtmux.common, "tmux_cmd", mock_tmux_cmd)
+
+    assert has_minimum_version()
+    assert has_gte_version(TMUX_MIN_VERSION)
+    assert has_gt_version(TMUX_MAX_VERSION), "Greater than the max-supported version"
+    assert TMUX_NEXT_VERSION == get_version()
+
+
+def test_get_version_openbsd(monkeypatch):
+    def mock_tmux_cmd(param):
+        class Hi:
+            stderr = ["tmux: unknown option -- V"]
+
+        return Hi()
+
+    monkeypatch.setattr(libtmux.common, "tmux_cmd", mock_tmux_cmd)
+    monkeypatch.setattr(sys, "platform", "openbsd 5.2")
+    assert has_minimum_version()
+    assert has_gte_version(TMUX_MIN_VERSION)
+    assert has_gt_version(TMUX_MAX_VERSION), "Greater than the max-supported version"
+    assert (
+        "%s-openbsd" % TMUX_MAX_VERSION == get_version()
+    ), "Is the latest supported version with -openbsd appended"
+
+
+def test_get_version_too_low(monkeypatch):
+    def mock_tmux_cmd(param):
+        class Hi:
+            stderr = ["tmux: unknown option -- V"]
+
+        return Hi()
+
+    monkeypatch.setattr(libtmux.common, "tmux_cmd", mock_tmux_cmd)
+    with pytest.raises(LibTmuxException) as exc_info:
+        get_version()
+    exc_info.match("is running tmux 1.3 or earlier")
+
+
+def test_ignores_letter_versions():
+    """Ignore letters such as 1.8b.
+
+    See ticket https://github.com/tmux-python/tmuxp/issues/55.
+
+    In version 0.1.7 this is adjusted to use LooseVersion, in order to
+    allow letters.
+
+    """
+    result = has_minimum_version("1.9a")
+    assert result
+
+    result = has_minimum_version("1.8a")
+    assert result
+
+    # Should not throw
+    assert type(has_version("1.8")) is bool
+    assert type(has_version("1.8a")) is bool
+    assert type(has_version("1.9a")) is bool
+
+
+def test_error_version_less_1_7(monkeypatch):
+    def mock_get_version():
+        return LooseVersion("1.7")
+
+    monkeypatch.setattr(libtmux.common, "get_version", mock_get_version)
+    with pytest.raises(LibTmuxException) as excinfo:
+        has_minimum_version()
+        excinfo.match(r"libtmux only supports")
+
+    with pytest.raises(LibTmuxException) as excinfo:
+        has_minimum_version()
+
+        excinfo.match(r"libtmux only supports")
+
+
+def test_has_version():
+    assert has_version(str(get_version()))
+
+
+def test_has_gt_version():
+    assert has_gt_version("1.6")
+    assert has_gt_version("1.6b")
+
+    assert not has_gt_version("4.0")
+    assert not has_gt_version("4.0b")
+
+
+def test_has_gte_version():
+    assert has_gte_version("1.6")
+    assert has_gte_version("1.6b")
+    assert has_gte_version(str(get_version()))
+
+    assert not has_gte_version("4.0")
+    assert not has_gte_version("4.0b")
+
+
+def test_has_lt_version():
+    assert has_lt_version("4.0a")
+    assert has_lt_version("4.0")
+
+    assert not has_lt_version("1.7")
+    assert not has_lt_version(str(get_version()))
+
+
+def test_has_lte_version():
+    assert has_lte_version("4.0a")
+    assert has_lte_version("4.0")
+    assert has_lte_version(str(get_version()))
+
+    assert not has_lte_version("1.7")
+    assert not has_lte_version("1.7b")
+
+
+def test_which_no_bin_found():
+    assert which("top")
+    assert which("top", default_paths=[])
+    assert not which("top", default_paths=[], append_env_path=False)
+    assert not which("top", default_paths=["/"], append_env_path=False)
+
+
+def test_tmux_cmd_raises_on_not_found():
+    with pytest.raises(TmuxCommandNotFound):
+        tmux_cmd("-V", tmux_search_paths=[], append_env_path=False)
+
+    tmux_cmd("-V")
+
+
+def test_tmux_cmd_unicode(session):
+    session.cmd("new-window", "-t", 3, "-n", "юникод", "-F", "Ελληνικά")
+
+
+@pytest.mark.parametrize(
+    "session_name,raises,exc_msg_regex",
+    [
+        ("", True, "may not be empty"),
+        (None, True, "may not be empty"),
+        ("my great session.", True, "may not contain periods"),
+        ("name: great session", True, "may not contain colons"),
+        ("new great session", False, None),
+        ("ajf8a3fa83fads,,,a", False, None),
+    ],
+)
+def test_session_check_name(session_name, raises, exc_msg_regex):
+    if raises:
+        with pytest.raises(BadSessionName) as exc_info:
+            session_check_name(session_name)
+        assert exc_info.match(exc_msg_regex)
+    else:
+        session_check_name(session_name)
+
+
+def test_get_libtmux_version():
+    from libtmux.__about__ import __version__
+
+    version = get_libtmux_version()
+    assert isinstance(version, LooseVersion)
+    assert LooseVersion(__version__) == version
---
+++ packages/libtmux/libtmux-0.12.0.tar.gz/libtmux-0.12.0/tests/test_window.py
@@ -0,0 +1,275 @@
+"""Test for libtmux Window object."""
+import logging
+
+import pytest
+
+from libtmux import exc
+from libtmux.common import has_gte_version
+from libtmux.pane import Pane
+from libtmux.server import Server
+from libtmux.window import Window
+
+logger = logging.getLogger(__name__)
+
+
+def test_select_window(session):
+    window_count = len(session._windows)
+    # to do, get option for   base-index from tmux
+    # for now however, let's get the index from the first window.
+    assert window_count == 1
+
+    window_base_index = int(session.attached_window.index)
+
+    window = session.new_window(window_name="testing 3")
+
+    # self.assertEqual(2,
+    # int(session.attached_window.index))
+    assert int(window_base_index) + 1 == int(window.index)
+
+    session.select_window(window_base_index)
+    assert window_base_index == int(session.attached_window.index)
+
+    session.select_window("testing 3")
+    assert int(window_base_index) + 1 == int(session.attached_window.index)
+
+    assert len(session._windows) == 2
+
+
+def test_zfresh_window_data(session):
+    pane_base_index = int(
+        session.attached_window.show_window_option("pane-base-index", g=True)
+    )
+
+    assert len(session.windows) == 1
+
+    assert len(session.attached_window.panes) == 1
+    current_windows = len(session._windows)
+    assert session.get("session_id") != "@0"
+    assert current_windows == 1
+
+    assert len(session.attached_window.panes) == 1
+    assert isinstance(session.server, Server)
+    # len(session.attached_window.panes))
+
+    assert len(session.windows), 1
+    assert len(session.attached_window.panes) == 1
+    for w in session.windows:
+        assert isinstance(w, Window)
+    window = session.attached_window
+    assert isinstance(window, Window)
+    assert len(session.attached_window.panes) == 1
+    window.split_window()
+    session.attached_window.select_pane(pane_base_index)
+    session.attached_pane.send_keys("cd /srv/www/flaskr")
+    session.attached_window.select_pane(pane_base_index + 1)
+    session.attached_pane.send_keys("source .venv/bin/activate")
+    session.new_window(window_name="second")
+    current_windows += 1
+    assert current_windows == len(session._windows)
+    session.new_window(window_name="hey")
+    current_windows += 1
+    assert current_windows == len(session._windows)
+
+    session.select_window(1)
+    session.kill_window(target_window="hey")
+    current_windows -= 1
+    assert current_windows == len(session._windows)
+
+
+def test_newest_pane_data(session):
+    window = session.new_window(window_name="test", attach=True)
+    assert isinstance(window, Window)
+    assert len(window.panes) == 1
+    window.split_window(attach=True)
+
+    assert len(window.panes) == 2
+    # note: the below used to accept -h, removing because split_window now
+    # has attach as its only argument now
+    window.split_window(attach=True)
+    assert len(window.panes) == 3
+
+
+def test_attached_pane(session):
+    """Window.attached_window returns active Pane."""
+
+    window = session.attached_window  # current window
+    assert isinstance(window.attached_pane, Pane)
+
+
+def test_split_window(session):
+    """Window.split_window() splits window, returns new Pane, vertical."""
+    window_name = "test split window"
+    window = session.new_window(window_name=window_name, attach=True)
+    pane = window.split_window()
+    assert len(window.panes) == 2
+    assert isinstance(pane, Pane)
+    assert float(window.panes[0].height) <= ((float(window.width) + 1) / 2)
+
+
+def test_split_window_shell(session):
+    """Window.split_window() splits window, returns new Pane, vertical."""
+    window_name = "test split window"
+    cmd = "sleep 1m"
+    window = session.new_window(window_name=window_name, attach=True)
+    pane = window.split_window(shell=cmd)
+    assert len(window.panes) == 2
+    assert isinstance(pane, Pane)
+    assert float(window.panes[0].height) <= ((float(window.width) + 1) / 2)
+    if has_gte_version("3.2"):
+        assert pane.get("pane_start_command").replace('"', "") == cmd
+    else:
+        assert pane.get("pane_start_command") == cmd
+
+
+def test_split_window_horizontal(session):
+    """Window.split_window() splits window, returns new Pane, horizontal."""
+    window_name = "test split window"
+    window = session.new_window(window_name=window_name, attach=True)
+    pane = window.split_window(vertical=False)
+    assert len(window.panes) == 2
+    assert isinstance(pane, Pane)
+    assert float(window.panes[0].width) <= ((float(window.width) + 1) / 2)
+
+
+@pytest.mark.parametrize(
+    "window_name_before,window_name_after",
+    [("test", "ha ha ha fjewlkjflwef"), ("test", "hello \\ wazzup 0")],
+)
+def test_window_rename(session, window_name_before, window_name_after):
+    """Window.rename_window()."""
+    window_name_before = "test"
+    window_name_after = "ha ha ha fjewlkjflwef"
+
+    session.set_option("automatic-rename", "off")
+    window = session.new_window(window_name=window_name_before, attach=True)
+
+    assert window == session.attached_window
+    assert window.get("window_name") == window_name_before
+
+    window.rename_window(window_name_after)
+
+    window = session.attached_window
+
+    assert window.get("window_name") == window_name_after
+
+    window = session.attached_window
+
+    assert window.get("window_name") == window_name_after
+
+
+def test_kill_window(session):
+    session.new_window()
+    # create a second window to not kick out the client.
+    # there is another way to do this via options too.
+
+    w = session.attached_window
+
+    w.get("window_id")
+
+    w.kill_window()
+    with pytest.raises(IndexError):
+        w.get("window_id")
+
+
+def test_show_window_options(session):
+    """Window.show_window_options() returns dict."""
+    window = session.new_window(window_name="test_window")
+
+    options = window.show_window_options()
+    assert isinstance(options, dict)
+
+
+def test_set_show_window_options(session):
+    """Set option then Window.show_window_options(key)."""
+    window = session.new_window(window_name="test_window")
+
+    window.set_window_option("main-pane-height", 20)
+    assert window.show_window_options("main-pane-height") == 20
+
+    window.set_window_option("main-pane-height", 40)
+    assert window.show_window_options("main-pane-height") == 40
+    assert window.show_window_options()["main-pane-height"] == 40
+
+    if has_gte_version("2.3"):
+        window.set_window_option("pane-border-format", " #P ")
+        assert window.show_window_options("pane-border-format") == " #P "
+
+
+def test_empty_window_option_returns_None(session):
+    window = session.new_window(window_name="test_window")
+    assert window.show_window_option("alternate-screen") is None
+
+
+def test_show_window_option(session):
+    """Set option then Window.show_window_option(key)."""
+    window = session.new_window(window_name="test_window")
+
+    window.set_window_option("main-pane-height", 20)
+    assert window.show_window_option("main-pane-height") == 20
+
+    window.set_window_option("main-pane-height", 40)
+    assert window.show_window_option("main-pane-height") == 40
+    assert window.show_window_option("main-pane-height") == 40
+
+
+def test_show_window_option_unknown(session):
+    """Window.show_window_option raises UnknownOption for bad option key."""
+    window = session.new_window(window_name="test_window")
+
+    cmd_exception = exc.UnknownOption
+    if has_gte_version("3.0"):
+        cmd_exception = exc.InvalidOption
+    with pytest.raises(cmd_exception):
+        window.show_window_option("moooz")
+
+
+def test_show_window_option_ambiguous(session):
+    """show_window_option raises AmbiguousOption for ambiguous option."""
+    window = session.new_window(window_name="test_window")
+
+    with pytest.raises(exc.AmbiguousOption):
+        window.show_window_option("clock-mode")
+
+
+def test_set_window_option_ambiguous(session):
+    """set_window_option raises AmbiguousOption for ambiguous option."""
+    window = session.new_window(window_name="test_window")
+
+    with pytest.raises(exc.AmbiguousOption):
+        window.set_window_option("clock-mode", 12)
+
+
+def test_set_window_option_invalid(session):
+    """Window.set_window_option raises ValueError for invalid option key."""
+
+    window = session.new_window(window_name="test_window")
+
+    if has_gte_version("2.4"):
+        with pytest.raises(exc.InvalidOption):
+            window.set_window_option("afewewfew", 43)
+    else:
+        with pytest.raises(exc.UnknownOption):
+            window.set_window_option("afewewfew", 43)
+
+
+def test_move_window(session):
+    """Window.move_window results in changed index"""
+
+    window = session.new_window(window_name="test_window")
+    new_index = str(int(window.index) + 1)
+    window.move_window(new_index)
+    assert window.index == new_index
+
+
+def test_move_window_to_other_session(server, session):
+    window = session.new_window(window_name="test_window")
+    new_session = server.new_session("test_move_window")
+    window.move_window(session=new_session.get("session_id"))
+    assert new_session.get_by_id(window.get("window_id")) == window
+
+
+def test_select_layout_accepts_no_arg(server, session):
+    """tmux allows select-layout with no arguments, so let's allow it here."""
+
+    window = session.new_window(window_name="test_window")
+    window.select_layout()
