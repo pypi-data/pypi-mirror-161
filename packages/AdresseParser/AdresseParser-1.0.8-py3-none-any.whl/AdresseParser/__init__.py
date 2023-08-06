75efd2cee119fafe5563e05e56c4ba91a806bbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:34 +0000

    Add package AdresseParser/1.0.8/AdresseParser-1.0.8-py3-none-any.whl

---
+++ packages/AdresseParser/AdresseParser-1.0.8-py3-none-any.whl/AdresseParser/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Ce module permet d'extraire d'une chaîne de caractères une adresse française et de retourner un dictionnaire
+    avec numéro de rue, rue, ville, code postal, département, région et pays.
+"""
+
+from .AdresseParser import AdresseParser
+
+__version__="1.0.8"
+__title__="AddresseParser"
