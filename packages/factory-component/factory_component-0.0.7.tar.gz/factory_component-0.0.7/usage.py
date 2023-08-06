974f3fe6125dd9fe2ace6d1f83f9b7c97de8f2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:21 +0000

    Add package factory-component/0.0.7/factory_component-0.0.7.tar.gz

---
+++ packages/factory-component/factory_component-0.0.7.tar.gz/factory_component-0.0.7/usage.py
@@ -0,0 +1,46 @@
+import factory_component
+from dash import Dash, html, Input, Output
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.Div(
+        id='div-position'
+    ),
+    html.Div(
+        id='div-size'
+    ),
+    factory_component.Rnd(
+        default={
+            'x':0,
+            'y':0,
+            'width':200,
+            'height':200,
+        },
+        resizeGrid=[50,50],
+        dragGrid=[50,50],
+        style={
+            'background':'red',
+        },
+        id='teste'
+    )
+])
+
+@app.callback(
+    Output('div-position','children'),
+    Input('teste','position'),
+)
+def update_position(position):
+    print(position)
+    return str(position)
+
+@app.callback(
+    Output('div-size','children'),
+    Input('teste','size'),
+)
+def update_position(size):
+    print(size)
+    return str(size)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
