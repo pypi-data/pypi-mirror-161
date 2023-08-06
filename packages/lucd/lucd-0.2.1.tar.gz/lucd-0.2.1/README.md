6defcc0cf11b0ef9a44a68292b06c0aa36c67f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:00 +0000

    Add package lucd/0.2.1/lucd-0.2.1.tar.gz

---
+++ packages/lucd/lucd-0.2.1.tar.gz/lucd-0.2.1/README.md
@@ -0,0 +1,114 @@
+
+# Create Local Undetectable Chrome Driver (LUCD)
+
+
+This program will download and patch a Chrome driver to make it undetectable. So that you can use an undetectable Chrome driver for your Python Selenium code.
+
+
+## Passed the antibot test [](https://bot.sannysoft.com)
+
+
+# ![](https://github.com/SelmiAbderrahim/CreateUndetectableChromeDriver/blob/master/lucd/screenshots/antibot-tested.png?raw=true)
+
+
+# Features
+
+- Download the exact chrome driver based on your OS and installed chrome version.
+- Remove browser control flag
+- Remove signature in javascript
+- Set User-Agent
+- Start maximum resolution
+- Open Chrome Instance on debugging mode
+- Control an existing Chrome instance.
+- Unmute the sounds of the browser.
+- Save/ load Chrome profiles.
+- Run Chrome driver on headless mode.
+
+<br><br>
+# Installation
+
+```
+pip install lucd 
+```
+
+or 
+
+```
+pip install git+https://github.com/SelmiAbderrahim/CreateUndetectableChromeDriver/blob/master/lucd/screenshots/antibot-tested.png
+```
+<br><br>
+# Example
+
+```
+from lucd.driver import Driver
+driver = Driver()
+chrome = driver.create()
+```
+<br><br>
+# Usage
+
+## Create an undetectable Chrome driver
+
+```
+from lucd.driver import Driver
+driver = Driver()
+
+# headless browser
+driver.headless = True
+
+# Mute the browser
+driver.mute = True
+
+# Set the profile path
+driver.profile = r"C:\\path\\profile"
+
+# Change useragent
+driver.useragent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36"
+
+chrome = driver.create()
+
+```
+
+## Control/open an existing Chrome driver
+
+```
+from lucd.driver import Remote
+driver = Remote()
+
+# Open Chrome on debugging mode and control it
+chrome = driver.create()
+
+# Control an existing Chrome driver
+chrome = driver.create(control_existing_instance, port = 9222)
+```
+
+<br><br>
+
+# Tests
+
+
+**Clone the project**
+
+```
+git clone https://github.com/SelmiAbderrahim/CreateUndetectableChromeDriver/blob/master/lucd/screenshots/antibot-tested.png
+```
+
+**Create virtual environment**
+
+```
+pip install virtualenv
+virtualenv env
+source env/bin/activate
+```
+
+**Install requirements**
+
+```
+pip install -r requirements.txt
+```
+
+**Run tests**
+
+```
+python -m pytest lucd/tests/
+```
---
+++ packages/lucd/lucd-0.2.1.tar.gz/lucd-0.2.1/lucd/download.py
@@ -0,0 +1,164 @@
+import shutil
+import requests
+import os
+import platform
+import subprocess
+import re
+import zipfile
+from random import choices
+from pathlib import Path
+from bs4 import BeautifulSoup
+from termcolor import colored
+from colorama import init
+from .utils import Util
+
+
+OSNAME = platform.system()
+init()
+DRIVER = Path(__file__).resolve().absolute().parent / "driver"
+
+
+class Download(Util):
+    """
+    It will get the installed Chrome driver and based on the operating system
+    it will download the compatible chromedriver.
+    """
+    if OSNAME == "Windows":
+        system = "win"
+    elif OSNAME == "Linux":
+        system = "linux"
+    else:
+        system = "mac"
+
+    def update_binary(self, path: Path):
+        word = "cdc_".encode()
+        new = (("".join(
+            choices(['a', 'b', 'c', 'd', 'f', 'g'], k=3))
+        ) + "_").encode()
+        while True:
+            string = b""
+            Flag = 0
+            with open(path, 'r+b') as file:
+                pos = 0
+                data = string = file.read(1)
+                while data:
+                    data = file.read(1)
+                    if data == b" ":
+                        if word in string:
+                            new_tring = string.decode().replace(word.decode(), new.decode())
+                            file.seek(pos)
+                            file.write(new_tring.encode())
+                            Flag = 1
+                            break
+                        else:
+                            pos = file.tell()
+                            data = string = file.read(1)
+                    else:
+                        string += data
+                        continue
+            if not Flag:
+                break
+
+    def remove_signature_in_javascript(self, path: Path):
+        if OSNAME == "Windows":
+            os.chmod(path, 755)
+        else:
+            subprocess.Popen(f"sudo chmod 755 {path}", stdout=subprocess.PIPE, shell=True)
+        self.update_binary(path)
+
+    def check_installed_chrome_version(self):
+        try:
+            if OSNAME == "Windows":
+                cmd_version_output = (
+                    subprocess.Popen(
+                        'reg query "HKEY_CURRENT_USER\Software\Google\Chrome\BLBeacon" /v version',
+                        shell=True,
+                        stdout=subprocess.PIPE,
+                    )
+                    .stdout.read()
+                    .decode("utf-8")
+                )
+            elif OSNAME == "Linux":
+                cmd_version_output = (
+                    subprocess.Popen(
+                        "google-chrome --version", shell=True, stdout=subprocess.PIPE
+                    )
+                    .stdout.read()
+                    .decode("utf-8")
+                )
+            elif OSNAME == "Darwin":
+                cmd_version_output = (
+                    subprocess.Popen(
+                        "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version",
+                        shell=True,
+                        stdout=subprocess.PIPE,
+                    )
+                    .stdout.read()
+                    .decode("utf-8")
+                )
+        except Exception as error:
+            print(
+                colored("X [Error]", "red")\
+                + " We couldn't find the version of the installed chrome browser."
+            )
+            print(f"--> {error}")
+            return None
+        else:
+            installed_chrome_version = re.findall(
+                r"([\d]+\.[\d]+\.[\d]+\.[\d]+)", cmd_version_output
+            )[0].split(".")[0]
+            print(
+                colored("[+]", "green")\
+                + " You have Chrome version "\
+                + colored(installed_chrome_version, "blue")\
+                + " installed."
+            )
+            self.update_chrome_driver_config(version=installed_chrome_version)
+            return installed_chrome_version
+
+    def get_chrome_driver_download_link(self, version):
+        base_url = "https://chromedriver.storage.googleapis.com"
+        headers = {
+            "user-agent": (
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36"
+                " (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
+            )
+        }
+        response = requests.get(base_url, headers=headers)
+        content = response.content.decode("utf-8")
+        soup = BeautifulSoup(content, features="xml")
+        keys = [key.text for key in soup.find_all("Key")]
+        try:
+            return [key for key in keys if self.system in key and version in key][0]
+        except IndexError:
+            return None
+
+    def download_chrome_driver(self, chrome_driver_file):
+        base_url = "https://chromedriver.storage.googleapis.com"
+        headers = {
+            "user-agent": (
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36"
+                " (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
+            )
+        }
+        download_link = base_url + "/" + chrome_driver_file
+        chrome_file_name = chrome_driver_file.split("/")[1]
+        with requests.get(download_link, stream=True, headers=headers) as r:
+            print("downloading " + colored(f"{chrome_file_name}", "blue") + " ...")
+            with open(os.path.join(DRIVER, chrome_file_name), "wb") as f:
+                shutil.copyfileobj(r.raw, f)
+        return chrome_driver_file
+
+    def extract_chrome_driver_zip(self, chrome_driver_file):
+        global filename
+        chrome_file_name = chrome_driver_file.split("/")[1]
+        path = os.path.join(DRIVER, chrome_file_name)
+        with zipfile.ZipFile(path, "r") as zip_ref:
+            zip_ref.extractall(DRIVER)
+            filename = zip_ref.namelist()[0]
+        os.remove(path)
+        print(colored("[+]", "green") + " Chrome driver has been installed.")
+        chromedriver_path = os.path.join(DRIVER, filename)
+        self.update_chrome_driver_config(chromedriver=chromedriver_path)
+        self.remove_signature_in_javascript(chromedriver_path)
+        return filename
