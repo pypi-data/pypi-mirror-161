8f23effa6c52875d1c2881909a152931e15e7604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:53 +0000

    Add package alphaDigger/0.0.1/alphaDigger-0.0.1.tar.gz

---
+++ packages/alphaDigger/alphaDigger-0.0.1.tar.gz/alphaDigger-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name = "alphaDigger",
+	version = "0.0.1",
+	author = "Xi",
+	description = "common A Share alpha and risk factors",
+	packages = ["alphaSeeker"]
+	)
