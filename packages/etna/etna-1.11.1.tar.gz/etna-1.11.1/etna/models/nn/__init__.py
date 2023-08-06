0a2c7e0d48827132b5d8a90be7d8f3bfc0ad7470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:33 +0000

    Add package etna/1.11.1/etna-1.11.1.tar.gz

---
+++ packages/etna/etna-1.11.1.tar.gz/etna-1.11.1/etna/models/nn/__init__.py
@@ -0,0 +1,6 @@
+from etna import SETTINGS
+
+if SETTINGS.torch_required:
+    from etna.models.nn.deepar import DeepARModel
+    from etna.models.nn.rnn import RNNModel
+    from etna.models.nn.tft import TFTModel
