5d1ae3edc4e0f1d46b167c137feb13ad9f5f26ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:41 +0000

    Add package term-image/0.4.1/term-image-0.4.1.tar.gz

---
+++ packages/term-image/term-image-0.4.1.tar.gz/term-image-0.4.1/term_image/exit_codes.py
@@ -0,0 +1,14 @@
+"""Exit codes for fatal errors"""
+
+from __future__ import annotations
+
+codes = {
+    0: "SUCCESS",
+    1: "FAILURE",
+    2: "INVALID_ARG",
+    3: "INTERRUPTED",
+    4: "CONFIG_ERROR",
+    5: "NO_VALID_SOURCE",
+}
+
+globals().update(((v, k) for k, v in codes.items()))
