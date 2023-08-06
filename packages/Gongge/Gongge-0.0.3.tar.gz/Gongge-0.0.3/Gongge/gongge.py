4714c1580479722c78c7127e543d7a165e25c2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:04 +0000

    Add package Gongge/0.0.3/Gongge-0.0.3.tar.gz

---
+++ packages/Gongge/Gongge-0.0.3.tar.gz/Gongge-0.0.3/Gongge/gongge.py
@@ -0,0 +1,47 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+def split(img_path, size=3):
+    # 读取图片
+    im = Image.open(img_path)
+    # 宽高各除 3，获取裁剪后的单张图片大小
+    width, height = im.size[0] // size, im.size[1] // size
+    # 裁剪图片的左上角坐标
+    start_x, start_y = 0, 0
+    # 用于给图片命名
+    number = 1
+    img_name = img_path.split(".")[0]+f"-{size*size}"
+    # 创建目录
+    if not os.path.exists(img_name):
+        os.makedirs(img_name)
+    # 循环裁剪图片
+    for i in range(size):
+        for n in range(size):
+            # 裁剪图片并保存
+            crop = im.crop((start_x, start_y, start_x + width, start_y + height))
+            crop.save(os.path.join(img_name, f"{number}.jpg"))
+            # 将左上角坐标的 x 轴向右移动
+            start_x += width
+            number += 1
+        # 当第一行裁剪完后 x 继续从 0 开始裁剪
+        start_x = 0
+        # 裁剪第二行
+        start_y += height
+    """预览图"""
+    np_img = np.array(im)
+    np_img_height, np_img_width, np_size = np_img.shape
+    h_stage, h_init = int(np_img_height / size), int(np_img_height / size)
+    w_stage, w_init = int(np_img_width / size), int(np_img_width / size)
+    for i in range(size):
+        np_img[h_stage:h_stage + 10, :, :] = 255
+        np_img[:, w_stage:w_stage + 10, :] = 255
+        h_stage += h_init
+        w_stage += w_init
+    file = Image.fromarray(np_img)
+    file.save(os.path.join(img_name, f"{img_name}.jpg"))
+    plt.imshow(np_img)
+    plt.show()
+
---
+++ packages/Gongge/Gongge-0.0.3.tar.gz/Gongge-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os import path
+
+# 读取readme文件，这样可以直接显示在主页上
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Gongge',
+    version='0.0.3',
+    keywords='img',
+    description='九宫格图片分割，最简单的Python图像分割工具。',
+    license='MIT License',
+    url='https://github.com/xuehang00126',
+    author='学航',
+    author_email='30290382@qq.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'Pillow~=9.2.0',
+        'numpy~=1.23.1',
+        'matplotlib~=3.5.2'
+    ],
+)
