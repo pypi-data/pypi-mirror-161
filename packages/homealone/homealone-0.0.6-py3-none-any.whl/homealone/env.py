2e5820393351785342a5cba4ba13ea9e31e719f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:39 +0000

    Add package homealone/0.0.6/homealone-0.0.6-py3-none-any.whl

---
+++ packages/homealone/homealone-0.0.6-py3-none-any.whl/homealone/env.py
@@ -0,0 +1,25 @@
+import os
+import socket
+
+# directory structure
+hostname = socket.gethostname()
+rootDir = os.path.expanduser("~")+"/"
+confDir = rootDir+"conf/"
+keyDir = rootDir+"keys/"
+stateDir = rootDir+"state/"
+soundDir = rootDir+"sounds/"
+dataLogDir = rootDir+"data/"
+dataLogFileName = ""
+
+# global variables that must be set here
+sysLogging = False
+debugEnable = False
+
+# Localization - define these in the config file
+latLong = (0.0, 0.0)
+elevation = 0 # elevation in feet
+tempScale = "F"
+localController = "localhost"
+
+# import local variables
+from conf import *
