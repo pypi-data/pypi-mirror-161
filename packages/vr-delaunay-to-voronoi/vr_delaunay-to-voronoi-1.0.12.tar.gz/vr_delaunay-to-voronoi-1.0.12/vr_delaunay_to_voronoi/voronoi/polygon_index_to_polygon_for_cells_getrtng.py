3a0113f84c886dd322e1d2332123d72fdb3b4e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:02 +0000

    Add package vr-delaunay-to-voronoi/1.0.12/vr_delaunay-to-voronoi-1.0.12.tar.gz

---
+++ packages/vr-delaunay-to-voronoi/vr_delaunay-to-voronoi-1.0.12.tar.gz/vr_delaunay-to-voronoi-1.0.12/vr_delaunay_to_voronoi/voronoi/polygon_index_to_polygon_for_cells_getrtng.py
@@ -0,0 +1,25 @@
+from typing import DefaultDict, Any, List, Tuple, Dict
+
+from vr_delaunay_to_voronoi.voronoi.ordered_edges_by_edge_indices_getting \
+    import \
+    get_ordered_edges_by_edge_indices
+
+
+def get_polygon_index_to_polygon_for_cells(
+    *,
+    cells: DefaultDict[Any, List[Tuple]],
+) -> Dict[int, List[int]]:
+    """
+    Create polygons by storing vertex indices of a cell in order.
+
+    The order can either be clockwise or counter-clockwise.
+    """
+    polygons: Dict[int, List[int]] = {}
+    for polygon_index, edge_indices in cells.items():
+        ordered_edges = \
+            get_ordered_edges_by_edge_indices(edge_indices=edge_indices)
+        polygons[polygon_index] = [
+            edge_start for (edge_start, edge_end) in ordered_edges
+        ]
+
+    return polygons
