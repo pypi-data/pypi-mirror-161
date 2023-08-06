729b19f1f91d64dc01c9a62e212fdf33e13ff172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:55 +0000

    Add package ylearn/0.1.3/ylearn-0.1.3-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/ylearn/ylearn-0.1.3-cp38-cp38-macosx_10_15_x86_64.whl/ylearn/utils/__init__.py
@@ -0,0 +1,14 @@
+import sys as sys_
+
+is_os_windows = sys_.platform.find('win') == 0
+is_os_darwin = sys_.platform.find('darwin') == 0
+is_os_linux = sys_.platform.find('linux') == 0
+
+try:
+    from packaging.version import Version
+except ModuleNotFoundError:
+    from distutils.version import LooseVersion as Version
+
+from ._common import const, set_random_state, infer_task_type, unique, to_repr
+from ._common import to_df, context, is_notebook, view_pydot
+from ._common import to_snake_case, to_camel_case, drop_none
