b2e3daa6d6067600d16834ab00716c58c1b2670a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:39 +0000

    Add package kisters.network-store.service/1.3.1/kisters.network_store.service-1.3.1-py3-none-any.whl

---
+++ packages/kisters.network-store.service/kisters.network_store.service-1.3.1-py3-none-any.whl/kisters/network_store/service/padp/asgi/settings.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings
+
+
+class NetworkStoreAPIEnvironmentVariables(BaseSettings):
+    class Config:
+        env_file = "kisters_water.env"
+
+    api_path: str = "/network-store"
+    deployment_url: str = ""
+    enable_access_control: bool = False
+    enable_static_assets: bool = False
+    enable_viewer: bool = True
