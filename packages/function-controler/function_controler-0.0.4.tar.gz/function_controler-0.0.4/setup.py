d5264403334882992ab996ee4fd5dd95d4f5b54e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:03 +0000

    Add package function-controler/0.0.4/function_controler-0.0.4.tar.gz

---
+++ packages/function-controler/function_controler-0.0.4.tar.gz/function_controler-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import io
+
+setup(
+    name = "function_controler",     
+    version = "0.0.4", 
+    keywords = ["pip", "function_controler","running time","control"],			
+    description = "Control the maximum running time of a function.",	
+    long_description=io.open("README.md", "r", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    license = "MIT Licence",		
+
+    url = "https://github.com/dongshuyan/function_controler", 
+    author = "sauterne",			
+    author_email = "ssauterne@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["loguru"] 
+)
