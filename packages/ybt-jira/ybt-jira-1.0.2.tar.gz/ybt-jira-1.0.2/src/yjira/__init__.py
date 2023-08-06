07a9ea1a1eb89261ee77446515907a04fe54a4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:37 +0000

    Add package ybt-jira/1.0.2/ybt-jira-1.0.2.tar.gz

---
+++ packages/ybt-jira/ybt-jira-1.0.2.tar.gz/ybt-jira-1.0.2/src/yjira/__init__.py
@@ -0,0 +1,11 @@
+import os
+import tomli
+
+# Version package
+__version__ = "1.0.2"
+
+# Read URL and TOKEN from config file
+with open(f"{os.path.expanduser('~')}/.yjira/config.toml", "rb") as f:
+    _cfg = tomli.load(f)
+    URL = _cfg["yjira"]["url"]
+    TOKEN = _cfg["yjira"]["token"]
