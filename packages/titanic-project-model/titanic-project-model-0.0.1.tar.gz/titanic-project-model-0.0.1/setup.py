72d54e544880e442f9311d8cb4decc02696a8e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:59 +0000

    Add package titanic-project-model/0.0.1/titanic-project-model-0.0.1.tar.gz

---
+++ packages/titanic-project-model/titanic-project-model-0.0.1.tar.gz/titanic-project-model-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+
+# Where the magic happens:
+setup(
+    name='titanic-project-model',
+    version='0.0.1',
+    description="Example regression model package from Train In Data.",
+    author='Liliya Kazykhanova',
+    author_email='kazychanowa.liliya@gmail.com',
+    python_requires=">=3.8.0",
+    url='https://github.com/LiliyaKazykhanova/titanic-project',
+)
