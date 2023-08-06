6c0dbeaabc21df987acd9db4b010624880cd57c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:05 +0000

    Add package jello/1.5.4/jello-1.5.4.tar.gz

---
+++ packages/jello/jello-1.5.4.tar.gz/jello-1.5.4/jello/__init__.py
@@ -0,0 +1,8 @@
+"""jello - query JSON at the command line with python syntax"""
+
+
+__version__ = '1.5.4'
+AUTHOR = 'Kelly Brazil'
+WEBSITE = 'https://github.com/kellyjonbrazil/jello'
+COPYRIGHT = '© 2020-2022 Kelly Brazil'
+LICENSE = 'MIT License'
