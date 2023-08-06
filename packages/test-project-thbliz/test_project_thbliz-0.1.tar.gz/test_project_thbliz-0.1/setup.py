f78d0492ae435df61a058f34c2d3c7843e9f9696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:32 +0000

    Add package test-project-thbliz/0.1/test_project_thbliz-0.1.tar.gz

---
+++ packages/test-project-thbliz/test_project_thbliz-0.1.tar.gz/test_project_thbliz-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='test_project_thbliz',
+    version='0.1',
+    license='MIT',
+    author="Preetham Rakshith",
+    author_email='preethamrakshith11@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords='example project',
+    install_requires=[],
+
+)
+
---
+++ packages/test-project-thbliz/test_project_thbliz-0.1.tar.gz/test_project_thbliz-0.1/src/test_project/test.py
@@ -0,0 +1,3 @@
+
+
+print("MODULE SUCCSSFULLY IMPORTED !")
