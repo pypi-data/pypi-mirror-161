8b77c78f0e0a237df4ce449ec2bf95d15f82e069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:28 +0000

    Add package VBPatcher/0.8.0/VBPatcher-0.8.0-py3-none-any.whl

---
+++ packages/VBPatcher/VBPatcher-0.8.0-py3-none-any.whl/VBPatcher/main.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import sys
+from os import chdir
+from os.path import dirname
+from time import sleep
+from typing import NoReturn
+
+from PyLoadBar import PyLoadBar
+
+sys.path.insert(0, dirname(
+    dirname(__file__)))  # Ensure main module can be found by Python.
+chdir(dirname(__file__))  # Change cwd to main module directory.
+
+import VBPatcher.appglobals.appglobals
+from VBPatcher.applogger.applogger import logger, logger_stream
+from VBPatcher.downloader.downloader import _Downloader
+from VBPatcher.patching.patching import _Patcher
+from VBPatcher.subprocessing.subprocessing import _exitPatcher, _openValheim
+from VBPatcher.validation.validation import _Validate
+
+DL = _Downloader()  # Initialize Downloader.
+Patcher = _Patcher()  # Initialize Patcher.
+Validations = _Validate()  # Initialize Validations.
+cancel = PyLoadBar(False)  # Initialize text-based loading sequence.
+
+#$ ====================================================================================================== $#
+
+
+def main() -> None | NoReturn:
+    """Program entry point.
+
+    ---
+
+    :return: start VBPatcher.
+    :rtype: None | :class:`NoReturn`
+    """
+
+    logger.info(
+        f'Welcome to the Valheim Bepinex Patcher v{VBPatcher.appglobals.appglobals.__version__}!\n>> Session Start: {VBPatcher.appglobals.appglobals._datefmt}\n\n'
+    )
+
+    Validations._start_checks()  # Ensure presence of patch files.
+
+    while True:
+        logger.info('Display user menu...\n')
+        choosePatch: str = input(
+            f"Welcome to the Valheim Bepinex Patcher v{VBPatcher.appglobals.appglobals.__version__}!\n\nPlease Choose an Option by Entering its Corresponding Number:\n\n{VBPatcher.appglobals.appglobals._textborder}\n>> [1] Patch BepInEx to latest stable release: {VBPatcher.appglobals.appglobals.b_stable}\n>> [2] Patch BepInEx to latest development/bleeding-edge build: {VBPatcher.appglobals.appglobals.b_dev}\n>> [3] Apply both patches to BepInEx in chronological order of release ({VBPatcher.appglobals.appglobals.b_stable} then {VBPatcher.appglobals.appglobals.b_dev})\n>> [4] Check for/update to newest patch versions\n>> [5] Open Valheim\n>> [6] Exit Program\n\n> "
+        )
+
+        if choosePatch == '1':  # Patch BepInEx to latest stable release.
+            logger.info(
+                'Chose option [1] to patch BepInEx to latest stable release...'
+            )
+            Patcher._patch_stable()
+
+        elif choosePatch == '2':  # Patch BepInEx to latest development/bleeding-edge build.
+            logger.info(
+                'Chose option [2] to patch BepInEx to latest dev-build...')
+            Patcher._patch_dev()
+
+        elif choosePatch == '3':  # Apply both patches to BepInEx in chronological order of release.
+            logger.info(
+                'Chose option [3] to install both stable and development patch builds in order of release...'
+            )
+            Patcher._patch_full()
+
+        elif choosePatch == '4':  # Check for/update to newest patch versions.
+            logger.info('Chose option [4] to check for new patch updates...')
+            DL.UpdatePatcher()
+
+        elif choosePatch == '5':  # Open Valheim.
+            logger.info('Chose option [5] to start Valheim...')
+            _openValheim()
+
+        elif choosePatch == '6':  # Exit Program.
+            logger.info('Chose option [6] to close patcher...')
+            logger.info(
+                'Completed VBPatcher processes...\n>> Preparing to exit...\n')
+            cancel.start('Completed VBPatcher processes',
+                         'Exiting...',
+                         iter_total=5,
+                         txt_seq_speed=0.25)
+            return _exitPatcher()
+
+        else:  # Invalid input.
+            logger_stream.warning(
+                f'Invalid Input -\n\n>> Your Entry:  "{choosePatch}".\n\n>> Must ONLY enter:\n>> [1] for stable release {VBPatcher.appglobals.appglobals.b_stable}\n>> [2] for development build {VBPatcher.appglobals.appglobals.b_dev}\n>> [3] for FULL upgrade (apply both patches in order of release)\n>> [4] to update available patch versions/builds\n>> [5] to open Valheim\n>> [6] to exit program\n\n'
+            )
+            sleep(1.5)
+
+    return _exitPatcher()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/VBPatcher/VBPatcher-0.8.0-py3-none-any.whl/VBPatcher/validation/validation.py
@@ -0,0 +1,219 @@
+import os
+from zipfile import is_zipfile
+
+import VBPatcher.appglobals.appglobals
+import VBPatcher.downloader.downloader
+from PyLoadBar import PyLoadBar
+from VBPatcher.applogger.applogger import logger, logger_stream
+from VBPatcher.subprocessing.subprocessing import _exitPatcher
+
+DL = VBPatcher.downloader.downloader._Downloader()
+bar = PyLoadBar(False)
+
+
+class _Validate:
+    """Validate location and deployability of necessary patch files.
+
+    ---
+
+    - Contains the following validation methods:
+
+        - :func:`_validate_stable(url) -> bool`
+            - Validate presence of BepInEx stable release patch files.
+            - Static method.
+
+        - :func:`_validate_dev(url) -> bool:`
+            - Validate presence of BepInEx dev build patch files.
+            - Static method.
+
+        - :func:`_start_checks(self) -> None`
+            - Start patch file validation checks.
+    """
+
+    @staticmethod
+    def _validate_stable(url) -> bool:
+        """Validate presence of BepInEx stable release patch files.
+
+        ---
+
+        :param url: url to download BepInEx stable release from if not found.
+        :type url: :class:`Any`
+        :return: validation of patch files.
+        :rtype: :class:`bool`
+        """
+
+        logger.info(
+            f'Validating BepInEx stable-build {VBPatcher.appglobals.appglobals.b_stable} patch...'
+        )
+
+        patch_contents: list = [
+            ['.gitkeep', 'changelog.txt', 'winhttp.dll'], [],
+            [
+                '0Harmony.dll', '0Harmony.xml', '0Harmony20.dll',
+                'BepInEx.dll', 'BepInEx.Harmony.dll', 'BepInEx.Harmony.xml',
+                'BepInEx.Preloader.dll', 'BepInEx.Preloader.xml',
+                'BepInEx.xml', 'HarmonyXInterop.dll', 'Mono.Cecil.dll',
+                'Mono.Cecil.Mdb.dll', 'Mono.Cecil.Pdb.dll',
+                'Mono.Cecil.Rocks.dll', 'MonoMod.RuntimeDetour.dll',
+                'MonoMod.RuntimeDetour.xml', 'MonoMod.Utils.dll',
+                'MonoMod.Utils.xml'
+            ]
+        ]  # List of files to verify
+
+        stable_match: bool = False  # Initialize match flag
+
+        verified: list = []  # Initialize verified file list
+
+        try:
+            verified.extend(
+                file
+                for (root, dirs,
+                     file) in os.walk('./patch-files/stable', topdown=True
+                                      ))  # Get list of files in patch folder
+
+            if verified == patch_contents:
+                stable_match = True
+                logger.info(
+                    f'BepInEx stable-build {VBPatcher.appglobals.appglobals.b_stable} patch ready for deployment!\n'
+                )
+
+            else:
+                logger.info(
+                    f'Unable to locate BepInEx stable-build {VBPatcher.appglobals.appglobals.b_stable} patch...\n>> Attempting to download...'
+                )
+                DL._dl_stable(url)  # Download *.zip archive from url
+
+                if is_zipfile(
+                        f'./patch-files/stable/BepInEx_stable_{VBPatcher.appglobals.appglobals.b_stable}.zip'
+                ):  # Check if downloaded archive is a zip file
+                    DL._unzip_patch(  # Unzip archive
+                        f'./patch-files/stable/BepInEx_stable_{VBPatcher.appglobals.appglobals.b_stable}.zip',
+                        1)
+                    stable_match = True  # Update match flag
+                    logger.info(
+                        f'Download successful!\n>> BepInEx stable-build {VBPatcher.appglobals.appglobals.b_stable} patch ready for deployment!\n'
+                    )
+
+                else:
+                    stable_match = False  # Update match flag
+                    logger.error(
+                        'Download failed!\n>> BepInEx dev-build patch unable to be deployed!\n'
+                    )
+
+        except Exception as err:
+            stable_match = False
+            logger.error(
+                f'Encountered error during BepInEx stable-build {VBPatcher.appglobals.appglobals.b_stable} patch validation...\n>> Exception:\n{err}\n'
+            )
+
+        finally:
+            return stable_match  # Return result of validation
+
+    @staticmethod
+    def _validate_dev(url) -> bool:
+        """Validate presence of BepInEx development build patch files.
+
+        ---
+
+        :param url: url to download BepInEx development build from if not found.
+        :type url: :class:`PathLike` | :class:`str`
+        :return: validation of patch files.
+        :rtype: :class:`bool`
+        """
+
+        logger.info(
+            f'Validating BepInEx dev-build {VBPatcher.appglobals.appglobals.b_dev} patch...'
+        )
+
+        patch_contents: list = [
+            ['.gitkeep', 'changelog.txt', 'winhttp.dll'], [],
+            [
+                '0Harmony.dll', 'BepInEx.Core.dll', 'BepInEx.Core.xml',
+                'BepInEx.Preloader.Core.dll', 'BepInEx.Preloader.Core.xml',
+                'BepInEx.Preloader.Unity.dll', 'BepInEx.Preloader.Unity.xml',
+                'BepInEx.Unity.dll', 'BepInEx.Unity.xml', 'Mono.Cecil.dll',
+                'Mono.Cecil.Mdb.dll', 'Mono.Cecil.Pdb.dll',
+                'Mono.Cecil.Rocks.dll', 'MonoMod.RuntimeDetour.dll',
+                'MonoMod.Utils.dll', 'SemanticVersioning.dll'
+            ]
+        ]  # List of files to verify
+
+        dev_match: bool = False  # Initialize match flag
+
+        verified: list = []  # Initialize verified file list
+
+        try:
+            verified.extend(file for (
+                root, dirs,
+                file) in os.walk('./patch-files/development/', topdown=True)
+                            )  # Get list of files in patch folder
+
+            if verified == patch_contents:
+                dev_match = True  # Update match flag
+                logger.info(
+                    f'BepInEx dev-build {VBPatcher.appglobals.appglobals.b_dev} patch ready for deployment!\n'
+                )
+
+            else:
+                logger.info(
+                    f'Unable to locate BepInEx dev-build {VBPatcher.appglobals.appglobals.b_dev} patch...\n>> Attempting to download...'
+                )
+                DL._dl_dev(url)  # Download *.zip file from url
+
+                if is_zipfile(  # Check if downloaded archive is a zip file
+                        f'./patch-files/development/BepInEx_dev_{VBPatcher.appglobals.appglobals.b_dev}.zip'
+                ):
+                    DL._unzip_patch(  # Unzip archive
+                        f'./patch-files/development/BepInEx_dev_{VBPatcher.appglobals.appglobals.b_dev}.zip',
+                        2)
+
+                    dev_match = True  # Update match flag
+                    logger.info(
+                        f'Download successful!\n>> BepInEx dev-build {VBPatcher.appglobals.appglobals.b_dev} patch ready for deployment!\n'
+                    )
+
+                else:
+                    dev_match = False  # Update match flag
+                    logger.error(
+                        'Download failed!\n>> BepInEx dev-build patch unable to be deployed!\n'
+                    )
+
+        except Exception as err:
+            dev_match = False
+            logger.error(
+                f'Encountered error during BepInEx dev-build {VBPatcher.appglobals.appglobals.b_dev} patch validation...\n>> Exception:\n{err}\n'
+            )
+
+        finally:
+            return dev_match  # Return result of validation
+
+    def _start_checks(self) -> None:
+        """Verify necessary patcher components upon start.
+
+        - If any patch files are missing, attempt to download them.
+
+        ---
+
+        :return: continue to application if verification is successful, otherwise exits program.
+        :rtype: None
+        """
+
+        logger_stream.info('Initializing VBPatcher start checks...\n')
+
+        if self._validate_stable(VBPatcher.appglobals.appglobals.url_stable
+                                 ) and self._validate_dev(
+                                     VBPatcher.appglobals.appglobals.url_dev
+                                 ):  # Validate presence of patch files
+            logger_stream.info(
+                'VBPatcher start checks completed successfully!\n')
+
+        else:
+            logger.error(
+                'VBPatcher start checks failed!\n>> One or more patch files were not able to be verified...\n'
+            )
+            bar.start(
+                'ERROR: One or more patch files were not able to be verified and cannot be deployed',
+                'Exiting Patcher',
+                iter_total=3,
+                txt_seq_speed=0.25)
+            return _exitPatcher()  # Exit program
