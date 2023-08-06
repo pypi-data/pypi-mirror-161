85f1e5917fc6543fd8f500d1a6c252060d5670b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:00 +0000

    Add package jsl-tmp/4.0.0/jsl_tmp-4.0.0-py3-none-any.whl

---
+++ packages/jsl-tmp/jsl_tmp-4.0.0-py3-none-any.whl/johnsnowlabs/ocr.py
@@ -0,0 +1,8 @@
+from johnsnowlabs.abstract_base.lib_resolver import try_import_lib
+
+if try_import_lib('sparkocr', True) and  try_import_lib('sparknlp', True):
+    from sparkocr.transformers import *
+    from sparkocr.enums import *
+    import sparkocr
+else:
+    pass
