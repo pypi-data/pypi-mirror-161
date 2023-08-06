b43a0fb99e1cbc8540fff57aef4b9eb96985a8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:59 +0000

    Add package TikTokUsersPy/0.0.4/TikTokUsersPy-0.0.4-py3-none-any.whl

---
+++ packages/TikTokUsersPy/TikTokUsersPy-0.0.4-py3-none-any.whl/tiktokuserspy/__init__.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from main import TikTokScraper
+from main import User
+from main import Video
+"""
+TikTokUsersPy
+"""
+
+__version__ = "0.0.4"
+__author__ = 'Jason Bodzy'
---
+++ packages/TikTokUsersPy/TikTokUsersPy-0.0.4-py3-none-any.whl/tiktokuserspy/config.py
@@ -0,0 +1,15 @@
+# Presets from tiktok
+account_followers_xpath = "//*[@id=\"app\"]/div[2]/div[2]/div/div[1]/h2[1]/div[2]/strong"
+total_likes_xpath = "//*[@id=\"app\"]/div[2]/div[2]/div/div[1]/h2[1]/div[3]/strong"
+account_bio_xpath = "//*[@id=\"app\"]/div[2]/div[2]/div/div[1]/h2[2]"
+
+recent_video_xpath = "//*[@id=\"app\"]/div[2]/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/a"
+arrow_xpath = "//*[@id=\"app\"]/div[2]/div[3]/div[1]/button[3]"
+video_bio_xpath = "//*[@id=\"app\"]/div[2]/div[3]/div[2]/div[2]/div[1]"
+comments_xpath = "//*[@id=\"app\"]/div[2]/div[3]/div[2]/div[2]/div[2]/div[1]/div[1]/button[2]/strong"
+date_xpath = "//*[@id=\"app\"]/div[2]/div[3]/div[2]/div[1]/a[2]/span[2]/span[2]"
+likes_xpath = "//*[@id=\"app\"]/div[2]/div[3]/div[2]/div[2]/div[2]/div[1]/div[1]/button[1]/strong"
+
+xbutton_xpath = "//*[@id=\"app\"]/div[2]/div[3]/div[1]/button[1]"
+video_div_xpath = "//*[@id=\"app\"]/div[2]/div[2]/div/div[2]/div[2]"
+recent_views_1_xpath = "//*[@id=\"app\"]/div[2]/div[2]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/a/div/div[2]/strong"
