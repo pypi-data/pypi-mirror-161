e55f7b6e80a691b5aad8674db54058f9b498266a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:18 +0000

    Add package lodash-py/0.0.2/lodash-py-0.0.2.tar.gz

---
+++ packages/lodash-py/lodash-py-0.0.2.tar.gz/lodash-py-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lodash-py",
+    version="0.0.2",
+    author="Huang An Sheng",
+    author_email="andy19910102@gmail.com",
+    description="Lodash methods in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andy19910102/lodash.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[]
+)
