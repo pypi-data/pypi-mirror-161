7c6b6225e78da2f3e874e2daa86feff18b734e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:18 +0000

    Add package np-data-validator-2/0.1.3/np_data_validator_2-0.1.3-py3-none-any.whl

---
+++ packages/np-data-validator-2/np_data_validator_2-0.1.3-py3-none-any.whl/np_data_validator_2/validation_plan/resolvers.py
@@ -0,0 +1,17 @@
+from typing import Any, Optional, Tuple
+
+import glob
+import logging
+import os
+
+from ..dataclasses import ProcessorResult, Validator
+
+logger = logging.getLogger(__name__)
+
+
+def resolve_local_filepath(glob_pattern: str, local_dir: str) -> str:
+    resolved = glob.glob(os.path.join(local_dir, glob_pattern))
+    if len(resolved) > 1:
+        # logger.info("Multiple files globbed: %s" % resolved)  # todo add logging
+        raise Exception("Multiple files globbed: %s" % resolved)
+    return resolved[0]
