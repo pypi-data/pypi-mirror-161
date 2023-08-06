cde4d39a20a7f71778cd7616c9e3126c5b944a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:59 +0000

    Add package tilda/0.0.0/tilda-0.0.0.tar.gz

---
+++ packages/tilda/tilda-0.0.0.tar.gz/tilda-0.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="tilda",
+    version="0.0.0",
+    author="rdbende",
+    author_email="rdbende@gmail.com",
+    url="https://github.com/tukaan",
+    license="MIT license",
+    python_requires=">=3.7",
+    install_requires=["tukaan"],
+    description="An official audio processing and synth module for Tukaan",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Tcl",
+        "Programming Language :: C",
+        "Programming Language :: C++",
+        "Typing :: Typed",
+    ],
+)
