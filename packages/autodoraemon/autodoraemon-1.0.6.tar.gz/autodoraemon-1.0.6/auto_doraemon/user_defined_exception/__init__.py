3cea45b129bf40dcc77ffb29781ace42b1005581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:40 +0000

    Add package autodoraemon/1.0.6/autodoraemon-1.0.6.tar.gz

---
+++ packages/autodoraemon/autodoraemon-1.0.6.tar.gz/autodoraemon-1.0.6/auto_doraemon/user_defined_exception/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@author:zhaojiajun
+@file:__init__.py.py
+@time:2022/07/12
+"""
+
+__all__ = ['auto_exception']
+
+if __name__ == '__main__':
+    pass
---
+++ packages/autodoraemon/autodoraemon-1.0.6.tar.gz/autodoraemon-1.0.6/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+打包模块
+@author:zhaojiajun
+@file:setup.py
+@time:2022/07/11
+"""
+
+from setuptools import setup, find_packages
+
+__version__ = '1.0.6'
+__name__ = 'autodoraemon'
+
+require_packages = [
+    "setuptools==47.1.0",
+    "requests==2.25.1",
+    "xlrd==2.0.1",
+    "deepdiff==5.8.1"
+]
+
+setup(
+    name=__name__,
+    version=__version__,
+    description='自动化测试辅助测试项目，主要提供工具函数',
+    author='zhaojiajun',
+    author_email='zhaojiajun@baicizhan.com',
+    packages=find_packages(),
+    install_requires=require_packages
+)
