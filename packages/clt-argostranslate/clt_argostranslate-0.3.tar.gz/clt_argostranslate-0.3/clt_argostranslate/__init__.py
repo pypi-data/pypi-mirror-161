080bd2740978baf43bb455f379af184101b7698a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:57 +0000

    Add package clt-argostranslate/0.3/clt_argostranslate-0.3.tar.gz

---
+++ packages/clt-argostranslate/clt_argostranslate-0.3.tar.gz/clt_argostranslate-0.3/clt_argostranslate/__init__.py
@@ -0,0 +1,20 @@
+import os
+
+def configure_package_dir():
+    argos_packages_dir = '/clt_data/argos'
+    os.makedirs(argos_packages_dir)
+    os.environ['ARGOS_PACKAGES_DIR'] = argos_packages_dir
+
+
+def install_all_packages():
+    configure_package_dir()
+    print('install all packages for argos translate')
+    import argostranslate.package
+    argostranslate.package.update_package_index()
+    available_packages = argostranslate.package.get_available_packages()
+    print(f'found {len(available_packages)} packages')
+    for available_package in available_packages:
+        print(f'installing {available_package}')
+        download_path = available_package.download()
+        argostranslate.package.install_from_path(download_path)
+    
