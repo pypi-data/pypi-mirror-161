d34d015b54bdd7cb37296a507c4fe89c689371b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:49 +0000

    Add package feapder/1.7.8b2/feapder-1.7.8b2.tar.gz

---
+++ packages/feapder/feapder-1.7.8b2.tar.gz/feapder-1.7.8b2/feapder/commands/cmdline.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2020/5/8 2:24 PM
+---------
+@summary:
+---------
+@author: Boris
+@email: boris_liu@foxmail.com
+"""
+
+import sys
+from os.path import dirname, join
+
+from feapder.commands import create_builder
+from feapder.commands import shell
+from feapder.commands import zip
+
+HELP = """
+███████╗███████╗ █████╗ ██████╗ ██████╗ ███████╗██████╗
+██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+█████╗  █████╗  ███████║██████╔╝██║  ██║█████╗  ██████╔╝
+██╔══╝  ██╔══╝  ██╔══██║██╔═══╝ ██║  ██║██╔══╝  ██╔══██╗
+██║     ███████╗██║  ██║██║     ██████╔╝███████╗██║  ██║
+╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝     ╚═════╝ ╚══════╝╚═╝  ╚═╝
+
+Version: {version}
+Document: http://feapder.com
+
+Usage:
+  feapder <command> [options] [args]
+      
+Available commands:
+"""
+
+
+def _print_commands():
+    with open(join(dirname(dirname(__file__)), "VERSION"), "rb") as f:
+        version = f.read().decode("ascii").strip()
+
+    print(HELP.rstrip().format(version=version))
+    cmds = {
+        "create": "create project、spider、item and so on",
+        "shell": "debug response",
+        "zip": "zip project",
+    }
+    for cmdname, cmdclass in sorted(cmds.items()):
+        print("  %-13s %s" % (cmdname, cmdclass))
+
+    print('\nUse "feapder <command> -h" to see more info about a command')
+
+
+def execute():
+    try:
+        args = sys.argv
+        if len(args) < 2:
+            _print_commands()
+            return
+
+        command = args.pop(1)
+        if command == "create":
+            create_builder.main()
+        elif command == "shell":
+            shell.main()
+        elif command == "zip":
+            zip.main()
+        else:
+            _print_commands()
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    execute()
