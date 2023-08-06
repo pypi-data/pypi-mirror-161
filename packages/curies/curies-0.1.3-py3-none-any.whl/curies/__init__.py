1fa386975909b7de5cc44da06a0cd4c3c41b8aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:19 +0000

    Add package curies/0.1.3/curies-0.1.3-py3-none-any.whl

---
+++ packages/curies/curies-0.1.3-py3-none-any.whl/curies/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""Idiomatic conversion between URIs and compact URIs (CURIEs)."""
+
+from .api import Converter
+from .sources import (
+    get_bioregistry_converter,
+    get_go_converter,
+    get_go_obo_converter,
+    get_monarch_converter,
+    get_obo_converter,
+    get_prefixcommons_converter,
+)
+from .version import get_version
+
+__all__ = [
+    "Converter",
+    "get_version",
+    "get_obo_converter",
+    "get_prefixcommons_converter",
+    "get_monarch_converter",
+    "get_go_converter",
+    "get_bioregistry_converter",
+    "get_go_obo_converter",
+]
