a6043aa25a5373d70af4c7aed02e0566adf926d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:35 +0000

    Add package DTPySide/0.1.5/DTPySide-0.1.5-py3-none-any.whl

---
+++ packages/DTPySide/DTPySide-0.1.5-py3-none-any.whl/DTPySide/demo/main.py
@@ -0,0 +1,19 @@
+from DTPySide import *
+
+from DTPySide.demo.demoSession import DemoMainSession
+
+def run():
+
+	app=DTAPP(sys.argv)
+	
+	app.setAuthor("Holence")
+	app.setApplicationName("DTPySide Demo")
+	app.setLoginEnable(True)
+	app.loadTranslation()
+	mainsession=DemoMainSession(app)
+	app.setMainSession(mainsession)
+	# app.debugRun("123",True)
+	app.run()
+
+if __name__=="__main__":
+	run()
