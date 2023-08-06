72be45a47730fc94634f521fea230fa4f455da71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:51 +0000

    Add package talisman-dm/0.4.15/talisman_dm-0.4.15-py3-none-any.whl

---
+++ packages/talisman-dm/talisman_dm-0.4.15-py3-none-any.whl/tdm/abstract/datamodel/directive.py
@@ -0,0 +1,17 @@
+from abc import ABCMeta
+from enum import Enum
+
+
+class DirectiveType(str, Enum):
+    CREATE_CONCEPT = 'create_concept'
+
+
+class AbstractDirective(metaclass=ABCMeta):
+    __slots__ = ('_directive_type',)
+
+    def __init__(self, directive_type: DirectiveType):
+        self._directive_type = directive_type
+
+    @property
+    def directive_type(self) -> DirectiveType:
+        return self._directive_type
