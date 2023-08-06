eacd8372f003d894bab0de2058e4f96188a8ed6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:36 +0000

    Add package pds-nds-p/0.1/pds_nds_p-0.1.tar.gz

---
+++ packages/pds-nds-p/pds_nds_p-0.1.tar.gz/pds_nds_p-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pds_nds_p',
+      version='0.1',
+      description='Gaussian distributions',
+      author="Mohamed Javeed",
+      author_email='javeedrstar5@gmail.com',
+      packages=['pds_nds_p'],
+      zip_safe=False)
