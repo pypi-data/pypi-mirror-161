09ab6042038998d1d2cc7cf4c54031499266afa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:23 +0000

    Add package PathSelector/0.1.0/PathSelector-0.1.0.tar.gz

---
+++ packages/PathSelector/PathSelector-0.1.0.tar.gz/PathSelector-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# python package setup file
+
+
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+setup(
+    name='PathSelector',
+    version=VERSION,
+    description='PathSelector is a simple path selection module for python.',
+    author='Mustafa Ozan Çetin',
+    author_email='',
+    url='https://github.com/mozancetin/PathSelector',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'path', 'selector', 'file', 'directory'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+# end of file
