aa1d7a6d249706cb73172a35a5259056227e3231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:42 +0000

    Add package truinvest-broker/1.0.9/truinvest-broker-1.0.9.tar.gz

---
+++ packages/truinvest-broker/truinvest-broker-1.0.9.tar.gz/truinvest-broker-1.0.9/client/TradingInterface/TradingInterface.py
@@ -0,0 +1,36 @@
+class TradingInterface:
+    def Login(self):
+        pass
+
+    def PlaceOrder(self, qty, transaction_type, trading_symbol, exchange, product_type):
+        pass
+
+    def Margins(self):
+        pass
+
+    def Positions(self):
+        pass
+
+    def GenerateSession(self, mPin):
+        pass
+
+    def GetInstruments(self):
+        pass
+
+    def Holdings(self):
+        pass
+
+    def GetQuote(self, listInstruments, exchange):
+        pass
+
+    def Orders(self):
+        pass
+
+    def Profile(self):
+        pass
+
+    def GetAvailableMargin(self):
+        pass
+
+    def GetUtilizedMargin(self):
+        pass
