653128016f0fd50fc1d1ede50cef0f9e771ddb71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:52 +0000

    Add package pythonpackagesample-GroupE/0.1/pythonpackagesample-GroupE-0.1.tar.gz

---
+++ packages/pythonpackagesample-GroupE/pythonpackagesample-GroupE-0.1.tar.gz/pythonpackagesample-GroupE-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pythonpackagesample-GroupE',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/manoelgadi/pythonpackagesample',
+      author='Prof. Manoel Gadi',
+      author_email='mfalonso@faculty.ie.edu',
+      license='MIT',
+          packages=['pythonpackagesample-GroupE'],
+      zip_safe=False)
