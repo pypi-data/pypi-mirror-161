4054c97702119b858c393ed79a6c10e4eb8beb5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:11 +0000

    Add package mosartwmpy/0.3.1/mosartwmpy-0.3.1-py3-none-any.whl

---
+++ packages/mosartwmpy/mosartwmpy-0.3.1-py3-none-any.whl/mosartwmpy/input/demand.py
@@ -0,0 +1,44 @@
+import numpy as np
+import regex as re
+
+from datetime import datetime
+from xarray import open_dataset
+
+from benedict.dicts import benedict as Benedict
+from mosartwmpy.state.state import State
+from mosartwmpy.utilities.timing import timing
+
+
+# @timing
+def load_demand(state: State, config: Benedict, current_time: datetime, mask: np.ndarray) -> None:
+    """Loads water demand from file into the state for each grid cell.
+
+    Args:
+        state (State): the current model state; will be mutated
+        config (Benedict): the model configuration
+        current_time (datetime): the current time of the simulation
+        mask (ndarray): mask of active grid cells
+    """
+
+    # demand path can have placeholders for year and month and day, so check for those and replace if needed
+    path = config.get('water_management.demand.path')
+    path = re.sub('\{y[^}]*}', current_time.strftime('%Y'), path)
+    path = re.sub('\{m[^}]*}', current_time.strftime('%m'), path)
+    path = re.sub('\{d[^}]*}', current_time.strftime('%d'), path)
+
+    demand = open_dataset(path)
+
+    # if the demand file has a time axis, use it; otherwise assume data is just 2d
+    if config.get('water_management.demand.time', None) in demand:
+        state.grid_cell_demand_rate = np.array(demand[config.get('water_management.demand.demand')].sel({config.get('water_management.demand.time'): current_time}, method='pad')).flatten()[mask]
+    else:
+        state.grid_cell_demand_rate = np.array(demand[config.get('water_management.demand.demand')]).flatten()[mask]
+
+    # fill missing values with 0
+    state.grid_cell_demand_rate = np.where(
+        np.logical_not(np.isfinite(state.grid_cell_demand_rate)),
+        0,
+        state.grid_cell_demand_rate
+    )
+
+    demand.close()
---
+++ packages/mosartwmpy/mosartwmpy-0.3.1-py3-none-any.whl/mosartwmpy/input/runoff.py
@@ -0,0 +1,48 @@
+import numpy as np
+from datetime import datetime, time, timedelta
+from xarray import open_dataset
+
+from benedict.dicts import benedict as Benedict
+from mosartwmpy.grid.grid import Grid
+from mosartwmpy.state.state import State
+from mosartwmpy.utilities.timing import timing
+
+
+# @timing
+def load_runoff(state: State, grid: Grid, config: Benedict, current_time: datetime, mask: np.ndarray) -> None:
+    """Loads runoff from file into the state for each grid cell.
+
+    Args:
+        state (State): the current model state; will be mutated
+        grid (Grid): the model grid
+        config (Benedict): the model configuration
+        current_time (datetime): the current time of the simulation
+        mask (ndarray): mask of active grid cells
+    """
+    
+    # note that the forcing is provided in mm/s
+    # the flood section needs m3/s, but the routing needs m/s, so be aware of the conversions
+    # method="pad" means the closest time in the past is selected from the file
+    
+    runoff = open_dataset(config.get('runoff.path'))
+    
+    sel = {
+        config.get('runoff.time'): current_time
+    }
+    
+    if config.get('runoff.variables.surface_runoff', None) is not None:
+        state.hillslope_surface_runoff = 0.001 * grid.land_fraction * grid.area * np.array(
+            runoff[config.get('runoff.variables.surface_runoff')].sel(sel, method='pad')
+        ).flatten()[mask]
+    
+    if config.get('runoff.variables.subsurface_runoff', None) is not None:
+        state.hillslope_subsurface_runoff = 0.001 * grid.land_fraction * grid.area * np.array(
+            runoff[config.get('runoff.variables.subsurface_runoff')].sel(sel, method='pad')
+        ).flatten()[mask]
+    
+    if config.get('runoff.variables.wetland_runoff', None) is not None:
+        state.hillslope_wetland_runoff = 0.001 * grid.land_fraction * grid.area * np.array(
+            runoff[config.get('runoff.variables.wetland_runoff')].sel(sel, method='pad')
+        ).flatten()[mask]
+    
+    runoff.close()
