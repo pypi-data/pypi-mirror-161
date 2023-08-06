b92782580983f49e984ef417e950697cb8e21104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:22 +0000

    Add package obgp/0.0.40/obgp-0.0.40-py2.py3-none-any.whl

---
+++ packages/obgp/obgp-0.0.40-py2.py3-none-any.whl/bgp/__init__.py
@@ -0,0 +1,244 @@
+"""
+    __init__.py
+    ~~~~~~~~~~~
+
+    :copyright: (c) 2020 by OBGP
+    :license: see LICENSE for more details.
+"""
+
+__title__ = 'bgp'
+__author__ = 'OBGP'
+__version__ = '0.0.40'
+
+import copy
+import json
+import logging
+import os
+import requests
+import sys
+import tempfile
+import time
+
+import internetarchive as ia
+from bs4 import BeautifulSoup
+from internetarchive.config import get_config
+
+from bgp.modules.terms import (
+    FulltextProcessor,
+    IsbnExtractorModule,
+    NGramProcessor,
+    ReadingLevelModule,
+    UrlExtractorModule,
+    WordFreqModule,
+    CopyrightPageDetectorModule,
+    ChapterPageDetectorModule,
+    PageTypeProcessor,
+    BackpageIsbnExtractorModule
+)
+from bgp.utils import STOP_WORDS
+from subprocess import PIPE, Popen, STDOUT
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='obgp_errors.log')
+
+def _memoize_xml(self):
+    if not hasattr(self, '_xml'):
+        _memoize_xml_tic = time.perf_counter()
+        try:
+            self._xml = self.download(formats=['Djvu XML'], return_responses=True)[0].text
+        except requests.exceptions.Timeout as e:
+            logging.error('Timeout getting xml for item - ' + self.identifier + ' | ' + str(e))
+            raise Exception('Timeout getting xml for item - ' + self.identifier)
+        _memoize_xml_toc = time.perf_counter()
+        self.xml_time = round(_memoize_xml_toc - _memoize_xml_tic, 3)
+        self.xml_bytes = sys.getsizeof(self._xml)
+    return self._xml
+
+def _memoize_plaintext(self):
+    if not hasattr(self, '_plaintext'):
+        _memoize_plaintext_tic = time.perf_counter()
+        try:
+            self._plaintext = self.download(formats=['DjVuTXT'], return_responses=True)[0].text
+        except requests.exceptions.Timeout as e:
+            logging.error('Timeout getting txt for item - ' + self.identifier + ' | ' + str(e))
+            raise Exception('Timeout getting txt for item - ' + self.identifier)
+        _memoize_plaintext_toc = time.perf_counter()
+        self.plaintext_time = round(_memoize_plaintext_toc - _memoize_plaintext_tic, 3)
+        self.plaintext_bytes = sys.getsizeof(self._plaintext)
+    return self._plaintext
+
+ia.Item.xml = property(_memoize_xml)
+ia.Item.plaintext = property(_memoize_plaintext)
+
+def get_book_items(query, rows=100, page=1, scope_all=False):
+    """
+    :param str query: an search query for selecting/faceting books
+    :param int rows: limit how many results returned
+    :param int page: starting page to offset search results
+    :return: An `internetarchive` Item
+    :rtype: `internetarchive` Item
+    """
+    params = {'page': page, 'rows': rows}
+    if scope_all:
+        params['scope'] = 'all'
+    # this may need to get run as a session (priv'd access)
+    return ia.search_items(query, params=params).iter_as_items()
+
+def get_software_version():  # -> str:
+    cmd = "git rev-parse --short HEAD --".split()
+    return str(Popen(cmd, stdout=PIPE, stderr=STDOUT).stdout.read().decode().strip())
+
+
+class Sequencer:
+
+    class Sequence:
+        def __init__(self, pipeline, book, access=None, secret=None):
+            self.pipeline = pipeline
+            self.sequence_time = 0
+            self.book = book
+            self.access = access
+            self.secret = secret
+
+        def save(self, path=''):
+            item_path = path + self.book.identifier + '/'
+            # trailing slash needed for path
+            if getattr(self, 'book'):
+                if item_path and not os.path.exists(item_path):
+                    os.makedirs(item_path)
+                with open(item_path + 'book_genome.json', 'w') as txt:
+                    txt.write(json.dumps(self.results))
+
+        def upload(self):
+            itemid = self.results.get('metadata').get('identifier')
+            with tempfile.NamedTemporaryFile() as tmp:
+                tmp.write(json.dumps(self.results).encode())
+                tmp.flush()
+                self.ia.upload(itemid, {'book_genome.json': tmp},
+                               access_key=self.access,
+                               secret_key=self.secret)
+
+        @property
+        def results(self):
+            data = {}
+            meta = {}
+            processors = {}
+            for processor in self.pipeline:
+                processor_meta = self.pipeline[processor].results
+                # Remove module results from processor metadata dict
+                processor_meta.update({'modules': {}})
+                processors.update({processor: processor_meta})
+                for module in self.pipeline[processor].modules:
+                    # Add module results to root level of dict
+                    data[module] = self.pipeline[processor].results['modules'][module]['results']
+                    module_meta = self.pipeline[processor].results['modules'][module]
+                    # Remove module results from module metadata dict
+                    module_meta.pop('results')
+                    processors[processor]['modules'].update({module: module_meta})
+            meta['processors'] = processors
+            meta['sequence_time'] = self.sequence_time
+            meta['source'] = {
+                'xml': {
+                    'time': getattr(self.book, 'xml_time', None),
+                    'bytes': getattr(self.book, 'xml_bytes', None),
+                },
+                'txt': {
+                    'time': getattr(self.book, 'plaintext_time', None),
+                    'bytes': getattr(self.book, 'plaintext_bytes', None),
+                }
+            }
+            meta['version'] = get_software_version()
+            meta['timestamp'] = time.time()
+            meta['identifier'] = self.book.identifier
+            data['metadata'] = meta
+            return data
+
+    def __init__(self, pipeline, access=None, secret=None):
+        """
+        :param dict pipeline
+        """
+        self.pipeline = pipeline
+        self.configure(
+            access=access or get_config().get('s3', {}).get('access'),
+            secret=secret or get_config().get('s3', {}).get('secret')
+        )
+
+    def configure(self, access=None, secret=None):
+        self.access = access
+        self.secret = secret
+        self.ia = ia.get_session({'s3': {'access': access, 'secret': secret}})
+        self.ia.get_book_items = get_book_items
+
+    def sequence(self, book):
+        """
+        :param [NGramProcessor] pipeline: a list of NGramProcessors that run modules
+        :param  [str|ia.Item] book: an Archive.org book Item or Item.identifier
+        :param int rows: limit how many results returned
+        :param int page: starting page to offset search results
+        """
+        try:
+            sequence_tic = time.perf_counter()
+            try:
+                # possible conflict since ia.Item not from ia.get_session
+                _book = book if type(book) is ia.Item else self.ia.get_item(book)
+                sq = self.Sequence(
+                    copy.deepcopy(self.pipeline),
+                    _book,
+                    access=self.access,
+                    secret=self.secret
+                )
+            except requests.exceptions.ConnectionError:
+                raise Exception('Connection error retrieving metadata for - ' + book)
+                logging.error('Connection error retrieving metadata for - ' + book)
+            if sq.book.exists:
+                for processor in sq.pipeline:
+                    sq.pipeline[processor].run(sq.book)
+                sequence_toc = time.perf_counter()
+                sq.sequence_time = round(sequence_toc - sequence_tic, 3)
+                return sq
+            else:
+                raise Exception(sq.book.identifier + ' - Item cannot be found.')
+                logging.error(sq.book.identifier + ' - Item cannot be found.')
+        except IndexError:
+            raise Exception(sq.book.identifier + ' - does not have DjvuXML and/or DjvuTXT to be sequenced!')
+            logging.error(sq.book.identifier + ' - does not have DjvuXML and/or DjvuTXT to be sequenced!')
+        except requests.exceptions.HTTPError:
+            raise Exception(sq.book.identifier + ' - DjvuXML and/or DjvuTXT is forbidden and can\'t be sequenced!')
+            logging.error(sq.book.identifier + ' - DjvuXML and/or DjvuTXT is forbidden and can\'t be sequenced!')
+
+
+DEFAULT_SEQUENCER = Sequencer({
+    '3gram': NGramProcessor(modules={
+        '3grams': WordFreqModule()
+    }, n=3, threshold=2, stop_words=None),
+    '2gram': NGramProcessor(modules={
+        '2grams': WordFreqModule()
+    }, n=2, threshold=3, stop_words=STOP_WORDS),
+    '1gram': NGramProcessor(modules={
+        '1grams': WordFreqModule(),
+        'urls': UrlExtractorModule()
+    }, n=1, stop_words=None),
+    'fulltext': FulltextProcessor(modules={
+        'readinglevel': ReadingLevelModule()
+    }),
+    'pagetypes': PageTypeProcessor(modules={
+        'copyright_page': CopyrightPageDetectorModule(),
+        'backpage_isbn': BackpageIsbnExtractorModule()
+    })
+})
+
+MINIMAL_SEQUENCER = Sequencer({
+    '2gram': NGramProcessor(modules={
+        '2grams': WordFreqModule()
+    }, n=2, threshold=2, stop_words=STOP_WORDS),
+    '1gram': NGramProcessor(modules={
+        '1grams': WordFreqModule(),
+        'urls': UrlExtractorModule()
+    }, n=1, stop_words=None),
+    'pagetypes': PageTypeProcessor(modules={
+        'copyright_page': CopyrightPageDetectorModule(),
+        'backpage_isbn': BackpageIsbnExtractorModule()
+    })
+})
---
+++ packages/obgp/obgp-0.0.40-py2.py3-none-any.whl/bgp/modules/terms.py
@@ -0,0 +1,364 @@
+import re
+from collections import defaultdict
+
+import isbnlib
+import requests
+import time
+from lxml import etree
+
+
+PUNCTUATION = r'!"#$%&\'\/:()*+,.-;<=>?@[\\]^`{|}*'  # this needs improving
+
+class FulltextProcessor():
+
+    def __init__(self, modules):
+        self.modules = modules
+        self.time = 0
+
+    def run(self, book):
+        processor_tic = time.perf_counter()
+        for m in self.modules:
+            module_tic = time.perf_counter()
+            self.modules[m].run(book)
+            module_toc = time.perf_counter()
+            self.modules[m].time = round(module_toc - module_tic, 3)
+        processor_toc = time.perf_counter()
+        self.time = round(processor_toc - processor_tic, 3)
+
+    @property
+    def results(self):
+        return {
+            "modules": {m: self.modules[m].results for m in self.modules},
+            "total_time": self.time
+        }
+
+
+class ReadingLevelModule:
+
+    def __init__(self):
+        self.lexile = None
+        self.readability_fk_score= None
+        self.readability_s_score= None
+        self.time = 0
+
+    def run(self, book, **kwargs):
+        import nltk
+
+        try:
+            nltk.data.find('tokenizers/punkt')
+        except LookupError:
+            print('Downloading Punkt Tokenizer...')
+            nltk.download('punkt')
+
+        from readability import Readability
+        from readability.scorers.flesch_kincaid import ReadabilityException
+
+        doc = book.plaintext
+        isbn = 'isbn' in book.metadata and book.metadata['isbn'][0]
+
+        url = 'https://atlas-fab.lexile.com/free/books/' + str(isbn)
+
+        headers = {'accept': 'application/json; version=1.0'}
+        lexile = requests.get(url, headers=headers)
+        # Checks if lexile exists for ISBN. If doesn't exist value remains 'None'.
+        # If lexile does exist but no age range, value will be 'None'.
+        # If no ISBN, value will be 'None'.
+        if lexile.status_code == 200:
+            self.lexile = lexile.json()
+        try:
+            r = Readability(doc)
+            fk = r.flesch_kincaid()
+            s = r.smog()
+            self.readability_fk_score = fk.score
+            self.readability_s_score = s.score
+        # If less than 100 words
+        except ReadabilityException:
+            pass
+
+    @property
+    def results(self):
+        return {
+            "time": self.time,
+            "results": {
+                "lexile": self.lexile,
+                "readability": {
+                    "flesch_kincaid_score": self.readability_fk_score,
+                    "smog_score": self.readability_s_score,
+                }
+            }
+        }
+
+class NGramProcessor():
+
+    def __init__(self, modules, n=1, threshold=None, stop_words=None):
+        """
+        ngram processor takes a book of plaintext, splits the contents into tokens, and then passes them into each of its modules
+        the word frequency module is a common module for this processor
+        :param lambda modules: a dict of {'name': module}
+        :param int n: n-gram sequence length
+        :param int threshold: min occurrences threshold
+        """
+        self.modules = modules
+        self.n = n
+        self.threshold = threshold
+        self.stop_words = stop_words
+        # use token_count as word count for profiler
+        self.token_count = 0
+        self.time = 0
+        self.tokenization_time = 0
+
+    def run(self, book):
+        book.plaintext  # Priming memoization
+        processor_tic = time.perf_counter()
+        self.terms = self.fulltext_to_ngrams(
+            book.plaintext, n=self.n, stop_words=self.stop_words)
+        self.tokenization_time = round(time.perf_counter() - processor_tic, 3)
+        for m in self.modules:
+            module_tic = time.perf_counter()
+            for i, term in enumerate(self.terms):
+                self.token_count += 1
+                self.modules[m].run(term, threshold=self.threshold, index=i)
+            module_toc = time.perf_counter()
+            self.modules[m].time = round(module_toc - module_tic, 3)
+        processor_toc = time.perf_counter()
+        self.time = round(processor_toc - processor_tic, 3)
+
+    @property
+    def results(self):
+        return {
+            "modules": {
+                m: self.modules[m].results for m in self.modules
+            },
+            "total_time": self.time,
+            "tokenization_time": self.tokenization_time,
+            "total_tokens": self.token_count
+        }
+
+    @staticmethod
+    def tokens_to_ngrams(tokens, n=2):
+        ngrams = zip(*[tokens[i:] for i in range(n)])
+        return [" ".join(ngram) for ngram in ngrams]
+
+    @classmethod
+    def fulltext_to_ngrams(cls, fulltext, n=1, stop_words=None):
+        stop_words = stop_words or {}
+
+        def clean(fulltext):
+            return (
+                fulltext.lower()
+                .replace('’', "'")
+                .replace('. ', ' ')
+                .replace('! ', ' ')
+                .replace('? ', ' ')
+                .replace('\n-', '')
+                .replace('\n', ' ')
+            )
+        tokens = [
+            t.strip() for t in clean(fulltext).split(' ')
+            if t and t not in stop_words
+        ]
+        ngrams = cls.tokens_to_ngrams(tokens, n=n) if n > 1 else tokens
+        return ngrams
+
+def rmpunk(word, punctuation=PUNCTUATION):
+    return ''.join(
+        c.encode("ascii", "ignore").decode() for c in (word)
+        if c not in punctuation
+    )
+
+def replace_mistakes(word):
+    substitutions = [('I', '1'), ('O', '0'), ('l', '1'), ('S', '5')]
+    for sub in substitutions:
+        word = word.replace(*sub)
+    return word
+
+class WordFreqModule:
+
+    def __init__(self, punctuation=PUNCTUATION):
+        self.punctuation = punctuation
+        self.freqmap = defaultdict(int)
+        self.time = 0
+
+    def run(self, word, threshold=None, **kwargs):
+        clean_word = rmpunk(
+            word, punctuation=self.punctuation
+        )
+        self.threshold = threshold
+        # we could add more advanced regex to
+        # check if clean_word is something we care about
+        if clean_word and not clean_word.startswith(" ") and not clean_word.endswith(" "):
+            self.freqmap[clean_word] += 1
+
+    @property
+    def results(self):
+        return {
+            "time": self.time,
+            "results": sorted(
+                [items for items in self.freqmap.items()
+                 if not self.threshold or items[1] >= self.threshold],
+                key=lambda k_v: k_v[1], reverse=True)
+        }
+
+class ExtractorModule:
+
+    def __init__(self, extractor):
+        self.extractor = extractor
+        self.matches = []
+        self.time = 0
+
+    def run(self, term, **kwargs):
+        _term = self.extractor(term)
+        if _term:
+            self.matches.append(_term)
+
+    @property
+    def results(self):
+        return{
+            "results": self.matches,
+            "time": self.time
+        }
+
+
+class UrlExtractorModule(ExtractorModule):
+
+    @staticmethod
+    def validate_url(s):
+        s = s.lower()
+        if s.startswith('http'):
+            return s
+
+    def __init__(self):
+        super().__init__(self.validate_url)
+
+
+class IsbnExtractorModule(ExtractorModule):
+
+
+    @staticmethod
+    def validate_isbn(isbn):
+        isbn = rmpunk(isbn).replace(' ', '')
+        if len(isbn) == 9:
+            isbn = '0' + isbn
+        match10 = re.search(r'^(\d{9})(\d|X)', isbn)
+        match13 = re.search(r'^(\d{12})(\d)', isbn)
+
+        if match10:
+            if isbnlib.is_isbn10(match10.group()):
+                return match10.group()
+            elif match13:
+                if isbnlib.is_isbn13(match13.group()):
+                    return match13.group()
+            else:
+                return False
+
+    @staticmethod
+    def extract_isbn(page):
+        isbns = []
+        for line in page.iter('LINE'):
+            line_text = ' '.join(word.text for word in line.iter('WORD'))
+            line_text_clean = replace_mistakes(line_text)
+            isbnlike_list = isbnlib.get_isbnlike(line_text_clean, level='loose')
+            for candidate_isbn in isbnlike_list:
+                isbn = IsbnExtractorModule.validate_isbn(candidate_isbn)
+                if isbn:
+                    isbns.append(isbn)
+        return isbns
+
+    def __init__(self):
+        super().__init__(self.validate_isbn)
+
+
+class PageTypeProcessor:
+
+    def __init__(self, modules):
+        self.modules = modules
+        self.time = 0
+
+    def run(self, book):
+        processor_tic = time.perf_counter()
+        utf8_parser = etree.XMLParser(encoding='utf-8')
+        node = etree.fromstring(book.xml.encode('utf-8'), parser=utf8_parser)
+        for m in self.modules:
+            module_tic = time.perf_counter()
+            for page in node.iter('OBJECT'):
+                self.modules[m].run(page, node)
+            module_toc = time.perf_counter()
+            self.modules[m].time = round(module_toc - module_tic, 3)
+        processor_toc = time.perf_counter()
+        self.time = round(processor_toc - processor_tic, 3)
+    @property
+    def results(self):
+        return {
+            "modules": {m: self.modules[m].results for m in self.modules},
+            "total_time": self.time,
+        }
+
+class KeywordPageDetectorModule:
+
+    def __init__(self, keywords, extractor=None, match_limit=None):
+        self.extractor = extractor
+        self.keywords = keywords
+        self.matched_pages = []
+        self.match_limit = match_limit
+
+    def run(self, page, node):
+        if not self.match_limit or len(self.matched_pages) < self.match_limit:
+            # We think there's a better way to get text from the page
+            page_text = ' '.join([word.text for word in page.iter('WORD')])
+            for keyword in self.keywords:
+                if re.search(keyword, page_text, re.IGNORECASE):
+                    param = page[0].attrib['value'].split('.djvu')[0]
+                    current_page = param[-4:]
+                    match = {
+                        'page': current_page,
+                    }
+                    if self.extractor:
+                        match.update(self.extractor(page))
+                    self.matched_pages.append(match)
+                    # If we've found a match, we no longer need to
+                    # keep processing this page; exit for loop
+                    break
+    @property
+    def results(self):
+        return {
+            "results": self.matched_pages,
+            "time": self.time
+        }
+
+class ChapterPageDetectorModule(KeywordPageDetectorModule):
+
+    def __init__(self):
+        super().__init__([r'chap[.,]?(ter)? [0-9ivx]+'], match_limit=None)
+
+
+class CopyrightPageDetectorModule(KeywordPageDetectorModule):
+
+    def extractor(self, page):
+        isbns = IsbnExtractorModule.extract_isbn(page)
+
+        return {
+            'isbns': isbns,
+        }
+
+    def __init__(self):
+        super().__init__(['copyright', '©'], extractor=self.extractor, match_limit=1)
+
+
+class BackpageIsbnExtractorModule():
+
+    def __init__(self):
+        self.isbns = []
+
+    def run(self, page, node):
+        current_page = int(page[0].attrib['value'].split('.djvu')[0][-4:])
+        if not hasattr(self, 'last_page'):
+            self.last_page = int(node.xpath("//OBJECT")[-1][0].attrib['value'].split('.djvu')[0][-4:])
+        if current_page == self.last_page:
+            self.isbns = IsbnExtractorModule.extract_isbn(page)
+
+    @property
+    def results(self):
+        return {
+            "results": self.isbns,
+            "time": self.time
+        }
