bb038a4389effba59a5bf45ec9e4412a292a2ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:56 +0000

    Add package TheMixed/5.0.0/TheMixed-5.0.0.tar.gz

---
+++ packages/TheMixed/TheMixed-5.0.0.tar.gz/TheMixed-5.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='TheMixed',
+    version='5.0.0',
+    packages=['TheMixed'],
+    include_package_data=True,
+    url='https://github.com/ReddishXia/TheMainword',
+    license='MIT',
+    author='79988',
+    author_email='77957147@qq.com',
+    description='Get the main word in the paper'
+)
