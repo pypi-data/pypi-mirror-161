9ac0ebb1005593d1c4c6c8bf2ce5a3043fd92311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:19 +0000

    Add package themispy/0.0.14/themispy-0.0.14.tar.gz

---
+++ packages/themispy/themispy-0.0.14.tar.gz/themispy-0.0.14/themispy/scrapy/readers.py
@@ -0,0 +1,27 @@
+from azure.storage.blob import BlobClient
+
+
+def read_jsonl(blob_name: str, conn_str: str, container: str,
+               attr: str = 'url', encoding: str = 'UTF-8',
+               startswith: str = 'http') -> 'list[str]':
+    """Reads all JSON Lines datasources from the specified blob and container."""
+    attr = f'"{attr}": "'
+    
+    blob_client = BlobClient.from_connection_string(
+        conn_str=conn_str,
+        container_name=container, blob_name=blob_name
+    )
+    
+    stream = blob_client.download_blob()
+    content, datasources = [], []
+    
+    for i in stream.content_as_text(encoding=encoding).split(attr):
+        if i.startswith(f"'{startswith}'"):
+            content.append(i)
+        
+    for i in content:
+        idx = i.find('"')
+        i = i[:idx]
+        datasources.append(i)
+    
+    return datasources
