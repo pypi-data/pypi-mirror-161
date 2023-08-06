99b2c7886b3c4c67ebd72eaf0e01d26d24e9a106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:30 +0000

    Add package cipherer/0.0.1/cipherer-0.0.1.tar.gz

---
+++ packages/cipherer/cipherer-0.0.1.tar.gz/cipherer-0.0.1/cipherer/adfgx.py
@@ -0,0 +1,22 @@
+# 1. Do the square thing
+# 2. Get thing into table with second key
+# 3. Rearrange table by key's alphabetical order
+# Germans, U SUCK for making this so hard...
+#   A D F G X
+# A a b c d e
+# D f g h i j
+# F k l m n o
+# G p q r s t
+# X u v w x y
+
+# e.g. "abcdefghijklmnopqrstuvwxy", "oh" -> "FX DF"
+def square_thing(alphabet, first_input):
+    pass
+
+# e.g. "jih", "FX DF" -> ["FF", "X", "D"]
+def get_table(key, squareified_thing):
+    pass
+
+# e.g. "jih", ["FF", "X", "D"] -> ["D", "X", "FF"]
+def order_table(key, table):
+    pass
