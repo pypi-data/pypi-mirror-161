662373c37c5127cc3518964593736b2560cc496a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:08 +0000

    Add package bagbag/0.19.1/bagbag-0.19.1.tar.gz

---
+++ packages/bagbag/bagbag-0.19.1.tar.gz/bagbag-0.19.1/src/bagbag/Python.py
@@ -0,0 +1,18 @@
+from typing import Iterable
+
+def Range(startOrEnd:int, end:int=None) -> Iterable:
+    """
+    "If the second argument is provided, return a range from the first argument to the second argument,
+    otherwise return a range from 0 to the first argument."
+    
+    :param startOrEnd: The first number in the range. If end is not specified, this is the last number
+    in the range
+    :type startOrEnd: int
+    :param end: The end of the range
+    :type end: int
+    :return: A range object
+    """
+    if end:
+        return range(startOrEnd, end)
+    else:
+        return range(0, startOrEnd)
