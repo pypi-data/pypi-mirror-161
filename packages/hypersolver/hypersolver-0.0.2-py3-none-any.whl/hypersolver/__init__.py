48fb6a70caf71c8d987c05e2198a7c53dc7f51fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:49 +0000

    Add package hypersolver/0.0.2/hypersolver-0.0.2-py3-none-any.whl

---
+++ packages/hypersolver/hypersolver-0.0.2-py3-none-any.whl/hypersolver/__init__.py
@@ -0,0 +1,45 @@
+""" hyper---bolic partial differential equations---solver
+
+    hypersolver revolves around solving hyperbolic
+    partial differential equations (PDEs) of the form
+
+    ∂n/∂t + ∂(fn)/∂x = ∂n/∂t + f ∂n/∂x - n ∂f/∂x = g
+
+    where
+
+    n is a property of interest,
+    x is an independent variable of interest,
+    f is speed n moves along x, and
+    g lumps sources and sinks.
+
+    Functionally, n(x; t), f(x), and g(x; t; n).
+
+    Note, fn is the flux across x.
+
+    Usage:
+    >>> from hypersolver import select_solver
+    >>> solver = select_solver(method)
+    >>> solver(n0, x, t, f, g, **kwargs)
+
+    available methods:
+        - "lax_friedrichs" (default)
+        - "lax_wendroff"
+
+"""
+
+
+__version__ = "0.0.2"
+
+__hyper_solvers__ = [
+    "lax_friedrichs",
+    "lax_wendroff",
+]
+
+from hypersolver.base.basic_solver import solver
+
+
+def select_solver(method="lax_friedrichs"):
+    """ wrapper function to select solvers """
+    if method not in __hyper_solvers__:
+        raise ValueError("method not supported")
+    return solver(method)
