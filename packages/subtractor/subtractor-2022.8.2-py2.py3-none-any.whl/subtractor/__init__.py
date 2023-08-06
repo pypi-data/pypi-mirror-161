8914381db3e771cf848e86319424d2fe50ba8d74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:30 +0000

    Add package subtractor/2022.8.2/subtractor-2022.8.2-py2.py3-none-any.whl

---
+++ packages/subtractor/subtractor-2022.8.2-py2.py3-none-any.whl/subtractor/__init__.py
@@ -0,0 +1,23 @@
+"""Pixels, pixels, pixels."""
+import os
+from typing import List
+
+APP_NAME = 'Pixels, pixels, pixels.'
+APP_ALIAS = 'subtractor'
+APP_ENV = 'SUBTRACTOR'
+DEBUG = bool(os.getenv(f'{APP_ENV}_DEBUG', ''))
+VERBOSE = bool(os.getenv(f'{APP_ENV}_VERBOSE', ''))
+QUIET = False
+STRICT = bool(os.getenv(f'{APP_ENV}_STRICT', ''))
+ENCODING = 'utf-8'
+ENCODING_ERRORS_POLICY = 'ignore'
+DEFAULT_CONFIG_NAME = '.subtractor.json'
+DEFAULT_LF_ONLY = 'YES'
+
+# [[[fill git_describe()]]]
+__version__ = '2022.8.2+parent.1d9ba9cc'
+# [[[end]]] (checksum: 2fed94e583e75ddcda18db50b9126c1c)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
+__all__: List[str] = []
