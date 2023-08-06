9c36d0acb90e9087497592aec0974d450bd9b375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:05 +0000

    Add package cfx-account/0.1.0b2/cfx-account-0.1.0b2.tar.gz

---
+++ packages/cfx-account/cfx-account-0.1.0b2.tar.gz/cfx-account-0.1.0b2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0-beta.2'
+DESCRIPTION = 'Conflux account'
+LONG_DESCRIPTION = 'Conflux account that can sign a conflux transaction'
+
+# python3 -m build
+# twine upload --repository cfx-account dist/*
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="cfx-account",
+    version=VERSION,
+    author="The conflux foundation",
+    author_email="wangpan@conflux-chain.org",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        "eth-account>=0.6.0,<0.7.0",
+        "cfx-address"
+    ],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
