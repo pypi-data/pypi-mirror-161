262292bfa4c350a6c8d97feb4d1bd053f42d7a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:19 +0000

    Add package edsnlp/0.6.2/edsnlp-0.6.2-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/edsnlp/edsnlp-0.6.2-cp39-cp39-macosx_10_9_x86_64.whl/edsnlp/pipelines/ner/scores/sofa/factory.py
@@ -0,0 +1,52 @@
+import re
+from typing import Any, Callable, Dict, List, Union
+
+from spacy.language import Language
+
+from edsnlp.pipelines.ner.scores.sofa import Sofa, patterns
+from edsnlp.utils.deprecation import deprecated_factory
+
+DEFAULT_CONFIG = dict(
+    regex=patterns.regex,
+    after_extract=patterns.after_extract,
+    score_normalization=patterns.score_normalization_str,
+    attr="NORM",
+    window=10,
+    ignore_excluded=False,
+    flags=re.S,
+)
+
+
+@deprecated_factory(
+    "SOFA",
+    "eds.SOFA",
+    default_config=DEFAULT_CONFIG,
+    assigns=["doc.ents", "doc.spans"],
+)
+@Language.factory(
+    "eds.SOFA",
+    default_config=DEFAULT_CONFIG,
+    assigns=["doc.ents", "doc.spans"],
+)
+def create_component(
+    nlp: Language,
+    name: str,
+    regex: List[str],
+    after_extract: List[Dict[str, str]],
+    score_normalization: Union[str, Callable[[Union[str, None]], Any]],
+    attr: str,
+    window: int,
+    ignore_excluded: bool,
+    flags: Union[re.RegexFlag, int],
+):
+    return Sofa(
+        nlp,
+        score_name=name,
+        regex=regex,
+        after_extract=after_extract,
+        score_normalization=score_normalization,
+        attr=attr,
+        window=window,
+        ignore_excluded=ignore_excluded,
+        flags=flags,
+    )
