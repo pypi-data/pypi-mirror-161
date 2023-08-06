4508fc3238df9df56c9ba4c1a5ffeccae1184c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:07 +0000

    Add package ignition-api/8.1.19.post1/ignition_api-8.1.19.post1-py2-none-any.whl

---
+++ packages/ignition-api/ignition_api-8.1.19.post1-py2-none-any.whl/com/inductiveautomation/ignition/common/browsing/__init__.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+
+__all__ = ["Result", "Results"]
+
+from typing import Any, List, Optional
+
+from com.inductiveautomation.ignition.common.model.values import QualityCode
+from java.lang import Object, String
+
+
+class Result(object):
+    def getDisplayPath(self):
+        raise NotImplementedError
+
+    def getPath(self):
+        raise NotImplementedError
+
+    def getType(self):
+        raise NotImplementedError
+
+    def hasChildren(self):
+        raise NotImplementedError
+
+
+class Results(Object):
+    """The results of a browse operation.
+
+    May only represent a partial result set, which can be determined by
+    comparing the Total Available Size to the Returned Size. If there is
+    a mismatch, the continuation point should be non-null and can be
+    used in constructing the subsequent BrowseFilter to continue the
+    browse.
+    """
+
+    _continuationPoint = None  # type: Optional[String]
+    _resultQuality = None  # type: QualityCode
+    _results = None  # type: List[Any]
+
+    def __init__(self, *args):
+        # type: (Any) -> None
+        print(args)
+        self._continuationPoint = None
+        self._resultQuality = QualityCode.Good
+        self._results = []
+        self._totalAvailableResults = 0
+
+    @staticmethod
+    def error(result):
+        return Results(result)
+
+    def getContinuationPoint(self):
+        return self._continuationPoint
+
+    def getResultQuality(self):
+        return self._resultQuality
+
+    def getResults(self):
+        return self._results
+
+    def getReturnedSize(self):
+        return 0 if self._results is None else len(self._results)
+
+    def getTotalAvailableSize(self):
+        return self._totalAvailableResults
+
+    @staticmethod
+    def of(results):
+        return Results(results)
+
+    def setContinuationPoint(self, continuationPoint=None):
+        # type: (Optional[str]) -> None
+        self._continuationPoint = continuationPoint
+
+    def setResultQuality(self, value):
+        self._resultQuality = value
+
+    def setResults(self, results):
+        self._results = results
+
+    def setTotalAvailableResults(self, totalAvailableResults):
+        self._totalAvailableResults = totalAvailableResults
