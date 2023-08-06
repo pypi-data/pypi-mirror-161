a16a44d9780eebfcb8138550edb63a2407c0f283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:18 +0000

    Add package perceptor/0.3.0/perceptor-0.3.0-py3-none-any.whl

---
+++ packages/perceptor/perceptor-0.3.0-py3-none-any.whl/perceptor/drawers/__init__.py
@@ -0,0 +1,6 @@
+from .deep_image_prior import DeepImagePrior
+from .diffusion import BruteDiffusion, PseudoLinearSampler
+from .raw import Raw
+from .jpeg import JPEG
+from .rudalle import BruteRuDalle
+from .stylegan_xl import StyleGANXL
