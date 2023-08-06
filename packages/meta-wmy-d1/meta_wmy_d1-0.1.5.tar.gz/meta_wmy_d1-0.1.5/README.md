260d944af9d1590f7a4a828c8c250b5a2e1e20d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:42 +0000

    Add package meta-wmy-d1/0.1.5/meta_wmy_d1-0.1.5.tar.gz

---
+++ packages/meta-wmy-d1/meta_wmy_d1-0.1.5.tar.gz/meta_wmy_d1-0.1.5/README.md
@@ -0,0 +1,24 @@
+# python_library
+python_library
+
+ 
+1. pip install  twine
+2. python setup.py sdist　
+3. twine upload dist/*
+这里执行之后需要pypi的账号密码
+username wmy_meta
+password wang12367
+
+测试
+4. pip install  demo_wmy_meta
+
+
+setup程序参数
+
+name：需要唯一  以后pip的时候的name
+
+version  版本，  pipy上会记录历史版本
+
+url  gitlab/github的项目链接
+
+
---
+++ packages/meta-wmy-d1/meta_wmy_d1-0.1.5.tar.gz/meta_wmy_d1-0.1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'meta_wmy_d1',
+    version = '0.1.5',
+    keywords='MetaApp',
+    description = '手写python库',
+    license = 'MIT License',
+    url = 'https://github.com/Gutier14/CAAFinder',
+    author='mingyu.wang',
+    author_email = 'mingyu.wang@appshahe.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
