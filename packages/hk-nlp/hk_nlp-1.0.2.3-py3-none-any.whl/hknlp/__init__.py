8c4caff9da70f85d2563e05c7175195ef0a95a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:45 +0000

    Add package hk-nlp/1.0.2.3/hk_nlp-1.0.2.3-py3-none-any.whl

---
+++ packages/hk-nlp/hk_nlp-1.0.2.3-py3-none-any.whl/hknlp/__init__.py
@@ -0,0 +1,5 @@
+from .data import IterableDatasetWrapper
+from . import prompt, translation, utils, data
+from .synonym import SynonymReplacement
+
+__all__ = ["prompt", "translation", "utils", "IterableDatasetWrapper", "data", "SynonymReplacement"]
