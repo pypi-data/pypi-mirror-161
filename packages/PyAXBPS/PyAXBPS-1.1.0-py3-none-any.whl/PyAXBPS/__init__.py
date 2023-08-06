c24d548a52b0b29d07335d2b7d227774e473f13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:25 +0000

    Add package PyAXBPS/1.1.0/PyAXBPS-1.1.0-py3-none-any.whl

---
+++ packages/PyAXBPS/PyAXBPS-1.1.0-py3-none-any.whl/PyAXBPS/__init__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+
+from .AXBTProcessor import AXBTprocessor
+from .AXCTDProcessor import AXCTDprocessor
+from .AXCPProcessor import AXCPprocessor
+from . import fileinteraction
+
