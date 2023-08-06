34b73ddfec9281b8d69a7ae6e9dbfcf729edcd36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:01 +0000

    Add package TestAuto/0.0.3/TestAuto-0.0.3-py3-none-any.whl

---
+++ packages/TestAuto/TestAuto-0.0.3-py3-none-any.whl/TestAuto/core/basetest.py
@@ -0,0 +1,20 @@
+"""
+============================
+Author:柠檬班-木森
+Time:2020/7/30   16:09
+E-mail:3247119728@qq.com
+Company:湖南零檬信息技术有限公司
+============================
+"""
+import unittest
+
+
+class BaseTest(unittest.TestCase):
+    pass
+
+
+
+
+
+
+
