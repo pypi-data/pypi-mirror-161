9e675311118d5f319dc9292e16831fd2ac5146bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:43 +0000

    Add package autodoraemon/1.0.8/autodoraemon-1.0.8.tar.gz

---
+++ packages/autodoraemon/autodoraemon-1.0.8.tar.gz/autodoraemon-1.0.8/auto_doraemon/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@author:zhaojiajun
+@file:__init__.py.py
+@time:2022/07/11
+"""
+
+__all__ = ['util', 'user_defined_exception']
+
+if __name__ == '__main__':
+    pass
