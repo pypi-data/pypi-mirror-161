e985c36dcf97ed34eb80d42d08a195b76a3fe147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:52 +0000

    Add package simple-signal-bot/1.0.0/simple_signal_bot-1.0.0-py3-none-any.whl

---
+++ packages/simple-signal-bot/simple_signal_bot-1.0.0-py3-none-any.whl/simple_signal_bot/util/constants.py
@@ -0,0 +1,28 @@
+import logging
+import os
+
+_logger = logging.getLogger("constants")
+
+HOSTNAME = os.environ.get('SIGNAL_HOST', "localhost")
+
+TELNUMBER = os.environ.get('SIGNAL_NUMBER')
+if TELNUMBER is None:
+    raise SystemExit("Missing SIGNAL_NUMBER")
+
+POLLING_INTERVAL = int(os.environ.get('SIGNAL_INTERVAL', "15"))
+
+PROTOCOL = os.environ.get('SIGNAL_PROTOCOL', "https")
+
+TEST_MODE = os.environ.get('SIGNAL_TEST')
+if TEST_MODE is None:
+    TEST_MODE = False
+elif TEST_MODE.lower() is "false":
+    TEST_MODE = False
+else:
+    TEST_MODE = True
+
+
+def printAllConstants():
+    _logger.info("Config: SIGNAL_HOST = %s, SIGNAL_NUMBER = %s, SIGNAL_PROTOCOL = %s,"
+                 " SIGNAL_INTERVAL = %s, SIGNAL_TEST = %s", HOSTNAME, TELNUMBER, PROTOCOL,
+                 POLLING_INTERVAL, TEST_MODE)
