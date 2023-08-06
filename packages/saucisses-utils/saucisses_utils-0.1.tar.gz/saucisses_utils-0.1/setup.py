abc6069bd61caae81526e470ea7c116477eacb06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:14 +0000

    Add package saucisses-utils/0.1/saucisses_utils-0.1.tar.gz

---
+++ packages/saucisses-utils/saucisses_utils-0.1.tar.gz/saucisses_utils-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='saucisses_utils',
+    requires=[],
+    packages=['saucisses_utils'],
+    description='Saucisses Team project utils package',
+    long_description='Saucisses Team project utils package',
+    version='0.1',
+    url='https://github.com/saucisses-a-roulettes/saucisses_utils',
+    author='Saucisses Team',
+    keywords=['pip', 'bekindto', 'utils']
+)
