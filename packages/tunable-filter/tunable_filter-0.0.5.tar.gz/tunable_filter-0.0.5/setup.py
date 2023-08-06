d3db2bc85addc3457bae8acde4aca34b291cfa32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:53 +0000

    Add package tunable-filter/0.0.5/tunable_filter-0.0.5.tar.gz

---
+++ packages/tunable-filter/tunable_filter-0.0.5.tar.gz/tunable_filter-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup_requires = []
+
+install_requires = [
+    "numpy",
+    "opencv-python>=4.2.0",
+    "dataclasses",  # for 3.6
+    "PyYAML",
+    "types-PyYAML"
+]
+
+setup(
+    name='tunable_filter',
+    version='0.0.5',
+    description='Tunable image filter',
+    author='Hirokazu Ishida',
+    author_email='h-ishida@jsk.imi.i.u-tokyo.ac.jp',
+    license='MIT',
+    install_requires=install_requires,
+    packages=find_packages(exclude=('tests', 'docs')),
+    package_data={'tunable_filter': ['py.typed']}
+)
