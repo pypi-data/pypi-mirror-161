d0271af19652f968713b29cd5864828d24527fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:58 +0000

    Add package iminuit/2.15.2/iminuit-2.15.2-cp39-cp39-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/iminuit/iminuit-2.15.2-cp39-cp39-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/iminuit/version.py
@@ -0,0 +1,14 @@
+"""iminuit version."""
+
+# We use the semantic version scheme: MAJOR.MINOR.MAINTENANCE
+# - See https://packaging.python.org/guides/distributing-packages-using-setuptools
+# - Increase MAJOR when making breaking changes
+# - Increase MINOR when adding backward-compatible features
+# - Increase MAINTENANCE when fixing bugs without adding features
+# - During development, add suffix .devN with N >= 0
+# - For release candidates, add suffix .rcN with N >= 0
+# - For beta releases, add suffix .betaN with N >= 0
+version = "2.15.2"
+
+# We list the corresponding ROOT version of the C++ Minuit2 library here
+root_version = "v6-25-02-1013-ga4bb8f3342"
