6dc38859a9b390a0dc1e35505e29122e1a905e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:49 +0000

    Add package new-package-on-python1/0.1/new_package_on_python1-0.1.tar.gz

---
+++ packages/new-package-on-python1/new_package_on_python1-0.1.tar.gz/new_package_on_python1-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='new_package_on_python1',
+      version='0.1',
+      description='Gaussian New_packages_on_python1',
+      packages=['New_packages_on_python1'],
+      zip_safe=False)
