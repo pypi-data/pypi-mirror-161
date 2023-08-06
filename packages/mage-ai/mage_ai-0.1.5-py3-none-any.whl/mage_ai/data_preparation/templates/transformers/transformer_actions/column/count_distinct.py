4d55695316cd0800ee0f0880bdc315b2a215cdd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:32 +0000

    Add package mage-ai/0.1.5/mage_ai-0.1.5-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.1.5-py3-none-any.whl/mage_ai/data_preparation/templates/transformers/transformer_actions/column/count_distinct.py
@@ -0,0 +1,21 @@
+{% extends "transformers/transformer_actions/action.jinja" %}
+{% block action %}
+    """
+    Execute Transformer Action: ActionType.COUNT_DISTINCT
+
+    Docs: https://github.com/mage-ai/mage-ai/blob/master/docs/actions/transformer_actions/README.md#aggregation-actions
+    """
+    action = build_transformer_action(
+        df,
+        action_type=ActionType.COUNT_DISTINCT,
+        action_code='',  # Enter filtering condition on rows before aggregation
+        arguments=[],  # Enter the columns to compute aggregate over
+        axis=Axis.COLUMN,
+        options={'groupby_columns': []},  # Enter columns to group by
+        outputs=[
+            # The number of outputs below must match the number of arguments
+            {'uuid': 'new_aggregate_column_1', 'column_type': 'number'},
+            {'uuid': 'new_aggregate_column_2', 'column_type': 'number'},
+        ],
+    )
+{% endblock %}
