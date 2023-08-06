ead0d324abc33a2fc1364ada5266b0b25add56cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:37 +0000

    Add package endemic/0.1.6/endemic-0.1.6.tar.gz

---
+++ packages/endemic/endemic-0.1.6.tar.gz/endemic-0.1.6/src/endemic/rabbit/message.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from .constant import *
+
+
+class Message:
+    def __init__(self):
+        self.__link = None
+        self.__link = None
+
+    def default(self, project_flow: list, data: list) -> dict:
+        return {
+            PROJECT_FLOW: project_flow,
+            MESSAGE_CREATED: datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000000'),
+            LINK: self.__link,
+            DATA: data if data else []
+        }
+
+    @property
+    def link(self):
+        return self.__link
+
+    @link.setter
+    def link(self, value: str):
+        self.__link = value
