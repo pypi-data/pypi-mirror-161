ca59d55be9080578188d62e978a0d73dfb737d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:35 +0000

    Add package pyEmpatica/0.6.0/pyEmpatica-0.6.0.tar.gz

---
+++ packages/pyEmpatica/pyEmpatica-0.6.0.tar.gz/pyEmpatica-0.6.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyEmpatica",
+    version="0.6.0",
+    author="Walker Arce",
+    author_email="walker.arce@unmc.edu",
+    description="Communicate with your Empatica E4 in your Python scripts.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Munroe-Meyer-Institute-VR-Laboratory/pyEmpatica",
+    project_urls={
+        "Bug Tracker": "https://github.com/Munroe-Meyer-Institute-VR-Laboratory/pyEmpatica/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
