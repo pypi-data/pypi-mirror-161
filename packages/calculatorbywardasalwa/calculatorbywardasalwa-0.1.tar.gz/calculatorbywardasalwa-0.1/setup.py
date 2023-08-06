23202ec5dc66f6af3019390ce670642de4b7cb45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:07 +0000

    Add package calculatorbywardasalwa/0.1/calculatorbywardasalwa-0.1.tar.gz

---
+++ packages/calculatorbywardasalwa/calculatorbywardasalwa-0.1.tar.gz/calculatorbywardasalwa-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name="calculatorbywardasalwa",
+version="0.1",
+description="this is a package",
+long_description="this is a very very long description",
+author = "Warda and Salwa",
+packages = [""],
+install_packages = []
+)
