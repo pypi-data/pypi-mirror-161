dcce9c071cefefef112f4076964c5b7c5e3bec74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:17 +0000

    Add package bagbag/0.21.10/bagbag-0.21.10-py3-none-any.whl

---
+++ packages/bagbag/bagbag-0.21.10-py3-none-any.whl/bagbag/Tools/Web.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import request
+from flask import make_response
+from flask import abort, redirect
+from flask import render_template
+
+try:
+    from ..Thread import Thread
+except:
+    import sys 
+    sys.path.append("..")
+    from ..Thread import Thread
+
+class Response():
+    Make = make_response
+    Abort = abort
+    Redirect = redirect
+    Render = render_template
+
+class RequestArgs():
+    def Get(self, *args, **kwargs) -> str | None:
+        return request.args.get(*args, **kwargs)
+
+class Request():
+    def Method(self) -> str:
+        return request.method
+    Args = RequestArgs()
+
+class Web():
+    def __init__(self, name:str=__name__):
+        self.app = Flask(name)
+        
+        self.Route = self.app.route 
+        self.Request = Request()
+        self.Response = Response()
+        
+    def Run(self, host:str, port:int, block:bool=True):
+        if block:
+            self.app.run(host, port, True)
+        else:
+            Thread(self.app.run, host, port, True)
