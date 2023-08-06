86a7ae1052305599cbaaa4b6d4bb355d093dd032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:34 +0000

    Add package osbm/0.0.20/osbm-0.0.20.tar.gz

---
+++ packages/osbm/osbm-0.0.20.tar.gz/osbm-0.0.20/osbm/__init__.py
@@ -0,0 +1,14 @@
+from . import utils
+
+module_path = __file__.split("__init__.py")[0]
+__version__ = open(module_path + "VERSION").read()
+
+
+def download_gist(url: str = None):
+
+    assert url is not None, "Please provide an URL"
+    raise NotImplementedError
+
+
+def set_matplotlib_rc(setting: str = "notebook"):
+    raise NotImplementedError
