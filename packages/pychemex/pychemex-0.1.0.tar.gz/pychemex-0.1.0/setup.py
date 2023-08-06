fd5747eaebf48e39354be4b0a4660bb24dd2a5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:38 +0000

    Add package pychemex/0.1.0/pychemex-0.1.0.tar.gz

---
+++ packages/pychemex/pychemex-0.1.0.tar.gz/pychemex-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='pychemex',
+    version='0.1.0',
+    description='A example of the compressor class unit operation',
+    author='Odhran',
+    author_email='odhran.ocallaghan@ucdconnect.ie',
+    license='BSD 2-clause',
+    packages=['pychemex'],
+    install_requires=['numpy'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
