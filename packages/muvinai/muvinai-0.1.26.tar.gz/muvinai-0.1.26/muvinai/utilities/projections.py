7ce610d97b87f5a9a75d1f3f2497c30cd4fd208f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:19 +0000

    Add package muvinai/0.1.26/muvinai-0.1.26.tar.gz

---
+++ packages/muvinai/muvinai-0.1.26.tar.gz/muvinai-0.1.26/muvinai/utilities/projections.py
@@ -0,0 +1,17 @@
+proj_accesso_cliente = {'token_spice': 1,
+                        "token_unauthorized": 1,
+                               '_id': 1,
+                               "fecha_vigencia": 1,
+                               "nivel_de_acceso": 1,
+                               "status": 1,
+
+                               "nombre": 1,
+                               "apellido": 1,
+                               "nacimiento": 1,
+                                "active_plan_id": 1,
+                        "email": 1,
+                        "documento": 1,
+                        "plan_corporativo": 1,
+                        "plan": 1,
+                        "sede": 1
+                        }
