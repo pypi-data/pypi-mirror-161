991353d2d47ae54909a5d98a9d687771e1d60f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:30 +0000

    Add package cast-from-env/0.0.2/cast_from_env-0.0.2.tar.gz

---
+++ packages/cast-from-env/cast_from_env-0.0.2.tar.gz/cast_from_env-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from unittest import defaultTestLoader
+from setuptools import setup
+
+setup(
+    name='cast_from_env',
+    version='0.0.2',
+    description='Get and cast environment variables',
+    author='Nic Wolff',
+    author_email='nwolff@hearst.com',
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='http://github.com/Media-Platforms/cast-from-env',
+    py_modules=['cast_from_env'],
+)
