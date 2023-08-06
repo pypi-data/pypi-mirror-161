9458c846bb3711a0678142dee2027406e695ab54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:34 +0000

    Add package ad9546/0.0.1/ad9546-0.0.1-py3.8.egg

---
+++ packages/ad9546/ad9546-0.0.1-py3.8.egg/EGG-INFO/scripts/misc.py
@@ -0,0 +1,46 @@
+#!/home/gbressaix/miniconda3/bin/python3
+#################################################################
+# Guillaume W. Bres, 2022          <guillaume.bressaix@gmail.com>
+#################################################################
+# misc.py: AD9546 miscellaneous features
+#################################################################
+import sys
+import argparse
+from ad9546 import *
+def main (argv):
+    parser = argparse.ArgumentParser(description="AD9546 misc control tool")
+    parser.add_argument(
+        "bus",
+        type=int,
+        help="I2C bus (int)",
+    )
+    parser.add_argument(
+        "address",
+        type=str,
+        help="I2C slv address (hex format)",
+    )
+    flags = [
+        ('temp-thres-high', int, 'Set temperature warning threshold high value'),
+        ('temp-thres-low', int, 'Set temperature warning threshold low value'),
+    ]
+    for (flag, v_type, helper) in flags:
+        parser.add_argument(
+            "--{}".format(flag), 
+            type=v_type,
+            help=helper,
+        )
+    args = parser.parse_args(argv)
+    # open device
+    dev = AD9546(args.bus, int(args.address, 16))
+
+    if args.temp_thres_high:
+        value = int(args.temp_thres_high * pow(2,7))
+        write_data(handle, address, 0x2905, value & 0xFF)
+        write_data(handle, address, 0x2906, (value & 0xFF00)>>8)
+    if args.temp_thres_low:
+        value = int(args.temp_thres_high * pow(2,7))
+        write_data(handle, address, 0x2903, value & 0xFF)
+        write_data(handle, address, 0x2904, (value & 0xFF00)>>8)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ packages/ad9546/ad9546-0.0.1-py3.8.egg/EGG-INFO/scripts/reset.py
@@ -0,0 +1,54 @@
+#!/home/gbressaix/miniconda3/bin/python3
+#################################################################
+# Guillaume W. Bres, 2022          <guillaume.bressaix@gmail.com>
+#################################################################
+# reset.py: AD9546 reset macros
+#################################################################
+import sys
+import argparse
+from ad9546 import *
+
+def main (argv):
+    parser = argparse.ArgumentParser(description="AD9546 reset tool")
+    parser.add_argument(
+        "bus",
+        type=int,
+        help="I2C bus (int)",
+    )
+    parser.add_argument(
+        "address",
+        type=str,
+        help="I2C slv address (hex)",
+    )
+    flags = [
+        ('soft', """Performs a soft reset.
+        If Mx pins are configured for automated EEPROM download, download gets initiated."""),
+        ('sans', 'Performs a soft reset but maintains current registers value.'),
+        ('watchdog', 'Resets watchdog timer'),
+    ]
+    for (flag, helper) in flags:
+        parser.add_argument(
+            "--{}".format(flag),
+            action="store_true",
+            help=helper,
+        )
+    args = parser.parse_args(argv)
+    # open device
+    dev = AD9546(args.bus, int(args.address, 16))
+
+    if args.soft:
+        r = dev.read_data(0x0000)
+        r &= 0x7E # clear bits
+        dev.write_data(0x0000, r | 0x01 | 0x80)
+        dev.write_data(0x0000, r)
+    if args.sans:
+        r = dev.read_data(0x0001)
+        r &= 0xFB # clear bit
+        dev.write_data(0x0001, r | 0x04)
+        dev.write_data(0x0001, r)
+    if args.watchdog:
+        r = dev.read_data(0x2005)
+        dev.write_data(0x0001, r | 0x80)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
