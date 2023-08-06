0c592f61d9a4783cdc2987ba34698529c8728ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:53 +0000

    Add package cs-commons/0.0.1/cs-commons-0.0.1.tar.gz

---
+++ packages/cs-commons/cs-commons-0.0.1.tar.gz/cs-commons-0.0.1/cs_commons/__init__.py
@@ -0,0 +1,4 @@
+"""ChillServices commons"""
+
+__version__ = "0.0.1"
+__author__ = "Anders Petersson <petersson@chillservices.com>"
---
+++ packages/cs-commons/cs-commons-0.0.1.tar.gz/cs-commons-0.0.1/publish.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+flit publish --repository stevepypi
