7ba9865b50e9295af3e9bf33a078f549cd7a666e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:48 +0000

    Add package cfinterface/0.0.20/cfinterface-0.0.20-py3-none-any.whl

---
+++ packages/cfinterface/cfinterface-0.0.20-py3-none-any.whl/cfinterface/components/literalfield.py
@@ -0,0 +1,48 @@
+from typing import Optional, Union
+import pandas as pd  # type: ignore
+from cfinterface.components.field import Field
+
+from cfinterface.adapters.field.repository import Repository
+from cfinterface.adapters.field.textualrepository import TextualRepository
+
+
+class LiteralField(Field):
+    """
+    Class for representing an literal field for being read from and
+    written to a file.
+    """
+
+    def __init__(
+        self,
+        size: int = 80,
+        starting_position: int = 0,
+        value: Optional[str] = None,
+        interface: Repository = TextualRepository(),
+    ) -> None:
+        super().__init__(size, starting_position, value, interface)
+
+    # Override
+    def read(self, line: Union[str, bytes]) -> str:
+        readline = self._interface.read(line)
+        self._value = readline[
+            self._starting_position : self._ending_position
+        ].strip()
+        return self._value
+
+    # Override
+    def write(self, line: Union[str, bytes]) -> Union[str, bytes]:
+        if self.value is None or pd.isnull(self.value):
+            value = "".ljust(self._size)
+        else:
+            value = self.value
+        return self._interface.write(
+            value, line, self._starting_position, self._ending_position
+        )
+
+    @property
+    def value(self) -> Optional[str]:
+        return self._value
+
+    @value.setter
+    def value(self, val: str):
+        self._value = val
