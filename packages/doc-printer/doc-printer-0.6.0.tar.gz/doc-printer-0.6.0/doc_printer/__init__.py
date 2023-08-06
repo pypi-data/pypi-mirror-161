d0081736c93b8c64ca4b8cdd283c5c87070447d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:23 +0000

    Add package doc-printer/0.6.0/doc-printer-0.6.0.tar.gz

---
+++ packages/doc-printer/doc-printer-0.6.0.tar.gz/doc-printer-0.6.0/doc_printer/__init__.py
@@ -0,0 +1,51 @@
+from .doc import (
+    DocLike as DocLike,
+    DocClassWithUnpack as DocClassWithUnpack,
+    Doc as Doc,
+    Text as Text,
+    Alt as Alt,
+    Cat as Cat,
+    RowInfo as RowInfo,
+    Row as Row,
+    Table as Table,
+    Nest as Nest,
+    RowCandidate as RowCandidate,
+    Empty as Empty,
+    Space as Space,
+    Line as Line,
+    Fail as Fail,
+    SoftLine as SoftLine,
+    splat as splat,
+    cat as cat,
+    row as row,
+    table as table,
+    alt as alt,
+    nest as nest,
+    parens as parens,
+    brackets as brackets,
+    braces as braces,
+    angles as angles,
+    quote as quote,
+    create_tables as create_tables,
+)
+from .typing import (
+    RenderError as RenderError,
+    OnEmit as OnEmit,
+    Token as Token,
+    TokenStream as TokenStream,
+    TokenBuffer as TokenBuffer,
+)
+from .table import (
+    CellBuffer as CellBuffer,
+    RowBuffer as RowBuffer,
+    TableBuffer as TableBuffer,
+)
+from .abc import DocRenderer as DocRenderer
+from .simple import (
+    SimpleDocRenderer as SimpleDocRenderer,
+)
+from .smart import (
+    LineWidthExceeded as LineWidthExceeded,
+    SmartDocRenderer as SmartDocRenderer,
+)
+
