062f3518d0e3dd2cbcdb7dc698cf2b929173d4c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:19:46 +0000

    Add package setuptools/63.3.0/setuptools-63.3.0.tar.gz

---
+++ packages/setuptools/setuptools-63.3.0.tar.gz/setuptools-63.3.0/setuptools/_distutils/tests/test_install_headers.py
@@ -0,0 +1,36 @@
+"""Tests for distutils.command.install_headers."""
+import os
+import unittest
+
+import pytest
+
+from distutils.command.install_headers import install_headers
+from distutils.tests import support
+
+
+@pytest.mark.usefixtures('save_env')
+class InstallHeadersTestCase(
+    support.TempdirManager,
+    support.LoggingSilencer,
+    unittest.TestCase,
+):
+    def test_simple_run(self):
+        # we have two headers
+        header_list = self.mkdtemp()
+        header1 = os.path.join(header_list, 'header1')
+        header2 = os.path.join(header_list, 'header2')
+        self.write_file(header1)
+        self.write_file(header2)
+        headers = [header1, header2]
+
+        pkg_dir, dist = self.create_dist(headers=headers)
+        cmd = install_headers(dist)
+        self.assertEqual(cmd.get_inputs(), headers)
+
+        # let's run the command
+        cmd.install_dir = os.path.join(pkg_dir, 'inst')
+        cmd.ensure_finalized()
+        cmd.run()
+
+        # let's check the results
+        self.assertEqual(len(cmd.get_outputs()), 2)
