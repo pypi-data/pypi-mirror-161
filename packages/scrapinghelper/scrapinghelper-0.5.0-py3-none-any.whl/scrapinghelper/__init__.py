3ea2c982e9f95fbb738e9a3f7a2f44f241f06dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:14 +0000

    Add package scrapinghelper/0.5.0/scrapinghelper-0.5.0-py3-none-any.whl

---
+++ packages/scrapinghelper/scrapinghelper-0.5.0-py3-none-any.whl/scrapinghelper/__init__.py
@@ -0,0 +1,38 @@
+import sys
+from .scraper import (
+    Scraper, TAG_LINK,
+    HTMLSession, AsyncHTMLSession, HTML, HTMLResponse, Element, user_agent
+)
+from .user_agents import UserAgent, user_agent
+from .url import URL, remove_urls, replace_urls
+from .proxy import ProxyManager, PROXY, ProxyRotate
+from .logging import LogConfig, LOG_LEVEL
+from .versions import __VERSION__
+
+# Sanity checking.
+try:
+    assert sys.version_info.major == 3
+    assert sys.version_info.minor > 7
+except AssertionError:
+    raise RuntimeError('Requests-HTML requires Python 3.8+!')
+
+__all__ = [
+    "Scraper",
+    "TAG_LINK",
+    "HTMLSession",
+    "AsyncHTMLSession",
+    "HTML",
+    "HTMLResponse",
+    "Element",
+    "user_agent",
+    "UserAgent",
+    "URL",
+    "remove_urls",
+    "replace_urls",
+    "ProxyManager",
+    "ProxyRotate",
+    "PROXY",
+    "LogConfig",
+    "LOG_LEVEL",
+    "__VERSION__",
+]
