0496054c7aec756d4e1313a5c4d498387390cb92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:26 +0000

    Add package whylogs/1.0.9/whylogs-1.0.9-py3-none-any.whl

---
+++ packages/whylogs/whylogs-1.0.9-py3-none-any.whl/whylogs/core/metrics/__init__.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+from whylogs.core.metrics.column_metrics import ColumnCountsMetric, TypeCountersMetric
+from whylogs.core.metrics.metrics import (
+    CardinalityMetric,
+    DistributionMetric,
+    FrequentItemsMetric,
+    IntsMetric,
+    Metric,
+    MetricConfig,
+)
+from whylogs.core.metrics.unicode_range import UnicodeRangeMetric
+
+
+class StandardMetric(Enum):
+    types = TypeCountersMetric
+    distribution = DistributionMetric
+    counts = ColumnCountsMetric
+    ints = IntsMetric
+    cardinality = CardinalityMetric
+    frequent_items = FrequentItemsMetric
+    unicode_range = UnicodeRangeMetric
+
+    def __init__(self, clz: Metric):
+        self._clz = clz
+
+    def zero(self, config: MetricConfig) -> Metric:  # type: ignore
+        return self._clz.zero(config)
