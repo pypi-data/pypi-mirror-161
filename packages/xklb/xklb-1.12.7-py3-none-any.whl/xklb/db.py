fc339cc23ada1359e4d9feda85a5a696e341ed94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:32 +0000

    Add package xklb/1.12.7/xklb-1.12.7-py3-none-any.whl

---
+++ packages/xklb/xklb-1.12.7-py3-none-any.whl/xklb/db.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+def sqlite_con(db="./videos.db"):
+    con = sqlite3.connect(db)
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def fetchall_dict(con, *args):
+    return [dict(r) for r in con.execute(*args).fetchall()]
