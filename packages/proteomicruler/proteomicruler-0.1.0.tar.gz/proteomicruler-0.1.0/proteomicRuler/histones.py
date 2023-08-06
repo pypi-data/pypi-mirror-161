44403e64c9a30a5bcfa4769a3f880acdbd49160c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:27 +0000

    Add package proteomicruler/0.1.0/proteomicruler-0.1.0.tar.gz

---
+++ packages/proteomicruler/proteomicruler-0.1.0.tar.gz/proteomicruler-0.1.0/proteomicRuler/histones.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import os
+
+class HistoneDB:
+    def __init__(self):
+        self.df = pd.DataFrame()
+
+    def get_histones(self):
+        d, _ = os.path.split(__file__)
+        with open(os.path.join(d, "organisms.json"), "rb") as jsonfile:
+            df = pd.read_json(jsonfile).T
+            df = df.explode("histone_ids").reset_index(drop=True)
+            df.set_index("histone_ids", inplace=True)
+            self.df = df
+
+    def check_histones(self, ids, organism=None):
+        share = np.intersect1d(ids, self.df.index)
+        result = self.df.loc[share]
+        if organism:
+            result = result[result["name"] == organism]
+        return result
+
+    def get_organism(self, ids):
+        result = self.check_histones(ids)
+        data = result.groupby(["name", "genome_size"]).size()
+        return data.sort_values().index[-1]
+
+
+
