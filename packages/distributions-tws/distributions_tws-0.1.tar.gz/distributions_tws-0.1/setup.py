581f88e0db78ba9b0696c28ec85263a507d37adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:38 +0000

    Add package distributions-tws/0.1/distributions_tws-0.1.tar.gz

---
+++ packages/distributions-tws/distributions_tws-0.1.tar.gz/distributions_tws-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_tws',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_tws'],
+      zip_safe=False)
