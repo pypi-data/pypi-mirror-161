66df51580d54aa70d9d0e9576a7572dac39d806d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:47 +0000

    Add package converter-package/0.1/converter_package-0.1.tar.gz

---
+++ packages/converter-package/converter_package-0.1.tar.gz/converter_package-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='converter_package',
+      version='0.1',
+      description='This converter library is able to transform the ACCORDION application model to K3s configuration files',
+      author='Giannis Korontanis',
+      author_email='gkorod2@gmail.com',
+      license='MIT',
+      packages=['converter_package'],
+      zip_safe=False)
