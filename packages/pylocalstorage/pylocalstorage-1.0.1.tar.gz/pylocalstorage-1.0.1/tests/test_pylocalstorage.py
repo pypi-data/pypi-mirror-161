66b25802ab129b0b1b115ecb7e0a096fe9d1b082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:19 +0000

    Add package pylocalstorage/1.0.1/pylocalstorage-1.0.1.tar.gz

---
+++ packages/pylocalstorage/pylocalstorage-1.0.1.tar.gz/pylocalstorage-1.0.1/tests/test_pylocalstorage.py
@@ -0,0 +1,41 @@
+"""Tests for LocalStorage."""
+
+from pylocalstorage import LocalStorage, WriteStorageError
+import pytest
+
+
+class TestLocalStorage:
+
+    @pytest.mark.parametrize(
+        "exception, value",
+        (
+            (WriteStorageError, int),
+            (WriteStorageError, str),
+        ),
+    )
+    def test_invalid_value(self, exception, value):
+        try:
+            storage = LocalStorage()
+            storage.setItem("x", value)
+        except Exception as err:
+            assert isinstance(err, WriteStorageError)
+
+    @pytest.mark.parametrize(
+        "key, value",
+        (
+            ("x", "this"),
+            ("x", "localhost"),
+        ),
+    )
+    def test_setItem(self, key, value):
+        storage = LocalStorage()
+        storage.setItem(key, value)
+        x = storage.getItem(key)
+        assert x == value
+
+    def test_removeItem(self):
+        storage = LocalStorage()
+        storage.setItem("x", "this")
+        storage.removeItem("x")
+        x = storage.getItem("x")
+        assert x == None
