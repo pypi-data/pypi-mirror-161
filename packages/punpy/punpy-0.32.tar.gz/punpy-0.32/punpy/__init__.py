1295c32e7c2419d281bc00bf80b18ba076aef48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:35 +0000

    Add package punpy/0.32/punpy-0.32.tar.gz

---
+++ packages/punpy/punpy-0.32.tar.gz/punpy-0.32/punpy/__init__.py
@@ -0,0 +1,15 @@
+from punpy.digital_effects_table.measurement_function import MeasurementFunction
+from punpy.lpu.lpu_propagation import LPUPropagation
+from punpy.mc.mc_propagation import MCPropagation
+
+from ._version import get_versions
+
+"""___Authorship___"""
+__author__ = "Pieter De Vis"
+__created__ = "01/03/2020"
+__maintainer__ = "Pieter De Vis"
+__email__ = "pieter.de.vis@npl.co.uk"
+__status__ = "Development"
+
+__version__ = get_versions()["version"]
+del get_versions
