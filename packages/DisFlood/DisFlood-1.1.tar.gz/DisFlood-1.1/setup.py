7636519496e45e48e37a7a77b5136811069cdd83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:04 +0000

    Add package DisFlood/1.1/DisFlood-1.1.tar.gz

---
+++ packages/DisFlood/DisFlood-1.1.tar.gz/DisFlood-1.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='DisFlood',
+	version='1.1',
+	author='Lcvb_X',
+	author_email='podygka1990@gmail.com',
+	description='this library flood to discord on discontrol',
+	packages=['DisFlood'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
