397018448b1385733b421299a7d3fbe3d07d9da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:28 +0000

    Add package aifactory/0.1.8/aifactory-0.1.8.tar.gz

---
+++ packages/aifactory/aifactory-0.1.8.tar.gz/aifactory-0.1.8/aifactory/submit.py
@@ -0,0 +1,11 @@
+import os
+from sys import argv
+import requests
+
+api_url = "http://localhost:9123/grade"
+
+def submit(key, submit_file_path):
+  values = {"key": key}
+  res = requests.post(api_url, files = {'file': open(submit_file_path,'rb', )}, data=values)  
+  print(res.status_code)
+  print(res.text)
