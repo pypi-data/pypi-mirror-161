80b57a32e1131b0841491a301322a191eace67df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:39 +0000

    Add package drf-kit/1.14.0/drf_kit-1.14.0-py3-none-any.whl

---
+++ packages/drf-kit/drf_kit-1.14.0-py3-none-any.whl/drf_kit/views/__init__.py
@@ -0,0 +1,48 @@
+from drf_kit.views.nested_viewsets import (
+    NestedModelViewSet,
+    CachedNestedModelViewSet,
+    ReadOnlyNestedModelViewSet,
+    CachedReadOnlyNestedModelViewSet,
+    WriteOnlyNestedModelViewSet,
+)
+from drf_kit.views.single_nested_viewsets import (
+    SingleNestedModelViewSet,
+    CachedSingleNestedModelViewSet,
+    ReadOnlySingleNestedModelViewSet,
+    CachedReadOnlySingleNestedModelViewSet,
+)
+from drf_kit.views.stats_views import (
+    StatsViewMixin,
+)
+from drf_kit.views.viewsets import (
+    ModelViewSet,
+    CachedModelViewSet,
+    ReadOnlyModelViewSet,
+    CachedReadOnlyModelViewSet,
+    NonDestructiveModelViewSet,
+    CachedNonDestructiveModelViewSet,
+    WriteOnlyModelViewSet,
+    UpsertMixin,
+    BulkMixin,
+)
+
+__all__ = (
+    "NestedModelViewSet",
+    "CachedNestedModelViewSet",
+    "ReadOnlyNestedModelViewSet",
+    "CachedReadOnlyNestedModelViewSet",
+    "NonDestructiveModelViewSet",
+    "CachedNonDestructiveModelViewSet",
+    "WriteOnlyNestedModelViewSet",
+    "SingleNestedModelViewSet",
+    "CachedSingleNestedModelViewSet",
+    "ReadOnlySingleNestedModelViewSet",
+    "CachedReadOnlySingleNestedModelViewSet",
+    "ModelViewSet",
+    "CachedModelViewSet",
+    "ReadOnlyModelViewSet",
+    "CachedReadOnlyModelViewSet",
+    "WriteOnlyModelViewSet",
+    "UpsertMixin",
+    "StatsViewMixin",
+)
