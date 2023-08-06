bbaf6d055f6e07d56851bed9e3b5ce072c1e16bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:34 +0000

    Add package dpmdtools/0.1.1/dpmdtools-0.1.1.tar.gz

---
+++ packages/dpmdtools/dpmdtools-0.1.1.tar.gz/dpmdtools-0.1.1/dptools/cli.py
@@ -0,0 +1,47 @@
+from dptools import __version__
+from importlib import import_module
+import argparse
+
+command2module = {
+    "input": "dptools.input",
+    "parity": "dptools.parity",
+    "run": "dptools.lmp.simulations",
+    #"sample": "dptools.sample",
+    "convert": "dptools.utils",
+    "set": "dptools.env",
+    "get": "dptools.lmp.parameters",
+    #"info": "dptools.?", show env stuff
+}
+
+class BaseCLI:
+    help_info = ""
+    def __init__(self, parser):
+        self.parser = parser
+
+    def add_args(self):
+        '''Command specific arguments'''
+        pass
+
+    def main(self, args):
+        '''Command specific main method'''
+        pass
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="dptools",
+                                     description="DPTools CLI for doing stuff with deepmd-kit",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     )
+    parser.add_argument("--version", action="version", version=__version__)
+    subparsers = parser.add_subparsers(dest="command")
+    # TODO: Add logging
+    command_clis = {}
+    for comm, mod in command2module.items():
+        subparser = subparsers.add_parser(comm, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        CLI = import_module(mod).CLI
+        cli = CLI(subparser)
+        cli.add_args()
+        command_clis[comm] = cli
+
+    parsed_args = parser.parse_args()
+    command_clis[parsed_args.command].main(parsed_args)
